spack/spack-packages

commit 38dc3563f8432354b501e292f5ae89a614102cbd

Automated git version fixes (#39637)
Use full length commit sha instead of short prefixes, to improve
reproducibility (future clashes) and guard against compromised repos and
man in the middle attacks.

Abbreviated commit shas are expanded to full length, to guard against future
clashes on short hash. It also guards against compromised repos and
man in the middle attacks, where attackers can easily fabricate a malicious
commit with a shasum prefix collision.

Versions with just tags now also get a commit sha, which can later be used to
check for retagged commits.

python/builtin/packages/intel-xed/package.py
@@ -21,14 +21,14 @@ class IntelXed(Package):
 
     # Current versions now have actual releases and tags.
     version("main", branch="main")
-    version("2023.07.09", tag="v2023.07.09")
-    version("2023.06.07", tag="v2023.06.07")
-    version("2023.04.16", tag="v2023.04.16")
-    version("2022.10.11", tag="v2022.10.11")
-    version("2022.08.11", tag="v2022.08.11")
-    version("2022.04.17", tag="v2022.04.17")
-    version("12.0.1", tag="12.0.1")
-    version("11.2.0", tag="11.2.0")
+    version("2023.07.09", tag="v2023.07.09", commit="539a6a349cf7538a182ed3ee1f48bb9317eb185f")
+    version("2023.06.07", tag="v2023.06.07", commit="4dc77137f651def2ece4ac0416607b215c18e6e4")
+    version("2023.04.16", tag="v2023.04.16", commit="a3055cd0209f5c63c88e280bbff9579b1e2942e2")
+    version("2022.10.11", tag="v2022.10.11", commit="9fc12ab6c0ba7a9eaadb20135369b4b4107fa670")
+    version("2022.08.11", tag="v2022.08.11", commit="1ce1036aa4ab280f9a498136b37421ab390e42db")
+    version("2022.04.17", tag="v2022.04.17", commit="ef19f00de14a9c2c253c1c9b1119e1617280e3f2")
+    version("12.0.1", tag="12.0.1", commit="5976632eeaaaad7890c2109d0cfaf4012eaca3b8")
+    version("11.2.0", tag="11.2.0", commit="40125558530137444b4ee6fd26b445bfa105b543")
 
     # The old 2019.03.01 version (before there were tags).
     version("10.2019.03", commit="b7231de4c808db821d64f4018d15412640c34113", deprecated=True)
@@ -43,7 +43,14 @@ class IntelXed(Package):
         name="mbuild", placement=mdir, git=mbuild_git, tag="v2022.07.28", when="@2022.07:9999"
     )
 
-    resource(name="mbuild", placement=mdir, git=mbuild_git, tag="v2022.04.17", when="@:2022.06")
+    resource(
+        name="mbuild",
+        placement=mdir,
+        git=mbuild_git,
+        tag="v2022.04.17",
+        commit="ef19f00de14a9c2c253c1c9b1119e1617280e3f2",
+        when="@:2022.06",
+    )
 
     variant("debug", default=False, description="Enable debug symbols")
     variant("pic", default=False, description="Compile with position independent code.")

python/builtin/packages/aluminum/package.py
@@ -48,7 +48,7 @@ class Aluminum(CMakePackage, CudaPackage, ROCmPackage):
         sha256="78b03e36e5422e8651f400feb4d8a527f87302db025d77aa37e223be6b9bdfc9",
         deprecated=True,
     )
-    version("1.0.0-lbann", tag="v1.0.0-lbann")
+    version("1.0.0-lbann", tag="v1.0.0-lbann", commit="40a062b1f63e84e074489c0f926f36b806c6b8f3")
     version("1.0.0", sha256="028d12e271817214db5c07c77b0528f88862139c3e442e1b12f58717290f414a")
     version(
         "0.7.0",

python/builtin/packages/amg2013/package.py
@@ -18,9 +18,9 @@ class Amg2013(MakefilePackage):
     git = "https://github.com/LLNL/AMG.git"
 
     version("develop", branch="master")
-    version("1.2", tag="1.2")
-    version("1.1", tag="1.1")
-    version("1.0", tag="1.0")
+    version("1.2", tag="1.2", commit="3ada8a128e311543e84d9d66344ece77924127a8")
+    version("1.1", tag="1.1", commit="09fe8a78baf6ba5eaef7d2804f7b653885d60fee")
+    version("1.0", tag="1.0", commit="f5b864708ca3ef48a86e1e46fcb812cbbfa80c51")
 
     variant("openmp", default=True, description="Build with OpenMP support")
     variant("optflags", default=False, description="Additional optimizations")

python/builtin/packages/ams/package.py
@@ -15,7 +15,12 @@ class Ams(CMakePackage, CudaPackage):
     maintainers("koparasy", "lpottier")
 
     version("develop", branch="develop", submodules=False)
-    version("07.25.23-alpha", tag="07.25.23-alpha", submodules=False)
+    version(
+        "07.25.23-alpha",
+        tag="07.25.23-alpha",
+        commit="3aa8421f1f1ce1ae448d017214c602b9def19c90",
+        submodules=False,
+    )
 
     variant(
         "faiss",

python/builtin/packages/ascent/package.py
@@ -52,19 +52,37 @@ class Ascent(CMakePackage, CudaPackage):
 
     version("develop", branch="develop", submodules=True)
 
-    version("0.9.2", tag="v0.9.2", submodules=True, preferred=True)
+    version(
+        "0.9.2",
+        tag="v0.9.2",
+        commit="b842516d12640e4a0d9433a18c7249440ef6fc3d",
+        submodules=True,
+        preferred=True,
+    )
 
-    version("0.9.1", tag="v0.9.1", submodules=True)
+    version(
+        "0.9.1", tag="v0.9.1", commit="027a2fe184f65a4923817a8cdfed0b0c61c2c75a", submodules=True
+    )
 
-    version("0.9.0", tag="v0.9.0", submodules=True)
+    version(
+        "0.9.0", tag="v0.9.0", commit="a31c88c579c8d0026e0025de8bace0cf22f6305b", submodules=True
+    )
 
-    version("0.8.0", tag="v0.8.0", submodules=True)
+    version(
+        "0.8.0", tag="v0.8.0", commit="08504374908518e013d7fe8d8882cfb1c2378e3b", submodules=True
+    )
 
-    version("0.7.1", tag="v0.7.1", submodules=True)
+    version(
+        "0.7.1", tag="v0.7.1", commit="79d35b2f48e92eb151313f0217e9bd7c15779582", submodules=True
+    )
 
-    version("0.7.0", tag="v0.7.0", submodules=True)
+    version(
+        "0.7.0", tag="v0.7.0", commit="cfed1b0a469e4dcc970fd7e0bcd78b522d97ea53", submodules=True
+    )
 
-    version("0.6.0", tag="v0.6.0", submodules=True)
+    version(
+        "0.6.0", tag="v0.6.0", commit="9ade37b0a9ea495e45adb25cda7498c0bf9465c5", submodules=True
+    )
 
     ###########################################################################
     # package variants

python/builtin/packages/aws-sdk-cpp/package.py
@@ -18,11 +18,36 @@ class AwsSdkCpp(CMakePackage):
     homepage = "https://github.com/aws/aws-sdk-cpp"
     git = "https://github.com/aws/aws-sdk-cpp.git"
 
-    version("1.11.144", tag="1.11.144", submodules=True)
-    version("1.10.57", tag="1.10.57", submodules=True)
-    version("1.10.32", tag="1.10.32", submodules=True)
-    version("1.9.379", tag="1.9.379", submodules=True)
-    version("1.9.247", tag="1.9.247", submodules=True)
+    version(
+        "1.11.144",
+        tag="1.11.144",
+        commit="498339d47146f8e3ee9ae0fad3fcda5acbaea1e6",
+        submodules=True,
+    )
+    version(
+        "1.10.57",
+        tag="1.10.57",
+        commit="777e0dd4b90eda8bf65bc4b5fce2f90febdbd2b8",
+        submodules=True,
+    )
+    version(
+        "1.10.32",
+        tag="1.10.32",
+        commit="40594bc05a3f68fe14f2f1a54c5c3e6752b1e290",
+        submodules=True,
+    )
+    version(
+        "1.9.379",
+        tag="1.9.379",
+        commit="94d02db44730b0a5cef98ce33deedf43f5333700",
+        submodules=True,
+    )
+    version(
+        "1.9.247",
+        tag="1.9.247",
+        commit="4bf33aa0172704eb5fc1da8086686670711bd801",
+        submodules=True,
+    )
 
     depends_on("cmake@3.1:", type="build")
     depends_on("zlib-api")

python/builtin/packages/axom/package.py
@@ -40,16 +40,16 @@ class Axom(CachedCMakePackage, CudaPackage, ROCmPackage):
 
     version("main", branch="main")
     version("develop", branch="develop")
-    version("0.7.0", tag="v0.7.0")
-    version("0.6.1", tag="v0.6.1")
-    version("0.6.0", tag="v0.6.0")
-    version("0.5.0", tag="v0.5.0")
-    version("0.4.0", tag="v0.4.0")
-    version("0.3.3", tag="v0.3.3")
-    version("0.3.2", tag="v0.3.2")
-    version("0.3.1", tag="v0.3.1")
-    version("0.3.0", tag="v0.3.0")
-    version("0.2.9", tag="v0.2.9")
+    version("0.7.0", tag="v0.7.0", commit="ea5158191181c137117ae37959879bdc8b107f35")
+    version("0.6.1", tag="v0.6.1", commit="ee240d3963d7879ae0e9c392902195bd7b04e37d")
+    version("0.6.0", tag="v0.6.0", commit="65287dc00bc7c271a08cb86c632f5909c30e3506")
+    version("0.5.0", tag="v0.5.0", commit="db137349b3e28617c3e0570dbd18e4a91654da98")
+    version("0.4.0", tag="v0.4.0", commit="38c0d7495ece35a30fca5f5b578b8f9d54346bd2")
+    version("0.3.3", tag="v0.3.3", commit="f0539ef0525469ffda054d86144f310c15b4f9e0")
+    version("0.3.2", tag="v0.3.2", commit="c446b496e20e6118b8cba7e80f1f84c76a49e463")
+    version("0.3.1", tag="v0.3.1", commit="cbefc0457a229d8acfb70622360d0667e90e50a2")
+    version("0.3.0", tag="v0.3.0", commit="20068ccab4b4f70055918b4f17960ec3ed6dbce8")
+    version("0.2.9", tag="v0.2.9", commit="9e9a54ede3326817c05f35922738516e43b5ec3d")
 
     # https://github.com/spack/spack/issues/31829
     patch("examples-oneapi.patch", when="@0.6.1 +examples %oneapi")

python/builtin/packages/binder/package.py
@@ -22,10 +22,10 @@ class Binder(CMakePackage):
     maintainers("lyskov", "kliegeois")
 
     version("master", branch="master")
-    version("1.3.0", tag="v1.3.0")
-    version("1.2.0", tag="v1.2.0")
-    version("1.1.0", tag="v1.0.0")
-    version("1.0.0", tag="v1.0.0")
+    version("1.3.0", tag="v1.3.0", commit="e9b55985af297ca161d615058e4a5da07c22bc77")
+    version("1.2.0", tag="v1.2.0", commit="90cf5b31b6f4ecad3fe87518ca2b949dc9e8ed1a")
+    version("1.1.0", tag="v1.0.0", commit="3de7949343197295250f988716d511a264b21324")
+    version("1.0.0", tag="v1.0.0", commit="3de7949343197295250f988716d511a264b21324")
 
     # Add dependencies
     depends_on("llvm+clang+llvm_dylib@7.0:9")

commit da5dbc79ea85e734e89677b5e07ccf435e89bbb6

intel-xed: fix git hash for mbuild, add version 2023.10.11 (#40922)
* intel-xed: fix git hash for mbuild, add version 2023.10.11
   Fixes #40912
* Fix the git commit hash for mbuild 2022.04.17.  This was broken in
   commit 38dc3563f843 by mixing up the hashes for xed versus mbuild.
* Add versions 2023.08.21 and 2023.10.11.
* fix style

python/builtin/packages/intel-xed/package.py
@@ -21,6 +21,8 @@ class IntelXed(Package):
 
     # Current versions now have actual releases and tags.
     version("main", branch="main")
+    version("2023.10.11", tag="v2023.10.11", commit="d7d46c73fb04a1742e99c9382a4acb4ed07ae272")
+    version("2023.08.21", tag="v2023.08.21", commit="01a6da8090af84cd52f6c1070377ae6e885b078f")
     version("2023.07.09", tag="v2023.07.09", commit="539a6a349cf7538a182ed3ee1f48bb9317eb185f")
     version("2023.06.07", tag="v2023.06.07", commit="4dc77137f651def2ece4ac0416607b215c18e6e4")
     version("2023.04.16", tag="v2023.04.16", commit="a3055cd0209f5c63c88e280bbff9579b1e2942e2")
@@ -40,7 +42,12 @@ class IntelXed(Package):
 
     # Match xed more closely with the version of mbuild at the time.
     resource(
-        name="mbuild", placement=mdir, git=mbuild_git, tag="v2022.07.28", when="@2022.07:9999"
+        name="mbuild",
+        placement=mdir,
+        git=mbuild_git,
+        tag="v2022.07.28",
+        commit="75cb46e6536758f1a3cdb3d6bd83a4a9fd0338bb",
+        when="@2022.07:9999",
     )
 
     resource(
@@ -48,7 +55,7 @@ class IntelXed(Package):
         placement=mdir,
         git=mbuild_git,
         tag="v2022.04.17",
-        commit="ef19f00de14a9c2c253c1c9b1119e1617280e3f2",
+        commit="b41485956bf65d51b8c2379768de7eaaa7a4245b",
         when="@:2022.06",
     )
 