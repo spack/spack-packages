9747dbe02065ae7fe513cb6cc069c55722776dcf
Benjamin Meyers <bsmits@rit.edu>
2025-03-03 08:29:25 -0500

    Update py-transformers ^py-sentence-transformers

--- repos/spack_repo/builtin/packages/py_sentence_transformers/package.py
+++ repos/spack_repo/builtin/packages/py_sentence_transformers/package.py
@@ -19,17 +19,32 @@ class PySentenceTransformers(PythonPackage):
 
     maintainers("meyersbs")
 
+    version("3.4.1", sha256="68daa57504ff548340e54ff117bd86c1d2f784b21e0fb2689cf3272b8937b24b")
     version("2.2.2", sha256="dbc60163b27de21076c9a30d24b5b7b6fa05141d68cf2553fa9a77bf79a29136")
 
+    def url_for_version(self, version):
+        url = "https://files.pythonhosted.org/packages/source/s/sentence-transformers/{0}-{1}.tar.gz"
+        if version == Version("2.2.2"):
+            name = "sentence-transformers"
+        else:
+            name = "sentence_transformers"
+        return url.format(name, version.dotted)
+
+    depends_on("py-setuptools@42:", when="@3.4.1", type="build")
     depends_on("py-setuptools", type="build")
+    depends_on("python@3.9.0:", when="@3.4.1", type=("build", "run"))
     depends_on("python@3.6.0:", type=("build", "run"))
-    depends_on("py-transformers@4.6.0:4", type=("build", "run"))
+    depends_on("py-transformers@4.41.0:4", when="@3.4.1", type=("build", "run"))
+    depends_on("py-transformers@4.6.0:4", when="@2.2.2", type=("build", "run"))
     depends_on("py-tqdm", type=("build", "run"))
-    depends_on("py-torch@1.6.0:", type=("build", "run"))
+    depends_on("py-torch@1.11.0:", when="@3.4.1", type=("build", "run"))
+    depends_on("py-torch@1.6.0:", when="@2.2.2", type=("build", "run"))
     depends_on("py-torchvision", type=("build", "run"))
-    depends_on("py-numpy", type=("build", "run"))
+    depends_on("py-numpy", when="@2.2.2", type=("build", "run"))
     depends_on("py-scikit-learn", type=("build", "run"))
     depends_on("py-scipy", type=("build", "run"))
-    depends_on("py-nltk", type=("build", "run"))
-    depends_on("py-sentencepiece", type=("build", "run"))
-    depends_on("py-huggingface-hub@0.4.0:", type=("build", "run"))
+    depends_on("py-nltk", when="@2.2.2", type=("build", "run"))
+    depends_on("py-sentencepiece", when="@2.2.2", type=("build", "run"))
+    depends_on("py-huggingface-hub@0.20.0:", when="@3.4.1", type=("build", "run"))
+    depends_on("py-huggingface-hub@0.4.0:", when="@2.2.2", type=("build", "run"))
+    depends_on("py-pillow", when="@3.4.1", type=("build", "run"))
