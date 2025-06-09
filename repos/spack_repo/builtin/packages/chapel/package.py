46db569a2016fc1198a9533baba821d221fb75ce
Anna Rift <anna.rift@hpe.com>
2025-06-09 11:55:44 -0700

    Use more relevant issue link for chapel llvm 20 nvidia bug

--- repos/spack_repo/builtin/packages/chapel/package.py
+++ repos/spack_repo/builtin/packages/chapel/package.py
@@ -473,7 +473,7 @@ class Chapel(AutotoolsPackage, CudaPackage, ROCmPackage):
     conflicts(
         "^llvm@20",
         when="@:2.5 +cuda",
-        msg="Chapel through 2.5 does not support Nvidia GPUs with LLVM 20, see https://github.com/llvm/llvm-project/issues/141626",
+        msg="Chapel through 2.5 does not support Nvidia GPUs with LLVM 20, see https://github.com/chapel-lang/chapel/issues/27273",
     )
 
     conflicts("+rocm", when="@:1", msg="ROCm support in spack requires Chapel 2.0.0 or later")
