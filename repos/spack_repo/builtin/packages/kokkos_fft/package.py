4fcc017b84b7dc276b2d99f0dfaf79a481d5357d
Thomas Padioleau <thomas.padioleau@cea.fr>
2025-06-10 12:05:21 +0200

    Refactor with multi values variants

--- repos/spack_repo/builtin/packages/kokkos_fft/package.py
+++ repos/spack_repo/builtin/packages/kokkos_fft/package.py
@@ -18,37 +18,47 @@ class KokkosFft(CMakePackage):
 
     version("0.3.0", sha256="a13c423775afec5f9f79fa9a23dd6001d3d63bae9f4786b1e0cd3ed65b3993a3")
 
-    variant("fftw", default=True, description="Enable FFTW")
-    variant("cufft", default=False, description="Enable cufft")
-    variant("hipfft", default=False, description="Enable hipfft")
-    variant("onemkl", default=False, description="Enable oneMKL fft")
+    variant("host_backend", default="fftw-serial",
+            values=("fftw-serial", "fftw-openmp"),
+            multi=True,
+            description="Enable host backend")
+    variant("device_backend", default="none",
+            values=("none", "cufft", "hipfft", "onemkl"),
+            multi=False,
+            description="Enable device backend")
     variant("tests", default=False, description="Enable tests")
 
     depends_on("kokkos@4.4:4 +complex_align") # not supported in Spack 0.23.1
-    requires("^kokkos +cuda +wrapper", when="+cufft")
-    requires("^kokkos +rocm", when="+hipfft")
-    requires("^kokkos +sycl", when="+onemkl")
+    requires("^kokkos +cuda +wrapper", when="device_backend=cufft")
+    requires("^kokkos +rocm", when="device_backend=hipfft")
+    requires("^kokkos +sycl", when="device_backend=onemkl")
     depends_on("googletest@1.15:1", when="+tests")
 
-    depends_on("fftw@3.3:3 ~mpi precision=float,double", when="+fftw")
-    requires("^fftw +openmp", when="^kokkos +openmp")
-    # requires("^fftw +threads", when="^kokkos +threads") # the option +threads does not exist
-    depends_on("cuda@11:12", when="+cufft")
-    depends_on("hipfft@5.3:6", when="+hipfft")
-    depends_on("intel-oneapi-mkl@2023:2025", when="+onemkl")
+    depends_on("fftw@3.3:3 ~mpi precision=float,double", when="host_backend=fftw-serial")
+    depends_on("fftw@3.3:3 ~mpi precision=float,double", when="host_backend=fftw-openmp")
+    requires("^fftw +openmp", when="host_backend=fftw-openmp")
+    depends_on("cuda@11:12", when="device_backend=cufft")
+    depends_on("hipfft@5.3:6", when="device_backend=hipfft")
+    depends_on("intel-oneapi-mkl@2023:2025", when="device_backend=onemkl")
 
     def cmake_args(self):
         args = [
             self.define("KokkosFFT_ENABLE_INTERNAL_KOKKOS", False),
             self.define_from_variant("KokkosFFT_ENABLE_TESTS", "tests"),
-            self.define_from_variant("KokkosFFT_ENABLE_FFTW", "fftw"),
-            self.define_from_variant("KokkosFFT_ENABLE_CUFFT", "cufft"),
-            self.define_from_variant("KokkosFFT_ENABLE_HIPFFT", "hipfft"),
-            self.define_from_variant("KokkosFFT_ENABLE_ONEMKL", "onemkl"),
             self.define("KokkosFFT_ENABLE_DOCS", False),
             self.define("KokkosFFT_ENABLE_BENCHMARK", False),
             self.define("KokkosFFT_ENABLE_EXAMPLES", False),
         ]
+
+        if self.spec.satisfies("host_backend=fftw-serial") or self.spec.satisfies("host_backend=fftw-openmp"):
+            args.append(self.define("KokkosFFT_ENABLE_FFTW", "ON"))
+
+        if self.spec.satisfies("device_backend=cufft"):
+            args.append(self.define("KokkosFFT_ENABLE_CUFFT", "ON"))
+        if self.spec.satisfies("device_backend=hipfft"):
+            args.append(self.define("KokkosFFT_ENABLE_HIPFFT", "ON"))
+        if self.spec.satisfies("device_backend=onemkl"):
+            args.append(self.define("KokkosFFT_ENABLE_ONEMKL", "ON"))
 
         if self.spec.satisfies("^kokkos+rocm"):
             args.append(self.define("CMAKE_CXX_COMPILER", self.spec["hip"].hipcc))
