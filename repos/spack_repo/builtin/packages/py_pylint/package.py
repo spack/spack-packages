f7e7556899d7d4dd3406d4ecce998938d668398a
Röhrig-Zöllner, Melven <melven.roehrig-zoellner@dlr.de>
2025-06-11 20:15:59 +0200

    py-pylint: fix py-astroid version range for 3.3.7

--- repos/spack_repo/builtin/packages/py_pylint/package.py
+++ repos/spack_repo/builtin/packages/py_pylint/package.py
@@ -62,7 +62,7 @@ class PyPylint(PythonPackage):
     depends_on("py-astroid@2.11.6:2.11", when="@2.14.2:2.14", type=("build", "run"))
     depends_on("py-astroid@2.12.4:2.13", when="@2.15", type=("build", "run"))
     depends_on("py-astroid@2.14.2:2.15", when="@2.16:2", type=("build", "run"))
-    depends_on("py-astroid@3.3.8:3.3.9", when="@3.3.7:", type=("build", "run"))
+    depends_on("py-astroid@3.3.8:3.3", when="@3.3.7:", type=("build", "run"))
     depends_on("py-isort@4.2.5:", type=("build", "run"))
     depends_on("py-isort@4.2.5:4", when="@2.3.1:2.5", type=("build", "run"))
     depends_on("py-isort@4.2.5:5", when="@2.6:2", type=("build", "run"))
