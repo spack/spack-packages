daa4581ac24dbd3642b72c8ff2cc3fbd78f380d2
Brian Weir <94982354+WeirAE@users.noreply.github.com>
2025-06-11 12:59:39 -0500

    Apply suggestions from code review
    Co-authored-by: Dom Heinzeller <dom.heinzeller@icloud.com>

--- repos/spack_repo/builtin/packages/iotaa/package.py
+++ repos/spack_repo/builtin/packages/iotaa/package.py
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-from spack_repo.builtin.build_systems.python import PythonPackage, PythonPipBuilder
+from spack_repo.builtin.build_systems.python import PythonPackage
 
 from spack.package import *
 
