9cb5ae3dfd75ce01ae7c59b9eee900f3fa4db746
Daniel Nichols <danielnichols1998@gmail.com>
2025-06-11 13:21:30 +0200

    add ParEval package

---
+++ repos/spack_repo/builtin/packages/pareval/package.py
@@ -0,0 +1,60 @@
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack_repo.builtin.build_systems.cuda import CudaPackage
+from spack_repo.builtin.build_systems.generic import Package
+from spack_repo.builtin.build_systems.rocm import ROCmPackage
+
+from spack.package import *
+
+
+class Pareval(Package, CudaPackage, ROCmPackage):
+    """ParEval benchmark for evaluating LLMs' parallel code generation capabilities"""
+
+    url = "https://github.com/parallelcodefoundry/pareval"
+    git = "https://github.com/parallelcodefoundry/pareval.git"
+
+    maintainers("Dando18")
+
+    license("MIT", checked_by="Dando18")
+
+    version("develop", branch="develop")
+    version("1.2", tag="v1.2")
+    version("1.1", tag="v1.1")
+    version("1.0", tag="v1.0")
+
+    variant("generate", default=True, description="Build support for LLM generation")
+    variant("evaluate", default=True, description="Build support for running evaluation drivers")
+    variant("mpi", default=True, description="Build MPI driver support")
+    variant("kokkos", default=True, description="Build Kokkos driver support")
+
+    depends_on("py-tqdm")
+
+    with when("+generate"):
+        depends_on("py-transformers")
+        depends_on("py-torch")
+        depends_on("py-datasets")
+
+    with when("+evaluate"):
+        depends_on("cxx")
+        depends_on("gmake")
+        depends_on("cmake")
+        depends_on("mpi", when="+mpi")
+        depends_on("kokkos@4.1.00 +threads", when="+kokkos")
+
+    def install(self, spec, prefix):
+        mkdirp(prefix.pareval)
+        install_tree("bin", prefix.bin)
+
+        if spec.satisfies("+evaluate"):
+            with working_dir(join_path(self.stage.source_path, "drivers", "cpp")):
+                make()
+
+            install_tree(join_path(self.stage.source_path, "drivers"), prefix.pareval.drivers)
+
+        if spec.satisfies("+generate"):
+            install_tree(join_path(self.stage.source_path, "generate"), prefix.pareval.generate)
+
+    def setup_run_environment(self, env: EnvironmentModifications):
+        env.set("PAREVAL_ROOT", self.prefix.pareval)
