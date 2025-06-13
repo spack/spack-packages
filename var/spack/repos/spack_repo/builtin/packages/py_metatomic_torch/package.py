77324b1a8a5fe82ae086a66a84620a3534f65136
Rohit Goswami <rog32@hi.is>
2025-06-13 14:43:29 +0200

    ENH: Add in py-metatomic-torch

---
+++ var/spack/repos/spack_repo/builtin/packages/py_metatomic_torch/package.py
@@ -0,0 +1,35 @@
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack_repo.builtin.build_systems.python import PythonPackage
+from spack.package import *
+
+class PyMetatomicTorch(PythonPackage):
+    """Torchscript bindings for metatomic"""
+
+    homepage = "https://docs.metatensor.org/metatomic"
+    url = "https://github.com/metatensor/metatomic/releases/download/metatomic-torch-v0.1.2/metatomic_torch-0.1.2.tar.gz"
+    git = "https://github.com/metatensor/metatomic.git"
+    pypi = "metatomic-torch/metatomic-torch-0.1.2.tar.gz"
+
+    import_modules = ["metatomic.torch"]
+
+    maintainers("HaoZeke", "luthaf", "rmeli")
+    license("BSD-3-Clause", checked_by="HaoZeke")
+
+    extends("python")
+    depends_on("python@3.9:")
+    depends_on("py-numpy", type=("run", "build"))
+    depends_on("py-vesin", type=("run", "build"))
+    depends_on("py-torch@2.6:", type=("build", "run"))
+    depends_on("metatensor-torch@0.7.0:", type=("run", "build"))
+    conflicts("metatensor-torch@0.8.0:", type=("run", "build"))
+    # >=0.3.0 and <0.4.0
+    depends_on("py-metatensor-operations@0.3:", type="run", when="@0.3:")
+    conflicts("py-metatensor-operations@0.4.0:", when="@0.3:")
+
+    # pyproject.toml
+    depends_on("py-setuptools@77:", type="build")
+    depends_on("py-packaging@23:", type="build")
+    depends_on("cmake@3.16", type="build")
