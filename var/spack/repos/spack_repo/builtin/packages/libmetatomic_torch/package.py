0446d5ed65384b589ab4ed698a0d4db897cf4618
Rohit Goswami <rog32@hi.is>
2025-06-13 14:19:11 +0200

    ENH: Add in libmetatomic-torch

---
+++ var/spack/repos/spack_repo/builtin/packages/libmetatomic_torch/package.py
@@ -0,0 +1,31 @@
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack_repo.builtin.build_systems.cmake import CMakePackage
+from spack.package import *
+
+
+class LibmetatomicTorch(CMakePackage):
+    """TorchScript/C++ bindings to metatomic"""
+
+    homepage = "https://docs.metatensor.org/metatomic"
+    url = "https://github.com/metatensor/metatomic/releases/download/metatomic-torch-v0.1.2/metatomic_torch-0.1.2.tar.gz"
+    git = "https://github.com/metatensor/metatomic.git"
+
+    maintainers("HaoZeke", "luthaf", "rmeli")
+    license("BSD-3-Clause", checked_by="HaoZeke")
+
+    version("0.1.2", sha256="0d793b16b3d6eee915c89e9b1a385143ec2dbb6dc451bed8feee3a2445b3f63e")
+
+    depends_on("cmake@3.16:", type="build")
+    depends_on("cxx", type="build")
+    depends_on("c", type="build")
+    depends_on("libmetatensor-torch@0.7.6:", type=("build", "run"))
+    conflicts("libmetatensor-torch@0.2.0:")
+    depends_on("py-torch@2.1.0:")
+    conflicts("py-torch@2.7.0")
+
+    def cmake_args(self):
+        args = []
+        return args
