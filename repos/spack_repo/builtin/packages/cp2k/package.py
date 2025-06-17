75dc24aa06918f83e37b27ebcacb90deb46aa131
Niklas Bölter <niklas.boelter@gwdg.de>
2025-06-17 07:27:33 +0200

    Fix for BASIS_MOLOPT_UZH in CP2K 2025.1

--- repos/spack_repo/builtin/packages/cp2k/package.py
+++ repos/spack_repo/builtin/packages/cp2k/package.py
@@ -504,6 +504,7 @@ class Cp2k(MakefilePackage, CMakePackage, CudaPackage, ROCmPackage):
     patch("sirius-api-7.7.0.patch", when="@2024.2:2025.1 ^sirius@7.7.0")
 
     # Fix missing S in data/BASIS_MOLOPT_UZH
+    # https://github.com/cp2k/cp2k/pull/4140
     patch(
         "https://github.com/cp2k/cp2k/commit/da03128481adf8f78a8a04ebeae0490480c03b89.patch?full_index=1",
         sha256="0d542c414216866953c95e642d2590b3d313717dfaebbf12cfafedbdd3bf54a3",
