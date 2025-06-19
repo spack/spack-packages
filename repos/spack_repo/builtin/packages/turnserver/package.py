4e87b40e2702a963b24bd0e6c5b70b58367d0e83
Tamara Dahlgren <35777542+tldahlgren@users.noreply.github.com>
2025-06-19 10:18:56 -0700

    Use the correct sha256 for v4.6.2

--- repos/spack_repo/builtin/packages/turnserver/package.py
+++ repos/spack_repo/builtin/packages/turnserver/package.py
@@ -15,7 +15,7 @@ class Turnserver(AutotoolsPackage):
 
     license("BSD-3-Clause", checked_by="wdconinc")
 
-    version("4.6.2", sha256="408bf7fde455d641bb2a23ba2df992ea0ae87b328de74e66e167ef58d8e9713a")
+    version("4.6.2", sha256="13f2a38b66cffb73d86b5ed24acba4e1371d738d758a6039e3a18f0c84c176ad")
     version(
         "4.5.1.3",
         sha256="408bf7fde455d641bb2a23ba2df992ea0ae87b328de74e66e167ef58d8e9713a",
