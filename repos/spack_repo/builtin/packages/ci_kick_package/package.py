4453de6aebde862494bef70abe80c22e578bc71f
Ryan Krattiger <80296582+kwryankrattiger@users.noreply.github.com>
2025-06-24 11:19:06 -0500

    CI: Add debug to darwin for signing (#345)
    * CI: Add debug to darwin for signing
    
    * CI: Darwin mods for testing
    
    shorter timeout
    darwin can fail for now
    
    * Force kicking a darwin stack

---
+++ repos/spack_repo/builtin/packages/ci_kick_package/package.py
@@ -0,0 +1,18 @@
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack_repo.builtin.build_systems.bundle import BundlePackage
+from spack.package import *
+
+
+class CiKickPackage(BundlePackage):
+    """Package for forcing a to run in CI"""
+
+    homepage = "https://www.spack.io"
+
+    maintainers("kwryankrattiger", "eugenewalker")
+
+    def install(self):
+        """Force install failure to avoid this package ever existing in the build cache"""
+        exit(1)
