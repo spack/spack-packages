67f4f45f64963c49d6243fdd42cd1c18c8ca856c
dcdemen <dcdemen@sandia.gov>
2025-06-30 14:16:33 -0600

    Added cuDSS verson 0.6 to package

--- repos/spack_repo/builtin/packages/cudss/package.py
+++ repos/spack_repo/builtin/packages/cudss/package.py
@@ -9,6 +9,16 @@ from spack_repo.builtin.build_systems.generic import Package
 from spack.package import *
 
 _versions = {
+    "0.6.0": {
+        "Linux-x86_64": (
+            "159ce1d4e3e4bba13b0bd15cf943e44b869c53b7a94f9bac980768c927f02e75",
+            "https://developer.download.nvidia.com/compute/cudss/redist/libcudss/linux-x86_64/libcudss-linux-x86_64-0.6.0.5_cuda12-archive.tar.xz",
+        ),
+        "Linux-aarch64": (
+            "e6f5d5122d735f9dbfd42c9eaba067a557a5613ee4a6001806935de11aff4b09",
+            "https://developer.download.nvidia.com/compute/cudss/redist/libcudss/linux-aarch64/libcudss-linux-aarch64-0.6.0.5_cuda12-archive.tar.xz",
+        ),
+    },
     "0.5.0": {
         "Linux-x86_64": (
             "5245d2ba26a590839e2f1dd074f87e39ee5cc201c3b29245b35c7060d59c37a5",
