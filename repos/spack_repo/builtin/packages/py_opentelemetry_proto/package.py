af96aa8ca54d584e3c3b91098fd3211d4688d4e2
Fernando Ayats <ayatsfer@gmail.com>
2025-07-01 13:02:59 +0200

    py-opentelemetry-proto: init with 1.34.1

---
+++ repos/spack_repo/builtin/packages/py_opentelemetry_proto/package.py
@@ -0,0 +1,25 @@
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack_repo.builtin.build_systems.python import PythonPackage
+from spack.package import *
+
+
+class PyOpentelemetryProto(PythonPackage):
+    """OpenTelemetry Python Proto"""
+
+    homepage = "https://github.com/open-telemetry/opentelemetry-python"
+    pypi = "opentelemetry_api/opentelemetry_api-1.34.1.tar.gz"
+
+    maintainers("viperML")
+
+    license("Apache-2.0", checked_by="viperML")
+
+    version("1.34.1", sha256="64f0bd06d42824843731d05beea88d4d4b6ae59f9fe347ff7dfa2cc14233bbb3")
+
+    depends_on("python@3.9:", type=("build", "run"))
+    depends_on("py-hatchling", type="build")
+
+    with default_args(type="run"):
+        depends_on("py-protobuf@5:6")
