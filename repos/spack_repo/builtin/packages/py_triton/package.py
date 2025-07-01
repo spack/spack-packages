20488523418e6d662102d35294d89bd4fc3ef3bd
吴坎 <github@wu-kan.cn>
2025-07-01 15:56:40 +0800

    Update

--- repos/spack_repo/builtin/packages/py_triton/package.py
+++ repos/spack_repo/builtin/packages/py_triton/package.py
@@ -4,6 +4,9 @@
 
 from spack_repo.builtin.build_systems.python import PythonPackage
 
+import llnl.util.filesystem as fs
+
+from spack_repo.builtin import build_systems
 from spack.package import *
 
 
@@ -17,20 +20,65 @@ class PyTriton(PythonPackage):
     license("MIT")
 
     version("main", branch="main")
+    version("3.2.0", sha256="04eb07e2ff1b87bf4b26e132d696177248bfb9c71cecd4864e561a9c103de9b3")
     version("2.1.0", sha256="4338ca0e80a059aec2671f02bfc9320119b051f378449cf5f56a1273597a3d99")
 
     depends_on("c", type="build")  # generated
     depends_on("cxx", type="build")  # generated
 
-    depends_on("py-setuptools@40.8:", type="build")
-    depends_on("cmake@3.18:", type="build")
+    with default_args(type="build"):
+        depends_on("py-setuptools@40.8:")
+        depends_on("cmake@3.18:")
+        depends_on("ninja")
+
+    with default_args(type=("build", "link", "run")):
+        depends_on("py-pybind11")
+        depends_on("cuda")
+
     depends_on("py-filelock", type=("build", "run"))
     depends_on("zlib-api", type="link")
     conflicts("^openssl@3.3.0")
+
+    # avoid bdist_whell.dist_info_dir problems:
+    # pypa used to contain `bdist_wheel` but it is part of setuptools as of v70.1
+    # these patches change
+    #     wheel.bdist_wheel -> setuptools.command.bdist_wheel.bdist_wheel
+    # see https://github.com/pypa/wheel/pull/631
+    # and https://github.com/pypa/setuptools/pull/4684
+    patch("setup_v3.2.0.patch", when="@3.2.0 ^py-setuptools@70.1:")
 
     def setup_build_environment(self, env: EnvironmentModifications) -> None:
         """Set environment variables used to control the build"""
         if self.spec.satisfies("%clang"):
             env.set("TRITON_BUILD_WITH_CLANG_LLD", "True")
+        # set number of concurrent build jobs
+        env.set("MAX_JOBS", make_jobs)
+        # add a directory for triton's downloads
+        triton_home = f"{self.build_directory}/.triton_home"
+        env.set("TRITON_HOME", triton_home)
+        # use spack installed dependencies
+        env.set("PYBIND11_SYSPATH", self.spec["py-pybind11"].prefix)
+        env.set("TRITON_PTXAS_PATH", self.spec["cuda"].prefix)
+        env.set("TRITON_CUOBJDUMP_PATH", self.spec["cuda"].prefix)
+        env.set("TRITON_NVDISASM_PATH", self.spec["cuda"].prefix)
+        env.set("TRITON_CUDACRT_PATH", self.spec["cuda"].prefix)
+        env.set("TRITON_CUDART_PATH", self.spec["cuda"].prefix)
+        cupti_path = self.spec["cuda"].prefix.extras.CUPTI
+        env.set("TRITON_CUPTI_INCLUDE_PATH", f"{cupti_path}/include")
+        env.set("TRITON_CUPTI_LIB_PATH", f"{cupti_path}/lib64")
 
-    build_directory = "python"
+    # build_directory does not work since apparently one needs to call pip from
+    # the parent directory
+    # build_directory = "python"
+
+
+# override pip install to use python subdirectory from parent directory
+class PythonPipBuilder(python.PythonPipBuilder):
+    def install(self, pkg, spec, prefix):
+        pip = spec["python"].command
+        pip.add_default_arg("-m", "pip")
+        args = build_systems.python.PythonPipBuilder.std_args(pkg) + [f"--prefix={prefix}"]
+        # build directory specified manually as additional argument to pip install
+        args.append("./python")
+        with fs.working_dir(self.build_directory):
+            pip(*args)
