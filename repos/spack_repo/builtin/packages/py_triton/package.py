e999248f666f40be442eb77035c4d6d6024c8cd0
吴坎 <github@wu-kan.cn>
2025-07-01 18:08:09 +0800

    Fix style

--- repos/spack_repo/builtin/packages/py_triton/package.py
+++ repos/spack_repo/builtin/packages/py_triton/package.py
@@ -6,6 +6,7 @@ from spack_repo.builtin import build_systems
 from spack_repo.builtin.build_systems.python import PythonPackage
 
 import llnl.util.filesystem as fs
+
 from spack.package import *
 
 
