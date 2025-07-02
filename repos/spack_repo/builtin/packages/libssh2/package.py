66ae9fd573544ae5d95ed4dbbe89d831991bfb53
Derek Ryan Strong <dstrong@usc.edu>
2025-07-01 18:38:36 -0700

    Fix style

--- repos/spack_repo/builtin/packages/libssh2/package.py
+++ repos/spack_repo/builtin/packages/libssh2/package.py
@@ -21,8 +21,12 @@ class Libssh2(AutotoolsPackage, CMakePackage):
 
     with default_args(deprecated=True):
         # https://nvd.nist.gov/vuln/detail/CVE-2023-48795
-        version("1.11.0", sha256="3736161e41e2693324deb38c26cfdc3efe6209d634ba4258db1cecff6a5ad461")
-        version("1.10.0", sha256="2d64e90f3ded394b91d3a2e774ca203a4179f69aebee03003e5a6fa621e41d51")
+        version(
+            "1.11.0", sha256="3736161e41e2693324deb38c26cfdc3efe6209d634ba4258db1cecff6a5ad461"
+        )
+        version(
+            "1.10.0", sha256="2d64e90f3ded394b91d3a2e774ca203a4179f69aebee03003e5a6fa621e41d51"
+        )
         version("1.9.0", sha256="d5fb8bd563305fd1074dda90bd053fb2d29fc4bce048d182f96eaa466dfadafd")
         # https://nvd.nist.gov/vuln/detail/CVE-2019-3863
         version("1.8.0", sha256="39f34e2f6835f4b992cafe8625073a88e5a28ba78f83e8099610a7b3af4676d4")
