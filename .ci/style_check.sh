b8bb01377f4cae6059dbc14eda88fa2faf6e3475
Ryan Krattiger <ryan.krattiger@kitware.com>
2025-07-03 15:37:46 -0500

    Cleanup style check some more
    - remove unnecessary check for spack-core
    - run all checks even if one fails, report aggregated status
    - print which check(s) are running

--- .ci/style_check.sh
+++ .ci/style_check.sh
@@ -1,12 +1,16 @@
 #!/bin/sh -e
 ref="${1:-develop}"
-if [ ! -d "spack-core" ]; then
-  printf "No 'spack-core' dir found, should be a symlink or clone of 'spack/spack'\n" >&2
-  exit 1
-fi
+
 python_files() { git diff --name-only --diff-filter=ACMR -z "$ref" | grep -zE '\.pyi?$'; }
 python_files > /dev/null || exit 0
 python_files | xargs -0 printf "%s\n"
-python_files | xargs -0 -n 100 flake8
-python_files | xargs -0 -n 100 isort --check --diff
-python_files | xargs -0 -n 100 black --check --diff --color
+echo "Running flake8 check"
+python_files | xargs -0 -n 100 flake8 || flake_err=1
+echo "Running isort check"
+python_files | xargs -0 -n 100 isort --check --diff || isort_err=1
+echo "Running black check"
+python_files | xargs -0 -n 100 black --check --diff --color || black_err=1
+
+if [ $flake_err ] || [ $isort_err ] || [ $black_err ]; then
+  exit 1
+fi
