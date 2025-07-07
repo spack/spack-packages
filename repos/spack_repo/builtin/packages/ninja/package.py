792d858c608921d1f0aed8cf7d9a594cc38f21cc
Kathleen Shea <shea9@llnl.gov>
2025-07-07 16:15:16 -0700

    style fix

--- repos/spack_repo/builtin/packages/ninja/package.py
+++ repos/spack_repo/builtin/packages/ninja/package.py
@@ -37,7 +37,12 @@ class Ninja(Package):
     version("1.8.2", sha256="86b8700c3d0880c2b44c2ff67ce42774aaf8c28cbf57725cb881569288c1c6f4")
     version("1.7.2", sha256="2edda0a5421ace3cf428309211270772dd35a91af60c96f93f90df6bc41b16d9")
     version("1.6.0", sha256="b43e88fb068fe4d92a3dfd9eb4d19755dae5c33415db2e9b7b61b4659009cde7")
-    version("kitware", branch="features-for-fortran", git="https://github.com/Kitware/ninja.git", deprecated=True)
+    version(
+        "kitware",
+        branch="features-for-fortran",
+        git="https://github.com/Kitware/ninja.git",
+        deprecated=True
+    )
 
     # ninja@1.12: needs googletest source, but 1.12 itself needs a patch to use it
     resource(
