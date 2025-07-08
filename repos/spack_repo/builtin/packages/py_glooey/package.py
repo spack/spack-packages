4e0692f41f75f9b650867c897d2c1e04d20f6f1a
Brendan Moloney <moloney.brendan@gmail.com>
2025-07-08 14:58:39 -0700

    ENH: Add new package py-glooey

---
+++ repos/spack_repo/builtin/packages/py_glooey/package.py
@@ -0,0 +1,43 @@
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+# ----------------------------------------------------------------------------
+# If you submit this package back to Spack as a pull request,
+# please first remove this boilerplate and all FIXME comments.
+#
+# This is a template package file for Spack.  We've put "FIXME"
+# next to all the things you'll want to change. Once you've handled
+# them, you can save this file and test your package like this:
+#
+#     spack install py-glooey
+#
+# You can edit this file again by typing:
+#
+#     spack edit py-glooey
+#
+# See the Spack documentation for more information on packaging.
+# ----------------------------------------------------------------------------
+
+from spack_repo.builtin.build_systems.python import PythonPackage
+from spack.package import *
+
+
+class PyGlooey(PythonPackage):
+    """An object-oriented GUI library for pyglet."""
+
+    homepage = "https://glooey.readthedocs.io/en/latest/"
+    pypi = "glooey/glooey-0.3.6.tar.gz"
+
+    maintainers("moloney")
+
+    license("MIT", checked_by="moloney")
+
+    version("0.3.6", sha256="c025aca6c0e8d73f99793cb7b15aa547195da5499a11384379f2d3446f864520")
+
+    depends_on("python@3.6:", type=("build", "run"))
+    depends_on("py-flit", type="build")
+    with default_args(type=("build", "run")):
+        depends_on("py-pyglet")
+        depends_on("py-more-itertools")
+        depends_on("py-pyyaml")
