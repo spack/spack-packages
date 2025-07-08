9fbd5ce3f722206bae013b72818bbdd3de99bce7
Jon Rood <jon.rood@nrel.gov>
2025-07-08 14:29:39 -0600

    netcdf-c: always depends on cxx

--- repos/spack_repo/builtin/packages/netcdf_c/package.py
+++ repos/spack_repo/builtin/packages/netcdf_c/package.py
@@ -142,7 +142,7 @@ class NetcdfC(CMakePackage, AutotoolsPackage):
     variant("zstd", default=True, description="Enable Zstandard compression plugin")
 
     depends_on("c", type="build")
-    depends_on("cxx", type="build", when="build_system=cmake")
+    depends_on("cxx", type="build")
 
     with when("build_system=cmake"):
         # Based on the versions required by the root CMakeLists.txt:
