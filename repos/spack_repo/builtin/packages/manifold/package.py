db2a41ea35b6e4a6ecb30f1a432ca593012c8923
Brendan Moloney <moloney.brendan@gmail.com>
2025-07-09 12:39:10 -0700

    CLN: Remove boilerplate / fix style

--- repos/spack_repo/builtin/packages/manifold/package.py
+++ repos/spack_repo/builtin/packages/manifold/package.py
@@ -2,24 +2,8 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install manifold
-#
-# You can edit this file again by typing:
-#
-#     spack edit manifold
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack_repo.builtin.build_systems.cmake import CMakePackage
+
 from spack.package import *
 
 
