5b915ed4601a94857453608c2c01c8b434ad65cd
Caetano Melone <melone1@llnl.gov>
2025-07-10 14:55:03 -0700

    correct license and add passt tarball

--- repos/spack_repo/builtin/packages/passt/package.py
+++ repos/spack_repo/builtin/packages/passt/package.py
@@ -13,11 +13,10 @@ class Passt(MakefilePackage):
     virtual machines in user-mode without requiring any capabilities or privileges."""
 
     homepage = "https://passt.top"
-    git = "https://passt.top/passt"
 
-    license("GPL-2.0-or-later AND BSD-3-Clause", checked_by="cmelone")
+    license("GPL-2.0-or-later", checked_by="cmelone")
 
-    version("2025_01_21.4f2c8e7", tag="2025_01_21.4f2c8e7")
+    version("2025_01_21.4f2c8e7", sha256="c4e23a965db3a22776b89c8cef9a2cd66df04db5f1462f0f36ef499c70d2e607", url="https://passt.top/passt/snapshot/passt-2025_01_21.4f2c8e7.tar.gz")
 
     def install(self, spec, prefix):
         mkdirp(prefix.bin)
