ecc3fdbf92657e82d1d98a6ff74a6cd241610453
Jen Herting <jen@herting.cc>
2025-07-10 13:01:23 -0400

    [r-shinyfiletree] ran black

--- repos/spack_repo/builtin/packages/r_shinyfiletree/package.py
+++ repos/spack_repo/builtin/packages/r_shinyfiletree/package.py
@@ -15,8 +15,11 @@ class RShinyfiletree(RPackage):
 
     license("GPL-3.0")
 
-    version("0.0.0.9000", sha256="3df3d52798287b506613acf65eb07d9a535d55af97da0726f1b8ef202bd498df",
-            url="https://github.com/fbreitwieser/shinyFileTree/tarball/76c44e744c930c3908a98d4a055a5bff7fba1e0c")
+    version(
+        "0.0.0.9000",
+        sha256="3df3d52798287b506613acf65eb07d9a535d55af97da0726f1b8ef202bd498df",
+        url="https://github.com/fbreitwieser/shinyFileTree/tarball/76c44e744c930c3908a98d4a055a5bff7fba1e0c",
+    )
 
     depends_on("r@3.2.3:", type=("build", "run"))
     depends_on("r-htmlwidgets", type=("build", "run"))
