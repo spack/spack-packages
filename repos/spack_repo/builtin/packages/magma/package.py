27dff6ac1b48928cda5754584cdea8d7831cafa9
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2025-07-10 20:41:57 +0200

    magma: don't use set_env
    Signed-off-by: Massimiliano Culpo <massimiliano.culpo@gmail.com>

--- repos/spack_repo/builtin/packages/magma/package.py
+++ repos/spack_repo/builtin/packages/magma/package.py
@@ -1,13 +1,10 @@
 # Copyright Spack Project Developers. See COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
-
-
 from spack_repo.builtin.build_systems.cmake import CMakePackage
 from spack_repo.builtin.build_systems.cuda import CudaPackage
 from spack_repo.builtin.build_systems.rocm import ROCmPackage
 
-import spack.util.environment
 from spack.package import *
 
 
@@ -195,24 +192,22 @@ class Magma(CMakePackage, CudaPackage, ROCmPackage):
         """Run C examples"""
         test_dir = join_path(self.test_suite.current_test_cache_dir, self.test_src_dir)
         with working_dir(test_dir):
-            pkg_config_path = self.prefix.lib.pkgconfig
-            with spack.util.environment.set_env(PKG_CONFIG_PATH=pkg_config_path):
-                make = self.spec["gmake"].command
-                CC = "hipcc" if self.spec.satisfies("+rocm") else self.compiler.cc
-                make("c", f"CC={CC}")
-                tests = [
-                    ("example_sparse", "sparse solver"),
-                    ("example_sparse_operator", "sparse operator"),
-                    ("example_v1", "legacy v1 interface"),
-                    ("example_v2", "v2 interface"),
-                ]
+            make = self.spec["gmake"].command
+            CC = "hipcc" if self.spec.satisfies("+rocm") else self.compiler.cc
+            make("c", f"CC={CC}", extra_env={"PKG_CONFIG_PATH": self.prefix.lib.pkgconfig})
+            tests = [
+                ("example_sparse", "sparse solver"),
+                ("example_sparse_operator", "sparse operator"),
+                ("example_v1", "legacy v1 interface"),
+                ("example_v2", "v2 interface"),
+            ]
 
-                for test, desc in tests:
-                    with test_part(self, f"test_c_{test}", purpose=f"Run {desc} example"):
-                        exe = which(test)
-                        exe()
+            for test, desc in tests:
+                with test_part(self, f"test_c_{test}", purpose=f"Run {desc} example"):
+                    exe = which(test)
+                    exe()
 
-                make("clean")
+            make("clean")
 
     def test_fortran(self):
         """Run Fortran example"""
@@ -221,10 +216,8 @@ class Magma(CMakePackage, CudaPackage, ROCmPackage):
 
         test_dir = join_path(self.test_suite.current_test_cache_dir, self.test_src_dir)
         with working_dir(test_dir):
-            pkg_config_path = self.prefix.lib.pkgconfig
-            with spack.util.environment.set_env(PKG_CONFIG_PATH=pkg_config_path):
-                make = self.spec["gmake"].command
-                make("fortran")
-                example_f = which("example_f")
-                example_f()
-                make("clean")
+            make = self.spec["gmake"].command
+            make("fortran", extra_env={"PKG_CONFIG_PATH": self.prefix.lib.pkgconfig})
+            example_f = which("example_f")
+            example_f()
+            make("clean")
