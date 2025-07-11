9f17c7b1ea63eb06010d415f9bb6850b4d0fe3e1
Preshanth Jagannathan <pjaganna@nrao.edu>
2025-07-11 10:56:00 -0600

    Fixed formatting

--- repos/spack_repo/builtin/packages/parafeed/package.py
+++ repos/spack_repo/builtin/packages/parafeed/package.py
@@ -6,22 +6,22 @@ from spack_repo.builtin.build_systems.cmake import CMakePackage
 
 from spack.package import *
 
+
 class Parafeed(CMakePackage):
     """Parafeed is a C++ library to parse and interact with the commandline."""
 
     homepage = "https://github.com/sanbee/parafeed"
-    url      = "https://github.com/sanbee/parafeed/archive/refs/tags/v1.1.43.tar.gz"
+    url = "https://github.com/sanbee/parafeed/archive/refs/tags/v1.1.43.tar.gz"
 
     maintainers("sanbee", "sanbw", "preshanth")
 
-   
     license("GPL-3.0", checked_by="sanbee")
-    
+
     version("1.1.43", sha256="ded82f11b32535a02d805b26acfd06d5a406ddc9e4cc4a64606b513a65315338")
     version("1.1.41", sha256="a5cb034914cab8b661469f2f1aae6af7d88045d54f3535da8ff004a04cd8abd3")
-    version("1.1.4",  sha256="9ca767aa1139516926456fff14fec466890caba5646e46f0553c7b648bb5510e")
-    version("1.1.2",  sha256="74eb62034b0c4cbfd9a4a05872c10d568b184e8cf7543405d460fc0c52001139")
-    version("1.1.1",  sha256="757290ec76b3762febbaadca4c597f9d668be55767dbcfdba305ddc48cb07689")
+    version("1.1.4", sha256="9ca767aa1139516926456fff14fec466890caba5646e46f0553c7b648bb5510e")
+    version("1.1.2", sha256="74eb62034b0c4cbfd9a4a05872c10d568b184e8cf7543405d460fc0c52001139")
+    version("1.1.1", sha256="757290ec76b3762febbaadca4c597f9d668be55767dbcfdba305ddc48cb07689")
 
     # Build-time and run-time dependencies
     depends_on("readline")
@@ -35,13 +35,13 @@ class Parafeed(CMakePackage):
         "cxxstd",
         default="11",
         values=("11", "14", "17", "20", "23"),
-        description="C++ standard to use"
+        description="C++ standard to use",
     )
 
     def cmake_args(self):
         args = [
             self.define("CMAKE_CXX_STANDARD", self.spec.variants["cxxstd"].value),
             self.define("CMAKE_CXX_STANDARD_REQUIRED", "ON"),
-            self.define("CMAKE_CXX_EXTENSIONS", "OFF")
+            self.define("CMAKE_CXX_EXTENSIONS", "OFF"),
         ]
         return args
