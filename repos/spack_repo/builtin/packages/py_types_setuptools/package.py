0b32da905a120167f62c5d18dee94315c37ef0a0
Manuela Kuhn <manuela.kuhn@ovgu.de>
2025-07-14 23:24:24 +0200

    Fix wrong version limitations

--- repos/spack_repo/builtin/packages/py_types_setuptools/package.py
+++ repos/spack_repo/builtin/packages/py_types_setuptools/package.py
@@ -20,7 +20,7 @@ class PyTypesSetuptools(PythonPackage):
     version("68.2.0.0", sha256="a4216f1e2ef29d089877b3af3ab2acf489eb869ccaf905125c69d2dc3932fd85")
     version("65.5.0.3", sha256="17769171f5f2a2dc69b25c0d3106552a5cda767bbf6b36cb6212b26dae5aa9fc")
 
-    depends_on("py-setuptools@:77.0.3", type="build", when="@:79.0.0.20250422")
+    depends_on("py-setuptools@77.0.3:", type="build", when="@79.0.0.20250422:")
     depends_on("py-setuptools", type="build")
 
     def url_for_version(self, version):
