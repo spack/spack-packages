2e4544a4fd43f3f2f018feb3cbdc70d4935788ae
Joshua Finkelstein <39136329+finkeljos@users.noreply.github.com>
2025-07-15 13:35:57 -0600

    Update package.py
    trying to fix style issues

--- repos/spack_repo/builtin/packages/latte/package.py
+++ repos/spack_repo/builtin/packages/latte/package.py
@@ -2,11 +2,9 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-
 from spack_repo.builtin.build_systems.cmake import CMakePackage
 
 from spack.package import *
-
 
 class Latte(CMakePackage):
     """Open source density functional tight binding molecular dynamics."""
@@ -14,24 +12,24 @@ class Latte(CMakePackage):
     homepage = "https://github.com/lanl/latte"
     url = "https://github.com/lanl/latte/tarball/v1.2.1"
     git = "https://github.com/lanl/latte.git"
-
+    
     maintainers("jeanlucf22", "finkeljos")
 
     tags = ["ecp", "ecp-apps"]
-
+    
     license("LGPL-2.0-or-later")
-
+    
     version("master", branch="master")
     version("lattepy", branch="lattepy")
     version("1.2.2", sha256="ab1346939dbd70ffd89c5e5bf8d24946cb3655dc25b203bec7fc59c6c63e4c79")
     version("1.2.1", sha256="a21dda5ebdcefa56e9ff7296d74ef03f89c200d2e110a02af7a84612668bf702")
     version("1.0.1", sha256="67b2957639ad8e36b69bc6ea9a13085183a881562af9ca6d2b90b412ff073789")
-
+    
     variant("interface", default=False, description="Build with interfacing to use with sedacs")
     variant("mpi", default=True, description="Build with mpi")
     variant("progress", default=False, description="Use progress for fast solvers")
     variant("shared", default=True, description="Build shared libs")
-
+    
     depends_on("c", type="build")  # generated
     depends_on("fortran", type="build")  # generated
     depends_on("cmake@3.1:", type="build")
