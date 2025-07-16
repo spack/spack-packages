41351cc60241afba7bd583c2b5358e36eed3654f
David Boehme <boehme3@llnl.gov>
2025-07-16 09:47:57 -0700

    Caliper: Add v2.13.1

--- repos/spack_repo/builtin/packages/caliper/package.py
+++ repos/spack_repo/builtin/packages/caliper/package.py
@@ -26,7 +26,7 @@ class Caliper(CachedCMakePackage, CudaPackage, ROCmPackage):
 
     homepage = "https://github.com/LLNL/Caliper"
     git = "https://github.com/LLNL/Caliper.git"
-    url = "https://github.com/LLNL/Caliper/archive/v2.12.1.tar.gz"
+    url = "https://github.com/LLNL/Caliper/archive/v2.13.1.tar.gz"
     tags = ["e4s", "radiuss"]
 
     maintainers("daboehme", "adrienbernede")
@@ -36,11 +36,20 @@ class Caliper(CachedCMakePackage, CudaPackage, ROCmPackage):
     license("BSD-3-Clause")
 
     version("master", branch="master")
+    version("2.13.1", sha256="7cef0173e0e0673abb7943a2641b660adfbc3d6bc4b33941ab4f431f92a4d016")
     version("2.12.1", sha256="2b5a8f98382c94dc75cc3f4517c758eaf9a3f9cea0a8dbdc7b38506060d6955c")
     version("2.11.0", sha256="b86b733cbb73495d5f3fe06e6a9885ec77365c8aa9195e7654581180adc2217c")
     version("2.10.0", sha256="14c4fb5edd5e67808d581523b4f8f05ace8549698c0e90d84b53171a77f58565")
-    version("2.9.1", sha256="4771d630de505eff9227e0ec498d0da33ae6f9c34df23cb201b56181b8759e9e")
-    version("2.9.0", sha256="507ea74be64a2dfd111b292c24c4f55f459257528ba51a5242313fa50978371f")
+    version(
+        "2.9.1",
+        sha256="4771d630de505eff9227e0ec498d0da33ae6f9c34df23cb201b56181b8759e9e",
+        deprecated=True
+    )
+    version(
+        "2.9.0",
+        sha256="507ea74be64a2dfd111b292c24c4f55f459257528ba51a5242313fa50978371f",
+        deprecated=True,
+    )
     version(
         "2.8.0",
         sha256="17807b364b5ac4b05997ead41bd173e773f9a26ff573ff2fe61e0e70eab496e4",
