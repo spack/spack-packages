be3821c55604cc322a62e848fef53d4f0d8b8e9f
Joshua David Finkelstein <finkeljo@von-neumann.lanl.gov>
2025-07-16 13:34:02 -0600

    fixing style issues

--- repos/spack_repo/builtin/packages/shred/package.py
+++ repos/spack_repo/builtin/packages/shred/package.py
@@ -2,15 +2,15 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-from spack_repo.builtin.build_systems import cmake, makefile
 from spack_repo.builtin.build_systems.makefile import MakefilePackage
 
 from spack.package import *
 
 
 class Shred(MakefilePackage):
-    """Stochastic and Hybrid Representation Electronic structure by Density functional theory (SHRED). 
-       SHRED is a plane-wave DFT code similar to ABINIT, VASP and Quantum Espresso. """
+    """Stochastic and Hybrid Representation Electronic structure by Density
+       functional theory (SHRED). SHRED is a plane-wave DFT code similar to
+       ABINIT, VASP and Quantum Espresso."""
 
     url = "https://github.com/alwhite-LANL/SHRED/"
     git = "git@github.com:alwhite-LANL/SHRED.git"
@@ -19,10 +19,10 @@ class Shred(MakefilePackage):
 
     license("GPL-3.0-or-later")
 
-    version("develop",branch="new_develop")
+    version("develop", branch="new_develop")
 
-    depends_on("fortran", type="build") 
-    depends_on("c", type="build")  
+    depends_on("fortran", type="build")
+    depends_on("c", type="build")
 
     # Necessary package dependencies
     depends_on("mpi")
@@ -33,7 +33,7 @@ class Shred(MakefilePackage):
     depends_on("blas")
 
     # Need libxc version less than 6.0.0
-    conflicts('^libxc', when="@:6.0.0",msg='libxc version must be <6.0.0')
+    conflicts('^libxc', when="@:6.0.0", msg='libxc version must be <6.0.0')
 
     build_system("makefile", default="makefile")
 
@@ -51,8 +51,7 @@ class Shred(MakefilePackage):
         env.set('PLA_DIR', "%s" % spec["scalapack"].prefix)
 
     def install(self, spec, prefix):
-
-        # install to the spack view 
+        # install to the spack view
         mkdirp(prefix.bin)
         install("shred", prefix.bin)
 
