7630c72a4b3ae8909aaeed40eaf63ce1012400ce
Pavel Ondračka <pavel.ondracka@gmail.com>
2025-07-19 21:18:25 +0200

    openmx: fix aocc build

--- repos/spack_repo/builtin/packages/openmx/package.py
+++ repos/spack_repo/builtin/packages/openmx/package.py
@@ -40,6 +40,17 @@ class Openmx(MakefilePackage):
         when="@3.9",
     )
 
+    # Custom patch for clang: override Set_ProExpn_VNA.c
+    # See https://www.openmx-square.org/forum/patio.cgi?mode=view&no=2955
+    resource(
+        name="set_proexpn_patch",
+        url="https://gist.githubusercontent.com/Ncmexp2717/aaffd91eb1bc15e1909b057cc2e83c1f/raw/4e7dfd4a9346b82de69ce9b1d270042bebcd1610/Set_ProExpn_VNA.c",
+        sha256="d33de4cbae9af19f39bdf880d33a487cb8fb65beb9fbaa40f4202315dc6ec87b",
+        placement="set_proexpn_patch",
+        when="@3.9",
+        expand=False,
+    )
+
     depends_on("mpi")
     depends_on("fftw-api@3")
     depends_on("blas")
@@ -61,6 +72,8 @@ class Openmx(MakefilePackage):
         # http://www.openmx-square.org/bugfixed/21Oct17/README.txt
         if spec.satisfies("@3.9"):
             copy(join_path("source", "kpoint.in"), "work")
+            if "%aocc" in spec or "%llvm" in spec:
+                copy("set_proexpn_patch/Set_ProExpn_VNA.c", "source/Set_ProExpn_VNA.c")
         makefile = FileFilter("./source/makefile")
         makefile.filter("^DESTDIR.*$", "DESTDIR = {0}/bin".format(prefix))
         mkdirp(prefix.bin)
@@ -95,6 +108,8 @@ class Openmx(MakefilePackage):
                 lib_option.append("-lgfortran")
                 if spec.satisfies("%gcc@10:"):
                     fc_option.append("-fallow-argument-mismatch")
+            if "%llvm" in spec or "%aocc" in spec:
+                lib_option.extend(["-lflang", "-lpgmath"])
 
         return [
             "CC={0} -Dscalapack {1} ".format(" ".join(cc_option), " ".join(common_option)),
