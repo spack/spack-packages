02303606f182b79424e400539e94943fe89bbbc9
Afzal Patel <Afzal.Patel@amd.com>
2025-07-21 11:24:49 -0700

    fix style

--- repos/spack_repo/builtin/packages/rocprofiler_systems/package.py
+++ repos/spack_repo/builtin/packages/rocprofiler_systems/package.py
@@ -134,10 +134,10 @@ class RocprofilerSystems(CMakePackage):
     depends_on("automake", when="+rocm")
     depends_on("libtool", when="+rocm")
     with when("+rocm"):
-       for ver in ["6.3.0", "6.3.1", "6.3.2", "6.3.3"]:
+        for ver in ["6.3.0", "6.3.1", "6.3.2", "6.3.3"]:
             depends_on(f"roctracer-dev@{ver}", when=f"@{ver}")
             depends_on(f"rocprofiler-dev@{ver}", when=f"@{ver}")
-       for ver in ["6.3.0", "6.3.1", "6.3.2", "6.3.3", "6.4.0", "6.4.1"]:
+        for ver in ["6.3.0", "6.3.1", "6.3.2", "6.3.3", "6.4.0", "6.4.1"]:
             depends_on(f"rocm-smi-lib@{ver}", when=f"@{ver}")
             depends_on(f"hip@{ver}", when=f"@{ver}")
         for ver in ["6.4.0", "6.4.1"]:
