cb025740ae8e9b62f3dc528fa579cb9b02ec5eaf
Trévis Morvany <trevis.morvany@cea.fr>
2025-07-22 16:37:03 +0200

    Use the right copyright

--- repos/spack_repo/builtin/packages/ddc/package.py
+++ repos/spack_repo/builtin/packages/ddc/package.py
@@ -1,6 +1,6 @@
-# Copyright (C) The DDC development team, see COPYRIGHT.md file
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
 #
-# SPDX-License-Identifier: MIT
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 from spack_repo.builtin.build_systems.cmake import CMakePackage
 from spack_repo.builtin.build_systems.cuda import CudaPackage
