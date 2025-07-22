64484f4bd3343ae1f7e31e871d477f4f4359b91d
Brian Vanderwende <vanderwb@ucar.edu>
2025-07-22 12:54:13 -0600

    esmf: fix intel compiler name issue (#699)
    The ESMF package needs to determine compiler family based on the compiler name, but instead of giving `oneapi`, the compiler.name field now gives `intel-oneapi-compilers`.

--- repos/spack_repo/builtin/packages/esmf/package.py
+++ repos/spack_repo/builtin/packages/esmf/package.py
@@ -246,7 +246,7 @@ class MakefileBuilder(makefile.MakefileBuilder):
         if spec["fortran"].name == "gcc" and spec["c"].name == "gcc":
             gfortran_major_version = int(spec["fortran"].version[0])
             env.set("ESMF_COMPILER", "gfortran")
-        elif self.pkg.compiler.name == "intel" or self.pkg.compiler.name == "oneapi":
+        elif spec["fortran"].name in ["intel-oneapi-compilers", "intel-oneapi-compilers-classic"]:
             env.set("ESMF_COMPILER", "intel")
         elif spec["fortran"].name == "gcc" and spec["c"].name in ["clang", "apple-clang"]:
             gfortran_major_version = int(spec["fortran"].version[0])
