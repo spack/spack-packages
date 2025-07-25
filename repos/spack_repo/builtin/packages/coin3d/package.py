38c410d75074085b5ff618f162518c7ab458cbb5
Wouter Deconinck <wdconinc@gmail.com>
2025-07-25 13:53:28 -0500

    coin3d: add v4.0.4

--- repos/spack_repo/builtin/packages/coin3d/package.py
+++ repos/spack_repo/builtin/packages/coin3d/package.py
@@ -19,6 +19,7 @@ class Coin3d(AutotoolsPackage, CMakePackage):
 
     license("BSD-3-Clause")
 
+    version("4.0.4", sha256="80efd056a445050939a265db307d106ac7524105774d4be924a71b0cff23a719")  
     version("4.0.0", sha256="e4f4bd57804b8ed0e017424ad2e45c112912a928b83f86c89963df9015251476")
     version("3.1.0", sha256="70dd5ef39406e1d9e05eeadd54a5b51884a143e127530876a97744ca54173dc3")
     version("3.0.0", sha256="d5c2eb0ecaa5c83d93daf0e9e275e58a6a8dfadc74c873d51b0c939011f81bfa")
@@ -61,7 +62,9 @@ class Coin3d(AutotoolsPackage, CMakePackage):
     )
 
     def url_for_version(self, version):
-        if version >= Version("4.0.0"):
+        if version >= Version("4.0.1"):
+            url = "https://github.com/coin3d/coin/releases/download/v{0}/coin-{0}-src.tar.gz"
+        elif version >= Version("4.0.0"):
             url = "https://github.com/coin3d/coin/releases/download/Coin-{0}/coin-{0}-src.tar.gz"
         else:
             url = "https://github.com/coin3d/coin/archive/Coin-{0}.tar.gz"
