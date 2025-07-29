d96cf1bff8c020f42dcd15b99968386ec5e02ac2
Scot Halverson <shalverson>
2025-07-29 14:20:24 -0700

    removing boilerplate

--- repos/spack_repo/builtin/packages/podman_compose/package.py
+++ repos/spack_repo/builtin/packages/podman_compose/package.py
@@ -1,22 +1,6 @@
 # Copyright Spack Project Developers. See COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install podman-compose
-#
-# You can edit this file again by typing:
-#
-#     spack edit podman-compose
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
 import sys
 
 from spack_repo.builtin.build_systems.python import PythonPackage
