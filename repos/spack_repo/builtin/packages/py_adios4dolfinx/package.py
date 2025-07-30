00e790e515667b61bc0bfd344119cfc8e5a116c3
jorgensd <dokken92@gmail.com>
2025-07-30 19:55:25 +0200

    style fix

--- repos/spack_repo/builtin/packages/py_adios4dolfinx/package.py
+++ repos/spack_repo/builtin/packages/py_adios4dolfinx/package.py
@@ -19,9 +19,7 @@ class PyAdios4dolfinx(PythonPackage):
     version("0.9.4", sha256="1d75d4f45f28c64821ad023df3273d933f1d4752c0e53b62f41030dca06bd627")
     version("0.9.3", sha256="0928f48dcc2ffaf5c97d3b2fedde6c4d79b2cbc5a3e2dbe0a0d50572c02590f5")
     version("0.9.2", sha256="a1b111a586ba8af6776e05d001850fdfc196cbc173eb501c8c1659ade993f49c")
-    version(
-        "0.9.1.post0", sha256="91a957bc967c1a3b0a454a5b8658600c38a38535c90b0c8b31bc0f52841c07fd"
-    )
+    version("0.9.1.post0", sha256="91a957bc967c1a3b0a454a5b8658600c38a38535c90b0c8b31bc0f52841c07fd")
     version("0.9.1", sha256="4dfd8ea3d8d21566adc40a3615830e5613d215e774e7f30de6d8c88b2078b549")
     version("0.9.0", sha256="5a72a8bc111d848b56d6eb776d7ca5963fe41e4126a3d875304224f80f15b8f6")
  
