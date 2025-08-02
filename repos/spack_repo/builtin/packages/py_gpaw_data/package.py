5f0ea4d38d9e723c75bc8496c1bfccac5d942eee
Ali K. Hamze <6139089+alikhamze@users.noreply.github.com>
2025-08-02 12:55:28 -0400

    Add python and setuptools dependencies and remove remaining fixme sections.

--- repos/spack_repo/builtin/packages/py_gpaw_data/package.py
+++ repos/spack_repo/builtin/packages/py_gpaw_data/package.py
@@ -20,25 +20,6 @@ class PyGpawData(PythonPackage):
     with default_args(deprecated=True):
         version("1.0.0", sha256="dabcdc4bb29450b37ed8bece6161d42da4ff609f05e0067a35c7811849e64223")
 
-    # FIXME: Only add the python/pip/wheel dependencies if you need specific versions
-    # or need to change the dependency type. Generic python/pip/wheel dependencies are
-    # added implicity by the PythonPackage base class.
-    # depends_on("python@2.X:2.Y,3.Z:", type=("build", "run"))
-    # depends_on("py-pip@X.Y:", type="build")
-    # depends_on("py-wheel@X.Y:", type="build")
+    depends_on("python@3.9:", type=("build", "run"))
 
-    # FIXME: Add a build backend, usually defined in pyproject.toml. If no such file
-    # exists, use setuptools.
-    # depends_on("py-setuptools", type="build")
-    # depends_on("py-hatchling", type="build")
-    # depends_on("py-flit-core", type="build")
-    # depends_on("py-poetry-core", type="build")
-
-    # FIXME: Add additional dependencies if required.
-    # depends_on("py-foo", type=("build", "run"))
-
-    def config_settings(self, spec, prefix):
-        # FIXME: Add configuration settings to be passed to the build backend
-        # FIXME: If not needed, delete this function
-        settings = {}
-        return settings
+    depends_on("py-setuptools", type="build")
