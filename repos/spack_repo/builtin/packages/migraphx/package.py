0fee9a07f28ac280cbb6aaaf8a14c25ea6cc4c54
Renjith Ravindran <Renjith.RavindranKannath@amd.com>
2025-08-04 12:09:54 -0700

    Adding hipblas & hipblaslt dependency

--- repos/spack_repo/builtin/packages/migraphx/package.py
+++ repos/spack_repo/builtin/packages/migraphx/package.py
@@ -123,6 +123,10 @@ class Migraphx(CMakePackage):
     ]:
         depends_on(f"rocmlir@{ver}", when=f"@{ver}")
 
+    for ver in ["6.3.0", "6.3.1", "6.3.2", "6.3.3", "6.4.0", "6.4.1", "6.4.2"]:
+        depends_on(f"hipblas@{ver}", when=f"@{ver}")
+        depends_on(f"hipblaslt@{ver}", when=f"@{ver}")
+
     @property
     def cmake_python_hints(self):
         """Include the python include path to the
