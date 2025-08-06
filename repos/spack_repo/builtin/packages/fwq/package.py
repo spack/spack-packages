6c27ac0ddad602f4b00b088c749ca56eccb0e7f4
Nathan Hanford <nhanford@llnl.gov>
2025-08-06 15:31:03 -0700

    first attempt at fwq package

---
+++ repos/spack_repo/builtin/packages/fwq/package.py
@@ -0,0 +1,53 @@
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack_repo.builtin.build_systems.makefile import MakefilePackage
+from spack.package import *
+
+import os.path
+
+class Fwq(MakefilePackage):
+    """This is a set of benchmarks designed to test system noise on HPC systems."""
+
+    homepage = "https://github.com/LLNL/system-noise"
+    url = "https://github.com/LLNL/system-noise.git"
+
+    maintainers("eleon", "nhanford")
+
+    license("MIT", checked_by="nhanford")
+
+    version("main")
+
+    variant("mpi", default=False, description="Build MPI-dependent benchmarks.")
+    variant("daxpy", default=False, description="Use DAXPY work.")
+    variant("vector", default=False, description="Use hardware vectorization support.")
+
+    depends_on("mpi", when="+mpi")
+    depends_on("gcc")
+    depends_on("py-pandas")
+
+    build_directory = "fwq"
+
+    @property
+    def build_targets(self):
+        targets = ["single", "thread"]
+        if "+mpi" in self.spec:
+            targets.append("mpi")
+        return targets
+    
+    @property
+    def install_targets(self):
+        targets = ["fwq", "fwq-th"]
+        if "+mpi" in self.spec:
+            targets.append("fwq-mpi")
+        return targets
+
+    def install(self, spec, prefix):
+        mkdir(prefix.bin)
+        with working_dir(self.build_directory):
+            for target in self.build_targets:
+                make(target)
+            for target in self.install_targets:
+                install(target, prefix.bin)
+        install(os.path.join("scripts", "fwq-stats.py"), prefix.bin)
