b12778de1fdfdec595d4446cd8dfb31b89cf016e
John Parent <john.parent@kitware.com>
2025-08-18 17:49:48 -0400

    msmpi: install_to_lib -> link_to_lib

--- repos/spack_repo/builtin/packages/msmpi/package.py
+++ repos/spack_repo/builtin/packages/msmpi/package.py
@@ -129,15 +129,15 @@ class MSBuildBuilder(msbuild.MSBuildBuilder):
         # glob statement matches any of the above.
         # symlink all linking related components (libs, .exp, etc)
         # to lib
-        def install_to_lib(file_type):
+        def link_to_lib(file_type):
             for x in glob.glob(os.path.join(prefix, "**", f"*.{file_type}")):
                 install_path = os.path.join(prefix.lib, os.path.basename(x))
                 symlink(x, install_path)
 
         mkdirp(prefix.lib)
-        install_to_lib("lib")
-        install_to_lib("pdb")
-        install_to_lib("exp")
+        link_to_lib("lib")
+        link_to_lib("pdb")
+        link_to_lib("exp")
 
         include_dir = os.path.join(os.path.join(base_build, "src"), "include")
         install_tree(include_dir, prefix.include)
