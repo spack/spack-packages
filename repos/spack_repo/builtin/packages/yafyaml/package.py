3c10268514cdabd6462df457cb3a2f0ff4c86767
Matthew Thompson <matthew.thompson@nasa.gov>
2025-08-19 08:52:20 -0400

    Fix style

--- repos/spack_repo/builtin/packages/yafyaml/package.py
+++ repos/spack_repo/builtin/packages/yafyaml/package.py
@@ -72,7 +72,9 @@ class Yafyaml(CMakePackage):
     # ifx (or rather fpp) 2025.2 has a bug that prevents yafyaml from compiling
     # This bug is still in 2025.2.1
     # https://community.intel.com/t5/Intel-Fortran-Compiler/Regression-with-fpp-2025-2-0/m-p/1703735
-    conflicts("%oneapi@2025.2:", msg="yaFyaml does not compile with ifx 2025.2 due to a bug in fpp")
+    conflicts(
+        "%oneapi@2025.2:", msg="yaFyaml does not compile with ifx 2025.2 due to a bug in fpp"
+    )
 
     variant(
         "build_type",
