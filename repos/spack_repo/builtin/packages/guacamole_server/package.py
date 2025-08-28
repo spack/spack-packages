434e9ae8fed9564e103aeef8f56514b1fbf8ee27
Adam J. Stewart <ajstewart426@gmail.com>
2025-08-28 13:54:50 +0200

    Deprecation removals: E-H
    Signed-off-by: Adam J. Stewart <ajstewart426@gmail.com>

--- repos/spack_repo/builtin/packages/guacamole_server/package.py
+++ repos/spack_repo/builtin/packages/guacamole_server/package.py
@@ -18,9 +18,6 @@ class GuacamoleServer(AutotoolsPackage):
     license("GPL-3.0-or-later")
 
     version("1.5.5", sha256="50430c0f0f3b92f2cd3e60436fab0cedee8c1a9f762696a666016347039c731e")
-    with default_args(deprecated=True):
-        # https://nvd.nist.gov/vuln/detail/CVE-2023-43826
-        version("1.1.0", sha256="d0f0c66ebfa7a4fd6689ae5240f21797b5177945a042388b691b15b8bd5c81a8")
 
     depends_on("c", type="build")
 
