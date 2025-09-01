60bd8fd8a11083defeb16d8c8a66091c565d91d4
AMD Toolchain Support <73240730+amd-toolchain-support@users.noreply.github.com>
2025-09-01 12:21:04 +0000

    bwa: add new versions

--- repos/spack_repo/builtin/packages/bwa/package.py
+++ repos/spack_repo/builtin/packages/bwa/package.py
@@ -17,6 +17,16 @@ class Bwa(Package):
 
     license("GPL-3.0-only")
 
+    version(
+        "0.7.19",
+        sha256="cdff5db67652c5b805a3df08c4e813a822c65791913eccfb3cf7d528588f37bc",
+        url="https://github.com/lh3/bwa/archive/refs/tags/v0.7.19.tar.gz",
+    )
+    version(
+        "0.7.18",
+        sha256="194788087f7b9a77c0114aa481b2ef21439f6abab72488c83917302e8d0e7870",
+        url="https://github.com/lh3/bwa/archive/refs/tags/v0.7.18.tar.gz",
+    )
     version("0.7.17", sha256="de1b4d4e745c0b7fc3e107b5155a51ac063011d33a5d82696331ecf4bed8d0fd")
     version("0.7.15", sha256="2f56afefa49acc9bf45f12edb58e412565086cc20be098b8bf15ec07de8c0515")
     version("0.7.13", sha256="559b3c63266e5d5351f7665268263dbb9592f3c1c4569e7a4a75a15f17f0aedc")
