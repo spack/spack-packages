4503bd1db657117da106f85dd8fbec87e2a19921
Jen Herting <jen@herting.cc>
2025-09-03 09:43:31 -0400

    [py-p2j] ran black

--- repos/spack_repo/builtin/packages/py_p2j/package.py
+++ repos/spack_repo/builtin/packages/py_p2j/package.py
@@ -15,7 +15,6 @@ class PyP2j(PythonPackage):
     license("MIT", checked_by="mll8777")
 
     version("1.3.2", sha256="6a492350953a87ceaf190b13141242f604b597efd668c2b026241c4a4f4777f5")
-    
-    depends_on("py-setuptools", type="build")
-    depends_on("python@3.6:", type=("build","run"))
 
+    depends_on("py-setuptools", type="build")
+    depends_on("python@3.6:", type=("build", "run"))
