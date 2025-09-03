ad68faeaf4ea05b6d1ee38f4254fe12d41251616
Mikael Simberg <mikael.simberg@iki.fi>
2025-09-03 11:00:21 +0200

    asio: add 1.36.0

--- repos/spack_repo/builtin/packages/asio/package.py
+++ repos/spack_repo/builtin/packages/asio/package.py
@@ -20,6 +20,7 @@ class Asio(AutotoolsPackage):
     license("BSL-1.0")
 
     # As uneven minor versions of asio are not considered stable, they wont be added anymore
+    version("1.36.0", sha256="0310a76b27e1854f09f696b30de57dc490b5e1b17faed1eb8c9a2891f956e52b")
     version("1.34.2", sha256="f3bac015305fbb700545bd2959fbc52d75a1ec2e05f9c7f695801273ceb78cf5")
     version("1.34.0", sha256="061ed6c8b97527756aed3e34d2cbcbcb6d3c80afd26ed6304f51119e1ef6a1cd")
     version("1.32.0", sha256="f1b94b80eeb00bb63a3c8cef5047d4e409df4d8a3fe502305976965827d95672")
