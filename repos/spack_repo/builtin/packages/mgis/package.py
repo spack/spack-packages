4283e4d819b43f8803995545d899f4596cd52eea
Thomas Helfer <thomas.helfer@cea.fr>
2025-09-08 10:59:04 +0200

    correctly define TFEL_DIR

--- repos/spack_repo/builtin/packages/mgis/package.py
+++ repos/spack_repo/builtin/packages/mgis/package.py
@@ -126,6 +126,8 @@ class Mgis(CMakePackage):
         args.append("-Denable-website=OFF")
         args.append("-Denable-doxygen-doc=OFF")
 
+        args.append("-DTFEL_DIR={0}/share/tfel/cmake".format(self.spec["tfel"].prefix))
+        
         for i in ["c", "fortran", "python"]:
             if "+" + i in self.spec:
                 args.append("-Denable-{0}-bindings=ON".format(i))
