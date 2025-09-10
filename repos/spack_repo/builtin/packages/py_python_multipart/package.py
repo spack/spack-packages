729e1d9e2927b02148fddcfadc68f30f3496b9f7
Thomas Bouvier <contact@thomas-bouvier.io>
2025-09-11 00:40:46 +0200

    py-python-multipart: add v0.0.20

--- repos/spack_repo/builtin/packages/py_python_multipart/package.py
+++ repos/spack_repo/builtin/packages/py_python_multipart/package.py
@@ -16,13 +16,17 @@ class PyPythonMultipart(PythonPackage):
 
     license("Apache-2.0")
 
+    version("0.0.20", sha256="8dd0cab45b8e23064ae09147625994d090fa46f5b0d1e13af944c331a7fa9d13")
     version("0.0.17", sha256="41330d831cae6e2f22902704ead2826ea038d0419530eadff3ea80175aec5538")
     version("0.0.5", sha256="f7bb5f611fc600d15fa47b3974c8aa16e93724513b49b5f95c81e6624c83fa43")
 
     depends_on("py-setuptools", type="build", when="@:0.0.5")
     depends_on("py-hatchling", type="build", when="@0.0.6:")
+    depends_on("python@3.8:", when="@0.0.9:", type=("build", "run"))
+    depends_on("python@3.7:", when="@0.0.6:", type=("build", "run"))
+    depends_on("python@2.7:3.3", when="@0.0.5", type=("build", "run"))
 
-    depends_on("py-six@1.4.0:", type=("build", "run"))
+    depends_on("py-six@1.4.0:", when="@0.0.5", type=("build", "run"))
 
     def url_for_version(self, version):
         url = "https://files.pythonhosted.org/packages/source/p/python-multipart/{}-{}.tar.gz"
