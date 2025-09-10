16bc0aa00c7856bdd052211e82e4237c38978141
Adrien M. BERNEDE <51493078+adrienbernede@users.noreply.github.com>
2025-09-10 17:26:11 +0200

    Fix style

--- repos/spack_repo/builtin/packages/umpire/package.py
+++ repos/spack_repo/builtin/packages/umpire/package.py
@@ -250,7 +250,7 @@ class Umpire(CachedCMakePackage, CudaPackage, ROCmPackage):
     depends_on("cmake@3.8:", type="build")
 
     depends_on("blt", type="build")
-    depends_on("blt@0.7.1:", type="build", when="@2025.09.0:") #2025.09.0 is a future version
+    depends_on("blt@0.7.1:", type="build", when="@2025.09.0:")  # 2025.09.0 is a future version
     depends_on("blt@0.7.0:", type="build", when="@2025.03.0:")
     depends_on("blt@0.6.2:", type="build", when="@2024.02.1:")
     depends_on("blt@0.6.1", type="build", when="@2024.02.0")
@@ -266,7 +266,7 @@ class Umpire(CachedCMakePackage, CudaPackage, ROCmPackage):
     depends_on("camp+openmp", when="+openmp")
     depends_on("camp~cuda", when="~cuda")
     depends_on("camp~rocm", when="~rocm")
-    depends_on("camp@2025.09.0:", when="@2025.09.0:") #2025.09.0 is a future version
+    depends_on("camp@2025.09.0:", when="@2025.09.0:")  # 2025.09.0 is a future version
     depends_on("camp@2024.07.0:", when="@2024.07.0:")
     depends_on("camp@2024.02.1", when="@2024.02.1")
     depends_on("camp@2024.02.0", when="@2024.02.0")
