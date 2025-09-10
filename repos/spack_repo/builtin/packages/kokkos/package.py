881440e57af6049b678f579b280eb44bd0e1dd7f
Cédric Chevalier <cedric.chevalier@cea.fr>
2025-09-10 17:54:30 +0200

    Add conflict for debug_dualview_modify_check
    Newer versions of Kokkos always check dual view usage.

--- repos/spack_repo/builtin/packages/kokkos/package.py
+++ repos/spack_repo/builtin/packages/kokkos/package.py
@@ -216,6 +216,8 @@ class Kokkos(CMakePackage, CudaPackage, ROCmPackage):
         "tests": [False, None, "Build for tests"],
     }
 
+    conflicts("~debug_dualview_modify_check", when="@4.7:")  # always enable from 4.7.00
+    
     spack_micro_arch_map = {
         "thunderx2": "THUNDERX2",
         "zen": "ZEN",
