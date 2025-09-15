4e1ba9461fd64445231e51f8b018589b0ee0e81e
Rocco Meli <r.meli@bluemail.ch>
2025-09-15 15:04:41 +0200

    Update repos/spack_repo/builtin/packages/py_metatomic_torch/package.py

--- repos/spack_repo/builtin/packages/py_metatomic_torch/package.py
+++ repos/spack_repo/builtin/packages/py_metatomic_torch/package.py
@@ -25,7 +25,7 @@ class PyMetatomicTorch(PythonPackage):
     depends_on("py-torch@2.1:", type=("build", "run"))
     depends_on("py-metatensor-torch@0.8.0:0.8", type=("build", "run"))
     # >=0.3.0 and <0.4.0
-    depends_on("py-metatensor-operations@0.3", type=("build", "run"))
+    depends_on("py-metatensor-operations@0.3.0:0.3", type=("build", "run"))
     # pyproject.toml
     depends_on("py-setuptools@77:", type="build")
     depends_on("py-packaging@23:", type="build")
