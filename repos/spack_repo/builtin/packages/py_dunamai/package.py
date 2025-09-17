52eade6ca92da037900acac27be01d32797a0d31
Manuela Kuhn <manuela.kuhn@ovgu.de>
2025-09-17 22:28:25 +0200

    py-dunamai: add v1.12.0

--- repos/spack_repo/builtin/packages/py_dunamai/package.py
+++ repos/spack_repo/builtin/packages/py_dunamai/package.py
@@ -20,6 +20,7 @@ class PyDunamai(PythonPackage):
     version("1.18.0", sha256="5200598561ea5ba956a6174c36e402e92206c6a6aa4a93a6c5cb8003ee1e0997")
     version("1.17.0", sha256="459381b585a1e78e4070f0d38a6afb4d67de2ee95064bf6b0438ec620dde0820")
     version("1.13.1", sha256="49597bdf653bdacdeb51ec6e0f1d4d2327309376fc83e6f1d42af6e29600515f")
+    version("1.12.0", sha256="fac4f09e2b8a105bd01f8c50450fea5aa489a6c439c949950a65f0dd388b0d20")
 
     depends_on("python@3.5:3", type=("build", "run"))
     depends_on("py-poetry-core@1:", type="build")
