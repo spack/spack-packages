b6bc47f782ba850606e0febb036d9e439083f649
Adam J. Stewart <ajstewart426@gmail.com>
2025-09-18 11:14:28 +0200

    Remove unused imports
    Signed-off-by: Adam J. Stewart <ajstewart426@gmail.com>

--- repos/spack_repo/builtin/packages/mysql/package.py
+++ repos/spack_repo/builtin/packages/mysql/package.py
@@ -3,7 +3,6 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 import os
-import tempfile
 
 from spack_repo.builtin.build_systems.cmake import CMakePackage
 from spack_repo.builtin.packages.boost.package import Boost
