c1eaabc73e9e6679f264e619e6267807a8448443
Bram Veenboer <bram.veenboer@gmail.com>
2025-09-24 06:26:08 +0000

    Add missing dependency on c

--- repos/spack_repo/builtin/packages/log4cplus/package.py
+++ repos/spack_repo/builtin/packages/log4cplus/package.py
@@ -21,4 +21,5 @@ class Log4cplus(CMakePackage):
     version("2.0.1", sha256="43baa7dec3db1ecc97dd9ecf3b50220439d2c7041d15860c36aa1d48dcf480b5")
     version("1.2.1", sha256="ada80be050033d7636beb894eb54de5575ceca95a5572e9437b0fc4ed7d877c4")
 
+    depends_on("c", type="build")
     depends_on("cxx", type="build")  # generated
