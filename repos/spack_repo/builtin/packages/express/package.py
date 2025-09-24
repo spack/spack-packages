9f1c0a34a15fbfe94a6282c7da419a62ff1bf36a
Emerson Jacobson <emwjacobson@gmail.com>
2025-09-24 12:38:01 -0700

    Update repos/spack_repo/builtin/packages/express/package.py
    Co-authored-by: Greg Becker <becker33@llnl.gov>

--- repos/spack_repo/builtin/packages/express/package.py
+++ repos/spack_repo/builtin/packages/express/package.py
@@ -59,7 +59,7 @@ class Express(CMakePackage):
 
             # Dont try and compile against specific file
             edit.filter(
-                r"\${PROTOBUF_LIB_DIR}/" "libprotobuf.a", "%s" % self.spec["protobuf"].libs
+                r"\${PROTOBUF_LIB_DIR}/libprotobuf.a", "%s" % self.spec["protobuf"].libs
             )
 
         if self.spec.satisfies("%boost@1.82:"):
