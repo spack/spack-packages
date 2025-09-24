ad01d99344ad205adbbdf7c704873e2d2ab69b1c
Paul Gessinger <hello@paulgessinger.com>
2025-09-24 11:04:58 +0200

    madgraph5: add version 3.5.9

--- repos/spack_repo/builtin/packages/madgraph5amc/package.py
+++ repos/spack_repo/builtin/packages/madgraph5amc/package.py
@@ -26,6 +26,7 @@ class Madgraph5amc(MakefilePackage):
     timeout = {"timeout": 60}
 
     with default_args(fetch_options=timeout):
+        version("3.5.9", sha256="1e707fcd18f5b967c3f6220b3e5538622c93472376cae6666c56d0f2c2dd4b92")
         version("3.5.6", sha256="d4f336196303df748074ac92f251db8e6592fca37b3059c2e0f2a764c7e50975")
         version(
             "2.9.20",
