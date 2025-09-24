5e48fee0d314fed3ca48d3849cadba1a8ea2fa07
Nathan Hanford <nhanford@llnl.gov>
2025-09-24 09:13:45 -0700

    remove dlopen

--- repos/spack_repo/builtin/packages/libfabric/package.py
+++ repos/spack_repo/builtin/packages/libfabric/package.py
@@ -238,7 +238,6 @@ class Libfabric(AutotoolsPackage, CudaPackage, ROCmPackage):
 
         if self.spec.satisfies("+rocm"):
             args.append(f"--with-rocr={self.spec['hip'].prefix}")
-            args.append("--enable-rocr-dlopen")
 
         return args
 
