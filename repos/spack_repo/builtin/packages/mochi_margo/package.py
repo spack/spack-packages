cbccad2b07ed6355bc0d5d50b0a987618ef88587
Matthieu Dorier <mdorier@anl.gov>
2025-09-24 13:47:07 +0100

    mochi-margo: changed order between cmake and autotools import

--- repos/spack_repo/builtin/packages/mochi_margo/package.py
+++ repos/spack_repo/builtin/packages/mochi_margo/package.py
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-from spack_repo.builtin.build_systems import cmake, autotools
+from spack_repo.builtin.build_systems import autotools, cmake
 from spack import *
 from spack.package import *
 
