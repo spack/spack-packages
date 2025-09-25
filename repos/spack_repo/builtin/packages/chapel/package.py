3aa68fa1c58968f99fe5a86a490c672e648a0195
Anna Rift <anna.rift@hpe.com>
2025-09-25 13:18:57 -0700

    Change single quotes to double quotes

--- repos/spack_repo/builtin/packages/chapel/package.py
+++ repos/spack_repo/builtin/packages/chapel/package.py
@@ -607,7 +607,7 @@ class Chapel(AutotoolsPackage, CudaPackage, ROCmPackage):
         with set_env(CHPL_MAKE_THIRD_PARTY=join_path(self.build_directory, "third-party")):
             # print all the explicitly set CHPL_* config variables to assist in debugging
             for var in self.chpl_env_vars:
-                tty.info(var + '=' + os.getenv(var, '<unset>'))
+                tty.info(var + "=" + os.getenv(var, "<unset>"))
             make()
             with set_env(CHPL_HOME=self.build_directory):
                 if spec.satisfies("+chpldoc"):
