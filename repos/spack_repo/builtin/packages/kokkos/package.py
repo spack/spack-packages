63bb0ae48935d78137c7945b43d5137ebe09fd1d
Thomas Padioleau <thomas.padioleau@cea.fr>
2025-09-29 21:03:57 +0200

    Kokkos: fix cmake conflict

--- repos/spack_repo/builtin/packages/kokkos/package.py
+++ repos/spack_repo/builtin/packages/kokkos/package.py
@@ -82,7 +82,7 @@ class Kokkos(CMakePackage, CudaPackage, ROCmPackage):
     depends_on("cxx", type="build")  # Kokkos requires a C++ compiler
 
     depends_on("cmake@3.16:", type="build")
-    conflicts("cmake@3.28", when="@:4.2.01 +cuda")
+    conflicts("^cmake@3.28", when="@:4.2.01 +cuda")
     conflicts("^cuda@13:", when="@:4.7.0")
 
     devices_variants = {
