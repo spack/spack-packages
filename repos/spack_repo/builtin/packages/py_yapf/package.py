12a7a56be1ac901dd57f1fa9df1f614ea4a2034c
Jon Rood <jon.rood@nrel.gov>
2025-09-29 14:13:39 -0600

    Fix duplicate line.

--- repos/spack_repo/builtin/packages/py_yapf/package.py
+++ repos/spack_repo/builtin/packages/py_yapf/package.py
@@ -32,6 +32,5 @@ class PyYapf(PythonPackage):
     depends_on("py-setuptools", type="build")
     depends_on("py-setuptools@58.5:", type="build", when="@0.40.2:")
     depends_on("py-platformdirs@3.5.1:", type=("build", "run"), when="@0.40:")
-    depends_on("py-platformdirs@3.5.1:", type=("build", "run"), when="@0.40:")
     depends_on("py-tomli@2.0.1:", type=("build", "run"), when="@0.33: ^python@:3.10")
     depends_on("py-importlib-metadata@6.6:", type=("build", "run"), when="@0.40:0.40.1")
