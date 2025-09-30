fb0cb4095cc042c9c7c934de4e4f412c765ef9d7
Ryan Krattiger <ryan.krattiger@kitware.com>
2025-09-30 16:03:37 -0500

    CI: write_dotenv.py (needed by #342)

---
+++ .ci/gitlab/scripts/common/write_dotenv.py
@@ -0,0 +1,47 @@
+import os
+import sys
+
+CI_PROJECT_DIR = "."
+CI_AUX_DIR = os.path.join(".ci", "tmp", "spack")
+
+
+def configure_packages_repo(dotenv):
+    dotenv["SPACK_PACKAGES_ROOT"] = CI_PROJECT_DIR
+    dotenv["SPACK_ROOT"] = CI_AUX_DIR
+
+
+def configure_core_repo(dotenv):
+    dotenv["SPACK_PACKAGES_ROOT"] = CI_AUX_DIR
+    dotenv["SPACK_ROOT"] = CI_PROJECT_DIR
+
+
+def read_dotenv(file):
+    dotenv = {}
+    for line in file:
+        if "=" in line:
+            key, value = line.rsplit("=", 1)
+            dotenv[key] = value.strip()
+    return dotenv
+
+
+def write_dotenv(env, file):
+    for var, value in env.items():
+        file.write(f"{var}={value}\n")
+
+
+if __name__ == "__main__":
+    dotenv = {}
+    base_dotenv = sys.argv[1] if len(sys.argv) > 1 else None
+    if os.path.exists(base_dotenv):
+        print(f"Found base dotenv: {base_dotenv}")
+        with open(base_dotenv, "r", encoding="utf-8") as fd:
+            dotenv = read_dotenv(fd)
+
+    repo_override = os.environ.get("CI_PROJECT_NAME")
+    if repo_override and not repo_override.endswith("packages"):
+        configure_core_repo(dotenv)
+    else:
+        configure_packages_repo(dotenv)
+
+    with open("env", "w", encoding="utf-8") as fd:
+        write_dotenv(dotenv, fd)
