65ef8bc8ee3c8ca0ad719d10d8ec90cc2c1f1927
Cédric Chevalier <cedric.chevalier@cea.fr>
2025-10-02 17:46:31 +0200

    Adding Thomas as a maintainer

--- repos/spack_repo/builtin/packages/kokkos/package.py
+++ repos/spack_repo/builtin/packages/kokkos/package.py
@@ -22,7 +22,7 @@ class Kokkos(CMakePackage, CudaPackage, ROCmPackage):
 
     test_requires_compiler = True
 
-    maintainers("cedricchevalier19", "nmm0", "lucbv")
+    maintainers("cedricchevalier19", "nmm0", "lucbv", "tpadioleau")
 
     license("Apache-2.0 WITH LLVM-exception")
 
