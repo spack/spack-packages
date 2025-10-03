72e8a33684cb3eb4e71f125b95631bae6563c9cf
Chris Marsh <chrismarsh.c2@gmail.com>
2025-10-03 23:05:00 +0000

    new py-sarif-tools

---
+++ repos/spack_repo/builtin/packages/py_sarif_tools/package.py
@@ -0,0 +1,23 @@
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack_repo.builtin.build_systems.python import PythonPackage
+
+from spack.package import *
+
+
+class PySarifTools(PythonPackage):
+    """ FIX ME """
+
+    homepage = "https://github.com/microsoft/sarif-tools"
+    pypi = "sarif_tools/sarif_tools-3.0.5.tar.gz"
+
+    version("3.0.5", sha256="52d09c101121231fb3489ad8c7af56896b8a95415cef726a6db5e7d74c6834d2")
+
+    depends_on("py-poetry", type="build")
+
+    depends_on("py-jinja2@3.1.6:", type=("build", "run"))
+    depends_on("py-jsonpath-ng@1.6.0:", type=("build", "run"))
+    depends_on("py-matplotlib@3.7:", type=("build", "run"))
+    depends_on("py-pyyaml@6.0.1:", type=("build", "run"))
