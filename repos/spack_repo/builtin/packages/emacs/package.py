ae011efc3b668eff5c0ee40d324f96853aa64983
Alec Scott <scott112@llnl.gov>
2025-10-03 04:53:55 -0700

    emacs: remove +strip requirement for gcc dep when +native (#1715)

--- repos/spack_repo/builtin/packages/emacs/package.py
+++ repos/spack_repo/builtin/packages/emacs/package.py
@@ -89,7 +89,7 @@ class Emacs(AutotoolsPackage, GNUMirrorPackage):
     # Optional dependencies
     depends_on("gnutls", when="+tls")
     depends_on("tree-sitter", when="+treesitter")
-    depends_on("gcc@11: +strip languages=jit", when="+native")
+    depends_on("gcc@11: languages=jit", when="+native")
     depends_on("jansson@2.7:", when="+json")
     depends_on("sqlite@3", when="+sqlite")
 
