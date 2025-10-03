7ab1a6ee1a05ca1620a8cb21870c209a5fef9544
Chris Marsh <chrismarsh.c2@gmail.com>
2025-10-03 23:08:02 +0000

    py-authlib

--- repos/spack_repo/builtin/packages/py_authlib/package.py
+++ repos/spack_repo/builtin/packages/py_authlib/package.py
@@ -8,11 +8,12 @@ from spack.package import *
 
 
 class PyAuthlib(PythonPackage):
-    """ FIX ME """
+    """ The ultimate Python library in building OAuth and OpenID Connect servers. JWS, JWK, JWA, JWT are included. """
 
     homepage = "https://github.com/authlib/authlib"
     pypi = "authlib/authlib-1.6.5.tar.gz"
 
+    license("BSD-3-Clause")
     version("1.6.5", sha256="6aaf9c79b7cc96c900f0b284061691c5d4e61221640a948fe690b556a6d6d10b")
 
     depends_on("py-setuptools", type="build")
