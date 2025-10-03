4a8d063ba8754bfe3fe8295247400014cfb1e460
Aiden McCormack <89662634+Aiden2244@users.noreply.github.com>
2025-10-03 15:46:05 -0400

    Add minor version to upper bound constraint
    Co-authored-by: John W. Parent <45471568+johnwparent@users.noreply.github.com>

--- repos/spack_repo/builtin/packages/qt/package.py
+++ repos/spack_repo/builtin/packages/qt/package.py
@@ -196,7 +196,7 @@ class Qt(Package):
     )
 
     # Do not define `wtf_ceil()` in MathExtras.h on macOS
-    patch("qt515-mathextras.patch", when="@5.15.4:5 platform=darwin")
+    patch("qt515-mathextras.patch", when="@5.15.4:5.15 platform=darwin")
 
     conflicts("%gcc@10:", when="@5.9:5.12.6 +opengl")
     conflicts("%gcc@11:", when="@5.8")
