fd56955bbf0c9b8c76f59bedc755c75fe669e668
nhanford <nhanford@users.noreply.github.com>
2025-10-06 22:10:25 +0000

    [@spackbot] updating style on behalf of nhanford

--- repos/spack_repo/builtin/packages/mvapich/package.py
+++ repos/spack_repo/builtin/packages/mvapich/package.py
@@ -6,9 +6,9 @@ import itertools
 import re
 import sys
 
+from spack_repo.builtin.build_systems.autotools import AutotoolsPackage
 from spack_repo.builtin.build_systems.cuda import CudaPackage
 from spack_repo.builtin.build_systems.rocm import ROCmPackage
-from spack_repo.builtin.build_systems.autotools import AutotoolsPackage
 from spack_repo.builtin.packages.mpich.package import MpichEnvironmentModifications
 
 from spack.package import *
