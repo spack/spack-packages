aad68de85466d43b733e71be604282f40a89be77
Jen Herting <jen@herting.cc>
2025-10-06 16:03:39 -0400

    [py-peft] added version 0.17.1

--- repos/spack_repo/builtin/packages/py_peft/package.py
+++ repos/spack_repo/builtin/packages/py_peft/package.py
@@ -19,10 +19,12 @@ class PyPeft(PythonPackage):
 
     license("Apache-2.0")
 
+    version("0.17.1", sha256="e6002b42517976c290b3b8bbb9829a33dd5d470676b2dec7cb4df8501b77eb9f")
     version("0.5.0", sha256="3cc03049ac6130426d82c22ca54cc6de849e7c6727181393e7751f7f512a9ced")
 
     depends_on("py-setuptools", type="build")
     depends_on("python@3.8.0:", type=("build", "run"))
+    depends_on("python@3.9.0:", type=("build", "run"), when="@0.17.1:")
     depends_on("py-numpy@1.17:", type=("build", "run"))
     depends_on("py-packaging@20.0:", type=("build", "run"))
     depends_on("py-psutil", type=("build", "run"))
@@ -31,4 +33,6 @@ class PyPeft(PythonPackage):
     depends_on("py-transformers", type=("build", "run"))
     depends_on("py-tqdm", type=("build", "run"))
     depends_on("py-accelerate", type=("build", "run"))
+    depends_on("py-accelerate@0.21:", type=("build", "run"), when="@0.17.1:")
     depends_on("py-safetensors", type=("build", "run"))
+    depends_on("py-huggingface-hub@0.25:", type=("build", "run"), when="@0.17.1:")
