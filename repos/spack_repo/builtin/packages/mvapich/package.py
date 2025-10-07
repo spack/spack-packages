5ba9312507c897dad7f4d8d741e06eacd5aea0d2
Nathan Hanford <nhanford@llnl.gov>
2025-10-07 09:40:41 -0700

    remove erroneous GPU support

--- repos/spack_repo/builtin/packages/mvapich/package.py
+++ repos/spack_repo/builtin/packages/mvapich/package.py
@@ -7,14 +7,12 @@ import re
 import sys
 
 from spack_repo.builtin.build_systems.autotools import AutotoolsPackage
-from spack_repo.builtin.build_systems.cuda import CudaPackage
-from spack_repo.builtin.build_systems.rocm import ROCmPackage
 from spack_repo.builtin.packages.mpich.package import MpichEnvironmentModifications
 
 from spack.package import *
 
 
-class Mvapich(MpichEnvironmentModifications, AutotoolsPackage, CudaPackage, ROCmPackage):
+class Mvapich(MpichEnvironmentModifications, AutotoolsPackage):
     """Mvapich is a High-Performance MPI Library for clusters with diverse
     networks (InfiniBand, Omni-Path, Ethernet/iWARP, and RoCE) and computing
     platforms (x86 (Intel and AMD), ARM and OpenPOWER)"""
@@ -97,7 +95,6 @@ class Mvapich(MpichEnvironmentModifications, AutotoolsPackage, CudaPackage, ROCm
     depends_on("zlib-api")
     depends_on("libpciaccess", when=(sys.platform != "darwin"))
     depends_on("libxml2")
-    depends_on("cuda", when="+cuda")
     depends_on("libfabric", when="netmod=ofi")
     depends_on("slurm", when="process_managers=slurm")
     depends_on("ucx", when="netmod=ucx")
@@ -224,16 +221,6 @@ class Mvapich(MpichEnvironmentModifications, AutotoolsPackage, CudaPackage, ROCm
             )
         else:
             args.append("--enable-fast=all")
-
-        if "+cuda" in self.spec:
-            args.extend(["--enable-cuda", "--with-cuda={0}".format(spec["cuda"].prefix)])
-        else:
-            args.append("--disable-cuda")
-
-        if "+rocm" in self.spec:
-            args.extend(["--enable-hip", "--with-hip={0}".format(spec["hip"].prefix)])
-        else:
-            args.append("--disable-hip")
 
         if "+regcache" in self.spec:
             args.append("--enable-registration-cache")
