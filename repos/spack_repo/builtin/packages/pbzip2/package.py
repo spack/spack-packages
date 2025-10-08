f10512a195a5e416dd70e709c670f0996c8a223f
Markus92 <Markus92@users.noreply.github.com>
2025-10-08 14:46:55 +0000

    [@spackbot] updating style on behalf of Markus92

--- repos/spack_repo/builtin/packages/pbzip2/package.py
+++ repos/spack_repo/builtin/packages/pbzip2/package.py
@@ -2,21 +2,22 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 from spack_repo.builtin.build_systems.makefile import MakefilePackage
+
 from spack.package import *
 
 
 class Pbzip2(MakefilePackage):
-    """ PBZIP2 is a parallel implementation of the bzip2 block-sorting file
+    """PBZIP2 is a parallel implementation of the bzip2 block-sorting file
     compressor that uses pthreads and achieves near-linear speedup on SMP
     machines. The output of this version is fully compatible with bzip2 v1.0.2
     or newer (ie: anything compressed with pbzip2 can be decompressed with
     bzip2). PBZIP2 should work on any system that has a pthreads compatible C++
     compiler (such as gcc)."""
 
-    homepage = "http://compression.great-site.net/pbzip2/" 
+    homepage = "http://compression.great-site.net/pbzip2/"
     url = "https://launchpad.net/pbzip2/1.1/1.1.13/+download/pbzip2-1.1.13.tar.gz"
 
-    maintainers("Markus92") 
+    maintainers("Markus92")
 
     license("bzip2-1.0.6", checked_by="Markus92")
 
