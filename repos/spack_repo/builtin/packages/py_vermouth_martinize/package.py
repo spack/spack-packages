fb31cf9c36035fda64ef8fc310c701613bc92c5e
Adam <awitmer@lanl.gov>
2025-10-09 10:55:40 -0600

    add vermouth-martinize

---
+++ repos/spack_repo/builtin/packages/py_vermouth_martinize/package.py
@@ -0,0 +1,28 @@
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack_repo.builtin.build_systems.python import PythonPackage
+
+from spack.package import *
+
+
+class PyVermouthMartinize(PythonPackage):
+    """
+    Vermouth (and Martinize2) - build coarse-grained
+    Martini topologies from atomistic structures.
+    """
+
+    homepage = "https://github.com/marrink-lab/vermouth-martinize"
+    pypi = "vermouth/vermouth-0.14.0.tar.gz"
+
+    maintainers("marrink-lab")
+
+    version("0.14.0", sha256="9f1c6221ea8b6b6da2a4eff32fe7374b76adced3f2fd85da5cbb1cfcfbf10696")
+
+    # Runtime dependencies
+    depends_on("py-numpy", type=("build", "run"))
+    depends_on("py-scipy", type=("build", "run"))
+    depends_on("py-networkx", type=("build", "run"))
+    depends_on("py-setuptools", type="build")
+    depends_on("py-pbr", type=("build", "run"))
