59575877ffc2f4c6f6e8f7868dc52f57db08abbc
Adam <awitmer@lanl.gov>
2025-10-09 11:18:31 -0600

    add py_simple_slurm

---
+++ repos/spack_repo/builtin/packages/py_simple_slurm/package.py
@@ -0,0 +1,22 @@
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack_repo.builtin.build_systems.python import PythonPackage
+
+from spack.package import *
+
+
+class PySimpleSlurm(PythonPackage):
+    """A simple Python wrapper for Slurm with flexibility in mind."""
+
+    homepage = "https://github.com/amq92/simple_slurm"
+    pypi = "simple-slurm/simple_slurm-0.3.6.tar.gz"
+
+    maintainers = "mamachra"
+
+    version("0.3.6", sha256="0f88cac96b39d7d7e34c5a124919cebb41eb8a8ef2ff4fb65c363d940ff8551e")
+
+    # No explicit dependencies declared on PyPI; adjust if necessary
+    depends_on("python@3.11:", type=("build", "run"))
+    depends_on("py-setuptools", type="build")
