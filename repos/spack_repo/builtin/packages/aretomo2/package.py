0180d330f96f917e224b7a9c52485bae07834818
Mark Janse <markus.janse@utsouthwestern.edu>
2025-10-15 15:59:33 -0500

    aretomo2: shorten description and clarify CUDA requirement

--- repos/spack_repo/builtin/packages/aretomo2/package.py
+++ repos/spack_repo/builtin/packages/aretomo2/package.py
@@ -11,16 +11,7 @@ class Aretomo2(MakefilePackage, CudaPackage):
     """AreTomo2 is a multi-GPU accelerated software package that fully automates
     motion-corrected marker-free tomographic alignment and reconstruction, now
     includes robust GPU-accelerated CTF (Contrast Transfer Function) estimation
-    in a single package. AreTomo2 is part of our endeavor to build a
-    fully-automated high-throughput processing pipeline that enables real-time
-    reconstruction of tomograms in parallel with tomographic data collection.
-    It strives to be fast and accurate, as well as provides for easy
-    integration into subtomogram processing workflows by generating IMod
-    compatible files containing alignment and CTF parameters needed to
-    bootstrap subtomogram averaging. AreTomo2 can also be used for on-the-fly
-    reconstruction of tomograms and CTF estimation in parallel with tilt series
-    collection, enabling real-time assessment of sample quality and adjustment
-    of collection parameters."""
+    in a single package."""
 
     homepage = "https://github.com/czimaginginstitute/AreTomo2"
     url = "https://github.com/czimaginginstitute/AreTomo2/archive/refs/tags/v1.1.2.tar.gz"
@@ -35,7 +26,7 @@ class Aretomo2(MakefilePackage, CudaPackage):
     depends_on("cxx", type="build")
     depends_on("gmake", type="build")
 
-    depends_on("cuda")
+    variant("cuda", default=True, description="Build with CUDA (required)")
     conflicts("~cuda")
 
     build_targets = ["exe"]
