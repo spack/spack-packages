d94ae3f4b941d3d11ebd254f0cec57cbe097534b
Mark Janse <markusjanse@gmail.com>
2025-10-15 16:08:17 -0500

    py-superqt: add pyconify variant

--- repos/spack_repo/builtin/packages/py_superqt/package.py
+++ repos/spack_repo/builtin/packages/py_superqt/package.py
@@ -18,6 +18,8 @@ class PySuperqt(PythonPackage):
     version("0.7.6", sha256="822fdba71dc391929c9d3db839f78ca2a861e2f2876926f969a288dfb2a9787e")
     version("0.6.1", sha256="f1a9e0499c4bbcef34b6f895eb57cd41301b3799242cd030029238124184dade")
 
+    variant("iconify", default=False, description="Use Iconify icons keys")
+
     depends_on("python@3.8:", type=("build", "run"))
     depends_on("py-hatchling", type="build")
     depends_on("py-hatch-vcs", type="build")
@@ -29,5 +31,9 @@ class PySuperqt(PythonPackage):
     depends_on("py-typing-extensions@4.12:", when="@0.7.6: ^python@3.13:", type=("build", "run"))
     depends_on("py-typing-extensions@4.5:", when="@0.7.6:", type=("build", "run"))
     depends_on("py-typing-extensions@3.7.4.3:", when="@0.6.1", type=("build", "run"))
+
+    with default_args(type=("build", "run")):
+        depends_on("py-pyconify", when="+iconify")
+
 
     conflicts("^py-typing-extensions@3.10.0.0")
