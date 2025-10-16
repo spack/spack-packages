1d18bbbfc601b018c4d25251a455f9da53e04118
Adam J. Stewart <ajstewart426@gmail.com>
2025-10-16 18:33:49 +0100

    py-pillow: add v12.0.0
    Signed-off-by: Adam J. Stewart <ajstewart426@gmail.com>

--- repos/spack_repo/builtin/packages/py_pillow/package.py
+++ repos/spack_repo/builtin/packages/py_pillow/package.py
@@ -46,6 +46,7 @@ class PyPillowBase(PythonPackage):
     # Required dependencies
     # https://pillow.readthedocs.io/en/stable/installation/python-support.html
     with default_args(type=("build", "link", "run")):
+        depends_on("python@3.10:3.14", when="@12:")
         depends_on("python@3.9:3.14", when="@11.3:")
         depends_on("python@3.9:3.13", when="@11.0:11.2")
         depends_on("python@3.8:3.13", when="@10.4")
@@ -59,6 +60,7 @@ class PyPillowBase(PythonPackage):
     # pyproject.toml
     with default_args(type="build"):
         depends_on("py-pip@22.1:", when="@10:")
+        depends_on("py-pybind11", when="@12:")
         depends_on("py-setuptools@77:", when="@11.2:")
         depends_on("py-setuptools@67.8:", when="@10:")
         depends_on("py-setuptools")
@@ -68,15 +70,16 @@ class PyPillowBase(PythonPackage):
     depends_on("zlib-api", when="+zlib")
     depends_on("jpeg", when="+jpeg")
     depends_on("libtiff", when="+tiff")
+    depends_on("freetype@2.9.1:", when="@12:+freetype")
     depends_on("freetype", when="+freetype")
     depends_on("libraqm", when="+raqm")
     depends_on("lcms@2:", when="+lcms")
     depends_on("libwebp", when="+webp")
     depends_on("libwebp+libwebpmux+libwebpdemux", when="+webpmux")
-    depends_on("openjpeg", when="+jpeg2000")
+    depends_on("openjpeg@2:", when="+jpeg2000")
     depends_on("libimagequant", when="+imagequant")
     depends_on("libxcb", when="+xcb")
-    depends_on("libavif", when="+avif")
+    depends_on("libavif@1:", when="+avif")
 
     patch(
         "https://github.com/python-pillow/Pillow/commit/1c11d4581c5705dfa21bc5a4f3b6980c556978bf.patch?full_index=1",
@@ -152,6 +155,7 @@ class PyPillow(PyPillowBase):
     homepage = "https://python-pillow.org/"
     pypi = "pillow/pillow-10.2.0.tar.gz"
 
+    version("12.0.0", sha256="87d4f8125c9988bfbed67af47dd7a953e2fc7b0cc1e7800ec6d2080d490bb353")
     version("11.3.0", sha256="3828ee7586cd0b2091b6209e5ad53e20d0649bbe87164a459d0676e035e8f523")
     version("11.2.1", sha256="a64dd61998416367b7ef979b73d3a85853ba9bec4c2925f74e588879a58716b6")
     version("11.1.0", sha256="368da70808b36d73b4b390a8ffac11069f8a5c85f29eff1f1b01bcf3ef5b2a20")
@@ -176,6 +180,7 @@ class PyPillow(PyPillowBase):
     depends_on("c", type="build")
 
     for ver in [
+        "12.0.0",
         "11.3.0",
         "11.2.1",
         "11.1.0",
