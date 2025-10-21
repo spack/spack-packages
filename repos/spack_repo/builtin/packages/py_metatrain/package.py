e1ff950284b2447c1b8e2c4514751744cf07dc13
Rocco Meli <r.meli@bluemail.ch>
2025-10-21 11:52:48 +0200

    latest metatrain

--- repos/spack_repo/builtin/packages/py_metatrain/package.py
+++ repos/spack_repo/builtin/packages/py_metatrain/package.py
@@ -17,19 +17,28 @@ class PyMetatrain(PythonPackage):
 
     license("BSD-3-Clause", checked_by="RMeli")
 
+    version("2025.11", sha256="4c1a78feb359885f29d659f97ccf0e0676892ba1aa120cf58d0c7a0442161255")
     version("2025.10", sha256="70ca3d422dbaf8eca14f05f81299497faf52134bcf478b29484e891ba0881549")
 
     variant("soap_bpnn", default=False, description="Enable SOAP-BPNN architecture")
 
+    # pyproject.toml [project]
+    depends_on("python@3.9:", type=("build", "run"), when="@2025.10:")
+    depends_on("python@3.10:", type=("build", "run"), when="@2025.11:")
+
+    # pyproject.toml [build-system]
     depends_on("py-setuptools@77:", type="build")
     depends_on("py-setuptools-scm@8:", type="build")
 
+    # pyproject.toml [project] dependencies
     depends_on("py-ase", type=("build", "run"))
     depends_on("py-huggingface-hub", type=("build", "run"))
     depends_on("py-metatensor-learn@0.3.2:0.3", type=("build", "run"))
     depends_on("py-metatensor-operations@0.3.4:0.3", type=("build", "run"))
-    depends_on("py-metatensor-torch@0.7.6:0.7", type=("build", "run"))
-    depends_on("py-metatomic-torch@0.1.2:0.1", type=("build", "run"))
+    depends_on("py-metatensor-torch@0.7.6:0.7", type=("build", "run"), when="@2025.10:")
+    depends_on("py-metatensor-torch@0.8.1:0.8", type=("build", "run"), when="@2025.11:")
+    depends_on("py-metatomic-torch@0.1.2:0.1", type=("build", "run"), when="@2025.10:")
+    depends_on("py-metatomic-torch@0.1.5:0.1", type=("build", "run"), when="@2025.11:")
     depends_on("py-jsonschema", type=("build", "run"))
     depends_on("py-omegaconf@2.3.0:", type=("build", "run"))
     depends_on("py-python-hostlist", type=("build", "run"))
@@ -37,5 +46,6 @@ class PyMetatrain(PythonPackage):
     depends_on("py-vesin", type=("build", "run"))
 
     # soap-bpnn
+    # pyproject.toml [project.optional-dependencies] soap-bpnn
     depends_on("py-wigners", type=("build", "run"), when="+soap_bpnn")
     depends_on("py-torch-spex", type=("build", "run"), when="+soap_bpnn")
