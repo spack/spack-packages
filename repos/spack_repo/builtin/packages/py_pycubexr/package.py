7c7c811c2ba355e26a608dabdeb9090b00be6768
Peter Arzt <peter.arzt@tu-darmstadt.de>
2025-10-22 13:57:22 +0200

    py_pycubexr: add v2.0.1, relax numpy version, add pearzt as maintainer

--- repos/spack_repo/builtin/packages/py_pycubexr/package.py
+++ repos/spack_repo/builtin/packages/py_pycubexr/package.py
@@ -15,7 +15,10 @@ class PyPycubexr(PythonPackage):
 
     license("BSD-3-Clause")
 
+    maintainers("pearzt")
+
+    version("2.0.1", sha256="699643c076b603fb1140564da6c4589b73ec9efd3b9112cd3d957f5bee646915")
     version("2.0.0", sha256="03504fbbc9cbd514943e8aeb57919ad49731fe264bdbab86711bf10851276924")
 
     depends_on("py-setuptools", type="build")
-    depends_on("py-numpy@1.18:1", type=("build", "run"))
+    depends_on("py-numpy@1.18:2", type=("build", "run"))
