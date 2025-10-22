f4d1be3958d7334ba3c2a2bb9d4fbe4538523e0c
Mark Janse <markusjanse@gmail.com>
2025-10-22 10:33:11 -0500

    py-vispy: added version 0.12.2 (for RELION comptability)

--- repos/spack_repo/builtin/packages/py_vispy/package.py
+++ repos/spack_repo/builtin/packages/py_vispy/package.py
@@ -19,15 +19,18 @@ class PyVispy(PythonPackage):
     license("BSD-3-Clause", checked_by="Markus92")
 
     version("0.15.2", sha256="d52d10c0697f48990555cea2a2bad3f9f5a772391856fda364ea4bbc69fd075c")
+    version("0.12.2", sha256="141c2ddccc1158555bc89f09010c4b1d754487e816357333f31e795a7146a024")
 
+    depends_on("py-setuptools@69.4.0:", when="@0.15:", type="build")
+    depends_on("py-setuptools@30.3.0:", type="build")
+    depends_on("py-setuptools-scm@8.1: +toml", when="@0.15:", type="build")
+    depends_on("py-setuptools-scm", type="build")
 
-    depends_on("py-setuptools@69.4.0:", type="build")
-    depends_on("py-setuptools-scm@8.1: +toml", type="build")
-
-    depends_on("py-numpy@2:", type=("build", "run"))
-    depends_on("py-cython@3:", type=("build", "run"))
+    depends_on("py-numpy@2:", when="@0.14.3:", type=("build", "run"))
+    depends_on("py-numpy@1", when="@:0.14.2", type=("build", "run"))  # https://github.com/numpy/numpy/issues/26191
+    depends_on("py-cython@3:", when="@0.15:", type=("build", "run"))
+    depends_on("py-cython@0.29.2:", type=("build", "run"))
     depends_on("py-freetype-py", type=("build", "run"))
     depends_on("py-hsluv", type=("build", "run"))
     depends_on("py-kiwisolver", type=("build", "run"))
     depends_on("py-packaging", type=("build", "run"))
-
