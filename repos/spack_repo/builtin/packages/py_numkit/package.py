e0d22511f43b68d7c66853e19706e28a8cac717a
Adam <awitmer@lanl.gov>
2025-10-24 10:19:46 -0600

    address reviewer comments
    * modify dependency versions
    * add python dependency
    * remove unnecessary ``@main`` dependency

--- repos/spack_repo/builtin/packages/py_numkit/package.py
+++ repos/spack_repo/builtin/packages/py_numkit/package.py
@@ -19,10 +19,11 @@ class PyNumkit(PythonPackage):
     version("1.3.1", sha256="18fba519ab4714d9426a818c04e4656bc60368169db0f65f5eece2ddae0e3211")
 
     # Build-system requirements
-    depends_on("py-setuptools", type="build")
+    depends_on("python@3.9:", type=("build", "run"))
+    depends_on("py-setuptools@42:", type="build")
     depends_on("py-wheel", type="build")
-    depends_on("py-setuptools-scm", type="build", when="@main")
+    depends_on("py-setuptools-scm", type="build")
 
     # Runtime / build deps
-    depends_on("py-numpy@1.20:", type=("build", "run"))
+    depends_on("py-numpy@1.9:", type=("build", "run"))
     depends_on("py-scipy", type=("build", "run"))
