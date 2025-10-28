0f8d72a059825343947ae8ed452c80675c17ccdc
Freifrau von Bleifrei <freifrauvonbleifrei@users.noreply.github.com>
2025-10-28 09:58:57 +0100

    salmon_tddft: actual SHA256 checksums

--- repos/spack_repo/builtin/packages/salmon_tddft/package.py
+++ repos/spack_repo/builtin/packages/salmon_tddft/package.py
@@ -25,32 +25,32 @@ class SalmonTddft(CMakePackage):
     version(
         "2.2.2",
         url="http://salmon-tddft.jp/download/SALMON-v.2.2.2.tar.gz",
-        sha256="581cbb6b45853e1378820675ac97f435",
+        sha256="4ca2afe2f03a455b86bede014004765b3c2f494459dea8d66925f3b3743adde3",
     )
     version(
         "2.2.1",
         url="http://salmon-tddft.jp/download/SALMON-v.2.2.1.tar.gz",
-        sha256="a8154f19e83424bee51e295bb9241366",
+        sha256="f665fc2541b2e664a82b39286ecc9a9ddaa9eb77640f31bcecf131bbb0efe2ba",
     )
     version(
         "2.2.0",
         url="http://salmon-tddft.jp/download/SALMON-v.2.2.0.tar.gz",
-        sha256="d71436df3a1ad507f665abb8453eee15",
+        sha256="b0b6eabad48f4547ae2a97f53771047ccb759b8c126ab0f1653b0e0f0d02a28b",
     )
     version(
         "2.1.0",
         url="http://salmon-tddft.jp/download/SALMON-v.2.1.0.tar.gz",
-        sha256="95e1fabbbf161359ce6ec563cace99f4",
+        sha256="18267818cdfa82ea762441e2d751abeff7b553c8ce92cabf5fb010248d2cfcbe",
     )
     version(
         "2.0.2",
         url="http://salmon-tddft.jp/download/SALMON-v.2.0.2.tar.gz",
-        sha256="5b72057b14ed898e5fb0ea9b468f0da6",
+        sha256="742007d3684a478199ba959ce135ad0020b70676a49f52a5e1dc25438123d50e",
     )
     version(
         "2.0.1",
         url="http://salmon-tddft.jp/download/SALMON-v.2.0.1.tar.gz",
-        sha256="277967063c57faf5ade2930025896f42",
+        sha256="6fcd72ddd484a1d2b4700bbe5c1717fabb1e449288da3253d74b4a8ab24e7255",
     )
     version("2.0.0", sha256="c3bb80bc5d338cba21cd8f345acbf2f2d81ef75af069a0a0ddbdc0acf358456c")
     version("1.2.1", sha256="a5045149e49abe9dd9edefe00cd1508a1323081bc3d034632176b728effdbaeb")
