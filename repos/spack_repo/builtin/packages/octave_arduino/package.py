cf544b6c204d9d4ade8a79fa1e6534abf57391e5
Olivier Cessenat <cessenat@gmail.com>
2025-11-02 18:16:40 +0100

    compiler dependency: octave_arduino new version 0.12.2 + new url

--- repos/spack_repo/builtin/packages/octave_arduino/package.py
+++ repos/spack_repo/builtin/packages/octave_arduino/package.py
@@ -13,13 +13,17 @@ class OctaveArduino(OctavePackage, SourceforgePackage):
     Arduino extension for Matlab."""
 
     homepage = "https://octave.sourceforge.io/arduino/"
-    sourceforge_mirror_path = "octave/arduino-0.2.0.tar.gz"
+    # sourceforge_mirror_path = "octave/arduino-0.2.0.tar.gz"
+    git = "https://github.com/gnu-octave/octave-arduino/"
+    url = "https://github.com/gnu-octave/octave-arduino/releases/download/release-0.12.2/arduino-0.12.2.tar.gz"
 
     license("GPL-3.0-or-later")
 
+    version("0.12.2", sha256="37c00bbc9a2615852afbc01250f865b1125f5c5532d88e772313d293feab52b8")
     version("0.2.0", sha256="0562ff48ea4b2cef28e2e03ccc4678dafa16f91d1580245bb7f9f488c4f56238")
 
     depends_on("cxx", type="build")  # generated
 
+    depends_on("octave-instrctl@0.5.0:", when="@0.12.2:")
     depends_on("octave-instrctl")
     extends("octave@3.6.0:")
