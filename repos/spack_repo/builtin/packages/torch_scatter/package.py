a9259d8869e3d6d3085aa811fbaeda744ba09da6
Thomas Madlener <thomas.madlener@desy.de>
2025-11-04 14:24:52 +0100

    torch-scatter: add cuda variant building off master and basic conflicts

--- repos/spack_repo/builtin/packages/torch_scatter/package.py
+++ repos/spack_repo/builtin/packages/torch_scatter/package.py
@@ -14,6 +14,7 @@ class TorchScatter(CMakePackage):
 
     license("MIT")
 
+    version("master", branch="master")
     version("2.1.2", sha256="6f375dbc9cfe03f330aa29ea553e9c7432e9b040d039b041f08bf05df1a8bf37")
     version("2.1.1", sha256="ea4f4ac4210a983d878df29e82190f60a7e6e9a791adbe0c0d635040e6256c4d")
     version("2.1.0", sha256="c587ddf7ffaf8e4ddc31d8016d7f445d5896ce491209574800166a9f85c29377")
@@ -25,13 +26,19 @@ class TorchScatter(CMakePackage):
     version("2.0.4", sha256="4fdadd6587f16ef3ff63c52f313f0c9dd97d13ae6496867fe566c309a4ea4036")
     version("2.0.3", sha256="ff2ca1468cb4e49b65bea8f889971f196f209231fbee0cc8bd1615ecb367400b")
 
+    variant("cuda", default=False, description="Build with CUDA support")
+
     depends_on("cxx", type="build")
     depends_on("c", type="build")
+
     depends_on("py-torch")
+    depends_on("py-torch +cuda", when="+cuda")
+
+    conflicts("py-torch@2.1:", when="@:2.1.2")
 
     def cmake_args(self):
         args = [
-            self.define("WITH_CUDA", "+cuda" in self.spec["py-torch"]),
+            self.define_from_variant("WITH_CUDA", "cuda"),
             self.define("CMAKE_CXX_STANDARD", "20"),
             self.define("WITH_PYTHON", True),
         ]
