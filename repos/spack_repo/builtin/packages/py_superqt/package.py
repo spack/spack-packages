b263008e70423a270f6441bf0ceb97db1a508305
Mark Janse <markusjanse@gmail.com>
2025-11-04 11:09:07 -0600

    py-superqt: use default_args for dependencies

--- repos/spack_repo/builtin/packages/py_superqt/package.py
+++ repos/spack_repo/builtin/packages/py_superqt/package.py
@@ -24,14 +24,13 @@ class PySuperqt(PythonPackage):
     depends_on("py-hatchling", type="build")
     depends_on("py-hatch-vcs", type="build")
 
-    depends_on("py-packaging", type=("build", "run"))
-    depends_on("py-pygments@2.4:", type=("build", "run"))
-    depends_on("py-qtpy@2.4:", when="@0.7.6:", type=("build", "run"))
-    depends_on("py-qtpy@1.1:", type=("build", "run"))
-    depends_on("py-typing-extensions@4.5:", when="@0.7.6:", type=("build", "run"))
-    depends_on("py-typing-extensions@3.7.4.3:", when="@0.6.1", type=("build", "run"))
-
     with default_args(type=("build", "run")):
+        depends_on("py-packaging")
+        depends_on("py-pygments@2.4:")
+        depends_on("py-qtpy@2.4:", when="@0.7.6:")
+        depends_on("py-qtpy@1.1:")
+        depends_on("py-typing-extensions@4.5:", when="@0.7.6:")
+        depends_on("py-typing-extensions@3.7.4.3:", when="@0.6.1")
         depends_on("py-pyconify", when="+iconify")
 
     conflicts("^py-typing-extensions@3.10.0.0")
