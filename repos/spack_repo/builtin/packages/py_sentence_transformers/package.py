3c1123b0600d1e0bb054a83e1d9d2680ae505932
Jen Herting <jen@herting.cc>
2025-11-05 11:18:19 -0500

    [py-sentence-transformers]
    - clean up dependency listing a bit
    - added version 5.1.2

--- repos/spack_repo/builtin/packages/py_sentence_transformers/package.py
+++ repos/spack_repo/builtin/packages/py_sentence_transformers/package.py
@@ -15,12 +15,13 @@ class PySentenceTransformers(PythonPackage):
     text is close and can efficiently be found using cosine similarity."""
 
     homepage = "https://github.com/UKPLab/sentence-transformers"
-    pypi = "sentence-transformers/sentence-transformers-2.2.2.tar.gz"
+    pypi = "sentence-transformers/sentence_transformers-5.1.2.tar.gz"
 
     maintainers("meyersbs")
 
     license("Apache-2.0")
 
+    version("5.1.2", sha256="0f6c8bd916a78dc65b366feb8d22fd885efdb37432e7630020d113233af2b856")
     version("3.4.1", sha256="68daa57504ff548340e54ff117bd86c1d2f784b21e0fb2689cf3272b8937b24b")
     version("2.2.2", sha256="dbc60163b27de21076c9a30d24b5b7b6fa05141d68cf2553fa9a77bf79a29136")
 
@@ -32,21 +33,25 @@ class PySentenceTransformers(PythonPackage):
             name = "sentence_transformers"
         return url.format(name, version.dotted)
 
-    depends_on("py-setuptools@42:", when="@3.4.1", type="build")
+    depends_on("python@3.6:", type=("build", "run"))
+    depends_on("python@3.9:", when="@3.4.1:", type=("build", "run"))
     depends_on("py-setuptools", type="build")
-    depends_on("python@3.9.0:", when="@3.4.1", type=("build", "run"))
-    depends_on("python@3.6.0:", type=("build", "run"))
-    depends_on("py-transformers@4.41.0:4", when="@3.4.1", type=("build", "run"))
-    depends_on("py-transformers@4.6.0:4", when="@2.2.2", type=("build", "run"))
+    depends_on("py-setuptools@42:", when="@3.4.1:", type="build")
+
+    depends_on("py-transformers@4.6:4", when="@2.2.2", type=("build", "run"))
+    depends_on("py-transformers@4.41:4", when="@3.4.1:", type=("build", "run"))
     depends_on("py-tqdm", type=("build", "run"))
-    depends_on("py-torch@1.11.0:", when="@3.4.1", type=("build", "run"))
-    depends_on("py-torch@1.6.0:", when="@2.2.2", type=("build", "run"))
-    depends_on("py-torchvision", type=("build", "run"))
-    depends_on("py-numpy", when="@2.2.2", type=("build", "run"))
+    depends_on("py-torch@1.6:", when="@2.2.2", type=("build", "run"))
+    depends_on("py-torch@1.11:", when="@3.4.1:", type=("build", "run"))
     depends_on("py-scikit-learn", type=("build", "run"))
     depends_on("py-scipy", type=("build", "run"))
-    depends_on("py-nltk", when="@2.2.2", type=("build", "run"))
+    depends_on("py-huggingface-hub@0.4:", when="@2.2.2", type=("build", "run"))
+    depends_on("py-huggingface-hub@0.20:", when="@3.4.1:", type=("build", "run"))
+    depends_on("pil", when="@3.4.1:", type=("build", "run"))
+    depends_on("py-typing-extensions@4.5:", when="@5.1.2:", type=("build", "run"))
+
+    # Historical
     depends_on("py-sentencepiece", when="@2.2.2", type=("build", "run"))
-    depends_on("py-huggingface-hub@0.20.0:", when="@3.4.1", type=("build", "run"))
-    depends_on("py-huggingface-hub@0.4.0:", when="@2.2.2", type=("build", "run"))
-    depends_on("py-pillow", when="@3.4.1", type=("build", "run"))
+    depends_on("py-torchvision", when="@2.2.2", type=("build", "run"))
+    depends_on("py-numpy", when="@2.2.2", type=("build", "run"))
+    depends_on("py-nltk", when="@2.2.2", type=("build", "run"))
