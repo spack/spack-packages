5a665dbaf42894c116891f1fc12a24c3cc9dc2a9
Ryan Krattiger <ryan.krattiger@kitware.com>
2025-11-07 15:46:29 -0600

    More Rust: Add versions 1.87..90.0
    Signed-off-by: Ryan Krattiger <ryan.krattiger@kitware.com>

--- repos/spack_repo/builtin/packages/rust/package.py
+++ repos/spack_repo/builtin/packages/rust/package.py
@@ -38,6 +38,10 @@ class Rust(Package):
     version("nightly")
 
     # Stable versions.
+    version("1.90.0", sha256="799a9f9cba4ed5351e071048bcf6b5560755d9009648def33a407dd4961f9b7e")
+    version("1.89.0", sha256="2576f9f440dd99b0151bd28f59aa0ac6102d5c4f3ed4ef8a810c8dd05057250d")
+    version("1.88.0", sha256="3a97544434848ae3d193d1d6bc83d6f24cb85c261ad95f955fde47ec64cfcfbe")
+    version("1.87.0", sha256="149bb9fd29be592da4e87900fc68f0629a37bf6850b46339dd44434c04fd8e76")
     version("1.86.0", sha256="022a27286df67900a044d227d9db69d4732ec3d833e4ffc259c4425ed71eed80")
     version("1.85.0", sha256="2f4f3142ffb7c8402139cfa0796e24baaac8b9fd3f96b2deec3b94b4045c6a8a")
     version("1.83.0", sha256="722d773bd4eab2d828d7dd35b59f0b017ddf9a97ee2b46c1b7f7fac5c8841c6e")
