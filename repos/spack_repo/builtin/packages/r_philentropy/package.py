0317e013c884e4e18ba00a4b3e46b51bc6f29a34
Harmen Stoppels <me@harmenstoppels.nl>
2025-11-07 17:42:07 +0100

    r ecosystem: add missing language deps
    This was missed back in July 2024 likely because the R packages failed
    to download. That's because in the script I used a direct fetch without
    Spack's mirror, which failed, because R's default URL property is
    incorrect.
    
    https://github.com/spack/spack/pull/45217
    
    Signed-off-by: Harmen Stoppels <me@harmenstoppels.nl>

--- repos/spack_repo/builtin/packages/r_philentropy/package.py
+++ repos/spack_repo/builtin/packages/r_philentropy/package.py
@@ -29,6 +29,8 @@ class RPhilentropy(RPackage):
     version("0.5.0", sha256="b39e9a825458f3377e23b2a133180566780e89019e9d22a6a5b7ca87c49c412f")
     version("0.4.0", sha256="bfd30bf5635aab6a82716299a87d44cf96c7ab7f4ee069843869bcc85c357127")
 
+    depends_on("cxx", type="build")  # generated
+
     depends_on("r@3.1.2:", type=("build", "run"))
     depends_on("r-rcpp", type=("build", "run"))
     depends_on("r-kernsmooth", type=("build", "run"))
