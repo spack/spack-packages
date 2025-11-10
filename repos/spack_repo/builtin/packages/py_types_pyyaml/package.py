c6f35c472a81a7cb209ec945239476c9ea0b55f6
Chris Marsh <chrismarsh.c2@gmail.com>
2025-11-10 19:22:50 +0000

    style

--- repos/spack_repo/builtin/packages/py_types_pyyaml/package.py
+++ repos/spack_repo/builtin/packages/py_types_pyyaml/package.py
@@ -8,7 +8,8 @@ from spack.package import *
 
 
 class PyTypesPyyaml(PythonPackage):
-    """ This is a type stub package for the PyYAML package. It can be used by type checkers to check code that uses PyYAML.  """
+    """This is a type stub package for the PyYAML package.
+    It can be used by type checkers to check code that uses PyYAML."""
 
     homepage = "https://github.com/Ericsson/codechecker"
     pypi = "types_PyYAML/types_pyyaml-6.0.12.20250915.tar.gz"
