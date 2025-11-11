2c334ed847c0bbf0209ebf986957505bddc8c218
Rocco Meli <r.meli@bluemail.ch>
2025-11-11 09:41:38 +0100

    Use only last metatrain version
    Co-authored-by: Guillaume Fraux <luthaf@luthaf.fr>

--- repos/spack_repo/builtin/packages/py_metatrain/package.py
+++ repos/spack_repo/builtin/packages/py_metatrain/package.py
@@ -11,14 +11,13 @@ class PyMetatrain(PythonPackage):
     """Train, fine-tune, and manipulate machine learning models for atomistic systems"""
 
     homepage = "https://docs.metatensor.org/metatrain/latest/index.html#"
-    pypi = "metatrain/metatrain-2025.10.tar.gz"
+    pypi = "metatrain/metatrain-2025.11.tar.gz"
 
     maintainers("RMeli", "luthaf", "HaoZeke")
 
     license("BSD-3-Clause", checked_by="RMeli")
 
     version("2025.11", sha256="4c1a78feb359885f29d659f97ccf0e0676892ba1aa120cf58d0c7a0442161255")
-    version("2025.10", sha256="70ca3d422dbaf8eca14f05f81299497faf52134bcf478b29484e891ba0881549")
 
     variant("soap_bpnn", default=False, description="Enable SOAP-BPNN architecture")
 
@@ -31,9 +30,7 @@ class PyMetatrain(PythonPackage):
     depends_on("py-huggingface-hub", type=("build", "run"))
     depends_on("py-metatensor-learn@0.3.2:0.3", type=("build", "run"))
     depends_on("py-metatensor-operations@0.3.4:0.3", type=("build", "run"))
-    depends_on("py-metatensor-torch@0.7.6:0.7", type=("build", "run"), when="@2025.10:")
     depends_on("py-metatensor-torch@0.8.1:0.8", type=("build", "run"), when="@2025.11:")
-    depends_on("py-metatomic-torch@0.1.2:0.1", type=("build", "run"), when="@2025.10:")
     depends_on("py-metatomic-torch@0.1.5:0.1", type=("build", "run"), when="@2025.11:")
     depends_on("py-jsonschema", type=("build", "run"))
     depends_on("py-omegaconf@2.3.0:", type=("build", "run"))
