10f1d9fd3f131488632071a69071a897e70326dc
Thomas Padioleau <thomas.padioleau@cea.fr>
2025-11-12 15:01:05 +0100

    Set C++17 in conflict with v5

--- repos/spack_repo/builtin/packages/kokkos/package.py
+++ repos/spack_repo/builtin/packages/kokkos/package.py
@@ -279,7 +279,8 @@ class Kokkos(CMakePackage, CudaPackage, ROCmPackage):
     variant("pic", default=False, description="Build position independent code")
 
     conflicts("cxxstd=11")
-    conflicts("cxxstd=14", when="@4.0:")
+    conflicts("cxxstd=14", when="@4:")
+    conflicts("cxxstd=17", when="@5:")
 
     conflicts("+cuda", when="cxxstd=17 ^cuda@:10")
     conflicts("+cuda", when="cxxstd=20 ^cuda@:11")
