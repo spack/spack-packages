c0240169a9a623752cee5147b60fa89364858a9c
LydDeb <lyderic.de@gmail.com>
2025-11-14 13:38:06 +0100

    py_chai_lab: New package
        Add dependencies:
        * py_antipickle
        * py_gemmi
        * py_ihm
        * py_modelcif
        * py_pandera
        * py_tmtools
    
        Update packages:
        * py_beartype: add version '0.21.0'
        * py_biopython: add versions '1.83', '1.84', '1.85'
        * py_typer: add version '0.12.5'
        * rdkit: add versions '2024_09_5', '2024_09_6'
    
        Fix the dependence to boost in rdkit:
        * 'package boost_numpy is considered to be NOT FOUND' => add variant +numpy
        * 'undefined reference to boost::program_options' => add variant +program_option

---
+++ repos/spack_repo/builtin/packages/py_ihm/package.py
@@ -0,0 +1,24 @@
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack_repo.builtin.build_systems.python import PythonPackage
+from spack.package import *
+
+
+class PyIhm(PythonPackage):
+    """Package for handling IHM mmCIF and BinaryCIF files."""
+
+    homepage = "https://github.com/ihmwg/python-ihm"
+    pypi = "ihm/ihm-2.7.tar.gz"
+
+    maintainers("LydDeb")
+
+    license("MIT", checked_by="LydDeb")
+
+    version("2.7", sha256="a3b9eba5545de1e07a15d110e9e6b70369807798d8f2c45908323db2b6fde82c")
+
+    depends_on("c", type="build")
+
+    depends_on("py-setuptools", type="build")
+    depends_on("py-msgpack", type=("build", "run"))
