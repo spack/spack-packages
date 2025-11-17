78d9ac3281316ef4562884d3d3d9b5acc24183f5
Alberto Invernizzi <alberto.invernizzi@cscs.ch>
2025-11-17 12:43:13 +0100

    bump version

--- repos/spack_repo/builtin/packages/cdi/package.py
+++ repos/spack_repo/builtin/packages/cdi/package.py
@@ -17,6 +17,7 @@ class Cdi(AutotoolsPackage):
     url = "https://gitlab.dkrz.de/mpim-sw/libcdi/-/archive/cdi-2.4.0/download.tar.gz"
     git = "https://gitlab.dkrz.de/mpim-sw/libcdi"
 
+    version("2.5.4", sha256="605d4a9192e9657d87bdbe544e31c96b81890e9acdfc137ecc8581721ce5390f")
     version("2.5.3", sha256="e6a97e4af7a18afcc8caee13e2018c61e6885d871fd20441b51d7eae123c8305")
     version("2.5.1.1", sha256="922a62c85ea7b7445f13821ec69516ce36f8ef0965e5fa6e6e560db57f264763")
     version("2.5.1", sha256="639985873f82cd11d5cbfc121939e8d6ea24dc8454314845d5a7abd056878f59")
