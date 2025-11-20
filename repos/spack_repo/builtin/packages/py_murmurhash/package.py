ace2db2ee20ebc64b032e2480904d2256bfd4c27
Jen Herting <jen@herting.cc>
2025-11-20 14:37:26 -0500

    [py-murmurhash] Added version 1.0.10
    Co-authored-by: Alex C Leute <aclrc@rit.edu>

--- repos/spack_repo/builtin/packages/py_murmurhash/package.py
+++ repos/spack_repo/builtin/packages/py_murmurhash/package.py
@@ -16,9 +16,11 @@ class PyMurmurhash(PythonPackage):
 
     license("MIT")
 
+    version("1.0.10", sha256="5282aab1317804c6ebd6dd7f69f15ba9075aee671c44a34be2bde0f1b11ef88a")
     version("1.0.2", sha256="c7a646f6b07b033642b4f52ae2e45efd8b80780b3b90e8092a0cec935fbf81e2")
 
     depends_on("cxx", type="build")  # generated
 
     depends_on("py-setuptools", type="build")
-    depends_on("py-wheel@0.32.0:0.32", type="build")
+    depends_on("py-wheel@0.32", type="build", when="@1.0.2")
+    depends_on("py-cython@0.25:", type="build")
