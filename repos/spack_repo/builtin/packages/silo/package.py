1c66947399b5fa3cdbfb1ad0d53ad15fb56499ee
Mark C. Miller <5720676+markcmiller86@users.noreply.github.com>
2025-11-22 13:47:50 -0800

    fix style

--- repos/spack_repo/builtin/packages/silo/package.py
+++ repos/spack_repo/builtin/packages/silo/package.py
@@ -317,7 +317,7 @@ class CMakeBuilder(cmake.CMakeBuilder):
             self.define_from_variant("SILO_ENABLE_PYTHON_MODULE", "python"),
             self.define_from_variant("SILO_ENABLE_HZIP", "hzip"),
             self.define_from_variant("SILO_ENABLE_FPZIP", "fpzip"),
-            self.define_from_variant("SILO_ENABLE_ZFP", "zfp"),  
+            self.define_from_variant("SILO_ENABLE_ZFP", "zfp"),
         ]
 
         if spec.satisfies("license=bsdonly"):
