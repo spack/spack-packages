c915af3b4b4b475b4a3f8f430afd0f470b0b1aa0
Adrien M. BERNEDE <51493078+adrienbernede@users.noreply.github.com>
2025-11-25 10:06:35 +0100

    Update radiuss packages with latest releases

--- repos/spack_repo/builtin/packages/raja/package.py
+++ repos/spack_repo/builtin/packages/raja/package.py
@@ -40,6 +40,18 @@ class Raja(CachedCMakePackage, CudaPackage, ROCmPackage):
     version("develop", branch="develop", submodules=submodules)
     version("main", branch="main", submodules=submodules)
     version(
+        "2025.09.1",
+        tag="v2025.09.1",
+        commit="1e0756eda3c344da362e483afb9100ebd8137a2c",
+        submodules=submodules,
+    )
+    version(
+        "2025.09.0",
+        tag="v2025.09.0",
+        commit="ca756788dbdd43fec2a3840389126ae94a905d5f",
+        submodules=submodules,
+    )
+    version(
         "2025.03.2",
         tag="v2025.03.2",
         commit="6e36a94380adbe88fed11a3213fc08461428ece0",
@@ -242,6 +254,7 @@ class Raja(CachedCMakePackage, CudaPackage, ROCmPackage):
     depends_on("cxx", type="build")
 
     depends_on("blt", type="build")
+    depends_on("blt@0.7.1:", type="build", when="@2025.09.0:")
     depends_on("blt@0.7.0:", type="build", when="@2025.03.0:")
     depends_on("blt@0.6.2:", type="build", when="@2024.02.1:")
     depends_on("blt@0.6.1", type="build", when="@2024.02.0")
@@ -258,6 +271,7 @@ class Raja(CachedCMakePackage, CudaPackage, ROCmPackage):
     depends_on("camp+omptarget", when="+omptarget")
     depends_on("camp+sycl", when="+sycl")
     depends_on("camp@main", when="@develop")
+    depends_on("camp@2025.09.2:", when="@2025.09.0:")
     depends_on("camp@2025.03.0:", when="@2025.03.0:")
     depends_on("camp@2024.07.0:", when="@2024.07.0:")
     depends_on("camp@2024.02.1:", when="@2024.02.1:")
@@ -270,6 +284,8 @@ class Raja(CachedCMakePackage, CudaPackage, ROCmPackage):
     depends_on("camp@0.1.0", when="@0.10.0:0.13.0")
 
     depends_on("cmake@3.23:", when="@2024.07.0:", type="build")
+    depends_on("cmake@3.24:", when="@2025.09.0:", type="build")
+    depends_on("cmake@3.23:", when="@2024.07.0:2025.03.2", type="build")
     depends_on("cmake@3.23:", when="@2022.10.0:2024.02.2+rocm", type="build")
     depends_on("cmake@3.20:", when="@2022.10.0:2024.02.2", type="build")
     depends_on("cmake@3.20:", when="@:2022.03+rocm", type="build")
@@ -361,7 +377,7 @@ class Raja(CachedCMakePackage, CudaPackage, ROCmPackage):
             hipcc_flags = []
             if self.spec.satisfies("^rocprim@7.0"):
                 hipcc_flags.append("-std=c++17")
-            if self.spec.satisfies("@2025.09.0:"):
+            elif self.spec.satisfies("@2025.09.0:"):
                 hipcc_flags.append("-std=c++17")
             elif self.spec.satisfies("@0.14.0:2025.09.0"):
                 hipcc_flags.append("-std=c++14")
