1f257465882ec8087e8bb7960c478631051281d8
Laren Spear <laren.spear@pnnl.gov>
2025-12-02 15:08:58 -0800

    25.11 and only clone amrex as submodule

--- repos/spack_repo/builtin/packages/erf/package.py
+++ repos/spack_repo/builtin/packages/erf/package.py
@@ -4,6 +4,12 @@ from spack_repo.builtin.build_systems.cuda import CudaPackage
 from spack.package import *
 
 
+def submodules(package):
+    submodules = []
+    submodules.append("Submodules/AMReX")
+
+    return submodules
+
 class Erf(CMakePackage, CudaPackage):
     """ERF solves the compressible Navier-Stokes on a Arakawa C-grid
     for large-scale weather modeling.
@@ -22,36 +28,37 @@ class Erf(CMakePackage, CudaPackage):
 
     license("BSD-3-Clause", checked_by="larenspear")
 
-    version("25.10", tag="25.10", submodules=True)
-    version("25.08", tag="25.08", submodules=True)
-    version("25.07", tag="25.07", submodules=True)
-    version("25.06", tag="25.06", submodules=True)
-    version("25.05", tag="25.05", submodules=True)
-    version("25.04", tag="25.04", submodules=True)
-    version("25.03", tag="25.03", submodules=True)
-    version("25.01", tag="25.01", submodules=True)
-    version("24.11", tag="24.11", submodules=True)
-    version("24.10", tag="24.10", submodules=True)
-    version("24.09", tag="24.09", submodules=True)
-    version("24.08", tag="24.08", submodules=True)
-    version("24.06", tag="24.06", submodules=True)
-    version("24.05", tag="24.05", submodules=True)
-    version("24.04", tag="24.04", submodules=True)
-    version("24.03", tag="24.03", submodules=True)
-    version("24.02", tag="24.02", submodules=True)
-    version("24.01", tag="24.01", submodules=True)
-    version("23.12", tag="23.12", submodules=True)
-    version("23.11", tag="23.11", submodules=True)
-    version("23.10", tag="23.10", submodules=True)
+    version("25.11", tag="25.11", submodules=submodules)
+    version("25.10", tag="25.10", submodules=submodules)
+    version("25.08", tag="25.08", submodules=submodules)
+    version("25.07", tag="25.07", submodules=submodules)
+    version("25.06", tag="25.06", submodules=submodules)
+    version("25.05", tag="25.05", submodules=submodules)
+    version("25.04", tag="25.04", submodules=submodules)
+    version("25.03", tag="25.03", submodules=submodules)
+    version("25.01", tag="25.01", submodules=submodules)
+    version("24.11", tag="24.11", submodules=submodules)
+    version("24.10", tag="24.10", submodules=submodules)
+    version("24.09", tag="24.09", submodules=submodules)
+    version("24.08", tag="24.08", submodules=submodules)
+    version("24.06", tag="24.06", submodules=submodules)
+    version("24.05", tag="24.05", submodules=submodules)
+    version("24.04", tag="24.04", submodules=submodules)
+    version("24.03", tag="24.03", submodules=submodules)
+    version("24.02", tag="24.02", submodules=submodules)
+    version("24.01", tag="24.01", submodules=submodules)
+    version("23.12", tag="23.12", submodules=submodules)
+    version("23.11", tag="23.11", submodules=submodules)
+    version("23.10", tag="23.10", submodules=submodules)
 
-    variant("mpi", default=False, description="Enable MPI support")
+    variant("mpi", default=True, description="Enable MPI support")
     variant("openmp", default=False, description="Enable OpenMP support")
-    variant("netcdf", default=False, description="Enable NetCDF support")
-    variant("particles", default=False, description="Enable particle support")
+    variant("netcdf", default=True, description="Enable NetCDF support")
+    variant("particles", default=True, description="Enable particle support")
     variant("multiblock", default=False, description="Enable multiblock support")
     variant("tests", default=False, description="Enable tests")
-    variant("fcompare", default=False, description="Enable fcompare")
-    variant("fft", default=False, description="Enable FFT support")
+    variant("fcompare", default=True, description="Enable fcompare")
+    variant("fft", default=True, description="Enable FFT support")
 
     with default_args(type="build"):
         depends_on("cmake@3.20:")
@@ -126,3 +133,4 @@ class Erf(CMakePackage, CudaPackage):
                 env.append_flags("CFLAGS", self.compiler.openmp_flag)
                 env.append_flags("CXXFLAGS", self.compiler.openmp_flag)
                 env.append_flags("FFLAGS", self.compiler.openmp_flag)
+
