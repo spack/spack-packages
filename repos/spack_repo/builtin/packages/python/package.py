25faeebecb5db4af9e9757a7614b7a0378eef230
Linus Bantel <linus.bantel@ipvs.uni-stuttgart.de>
2025-12-02 08:00:52 +0100

    changed ~gil to +freethreading, starting from 3.13

--- repos/spack_repo/builtin/packages/python/package.py
+++ repos/spack_repo/builtin/packages/python/package.py
@@ -173,7 +173,7 @@ class Python(Package):
     variant("tix", default=False, description="Build Tix module", when="+tkinter")
     variant("crypt", default=True, description="Build crypt module", when="@:3.12 platform=linux")
     variant("crypt", default=True, description="Build crypt module", when="@:3.12 platform=darwin")
-    variant("gil", default=True, description="Build with Global Interpreter Lock")
+    variant("freethreading", default=False, description="Removes the Global Interpreter Lock", when="@3.13:")
 
     depends_on("c", type="build")
     depends_on("cxx", type="build")
@@ -642,9 +642,8 @@ class Python(Package):
                 ]
             )
 
-        if self.version >= Version("3.14.0"):
-            if "~gil" in spec:
-                config_args.append("--disable-gil")
+        if "+freethreading" in spec:
+            config_args.append("--disable-gil")
 
         # Disable tkinter module in the configure script for Python 3.12 onwards if ~tkinter
         if spec.satisfies("@3.12:") and spec.satisfies("~tkinter"):
