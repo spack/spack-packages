d8396ed3c217c2f43a40e3f17ef353d92eb03df8
liuyangzhuan <liuyangzhuan@gmail.com>
2025-12-05 11:35:17 -0800

    format fix for gptune/package.py

--- repos/spack_repo/builtin/packages/gptune/package.py
+++ repos/spack_repo/builtin/packages/gptune/package.py
@@ -60,13 +60,25 @@ class Gptune(CMakePackage):
     depends_on("py-scikit-learn", type=("build", "run"))
     depends_on("py-matplotlib", type=("build", "run"))
     depends_on("py-pyyaml", type=("build", "run"))
-    depends_on("py-scikit-optimize@0.9.0", patches=[patch("space.patch")], type=("build", "run"),when="@:4.0.0")
-    depends_on("py-scikit-optimize@0.9.0", patches=[patch("gptunev5-skopt.patch")], type=("build", "run"),when="@5.0.0:")
+    depends_on(
+        "py-scikit-optimize@0.9.0", 
+        patches=[patch("space.patch")], 
+        type=("build", "run"),
+        when="@:4.0.0"
+    )
+    depends_on("py-scikit-optimize@0.9.0", 
+        patches=[patch("gptunev5-skopt.patch")], 
+        type=("build", "run"),
+        when="@5.0.0:"
+    )
     depends_on("py-gpy", type=("build", "run"))
     depends_on("py-lhsmdu", type=("build", "run"))
     depends_on("py-hpbandster", type=("build", "run"))
     depends_on("py-opentuner", type=("build", "run"))
-    depends_on("py-ytopt-autotune@1.1.0", patches=[patch("gptunev5-autotune.patch")], type=("build", "run"))
+    depends_on("py-ytopt-autotune@1.1.0", 
+        patches=[patch("gptunev5-autotune.patch")], 
+        type=("build", "run")
+    )
     depends_on("py-filelock", type=("build", "run"))
     depends_on("py-requests", type=("build", "run"))
     depends_on("py-pyaml", type=("build", "run"))
