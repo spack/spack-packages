9bc943e3586fd3a24b3a9fe4d42823222c7d3f87
gabe.turner <gabe.turner@sana.com>
2025-12-11 10:36:55 -0600

    Several preseq updates and enhancements
    - Add `preseq` 3.2.0
    - Add a missing `libefence` dependency for `preseq` 2.x and earlier.
      The `libefence` requirement was dropped in `preseq` 3.0.
    - Constrain `samtools` dependency to `preseq` 2.x and earlier. `preseq`
      3.0 drops the build-time dependency for `samtools`; the project
      developers indicate via issues and docs that they intend for `preseq`
      users to use `samtools` to perform any necessary actions on their
      inputs prior to passing those inputs to `preseq`.
    - Constrain `gsl` dependency to `preseq` 3.0.2 and earlier. Per the
      `preseq` changelog, 3.0.2 does not use `gsl` at all.
    - The `Preseq` now also extends the `AutotoolsPackage` class. While
      `preseq` 2.x and earlier simply use a Makefile, `preseq` 3.0 adopted
      autotools. When building `preseq` 2.x, select functions of
      `AutotoolsPackage`/`AutotoolsBuilder` are overridden to ensure that
      they do not cause the build to fail for autotools-related reason (e.g.
      a missing `configure` script).
    - Add the `hts` variant. `preseq` 3.0 and newer can optionally use
      `htslib` while converting BAM inputs to mapped read format. Despite
      using autotools, one cannot pass the location of `htslib`'s includes
      and libs via options to `configure` and must add `CPPFLAGS` and
      `LDFLAGS`, respectively.

--- repos/spack_repo/builtin/packages/preseq/package.py
+++ repos/spack_repo/builtin/packages/preseq/package.py
@@ -2,12 +2,13 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
+from spack_repo.builtin.build_systems.autotools import AutotoolsPackage
 from spack_repo.builtin.build_systems.makefile import MakefilePackage
 
 from spack.package import *
 
 
-class Preseq(MakefilePackage):
+class Preseq(MakefilePackage, AutotoolsPackage):
     """The preseq package is aimed at predicting and estimating the complexity
     of a genomic sequencing library, equivalent to predicting and
     estimating the number of redundant reads from a given sequencing depth
@@ -19,14 +20,40 @@ class Preseq(MakefilePackage):
 
     license("GPL-3.0-only")
 
+    version("3.2.0", sha256="95b81c9054e0d651de398585c7e96b807ad98f0bdc541b3e46665febbe2134d9")
     version("2.0.3", sha256="747ddd4227515a96a45fcff0709f26130386bff3458c829c7bc1f3306b4f3d91")
     version("2.0.2", sha256="1d7ea249bf4e5826e09697256643e6a2473bc302cd455f31d4eb34c23c10b97c")
+
+    variant("hts", default=False, description="Enable HTSlib to support BAM files", when="@3:")
 
     depends_on("c", type="build")  # generated
     depends_on("cxx", type="build")  # generated
 
-    depends_on("samtools")
-    depends_on("gsl")
+    # As of 3.0, preseq does not link libefence
+    depends_on("libefence", when="@:2")
 
-    def setup_build_environment(self, env: EnvironmentModifications) -> None:
+    # As of 3.0, preseq does not require samtools
+    depends_on("samtools", when="@:2")
+
+    # As of 3.0.2, preseq does not use gsl
+    depends_on("gsl", when="@:3.0.1")
+
+    with when("+hts"):
+        depends_on("htslib")
+
+    @when("@:2")
+    def setup_build_environment(self, env):
         env.set("PREFIX", self.prefix)
+
+    @when("@3:")
+    def setup_build_environment(self, env):
+        with when("+hts"):
+            env.set("CPPFLAGS", f"-I{self.spec["htslib"].prefix.include}")
+            env.set("LDFLAGS", f"-L{self.spec["htslib"].prefix.lib}")
+
+    @when("@:2")
+    def configure(self, spec, prefix):
+        return
+
+    with when("+hts"):
+        configure_args=["--enable-hts"]
