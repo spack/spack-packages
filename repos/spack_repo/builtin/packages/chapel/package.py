123bb30db27425de2e8e8bdc9986d5bd9a92849b
Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>
2025-12-12 14:20:43 -0600

    Style fixes
    Signed-off-by: Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>

--- repos/spack_repo/builtin/packages/chapel/package.py
+++ repos/spack_repo/builtin/packages/chapel/package.py
@@ -12,7 +12,7 @@ from spack_repo.builtin.build_systems.rocm import ROCmPackage
 
 # TODO be more selective in importing packages
 from spack.package import *
-from spack.util.environment import is_system_path # avoid deprecation warnings
+from spack.util.environment import is_system_path  # avoid deprecation warnings
 
 
 def slingshot_network():
@@ -65,8 +65,11 @@ class Chapel(AutotoolsPackage, CudaPackage, ROCmPackage):
 
     version("main", branch="main")
 
-    version("2.7.0", url="https://chapel-lang.org/tmp/chapel-2.7.0.tar.gz",
-            sha256="85e2fe7fcffb7f71e9173abb455678c14a1a8aa4997e10ed4d96d29d81672d0e")
+    version(
+            "2.7.0",
+            url="https://chapel-lang.org/tmp/chapel-2.7.0.tar.gz",
+            sha256="85e2fe7fcffb7f71e9173abb455678c14a1a8aa4997e10ed4d96d29d81672d0e"
+    )
     version("2.6.0", sha256="e469c35be601cf1f59af542ab885e8a14aa2b087b79af0d5372a4421976c74b6")
     version("2.5.0", sha256="020220ca9bf52b9f416e9a029bdc465bb1f635c1e274c6ca3c18d1f83e41fce1")
     version("2.4.0", sha256="a51a472488290df12d1657db2e7118ab519743094f33650f910d92b54c56f315")
@@ -495,7 +498,7 @@ class Chapel(AutotoolsPackage, CudaPackage, ROCmPackage):
         "https://github.com/chapel-lang/chapel/issues/27273",
     )
 
-    conflicts("cuda@12.9:", when="+cuda") # deprecation warnings otherwise
+    conflicts("cuda@12.9:", when="+cuda")  # deprecation warnings otherwise
 
     # ROCm conflicts and dependencies
     conflicts("+rocm", when="@:2.1", msg="ROCm support in spack requires Chapel 2.2.0 or later")
