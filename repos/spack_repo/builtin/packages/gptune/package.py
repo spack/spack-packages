6e3e28f6497fbc8a6aeebd579e7b048f926d79d0
liuyangzhuan <liuyangzhuan@gmail.com>
2025-12-15 15:31:01 -0800

    minor

--- repos/spack_repo/builtin/packages/gptune/package.py
+++ repos/spack_repo/builtin/packages/gptune/package.py
@@ -80,8 +80,13 @@ class Gptune(CMakePackage):
     depends_on("py-opentuner", type=("build", "run"))
     depends_on(
         "py-ytopt-autotune@1.1.0",
-        patches=[patch("gptunev5-autotune.patch", when="@5.0.0:")],
         type=("build", "run"),
+    )
+    depends_on(
+        "py-ytopt-autotune@1.1.0",
+        patches=[patch("gptunev5-autotune.patch")],
+        type=("build", "run"),
+        when="@5.0.0:",
     )
     depends_on("py-filelock", type=("build", "run"))
     depends_on("py-requests", type=("build", "run"))
