76fc111fa0da3a6cb26406451c7d2aba99d83594
LydDeb <lyderic.de@gmail.com>
2025-12-17 13:40:00 +0100

    py_trame_vtk: New package
    * add dependence:
      * py_trame_common
    * update dependence:
      * py_trame_client

--- repos/spack_repo/builtin/packages/py_trame_client/package.py
+++ repos/spack_repo/builtin/packages/py_trame_client/package.py
@@ -17,6 +17,17 @@ class PyTrameClient(PythonPackage):
 
     license("Apache-2.0", checked_by="johnwparent")
 
+    version("3.11.2", sha256="98b3f09d0fbdb09cd29eac61c945a76dcad4a08cfb4843abce5a148fd6fc7316")
     version("2.17.1", sha256="0841e569d0792c7fc218a502663c814ad69e318d2885cec82a7fe1d07fdf0bf4")
 
+    depends_on("python@3.9:", type=("build", "run"), when="@3.11.2")
     depends_on("py-setuptools@42:", type="build")
+    depends_on("py-trame-common@0.2.0:", type=("build", "run"), when="@3.11.2")
+
+    def url_for_version(self, version):
+        url = "https://files.pythonhosted.org/packages/source/t/{name}/{name}-{version}.tar.gz"
+        if self.spec.satisfies("@3.5.1:"):
+            name = "trame_client"
+        else:
+            name = "trame-client"
+        return url.format(name=name, version=version)
---
+++ repos/spack_repo/builtin/packages/py_trame_vtk/package.py
@@ -0,0 +1,26 @@
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack_repo.builtin.build_systems.python import PythonPackage
+
+from spack.package import *
+
+
+class PyTrameVtk(PythonPackage):
+    """trame-vtk extend trame widgets with components that can interface with VTK and/or ParaView."""
+
+    homepage = "https://kitware.github.io/trame/"
+    pypi = "trame-vtk/trame_vtk-2.10.0.tar.gz"
+
+    maintainers("LydDeb")
+
+    license("BSD License")
+
+    version("2.10.0", sha256="0e4cabd78c1e8b67da857ba5c3a404a2195cb3e849a252bae51575291bef01ad")
+
+    depends_on("python@3.9:", type=("build", "run"))
+    depends_on("py-hatchling", type="build")
+    depends_on("py-setuptools@42:", type="build")
+    depends_on("py-trame-client@3.4:3", type=("build", "run"))
+    depends_on("py-trame-server", type=("build", "run"))
