8889bbf6f39c8ec7c57f47a8ef935cb85d9b89a9
gabe.turner <gabe.turner@sana.com>
2025-12-17 11:33:44 -0600

    Set build_directory explicitly

--- repos/spack_repo/builtin/packages/csvtk/package.py
+++ repos/spack_repo/builtin/packages/csvtk/package.py
@@ -18,9 +18,9 @@ class Csvtk(GoPackage):
 
     version("0.36.0", sha256="0acea7e49c8af12ed76b11ec562ffc05a2fff28cb3c4e7b032e9271f13599ec8")
 
-    @property
-    def build_directory(self):
-        return f"{self.pkg.stage.source_path}/{self.name}"
+    depends_on("go@1.24:")
+
+    build_directory = f"{self.pkg.stage.source_path}/{self.name}"
 
     @run_after("install")
     def install_completions(self):
