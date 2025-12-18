36e308899b49932b80f55fdb3582f83b85a5b7f5
Cameron Cooper Harris <cameron.harris@adelaide.edu.au>
2025-12-18 18:58:26 +1030

    Passed b2luigi package through styling

--- repos/spack_repo/builtin/packages/py_b2luigi/package.py
+++ repos/spack_repo/builtin/packages/py_b2luigi/package.py
@@ -2,24 +2,7 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install b2luigi
-#
-# You can edit this file again by typing:
-#
-#     spack edit b2luigi
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
 from spack_repo.builtin.build_systems.python import PythonPackage
-
 
 from spack.package import *
 
@@ -31,12 +14,11 @@ class PyB2luigi(PythonPackage):
     pypi = "b2luigi/b2luigi-1.2.6.tar.gz"
 
     license("GNU")
-    
+
     # To be decided
-    #maintainers("github_user1", "github_user2")
+    # maintainers("github_user1", "github_user2")
 
     # We start at 1.2.6 as this was the change from retry2->tenacity dependency
-    # as py_tenacity is a spack package
     version("1.2.6", sha256="9f3be756f0961ca2241d36d9a9174ea5a23ebd7787cbfa78632047aae25f1202")
 
     depends_on("python@3.8:3.12", type=("build", "run"))
@@ -46,4 +28,4 @@ class PyB2luigi(PythonPackage):
     depends_on("py-colorama@0.3.9:", type=("build", "run"))
     depends_on("py-cachetools@2.1.1:", type=("build", "run"))
     depends_on("py-jinja2", type=("build", "run"))
-    depends_on("py-tenacity@8:9", type=("build", "run"))                              
+    depends_on("py-tenacity@8:9", type=("build", "run"))
