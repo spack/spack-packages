5ee922b4913ae07dd4dc96fdfb6b4aa990b38f13
Mateusz Jakub Fila <mateusz.jakub.fila@cern.ch>
2025-12-18 22:10:17 +0100

    gaudi: add v40.2

--- repos/spack_repo/builtin/packages/gaudi/package.py
+++ repos/spack_repo/builtin/packages/gaudi/package.py
@@ -18,6 +18,7 @@ class Gaudi(CMakePackage, CudaPackage):
     tags = ["hep"]
 
     version("master", branch="master")
+    version("40.2", sha256="93bf0ae5e33d7d3a5aa36504840ed62aeab9f6f8ddddd4ea1b23bc5455b51e41")
     version("40.1", sha256="f02010c865717d397b8fc8b8bf5d904e711ee2e416f3d12330cf04deaa7a4343")
     version("40.0", sha256="0cfe696967067b23382968a5c5ab1b4b7f38a7dd3ee2e321d1bff0dd8f99d2f9")
     version("39.4", sha256="dd698e0788811fa8325ed5f37ecf3fd9bde55720489224a517b52360819564d7")
