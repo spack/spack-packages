ab47298b34fd3f8a2cc8b055ab5a62cdd1a442d8
ajpotts <ajpotts@users.noreply.github.com>
2025-12-18 14:54:37 -0500

    fix style

--- repos/spack_repo/builtin/packages/chapel/package.py
+++ repos/spack_repo/builtin/packages/chapel/package.py
@@ -478,7 +478,9 @@ class Chapel(AutotoolsPackage, CudaPackage, ROCmPackage):
     conflicts("platform=windows")  # Support for windows is through WSL only
 
     # Only apply if concretizer picked gcc
-    conflicts("%gcc@:7.3", when="%gcc", msg="Chapel requires GCC >= 7.4 (C++17) per Chapel prerequisites")
+    conflicts(
+        "%gcc@:7.3", when="%gcc", msg="Chapel requires GCC >= 7.4 (C++17) per Chapel prerequisites"
+    )
 
     # Ensure GPU support is Sticky: never allow the concretizer to choose this
     variant("rocm", default=False, sticky=True, description="Enable AMD ROCm GPU support")
