da973e4ef65ca5feffd34c384054aad0b5b5bbf1
chapman39 <chapman39@llnl.gov>
2025-12-19 14:43:37 -0800

    Axom: Update ROCm root var

--- repos/spack_repo/builtin/packages/axom/package.py
+++ repos/spack_repo/builtin/packages/axom/package.py
@@ -374,7 +374,7 @@ class Axom(CachedCMakePackage, CudaPackage, ROCmPackage):
 
             hip_link_flags = ""
 
-            rocm_root = spec["llvm-amdgpu"].prefix
+            rocm_root = os.path.dirname(spec["llvm-amdgpu"].prefix)
             entries.append(cmake_cache_path("ROCM_ROOT_DIR", rocm_root))
 
             # Recommended MPI flags
