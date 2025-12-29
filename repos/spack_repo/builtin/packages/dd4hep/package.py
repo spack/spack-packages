bd1ca3e25c41ca3d4ecdf15600cfd76f291bbdb1
Seth R Johnson <johnsonsr@ornl.gov>
2025-12-29 12:41:06 -0500

    Revert changes to dd4hep

--- repos/spack_repo/builtin/packages/dd4hep/package.py
+++ repos/spack_repo/builtin/packages/dd4hep/package.py
@@ -230,6 +230,8 @@ class Dd4hep(CMakePackage):
         env.set("DD4HEP", self.prefix.examples)
         env.set("DD4hep_DIR", self.prefix)
         env.set("DD4hep_ROOT", self.prefix)
+        if len(self.libs.directories) > 0:
+            env.prepend_path("LD_LIBRARY_PATH", self.libs.directories[0])
 
     def url_for_version(self, version):
         # dd4hep releases are dashes and padded with a leading zero
