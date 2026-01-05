f1bd2cd046e740e779d86b82829bb75ee46b1c46
gabe.turner <gabe.turner@sana.com>
2026-01-05 14:46:02 -0600

    Add yamlfmt 0.21.0

--- repos/spack_repo/builtin/packages/yamlfmt/package.py
+++ repos/spack_repo/builtin/packages/yamlfmt/package.py
@@ -16,8 +16,10 @@ class Yamlfmt(GoPackage):
 
     license("Apache-2.0")
 
+    version("0.21.0", sha256="9ec915d70e527a94171eaaf6b785d1423222b5b82e7633f80dcc6b66e6a655aa")
     version("0.20.0", sha256="de6bc4373ba46c520d936dd4b60395868ec17aba338b9fd849594c1f41b6c057")
 
+    depends_on("go@1.22:", type="build", when="@0.21:")
     depends_on("go@1.21:", type="build")
 
     @property
