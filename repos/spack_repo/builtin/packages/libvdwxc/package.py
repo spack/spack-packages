2611e61e253d59c5d824882b954156470f471658
Rocco Meli <r.meli@bluemail.ch>
2026-01-08 17:11:27 +0100

    Revert "[libvdwxc] Fix the autotools mpi detection (#2935)"
    This reverts commit 770c90d1200271ad9af3d4c7cc7eb8281495a6bf.

--- repos/spack_repo/builtin/packages/libvdwxc/package.py
+++ repos/spack_repo/builtin/packages/libvdwxc/package.py
@@ -71,10 +71,3 @@ class Libvdwxc(AutotoolsPackage):
     # The relevant upstream fix for the m4 would be:
     # https://gitlab.com/libvdwxc/libvdwxc/-/commit/9340f857515c4a2e56d2aa7cf3a21c41ba8559c3.diff
     patch("fftw-detection.patch", when="@:0.4.0")
-
-    # fix a mpi detection error
-    patch(
-        "https://gitlab.com/libvdwxc/libvdwxc/-/merge_requests/25.diff",
-        sha256="23a44ef5de0cd15517a3f8750d516c0978ce6f83b8eda0a35d317058d650e1a2",
-        when="@0.5.0",
-    )
