106f0e77357d0b10a4e0fe6cd53819a221ac0077
John Biddiscombe <biddisco@cscs.ch>
2026-01-09 12:29:43 +0100

    pulseaudio: add new version and support new meson build system

--- repos/spack_repo/builtin/packages/pulseaudio/package.py
+++ repos/spack_repo/builtin/packages/pulseaudio/package.py
@@ -2,12 +2,14 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
+from spack_repo.builtin.build_systems import autotools, meson
 from spack_repo.builtin.build_systems.autotools import AutotoolsPackage
+from spack_repo.builtin.build_systems.meson import MesonPackage
 
 from spack.package import *
+import os
 
-
-class Pulseaudio(AutotoolsPackage):
+class Pulseaudio(AutotoolsPackage, MesonPackage):
     """PulseAudio is a sound system for POSIX OSes, meaning that it is a proxy
     for your sound applications.
 
@@ -19,10 +21,17 @@ class Pulseaudio(AutotoolsPackage):
     achieved using a sound server."""
 
     homepage = "https://www.freedesktop.org/wiki/Software/PulseAudio/"
-    url = "https://freedesktop.org/software/pulseaudio/releases/pulseaudio-13.0.tar.xz"
+    url = "https://freedesktop.org/software/pulseaudio/releases/pulseaudio-17.0.tar.xz"
 
     license("LGPL-2.1-or-later")
 
+    build_system(
+        conditional("autotools", when="@:16"),
+        conditional("meson", when="@17:"),
+        default="meson",
+    )
+
+    version("17.0", sha256="053794d6671a3e397d849e478a80b82a63cb9d8ca296bd35b73317bb5ceb87b5")
     version("13.0", sha256="961b23ca1acfd28f2bc87414c27bb40e12436efcf2158d29721b1e89f3f28057")
 
     variant("alsa", default=False, description="alsa support")
@@ -32,6 +41,13 @@ class Pulseaudio(AutotoolsPackage):
 
     depends_on("c", type="build")  # generated
     depends_on("cxx", type="build")  # generated
+    with when("build_system=autotools"):
+        depends_on("autoconf", type="build")
+        depends_on("automake", type="build")
+        depends_on("libtool", type="build")
+    with when("build_system=meson"):
+        depends_on("meson", type="build")
+        depends_on("gettext", type="build")
 
     depends_on("alsa-lib@1.0.19:", when="+alsa")
     depends_on("dbus@1.4.12:")
@@ -58,6 +74,9 @@ class Pulseaudio(AutotoolsPackage):
     depends_on("speexdsp@1.2:")
     depends_on("m4", type="build")
 
+#    if spec.satisfies("%gcc@8:"):
+    os.environ["LDFLAGS"] = "-Wl,--copy-dt-needed-entries"
+
     def configure_args(self):
         args = [
             "--disable-systemd-daemon",
@@ -71,8 +90,8 @@ class Pulseaudio(AutotoolsPackage):
             "--with-systemduserunitdir=no",
             "CPPFLAGS={0}".format(self.spec["libtool"].headers.cpp_flags),
             "LDFLAGS={0}".format(self.spec["libtool"].libs.search_flags),
+            "--libdir={0}".format(self.prefix.lib64),
         ]
-
         # toggle based on variants
         args += self.enable_or_disable("alsa")
         args += self.enable_or_disable("openssl")
@@ -97,3 +116,29 @@ class Pulseaudio(AutotoolsPackage):
         )
 
         return args
+
+class MesonBuilder(meson.MesonBuilder):
+
+    def meson_args(self):
+        return [
+            "-Ddatabase=gdbm",
+            "-Ddoxygen=false",
+            "-Dbluez5=disabled",
+            "-Dx11=disabled",
+            "-Dtests=false",
+            "-Ddefault_library=shared",
+            "-Dprefix={0}".format(self.prefix),
+            "-Dlibdir={0}/lib64".format(self.prefix),
+            "-Dbashcompletiondir={0}/share/bash-completion/completions".format(self.prefix),
+            "-Dsystemduserunitdir={0}/lib/systemd/user".format(self.prefix)
+]
+
+    def setup_build_environment(self, env):
+        env.append_flags("CPPFLAGS", "-I{0}".format(self.spec["libiconv"].prefix.include))
+        env.append_flags("LDFLAGS", "-L{0} -liconv".format(self.spec["libiconv"].prefix.lib))
+
+    def setup_run_environment(self, env):
+        env.prepend_path('ALSA_PLUGIN_DIR', self.prefix.lib.alsa-lib)
+        env.prepend_path('LD_LIBRARY_PATH', self.spec['pulseaudio'].prefix.lib64)
+
+
