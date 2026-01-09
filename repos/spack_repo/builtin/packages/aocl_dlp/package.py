d3c3937899b8b84de7f98793d1fc371854cc35be
amd-toolchain-support <toolchainsupport@amd.com>
2026-01-09 15:21:29 +0000

    aocl_dlp: more style fix

--- repos/spack_repo/builtin/packages/aocl_dlp/package.py
+++ repos/spack_repo/builtin/packages/aocl_dlp/package.py
@@ -8,11 +8,11 @@ from spack.package import *
 
 
 class AoclDlp(CMakePackage):
-    """AOCL-DLP is a library designed to provide optimized deep learning primitives for AMD processors.
-    It implements Low Precision GEMM (LPGEMM) and batch GEMM for deep learning applications, supporting
-    multiple data types as well as pre-operations and post-operations. The library is tailored to leverage
-    the full potential of AMD hardware, ensuring efficient computation, scalability, and accelerated
-    deep learning workloads."""
+    """AOCL-DLP is a library designed to provide optimized deep learning primitives for AMD
+    processors. It implements Low Precision GEMM (LPGEMM) and batch GEMM for deep learning
+    applications, supporting multiple data types as well as pre-operations and post-operations.
+    The library is tailored to leverage the full potential of AMD hardware, ensuring efficient
+    computation, scalability, and accelerated deep learning workloads."""
 
     _name = "aocl-dlp"
     homepage = "https://www.amd.com/en/developer/aocl/dlp.html"
