3c640b059f7a9e7ac75731b9f877702cee2608c1
吴坎 <github@wu-kan.cn>
2026-01-15 20:34:01 +0800

    Fix Py-Torch-C-Dlpack-ext dependency

--- repos/spack_repo/builtin/packages/py_torch_c_dlpack_ext/package.py
+++ repos/spack_repo/builtin/packages/py_torch_c_dlpack_ext/package.py
@@ -11,22 +11,17 @@ class PyTorchCDlpackExt(PythonPackage):
     """torch c dlpack ext"""
 
     homepage = "https://pypi.org/project/torch-c-dlpack-ext/"
-    pypi = "torch-c-dlpack-ext/torch_c_dlpack_ext-0.1.4.tar.gz"
+    pypi = "torch-c-dlpack-ext/torch_c_dlpack_ext-0.1.5.tar.gz"
 
     license("Apache-2.0")
 
-    version("0.1.4", sha256="ad292d17e285ab9523940e51e87d21ffce4982ce8beb46fb18b5c2b4760a1a10")
+    version("0.1.5", sha256="d06f0357d575d22a168cc77acb9020fc4bae30968ceb6718a055dcbe92bacabe")
 
-    depends_on("py-torch")
+    depends_on("py-torch", type=["build", "link", "run"])
 
     with default_args(type="build"):
-        depends_on("c")
         depends_on("cxx")
-        depends_on("cmake")
-        depends_on("ninja")
+        depends_on("ninja", when="^py-apache-tvm-ffi@:0.1.3")
+        depends_on("ninja", when="platform=windows")
         depends_on("py-setuptools@61.0:")
         depends_on("py-apache-tvm-ffi@0.1.1:")
-        depends_on("py-pybind11")
-
-    def setup_build_environment(self, env: EnvironmentModifications) -> None:
-        env.prepend_path("CPLUS_INCLUDE_PATH", self.spec["py-pybind11"].prefix.include)
