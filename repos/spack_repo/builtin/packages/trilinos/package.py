c69ce5e2613be8c405f334b96973be171f0cb1d5
Paul Kuberry <pakuber@sandia.gov>
2026-01-17 13:45:51 -0500

    trilinos: update cuda version conflict with stdcxx=20

--- repos/spack_repo/builtin/packages/trilinos/package.py
+++ repos/spack_repo/builtin/packages/trilinos/package.py
@@ -371,6 +371,7 @@ class Trilinos(CMakePackage, CudaPackage, ROCmPackage):
     conflicts("cxxstd=11", when="+wrapper ^cuda@6.5.14")
     conflicts("cxxstd=14", when="+wrapper ^cuda@6.5.14:8.0.61")
     conflicts("cxxstd=17", when="+wrapper ^cuda@6.5.14:10.2.89")
+    conflicts("cxxstd=20", when="+wrapper ^cuda@:11")
 
     # Multi-value gotype only applies to trilinos through 12.14
     conflicts("gotype=all", when="@12.15:")
