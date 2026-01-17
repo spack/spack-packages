5a8f69186efc4861e29e0b71a45b91b203af6aef
Wouter Deconinck <wdconinc@gmail.com>
2026-01-17 16:24:54 -0600

    coin3d: rm unused import
    Removed Boost package import from coin3d package.

--- repos/spack_repo/builtin/packages/coin3d/package.py
+++ repos/spack_repo/builtin/packages/coin3d/package.py
@@ -5,7 +5,6 @@
 from spack_repo.builtin.build_systems import autotools, cmake
 from spack_repo.builtin.build_systems.autotools import AutotoolsPackage
 from spack_repo.builtin.build_systems.cmake import CMakePackage
-from spack_repo.builtin.packages.boost.package import Boost
 
 from spack.package import *
 
