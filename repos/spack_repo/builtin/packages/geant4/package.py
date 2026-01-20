7261d61510102df6131cc2f9a4cfba4958c60f8e
Wouter Deconinck <wdconinc@gmail.com>
2026-01-19 18:57:26 -0600

    geant4: keep vecgeom min ver 1.2.6 in place for 11.3, 11.4

--- repos/spack_repo/builtin/packages/geant4/package.py
+++ repos/spack_repo/builtin/packages/geant4/package.py
@@ -169,8 +169,6 @@ class Geant4(CMakePackage):
 
     # Vecgeom specific versions for each Geant4 version
     with when("+vecgeom"):
-        depends_on("vecgeom@2.0.0:", when="@11.4:")
-        depends_on("vecgeom@1.2.8:", when="@11.3:")
         depends_on("vecgeom@1.2.6:", when="@11.2:")
         depends_on("vecgeom@1.2.0:", when="@11.1")
         depends_on("vecgeom@1.1.18:1.1", when="@11.0.0:11.0")
