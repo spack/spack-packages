8dae7038938bcecdf8c0d38c6a876a6b10702f0b
Lina Muryanto <muryanto1@llnl.gov>
2026-01-21 11:28:53 -0800

    fixed copyright, added back the python binding -- mistakenly missing from my update

--- repos/spack_repo/builtin/packages/adiak/package.py
+++ repos/spack_repo/builtin/packages/adiak/package.py
@@ -1,7 +1,8 @@
-# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
-# Spack Project Developers. See the top-level COPYRIGHT file for details.
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+import os
 
 from spack_repo.builtin.build_systems.cached_cmake import CachedCMakePackage, cmake_cache_option
 
@@ -20,14 +21,14 @@ class Adiak(CachedCMakePackage):
 
     variant("mpi", default=True, description="Build with MPI support")
     variant("shared", default=True, description="Build dynamic libraries")
-
+    variant("python", default=False, when="@0.5.0:", description="Build Python bindings")
+    
     license("MIT")
+    version("master", branch="master")
     version(
         "0.5.0", commit="f08c8375c613e13e9b9c6a1db271cbf8f0d3f3e3", submodules=True, preferred=True
     )
-    version(
-        "0.4.1", commit="7ac997111785bee6d9391664b1d18ebc2b3c557b", submodules=True, preferred=True
-    )
+    version("0.4.1", commit="7ac997111785bee6d9391664b1d18ebc2b3c557b", submodules=True)
     version("0.4.0", commit="7e8b7233f8a148b402128ed46b2f0c643e3b397e", submodules=True)
     version("0.2.2", commit="3aedd494c81c01df1183af28bc09bade2fabfcd3", submodules=True)
     version("0.2.1", commit="950e3bfb91519ecb7b7ee7fa3063bfab23c0e2c9", submodules=True)
@@ -40,6 +41,11 @@ class Adiak(CachedCMakePackage):
 
     depends_on("mpi", when="+mpi")
 
+    with when("+python"):
+        extends("python")
+        depends_on("python@3:", type=("build", "link", "run"))
+        depends_on("py-pybind11@3.0.0:", type=("build", "link", "run"))
+
     def initconfig_mpi_entries(self):
         spec = self.spec
 
@@ -50,11 +56,16 @@ class Adiak(CachedCMakePackage):
     def cmake_args(self):
         args = []
 
-        if self.spec.satisfies("+mpi"):
-            args.append("-DENABLE_PYTHON_BINDINGS=ON")
         if self.spec.satisfies("+shared"):
             args.append("-DBUILD_SHARED_LIBS=ON")
         else:
             args.append("-DBUILD_SHARED_LIBS=OFF")
 
+        if self.spec.satisfies("+python"):
+            args.append("-DENABLE_PYTHON_BINDINGS=ON")
+            pybind11_cmake = os.path.join(
+                self.spec["py-pybind11"].prefix, "pybind11", "share", "cmake", "pybind11"
+            )
+            args.append(f"-Dpybind11_DIR={pybind11_cmake}")
+            
         return args
