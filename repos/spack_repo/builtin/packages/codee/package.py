b44765b41db90baf340d7da2d75b9ebdfdbef589
Dom Heinzeller <dom.heinzeller@icloud.com>
2026-01-26 13:36:26 -0700

    Update codee package

--- repos/spack_repo/builtin/packages/codee/package.py
+++ repos/spack_repo/builtin/packages/codee/package.py
@@ -3,6 +3,8 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 import os
+import platform
+import sys
 import tarfile
 
 from spack_repo.builtin.build_systems.generic import Package
@@ -19,58 +21,47 @@ class Codee(Package):
     
     Codee consists of a formatter (Codee Formatter) and a static
     code analyzer (Codee Analyzer). The formatter is, with certain
-    limitations, free to use after accepting the end user license
-    agreement (EULA). Licensed users have access to all features
-    of the formatter and to the static code analyzer."""
+    limitations (free-form Fortran only), free to use after accepting
+    the end user license agreement (EULA). Licensed users have access
+    to all features of the formatter and to the static code analyzer."""
 
     homepage = "https://www.codee.com"
-    # fake sha256, add correct one
-    maintainers("climbfuji", "e2b09ec21660f01fecffb715e0120265216943f038d0e48a9868713e54f0zzzz")
 
-    version("2025.4.4", sha256="ADDSHA256HERE")
+    maintainers("climbfuji")
+
+    version("2025.4.6", sha256="39985b33b42621c17c4b79e37267fa8528de6702dfcde6bc330e114f25575f98")
+    version("2025.4.5", sha256="42688ec4214270da59da365ba054d1cbf744cb30593542d6d1e04c26e90bcb14")
+    version("2025.4.4", sha256="764bc109945561192c386c080d5359f3faa96f06d0a0b52de0f9064cbbf2799a")
 
     variant("accept-eula", default=False, description="Accept the EULA")
 
-    # Licensing
-    #FROM arm_forge license_required = False
-    #FROM arm_forge license_comment = "#"
-    #FROM arm_forge license_files = ["licences/Licence"]
-    #FROM arm_forge license_vars = [
-    #FROM arm_forge     "ALLINEA_LICENSE_DIR",
-    #FROM arm_forge     "ALLINEA_LICENCE_DIR",
-    #FROM arm_forge     "ALLINEA_LICENSE_FILE",
-    #FROM arm_forge     "ALLINEA_LICENCE_FILE",
-    #FROM arm_forge ]
-    #FROM arm_forge license_url = "https://developer.arm.com/documentation/101169/latest/Use-Arm-Licence-Server"
-
-    # TODO ADD CONFLICTS FOR ANYTHING ELSE THAN LINUX x86_64 here, or replace hardcoded linux-x86_64 below (2x)
-
     def url_for_version(self, version):
-        return f"https://codee.com/release/codee-{version}-linux-x86_64.tar.gz"
+        target = None
+        if sys.platform=="linux":
+            if platform.machine() == "aarch64":
+                target = "linux-arm64"
+            elif platform.machine() == "x86_64":
+                target = "linux-x86_64"
+        if not target:
+            raise InstallError(f"Platform {sys.platform}/{platform.machine()} not supported or configured")
+        return f"https://codee.com/release/codee-{version}-{target}.tar.gz"
 
     @run_before("install")
     def abort_without_eula_acceptance(self):
         install_example = "spack install codee +accept-eula"
-        license_terms_path = os.path.join(self.stage.source_path, "license_terms")
         if not self.spec.variants["accept-eula"].value:
+            spath = self.stage.source_path
             raise InstallError(
-                "\n\n\nNOTE:\nUse +accept-eula "
-                + "during installation "
-                + "to accept the license terms in:\n"
-                + f"Formatter (free): {join_path(license_terms_path, "EULA_Formatter.txt")}}\n"
-                + f"Formatter/Analyzer: {join_path(license_terms_path, "EULA.txt")}}\n"
-                + f"                    {join_path(license_terms_path, "Third_Party_Licenses.txt")}}\n"
-                + "Example: '{0}'\n".format(install_example)
+                "\n\n\nNOTE:\nUse +accept-eula during installation to accept "
+                + "the license terms for the appropriate product in:\n"
+                + f"Formatter (free): {join_path(spath, 'EULA_Formatter.txt')}\n"
+                + f"Formatter/Analyzer: {join_path(spath, 'EULA.txt')}\n"
+                + f"                    {join_path(spath, 'Third_Party_Licenses.txt')}\n"
+                + f"Example: '{install_example}'\n"
+                + "For the commercial (non-free) version of Codee, place your license "
+                + "(codee.lic) in the installation directory or point the environment "
+                + "variable CODEE_LICENSE_PATH to the location of the license file.\n"
             )
 
     def install(self, spec, prefix):
-        codee_tar = tarfile.open(
-                name=f"codee-{self.version}-linux-x86_64.tar.gz"
-            )
-        install_tree(self.install_dir[self.version.string], prefix)
-
-    #def setup_run_environment(self, env: EnvironmentModifications) -> None:
-    #    # Only PATH is needed for Codee.
-    #    # Adding lib to LD_LIBRARY_PATH can cause conflicts with Forge's internal libs.
-    #    env.clear()
-    #    env.prepend_path("PATH", join_path(self.prefix, "bin"))
+        install_tree(self.stage.source_path, prefix)
