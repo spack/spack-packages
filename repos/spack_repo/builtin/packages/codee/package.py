94b882fb5c67ec165e9531743a25f1be6c760641
Dom Heinzeller <dom.heinzeller@icloud.com>
2026-01-27 13:59:29 -0700

    Remove unnecessary EULA logic from repos/spack_repo/builtin/packages/codee/package.py

--- repos/spack_repo/builtin/packages/codee/package.py
+++ repos/spack_repo/builtin/packages/codee/package.py
@@ -33,8 +33,6 @@ class Codee(Package):
     version("2025.4.5", sha256="42688ec4214270da59da365ba054d1cbf744cb30593542d6d1e04c26e90bcb14")
     version("2025.4.4", sha256="764bc109945561192c386c080d5359f3faa96f06d0a0b52de0f9064cbbf2799a")
 
-    variant("accept-eula", default=False, description="Accept the EULA")
-
     def url_for_version(self, version):
         target = None
         if sys.platform=="linux":
@@ -45,23 +43,6 @@ class Codee(Package):
         if not target:
             raise InstallError(f"Platform {sys.platform}/{platform.machine()} not supported or configured")
         return f"https://codee.com/release/codee-{version}-{target}.tar.gz"
-
-    @run_before("install")
-    def abort_without_eula_acceptance(self):
-        install_example = "spack install codee +accept-eula"
-        if not self.spec.variants["accept-eula"].value:
-            spath = self.stage.source_path
-            raise InstallError(
-                "\n\n\nNOTE:\nUse +accept-eula during installation to accept "
-                + "the license terms for the appropriate product in:\n"
-                + f"Formatter (free): {join_path(spath, 'EULA_Formatter.txt')}\n"
-                + f"Formatter/Analyzer: {join_path(spath, 'EULA.txt')}\n"
-                + f"                    {join_path(spath, 'Third_Party_Licenses.txt')}\n"
-                + f"Example: '{install_example}'\n"
-                + "For the commercial (non-free) version of Codee, place your license "
-                + "(codee.lic) in the installation directory or point the environment "
-                + "variable CODEE_LICENSE_PATH to the location of the license file.\n"
-            )
 
     def install(self, spec, prefix):
         install_tree(self.stage.source_path, prefix)
