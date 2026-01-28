c3c92643cf5cc425eb1c6dc546e9c274fa168db8
Dom Heinzeller <dom.heinzeller@icloud.com>
2026-01-28 13:15:41 -0700

    Update version URLs in repos/spack_repo/builtin/packages/py_pygithub/package.py

--- repos/spack_repo/builtin/packages/py_pygithub/package.py
+++ repos/spack_repo/builtin/packages/py_pygithub/package.py
@@ -11,14 +11,14 @@ class PyPygithub(PythonPackage):
     """Typed interactions with the GitHub API v3"""
 
     homepage = "https://pygithub.readthedocs.io/"
-    pypi = "PyGithub/PyGithub-1.54.1.tar.gz"
+    pypi = "pygithub/pygithub-2.8.1.tar.gz"
 
     license("LGPL-3.0-only")
 
     version("2.8.1", sha256="341b7c78521cb07324ff670afd1baa2bf5c286f8d9fd302c1798ba594a5400c9")
-    version("2.1.1", sha256="ecf12c2809c44147bce63b047b3d2e9dac8a41b63e90fcb263c703f64936b97c")
-    version("1.59.1", sha256="c44e3a121c15bf9d3a5cc98d94c9a047a5132a9b01d22264627f58ade9ddc217")
-    version("1.55", sha256="1bbfff9372047ff3f21d5cd8e07720f3dbfdaf6462fcaed9d815f528f1ba7283")
+    version("2.1.1", sha256="ecf12c2809c44147bce63b047b3d2e9dac8a41b63e90fcb263c703f64936b97c", url=f"https://files.pythonhosted.org/packages/source/P/PyGithub/pygithub-2.1.1.tar.gz")
+    version("1.59.1", sha256="c44e3a121c15bf9d3a5cc98d94c9a047a5132a9b01d22264627f58ade9ddc217", url=f"https://files.pythonhosted.org/packages/source/P/PyGithub/PyGithub-1.59.1.tar.gz")
+    version("1.55", sha256="1bbfff9372047ff3f21d5cd8e07720f3dbfdaf6462fcaed9d815f528f1ba7283", url=f"https://files.pythonhosted.org/packages/source/P/PyGithub/PyGithub-1.55.tar.gz")
 
     depends_on("python@3.6:", type=("build", "run"))
     depends_on("python@3.7:", type=("build", "run"), when="@1.57:")
