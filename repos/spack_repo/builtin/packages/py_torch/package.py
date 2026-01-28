ca6c4fa0a9a24012d54a5ac7a905b5e2fa869b9e
renjithravindrankannath <rravindr@amd.com>
2026-01-27 23:12:20 -0500

    Updating aotriton dependency

--- repos/spack_repo/builtin/packages/py_torch/package.py
+++ repos/spack_repo/builtin/packages/py_torch/package.py
@@ -330,7 +330,7 @@ class PyTorch(PythonPackage, CudaPackage, ROCmPackage):
             )
         depends_on("rocminfo")
         depends_on("hipsparselt@7.0:", when="@2.9")
-        depends_on("aotriton@0.8b", when="@2.5:2.6")
+        depends_on("aotriton@0.8.1b", when="@2.5:2.6")
         depends_on("aotriton@0.9.2b", when="@2.7")
         depends_on("aotriton@0.10b", when="@2.8:")
     depends_on("mpi", when="+mpi")
