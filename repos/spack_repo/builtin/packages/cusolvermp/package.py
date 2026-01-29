155dc5035037e3c6d66569325c651addb46929fb
Alberto Invernizzi <alberto.invernizzi@cscs.ch>
2026-01-29 08:54:32 +0100

    add version 0.7.2

--- repos/spack_repo/builtin/packages/cusolvermp/package.py
+++ repos/spack_repo/builtin/packages/cusolvermp/package.py
@@ -19,7 +19,17 @@ _versions = {
             sha256="a0012c5be7ac742a26cf8894bed3c703edea84eddf0d5dca42d35582622ffb9b",
             url="https://developer.download.nvidia.com/compute/cusolvermp/redist/libcusolvermp/linux-sbsa/libcusolvermp-linux-sbsa-0.7.0.833_cuda12-archive.tar.xz",
         ),
-    }
+    },
+    "0.7.2.888": {
+        "Linux-x86_64": dict(
+            sha256="70243423e07861007ca5a95d35247af4ecdbcd0bf77aa09016a1abf3dcffb6a8",
+            url="https://developer.download.nvidia.com/compute/cusolvermp/redist/libcusolvermp/linux-x86_64/libcusolvermp-linux-x86_64-0.7.2.888_cuda12-archive.tar.xz",
+        ),
+        "Linux-aarch64": dict(
+            sha256="4c1a2688ec0072dec6d9d2303b489a48ad34a419e42ba05f51af68a43221fed6",
+            url="https://developer.download.nvidia.com/compute/cusolvermp/redist/libcusolvermp/linux-sbsa/libcusolvermp-linux-sbsa-0.7.2.888_cuda12-archive.tar.xz",
+        ),
+    },
 }
 
 
