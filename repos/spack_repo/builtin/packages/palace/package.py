f496e1b009250b953f5ed6830d7e5b9f8267d0f0
Gabriele Bozzola <gbozzola@amazon.com>
2026-01-30 09:35:07 -0800

    Update to MFEM 4.9

--- repos/spack_repo/builtin/packages/palace/package.py
+++ repos/spack_repo/builtin/packages/palace/package.py
@@ -148,12 +148,12 @@ class Palace(CMakePackage, CudaPackage, ROCmPackage):
     with when("@0.16:"):
         # +lapack means: use external lapack
         depends_on(
-            "mfem+mpi+metis+lapack cxxstd=17 commit=bf1b25d82e232909e1d3aa1f416dbdae9931a550",
+            # TODO: update to v4.9 == d9d6526cc1749980a2ba1da16e2c1ca1e07d82ec once spack updated
+            # https://github.com/spack/spack-packages/pull/3040
+            "mfem+mpi+metis+lapack cxxstd=17 commit=d9d6526cc1749980a2ba1da16e2c1ca1e07d82ec",
             patches=[
-                patch("patch_mesh_vis_dev.diff"),
                 patch("patch_par_tet_mesh_fix_dev.diff"),
                 patch("patch_gmsh_parser_performance.diff"),
-                patch("patch_race_condition_fix.diff"),
             ],
         )
         depends_on("mfem+shared", when="+shared")
