ccd24d2e244912d2a4e599ce30b4636f6bf59c7c
georges-da-costa <georges.da-costa@univ-tlse3.fr>
2026-02-04 15:12:44 +0100

    Merge the targeted branch in main

--- repos/spack_repo/builtin/packages/mojitos/package.py
+++ repos/spack_repo/builtin/packages/mojitos/package.py
@@ -15,7 +15,7 @@ class Mojitos(Package):
 
     homepage = "https://gitlab.irit.fr/sepia-pub/mojitos"
     git = "https://gitlab.irit.fr/sepia-pub/mojitos"
-    version("2.0.1", branch="guix")
+    version("2.0.2", branch="main")
     maintainers("georges-da-costa")
     license("GPL-3.0-or-later", checked_by="georges-da-costa")
 
