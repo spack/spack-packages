444353ebf83068a1fe68654779a48184deadb364
Brian Vanderwende <vanderwb@ucar.edu>
2026-02-05 13:44:07 -0700

    netcdf-c code and style improvements

--- repos/spack_repo/builtin/packages/netcdf_c/package.py
+++ repos/spack_repo/builtin/packages/netcdf_c/package.py
@@ -394,15 +394,18 @@ class CMakeBuilder(AnyBuilder, cmake.CMakeBuilder):
         else:
             base_cmake_args.append(self.define(nc + "FIND_SHARED_LIBS", False))
 
-        if self.spec.satisfies("@4.9.0:+shared"):
-            # The plugins are not built when the shared libraries are disabled:
-            if self.spec.satisfies("@4.9.3:"):
-                # This toggle is not defined in the top-level CMake parameters but is still
-                # used by the plugin config; so we work around this bug for now
-                base_cmake_args.append(self.define("ENABLE_PLUGIN_INSTALL", True))
-                base_cmake_args.append(self.define("NETCDF_WITH_PLUGIN_DIR", self.prefix.plugins))
-            else:
-                base_cmake_args.append(self.define("PLUGIN_INSTALL_DIR", self.prefix.plugins))
+        # The plugins are not built when the shared libraries are disabled:
+        if self.spec.satisfies("@4.9.3:+shared"):
+            # This toggle is not defined in the top-level CMake parameters but is still
+            # used by the plugin config; so we work around this bug for now
+            base_cmake_args.extend(
+                [
+                    self.define("ENABLE_PLUGIN_INSTALL", True),
+                    self.define("NETCDF_WITH_PLUGIN_DIR", self.prefix.plugins),
+                ]
+            )
+        elif self.spec.satisfies("@4.9.0:+shared"):
+            base_cmake_args.append(self.define("PLUGIN_INSTALL_DIR", self.prefix.plugins))
         return base_cmake_args
 
     @run_after("install")
