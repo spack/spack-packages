7f9315f5e7a5fb276efc81bc2dbc5f54cd7b0016
Harmen Stoppels <me@harmenstoppels.nl>
2026-02-10 17:29:34 +0100

    py-dm-tree: use cmake build system
    With pip calling setup.py it's impossible to pass options other than
    patching. And we should pass it a path to abseil etc, otherwise it
    downloads and compiles it on the fly.
    
    So, just make it a CMakePackage, and add a missing install step.
    
    Signed-off-by: Harmen Stoppels <me@harmenstoppels.nl>

--- repos/spack_repo/builtin/packages/py_dm_tree/package.py
+++ repos/spack_repo/builtin/packages/py_dm_tree/package.py
@@ -4,12 +4,14 @@
 
 import tempfile
 
+from spack_repo.builtin.build_systems import cmake, python
+from spack_repo.builtin.build_systems.cmake import CMakePackage
 from spack_repo.builtin.build_systems.python import PythonPackage
 
 from spack.package import *
 
 
-class PyDmTree(PythonPackage):
+class PyDmTree(CMakePackage, PythonPackage):
     """tree is a library for working with nested data structures. In a
     way, tree generalizes the builtin map() function which only
     supports flat sequences, and allows to apply a function to each
@@ -23,43 +25,52 @@ class PyDmTree(PythonPackage):
     license("Apache-2.0")
 
     version("0.1.9", sha256="a4c7db3d3935a5a2d5e4b383fc26c6b0cd6f78c6d4605d3e7b518800ecd5342b")
-    version("0.1.8", sha256="0fcaabbb14e7980377439e7140bd05552739ca5e515ecb3119f234acee4b9430")
-    version("0.1.7", sha256="30fec8aca5b92823c0e796a2f33b875b4dccd470b57e91e6c542405c5f77fd2a")
 
-    depends_on("cxx", type="build")
+    # horrible build system
+    with default_args(deprecated=True):
+        version("0.1.8", sha256="0fcaabbb14e7980377439e7140bd05552739ca5e515ecb3119f234acee4b9430")
+        version("0.1.7", sha256="30fec8aca5b92823c0e796a2f33b875b4dccd470b57e91e6c542405c5f77fd2a")
 
-    # Based on PyPI wheel availability
-    depends_on("python@3.10:3.13", when="@0.1.9:", type=("build", "run"))
-    depends_on("python@:3.11", when="@0.1.8", type=("build", "run"))
-    depends_on("python@:3.10", when="@0.1.6:0.1.7", type=("build", "run"))
+    build_system(
+        conditional("python_pip", when="@:0.1.8"),
+        conditional("cmake", when="@0.1.9:"),  # it's hard to pass cmake args through setup.py.
+        default="cmake",
+    )
 
-    depends_on("python@3.10:", type=("build", "run"), when="@0.1.9:")
-    depends_on("py-setuptools", type="build")
-    depends_on("cmake@3.24:", when="@0.1.9:", type="build")
-    depends_on("cmake@3.12:", when="@0.1.7:", type="build")
-    depends_on("py-pybind11@2.10.1:", when="@0.1.8:")
-    depends_on("abseil-cpp cxxstd=14", when="@0.1.8:")
-    depends_on("py-absl-py@0.6.1:", type=("build", "run"), when="@0.1.9:")
-    depends_on("py-attrs@18.2.0:", type=("build", "run"), when="@0.1.9:")
-    with default_args(type=("build", "run"), when="@0.1.9:"):
-        depends_on("py-numpy@1.21:")
-        depends_on("py-numpy@1.21.2:", when="^python@3.10:")
-        depends_on("py-numpy@1.23.3:", when="^python@3.11:")
-        depends_on("py-numpy@1.26.0:", when="^python@3.12:")
-        depends_on("py-numpy@2.1.0:", when="^python@3.13:")
-    depends_on("py-wrapt@1.11.2:", type=("build", "run"), when="@0.1.9:")
+    extends("python", when="build_system=cmake")
+
+    with default_args(type="build"):
+        depends_on("cxx")
+        depends_on("py-setuptools", when="@:0.1.8")
+        depends_on("cmake@3.24:", when="@0.1.9:")
+        depends_on("cmake@3.12:", when="@0.1.7:")
+        depends_on("py-pybind11@2.10.1:", when="@0.1.8:")
+
+    with default_args(type=("build", "run")):
+        depends_on("python@3.10:", when="@0.1.9:")
+        # Based on PyPI wheel availability
+        depends_on("python@:3.13", when="@:0.1.9")
+        depends_on("python@:3.11", when="@:0.1.8")
+        depends_on("python@:3.10", when="@:0.1.7")
+
+    depends_on("abseil-cpp cxxstd=14", when="@0.1.8:", type="link")
+    depends_on("py-attrs@18.2.0:", type="run", when="@0.1.9:")
+    depends_on("py-wrapt@1.11.2:", type="run", when="@0.1.9:")
 
     patch(
         "https://github.com/google-deepmind/tree/commit/63f25d4e05440ccbd4ba662be5f3f6eb460d29d8.patch?full_index=1",
         sha256="77dbd895611d412da99a5afbf312c3c49984ad02bd0e56ad342b2002a87d789c",
         when="@0.1.8",
     )
+    # Add missing an install(...) to CMakeLists.txt
+    patch("add-cmake-install.patch", when="@0.1.9")
+
     conflicts("%gcc@13:", when="@:0.1.7")
 
     # This is set later
     tmp_path = None
 
-    @run_after("install")
+    @run_after("install", when="@:0.1.8")
     def clean(self):
         remove_linked_tree(PyDmTree.tmp_path)
 
@@ -69,6 +80,7 @@ class PyDmTree(PythonPackage):
         else:
             return super().url_for_version(version)
 
+    @when("@:0.1.8")
     def patch(self):
         PyDmTree.tmp_path = tempfile.mkdtemp(prefix="spack")
         env["TEST_TMPDIR"] = PyDmTree.tmp_path
@@ -90,3 +102,24 @@ class PyDmTree(PythonPackage):
             "'--action_env', 'PYTHONPATH={0}',\n".format(env["PYTHONPATH"]),
         ]
         filter_file("'build',", " ".join(args), "setup.py")
+
+
+class CMakeBuilder(cmake.CMakeBuilder):
+    def cmake_args(self):
+        return [
+            self.define("USE_SYSTEM_ABSEIL", True),
+            self.define("USE_SYSTEM_PYBIND11", True),
+            self.define("PYTHON_INSTALL_DIR", join_path(python_platlib, "tree")),
+        ]
+
+    @run_before("build")
+    def make_tree_dir(self):
+        mkdir(join_path(self.build_directory, "tree"))
+
+    @property
+    def root_cmakelists_dir(self):
+        return "tree"
+
+
+class PythonPipBuilder(python.PythonPipBuilder):
+    pass
