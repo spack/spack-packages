5914cfeabff7a980c16aecd348c51e78167a9c4b
Harmen Stoppels <me@harmenstoppels.nl>
2026-02-10 10:57:09 +0100

    dont deprecate because dependents need it
    Signed-off-by: Harmen Stoppels <me@harmenstoppels.nl>

--- repos/spack_repo/builtin/packages/curl/package.py
+++ repos/spack_repo/builtin/packages/curl/package.py
@@ -31,17 +31,9 @@ class Curl(NMakePackage, AutotoolsPackage, CMakePackage):
     license("curl")
 
     version("8.18.0", sha256="ffd671a3dad424fb68e113a5b9894c5d1b5e13a88c6bdf0d4af6645123b31faf")
-
-    with default_args(deprecated=True):
-        version(
-            "8.17.0", sha256="230032528ce5f85594d4f3eace63364c4244ccc3c801b7f8db1982722f2761f4"
-        )
-        version(
-            "8.15.0", sha256="699a6d2192322792c88088576cff5fe188452e6ea71e82ca74409f07ecc62563"
-        )
-        version(
-            "8.14.1", sha256="5760ed3c1a6aac68793fc502114f35c3e088e8cd5c084c2d044abdf646ee48fb"
-        )
+    version("8.17.0", sha256="230032528ce5f85594d4f3eace63364c4244ccc3c801b7f8db1982722f2761f4")
+    version("8.15.0", sha256="699a6d2192322792c88088576cff5fe188452e6ea71e82ca74409f07ecc62563")
+    version("8.14.1", sha256="5760ed3c1a6aac68793fc502114f35c3e088e8cd5c084c2d044abdf646ee48fb")
 
     # TODO: add dependencies for other possible TLS backends
 
