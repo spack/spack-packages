9267ab938de09db835ca4bf8172750342957e662
mohd-afeef-badri <afeef.badri@gmail.com>
2026-02-16 12:24:14 +0100

    update: add 4.15 version as defualt 4.10 was from 2021

--- repos/spack_repo/builtin/packages/freefem/package.py
+++ repos/spack_repo/builtin/packages/freefem/package.py
@@ -16,7 +16,7 @@ class Freefem(AutotoolsPackage):
     """
 
     homepage = "https://freefem.org"
-    url = "https://github.com/FreeFem/FreeFem-sources/archive/refs/tags/v4.10.tar.gz"
+    url = "https://github.com/FreeFem/FreeFem-sources/archive/refs/tags/v4.15.tar.gz"
     git = "https://github.com/FreeFem/FreeFem-sources.git"
 
     maintainers("corentin-dev")
@@ -24,6 +24,7 @@ class Freefem(AutotoolsPackage):
     license("LGPL-3.0-only", checked_by="mohd-afeef-badri")
 
     version("develop", branch="develop")
+    version("4.15", sha256="a47af5a7c7006ae8c648845e55d732ea403837af869493000a008b2013c698e3")
     version("4.14", sha256="931cbfe9ef6f6530756c300c5ae47bfdaca21c560a5407cb33325a376a3b6af8")
     version("4.13", sha256="aefd4ff02333209f7433abef2e74acb621b6946063ff27e81cf2da43120b6ae4")
     version("4.12", sha256="291c5f46761711d6303914f9c4f165fd85a7b7b69141f7473e0b6484ce6ab0f5")
