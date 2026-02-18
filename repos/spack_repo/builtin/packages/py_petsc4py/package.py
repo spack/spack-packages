2f78e1fa060a2b15d646f38a2f676f841f031c88
Adam J. Stewart <ajstewart426@gmail.com>
2026-02-18 22:10:31 +0800

    py-petsc4py: fixed in next release
    Signed-off-by: Adam J. Stewart <ajstewart426@gmail.com>

--- repos/spack_repo/builtin/packages/py_petsc4py/package.py
+++ repos/spack_repo/builtin/packages/py_petsc4py/package.py
@@ -109,8 +109,8 @@ class PyPetsc4py(PythonPackage):
     depends_on("py-numpy", type=("build", "run"))
     depends_on("py-mpi4py", when="+mpi", type=("build", "run"))
 
-    # https://github.com/pypa/distutils/pull/335
-    depends_on("py-setuptools@:80", type="build")
+    # https://gitlab.com/petsc/petsc/-/merge_requests/9016
+    depends_on("py-setuptools@:80", when="@:3.24.4", type="build")
 
     depends_on("petsc+mpi", when="+mpi")
     depends_on("petsc~mpi", when="~mpi")
