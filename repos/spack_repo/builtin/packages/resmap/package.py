0f5dc993f3f3bfcd42c0bbe9f8c30be560231e39
Daniele Colombo <68348680+dacolombo@users.noreply.github.com>
2026-02-19 15:53:05 +0100

    resmap: style fix

--- repos/spack_repo/builtin/packages/resmap/package.py
+++ repos/spack_repo/builtin/packages/resmap/package.py
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 from spack.package import *
 
+
 class Resmap(Package):
     """ResMap (Resolution Map) is an easy to use software package for computing the local resolution of
     3D density maps studied in structural biology, primarily electron cryo-microscopy (cryo-EM)."""
@@ -10,7 +11,11 @@ class Resmap(Package):
     homepage = "https://resmap.sourceforge.net/"
     url = "https://sourceforge.net/projects/resmap/files/ResMap-1.1.4-linux64/download"
 
-    version("1.1.4", sha256="e85c8c535acc284def560d78aaf67dac4a16a1c4c7b46b341e99a3fbe761bd18", expand=False)
+    version(
+        "1.1.4",
+        sha256="e85c8c535acc284def560d78aaf67dac4a16a1c4c7b46b341e99a3fbe761bd18",
+        expand=False,
+    )
 
     def install(self, spec, prefix):
         mkdirp(prefix.bin)
