46bef50947f91ab898808dce2d598762ffe783b5
Jen Herting <jen@herting.cc>
2026-02-23 13:11:10 -0500

    [py-bibtexparser]
    - updated copyright
    - added import for PythonPackage
    - added version 1.4.4

--- repos/spack_repo/builtin/packages/py_bibtexparser/package.py
+++ repos/spack_repo/builtin/packages/py_bibtexparser/package.py
@@ -1,7 +1,8 @@
-# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
-# Spack Project Developers. See the top-level COPYRIGHT file for details.
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack_repo.builtin.build_systems.python import PythonPackage
 
 from spack.package import *
 
@@ -12,6 +13,7 @@ class PyBibtexparser(PythonPackage):
     homepage = "https://github.com/sciunto-org/python-bibtexparser"
     pypi = "bibtexparser/bibtexparser-1.4.1.tar.gz"
 
+    version("1.4.4", sha256="093b6c824f7a71d3a748867c4057b71f77c55b8dbc07efc993b781771520d8fb")
     version("1.4.1", sha256="e00e29e24676c4808e0b4333b37bb55cca9cbb7871a56f63058509281588d789")
 
     depends_on("py-setuptools", type="build")
