da1e1e82d8a5a15dd7a18063d762cefd4e0e7df5
LydDeb <lyderic.de@gmail.com>
2026-02-24 16:14:46 +0100

    py-trame-common: add version 1.1.1

--- repos/spack_repo/builtin/packages/py_trame_common/package.py
+++ repos/spack_repo/builtin/packages/py_trame_common/package.py
@@ -17,6 +17,7 @@ class PyTrameCommon(PythonPackage):
 
     license("Apache-2.0", checked_by="LydDeb")
 
+    version("1.1.1", sha256="6254970b75700510c58265e90fd38ba852b99c0e71293d24eed54819902bb01c")
     version("1.1.0", sha256="86280a72453571d91843f6a0cd8d0dbf283ddade6230915ccd244835d6bfa245")
 
     depends_on("python@3.9:", type=("build", "run"))
