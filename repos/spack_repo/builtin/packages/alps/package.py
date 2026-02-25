1bcb17c6543625fdbfebd57eca7dc014681185ee
Fei Lin <feilin.physics@gmail.com>
2026-02-24 21:01:03 -0500

    update alps version
    update alps version

--- repos/spack_repo/builtin/packages/alps/package.py
+++ repos/spack_repo/builtin/packages/alps/package.py
@@ -2,10 +2,9 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-from spack_repo.builtin.build_systems.cmake import CMakePackage
-
-from spack.package import *
 import os
+from spack_repo.builtin.build_systems.cmake import CMakePackage
+from spack.package import *
 
 class Alps(CMakePackage):
     """
