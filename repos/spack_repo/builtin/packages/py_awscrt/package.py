94838836cffc3f00bf6870fab4c542a1ca605a7e
Evan Parker <eparker@ucar.edu>
2026-02-25 16:13:03 -0700

    Package py-awscrt add version refs.

--- repos/spack_repo/builtin/packages/py_awscrt/package.py
+++ repos/spack_repo/builtin/packages/py_awscrt/package.py
@@ -17,6 +17,8 @@ class PyAwscrt(PythonPackage):
 
     license("Apache-2.0")
 
+    version("0.31.2", sha256="552555de1beff02d72a1f6d384cd49c5a7c283418310eae29d21bcb749c65792")
+    version("0.29.2", sha256="c78d81b1308d42fda1eb21d27fcf26579137b821043e528550f2cfc6c09ab9ff")
     version("0.20.9", sha256="243785ac9ee64945e0479c2384325545f29597575743ce84c371556d1014e63e")
     version("0.19.19", sha256="1c1511535dee146a6c26a382ed3ead56259a105b3b7d7d823553ae567d038dfe")
     version("0.19.18", sha256="350b6efd8ebee082ea3f3e52c59a3c3ec594cdaf01db8b4853dceb9fec90c89d")
