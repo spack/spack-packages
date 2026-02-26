b2bf2e44ad1dc7251fd83cec7135906fbf617548
Gaurav Harsha <gauravharsha05@gmail.com>
2026-02-26 16:56:24 -0500

    fix comma in multiline python statement

--- repos/spack_repo/builtin/packages/green_mbpt/package.py
+++ repos/spack_repo/builtin/packages/green_mbpt/package.py
@@ -26,7 +26,7 @@ class GreenMbpt(CMakePackage, CudaPackage):
     version(
         "0.3.0",
         sha256="181873fa442831d21662cf38c15f30ae97d89c07c5256d45232774a5c072574d",
-        deprecated=True
+        deprecated=True,
     )  # Known issues, use v0.3.1+
 
     # Variant for CUDA Kernels
