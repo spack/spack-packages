82e31061b409d4898a42f60a1e8e41be2bfd6f58
Lydéric Debusschère <lyderic.de@gmail.com>
2026-02-27 17:35:26 +0100

    Apply suggestion from @RMeli
    Co-authored-by: Rocco Meli <r.meli@bluemail.ch>

--- repos/spack_repo/builtin/packages/py_pandera/package.py
+++ repos/spack_repo/builtin/packages/py_pandera/package.py
@@ -12,7 +12,8 @@ class PyPandera(PythonPackage):
     A light-weight and flexible data validation and testing tool for statistical data objects.
     """
 
-    homepage = "https://github.com/pandera-dev/pandera"
+    homepage = "https://www.union.ai/pandera"
+    git = "https://github.com/unionai-oss/pandera.git"
     pypi = "pandera/pandera-0.24.0.tar.gz"
 
     maintainers("LydDeb")
