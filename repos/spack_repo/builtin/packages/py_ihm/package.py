6b86e5c26146b8eca502697fb0948bf394763a9c
LydDeb <lyderic.de@gmail.com>
2026-02-27 17:26:37 +0100

    py_ihm: add version 2.9

--- repos/spack_repo/builtin/packages/py_ihm/package.py
+++ repos/spack_repo/builtin/packages/py_ihm/package.py
@@ -18,6 +18,7 @@ class PyIhm(PythonPackage):
 
     license("MIT", checked_by="LydDeb")
 
+    version("2.9", sha256="2efc460217c66b4d359c1eb7509ffa417568c2e59e04c0e2609995db99fc937f")
     version("2.7", sha256="a3b9eba5545de1e07a15d110e9e6b70369807798d8f2c45908323db2b6fde82c")
 
     depends_on("c", type="build")
