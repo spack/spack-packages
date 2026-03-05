f56650a4eb200a9eec9ac5b594ebdda319c1377b
Doug Torrance <dtorrance@piedmont.edu>
2026-03-05 09:58:12 -0500

    4ti2: add version 1.6.15

--- repos/spack_repo/builtin/packages/_4ti2/package.py
+++ repos/spack_repo/builtin/packages/_4ti2/package.py
@@ -20,6 +20,7 @@ class _4ti2(AutotoolsPackage):
 
     license("GPL-2.0-or-later", checked_by="d-torrance")
 
+    version("1.6.15", sha256="070e639398fda1a4665b3291e5ea80f2ba280d9bffd50656ad8482d471b96965")
     version("1.6.14", sha256="1bc340173f93ca4abd30ea962118bd5057fdedf7e79c71d2a0c4cc9569f8b0b1")
     version("1.6.13", sha256="f59e1ea5563d2188b0e8ff61a8584845a899e3e54a570305f6f99b26c9b1e6b5")
 
