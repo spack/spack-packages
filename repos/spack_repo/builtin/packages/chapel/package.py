4a878dca6de86beedcef73cb0f8643369930328c
Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>
2026-03-06 16:48:32 -0500

    Add 2.8, deprecate 2.5
    Signed-off-by: Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>

--- repos/spack_repo/builtin/packages/chapel/package.py
+++ repos/spack_repo/builtin/packages/chapel/package.py
@@ -63,11 +63,17 @@ class Chapel(AutotoolsPackage, CudaPackage, ROCmPackage):
 
     version("main", branch="main")
 
+    version(
+        "2.8.0",
+        url="https://chapel-lang.org/tmp/chapel-2.8.0.tar.gz",
+        sha256="3e5e8da6c56"
+    )
+
     version("2.7.0", sha256="5e3269babdae334c80fc3f25114698fdfe53e84ea06626af22d2b54eeb75bee6")
     version("2.6.0", sha256="e469c35be601cf1f59af542ab885e8a14aa2b087b79af0d5372a4421976c74b6")
-    version("2.5.0", sha256="020220ca9bf52b9f416e9a029bdc465bb1f635c1e274c6ca3c18d1f83e41fce1")
 
     with default_args(deprecated=True):
+        version("2.5.0", sha256="020220ca9bf52b9f416e9a029bdc465bb1f635c1e274c6ca3c18d1f83e41fce1")
         version("2.4.0", sha256="a51a472488290df12d1657db2e7118ab519743094f33650f910d92b54c56f315")
         version("2.3.0", sha256="0185970388aef1f1fae2a031edf060d5eac4eb6e6b1089e7e3b15a130edd8a31")
         version("2.2.0", sha256="bb16952a87127028031fd2b56781bea01ab4de7c3466f7b6a378c4d8895754b6")
