908ea0a56ded23ac592a085ed33c9c83c03cca74
Abhishek Purandare <abhishek.purandare@inria.fr>
2026-03-06 14:19:00 +0100

    new package: py-validate-pyproject

---
+++ repos/spack_repo/builtin/packages/py_validate_pyproject/package.py
@@ -0,0 +1,41 @@
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack_repo.builtin.build_systems.python import PythonPackage
+
+from spack.package import *
+
+
+class PyValidatePyproject(PythonPackage):
+    """Validation library and CLI tool for checking pyproject.toml files
+    using JSON Schema definitions."""
+
+    homepage = "https://github.com/abravalheri/validate-pyproject"
+    pypi = "validate-pyproject/validate_pyproject-0.25.tar.gz"
+
+    # FIXME: Add a list of GitHub accounts to
+    # notify when the package is updated.
+    # maintainers("github_user1", "github_user2")
+
+    # FIXME: Add the SPDX identifier of the project's license below.
+    # See https://spdx.org/licenses/ for a list. Upon manually verifying
+    # the license, set checked_by to your Github username.
+    license("MPL-2.0", checked_by="abhishek1297")
+
+    version("0.25", sha256="e68c12d1cb0d8ddc269ffc42875a81727ddb7865000aa6d2f77d833b55c53f0b")
+
+    depends_on("python@3.8:", type=("build", "run"))
+    depends_on("py-setuptools@61.2:", type="build")
+    depends_on("py-setuptools-scm+toml@7.1:", type="build")
+    depends_on("py-wheel", type="build")
+
+    # runtime deps
+    depends_on("py-fastjsonschema@2.16.2:3", type=("build", "run"))
+    depends_on("py-packaging@24.2:", type=("build", "run"))
+    depends_on("py-tomli", type=("build", "run"))
+    depends_on("py-tomli@1.2.1:", when="^python@3.11", type=("build", "run"))
+    depends_on("py-trove-classifiers@2021.10.20:", type=("build", "run"))
+
+    # FIXME: test deps to be added (optional)
+    # depends_on("py-pytest", type="test")
