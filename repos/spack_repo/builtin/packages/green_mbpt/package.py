1fc75e69eb8b32b80e25f5fba10a45659ac77a20
Gaurav Harsha <gauravharsha05@gmail.com>
2026-03-07 17:13:59 -0500

    typo in cmake args. green-gpu uses "CUDA_ARCHS" to build

--- repos/spack_repo/builtin/packages/green_mbpt/package.py
+++ repos/spack_repo/builtin/packages/green_mbpt/package.py
@@ -61,7 +61,7 @@ class GreenMbpt(CMakePackage, CudaPackage):
         args.append(self.define("CMAKE_CXX_COMPILER", mpi.mpicxx))
         if "+cuda" in self.spec:
             args.append(self.define("CUSTOM_KERNELS", "https://github.com/Green-Phys/green-gpu"))
-            args.append(self.define("GPU_ARCH", self.spec.variants["cuda_arch"].value[0]))
+            args.append(self.define("GPU_ARCHS", self.spec.variants["cuda_arch"].value[0]))
         return args
 
     def setup_run_environment(self, env):
