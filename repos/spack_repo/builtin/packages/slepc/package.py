ad52f8e8fd1d4a44b1611ff3edbd17ca6887a1e7
Jose E. Roman <jroman@dsic.upv.es>
2026-03-09 10:36:28 +0100

    New patch release SLEPc 3.24.3

--- repos/spack_repo/builtin/packages/slepc/package.py
+++ repos/spack_repo/builtin/packages/slepc/package.py
@@ -25,6 +25,7 @@ class Slepc(Package, CudaPackage, ROCmPackage):
     test_requires_compiler = True
 
     version("main", branch="main")
+    version("3.24.3", sha256="3f13421f3fcd68fd720a143088506e0f91e24243844703997597eee793225452")
     version("3.24.2", sha256="6f1f7e45b9bbd15631562f193284832ae4e9655eb3af7f1ba59bdf8bdaefb638")
     version("3.24.1", sha256="b07e1c335eb620dfc50a2b8d4fb12db03c6929ae624f0338ff8acf879a072abf")
     version("3.24.0", sha256="6e2d14c98aa9138ac698a2a04a7c6a9f9569988f570b2cfbe4935d32364cb4e9")
