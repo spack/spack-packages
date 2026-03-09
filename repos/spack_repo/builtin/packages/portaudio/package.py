f0e1835a41edfdb2b4e657e0b3c131696016b79c
yizeyi18 <18586016708@163.com>
2026-03-09 17:15:50 +0800

    fix one more black error. Why local `spack style --fix` don't work?

--- repos/spack_repo/builtin/packages/portaudio/package.py
+++ repos/spack_repo/builtin/packages/portaudio/package.py
@@ -10,7 +10,7 @@ from spack.package import *
 class Portaudio(AutotoolsPackage):
     """PortAudio is a free, cross-platform, open-source, audio I/O library.  It lets you write
     simple audio programs in 'C' or C++ that will compile and run on many platforms including
-    Windows, Macintosh OS X, and Unix (OSS/ALSA). """
+    Windows, Macintosh OS X, and Unix (OSS/ALSA)."""
 
     homepage = "https://www.portaudio.com/"
     url = "https://github.com/PortAudio/portaudio/archive/refs/tags/v19.7.0.tar.gz"
