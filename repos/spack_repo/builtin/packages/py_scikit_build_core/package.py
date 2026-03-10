27873898111232217d04064a5e9aa150cc3e5a2e
Abhishek Purandare <abhishek.purandare@inria.fr>
2026-03-10 09:48:56 +0100

    minor fix

--- repos/spack_repo/builtin/packages/py_scikit_build_core/package.py
+++ repos/spack_repo/builtin/packages/py_scikit_build_core/package.py
@@ -61,7 +61,6 @@ class PyScikitBuildCore(PythonPackage):
     depends_on("py-tomli@1.2.2:", when="@0.9: ^python@:3.10", type=("build", "run"))
     depends_on("py-tomli@1.1:", when="^python@:3.10", type=("build", "run"))
     depends_on("py-typing-extensions@4:", when="@0.12.2: ^python@:3.10", type=("build", "run"))
-    depends_on("py-typing-extensions@3.10:", when="@0.11: ^python@:3.10", type=("build", "run"))
     depends_on("py-typing-extensions@3.10:", when="@0.8: ^python@:3.8", type=("build", "run"))
     depends_on("py-typing-extensions@3.10:", when="@:0.7 ^python@:3.7", type=("build", "run"))
     depends_on("cmake@3.15:", type=("build", "run"))
