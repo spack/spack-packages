77fabcb9823de0b3caa9ca5b387e4535f00e0fa8
Richard Berger <rberger@lanl.gov>
2026-03-11 14:54:59 -0600

    kokkos: allow other LLVM-based compilers for ROCm
    Instead of always forcing hipcc, allow kokkos to be compiled directly with
    either llvm or llvm-amdgpu (rocmcc).

--- repos/spack_repo/builtin/packages/kokkos_kernels/package.py
+++ repos/spack_repo/builtin/packages/kokkos_kernels/package.py
@@ -223,7 +223,11 @@ class KokkosKernels(CMakePackage, CudaPackage):
         ]
 
         options.append(self.define("Kokkos_ROOT", spec["kokkos"].prefix))
-        if spec.satisfies("^kokkos+rocm"):
+        if spec.satisfies("^kokkos+rocm") and not (
+            spec.satisfies("^kokkos %cxx=clang")
+            or spec.satisfies("^kokkos %cxx=cce")
+            or spec.satisfies("^kokkos %cxx=rocmcc")
+        ):
             options.append(self.define("CMAKE_CXX_COMPILER", spec["hip"].hipcc))
         else:
             options.append(self.define("CMAKE_CXX_COMPILER", self["kokkos"].kokkos_cxx))
