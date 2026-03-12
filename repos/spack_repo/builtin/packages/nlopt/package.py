fd9be89dd6095f4f50344d99f64714e6f05c131f
chapman39 <chapman39@llnl.gov>
2026-03-12 15:56:53 -0700

    nlopt: set python cmake option based on variant

--- repos/spack_repo/builtin/packages/nlopt/package.py
+++ repos/spack_repo/builtin/packages/nlopt/package.py
@@ -60,6 +60,7 @@ class Nlopt(CMakePackage):
             self.define_from_variant("BUILD_SHARED_LIBS", "shared"),
             self.define_from_variant("NLOPT_OCTAVE", "octave"),
             self.define_from_variant("NLOPT_CXX", "cxx"),
+            self.define_from_variant("NLOPT_PYTHON", "python"),
         ]
 
         if spec.satisfies("+matlab"):
