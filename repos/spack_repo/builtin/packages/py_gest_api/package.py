574403d34e3b49f2cec78a11c73a81cbf953c60b
shudson <shudson@anl.gov>
2026-03-12 13:20:05 -0500

    Add gest_api licence

--- repos/spack_repo/builtin/packages/py_gest_api/package.py
+++ repos/spack_repo/builtin/packages/py_gest_api/package.py
@@ -14,7 +14,8 @@ class PyGestApi(PythonPackage):
     pypi = "gest-api/gest_api-0.1.tar.gz"
     git = "https://github.com/campa-consortium/gest-api"
 
-    maintainers("shuds13", "jlnav")
+    maintainers("shuds13", "jlnav", "RemiLehe")
+    license("BSD-3-Clause")
 
     version("0.1", sha256="c5712721072fab8fdef7e976d4140db99729245f34ff36eefc0737c5197d25a8")
 
