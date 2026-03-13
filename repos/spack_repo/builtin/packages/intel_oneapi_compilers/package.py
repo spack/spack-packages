39c0ab2a476aa96b5d03bdbbd8b125c9c32d2f2b
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2026-03-13 09:36:12 +0100

    intel-oneapi: constrain gcc-runtime to gcc version
    Signed-off-by: Massimiliano Culpo <massimiliano.culpo@gmail.com>

--- repos/spack_repo/builtin/packages/intel_oneapi_compilers/package.py
+++ repos/spack_repo/builtin/packages/intel_oneapi_compilers/package.py
@@ -758,6 +758,14 @@ class IntelOneapiCompilers(IntelOneApiPackage, CompilerPackage):
             f"@{spec.versions}", when=f"%[deptypes=build] {spec.name}@{spec.versions}"
         )
 
+        # If the compiler depends on gcc@X.Y, the runtime must depend on gcc-runtime@X.Y
+        if spec.satisfies("%gcc"):
+            gcc = spec["gcc"]
+            pkg("intel-oneapi-runtime").requires(
+                f"@{spec.versions} %gcc-runtime@{gcc.version}",
+                when=f"%[deptypes=build] {spec.name}/{spec.dag_hash()}",
+            )
+
         # If a node used %intel-oneapi-runtime@X.Y its dependencies must use @:X.Y
         # (technically @:X is broader than ... <= @=X but this should work in practice)
         pkg("*").propagate(
