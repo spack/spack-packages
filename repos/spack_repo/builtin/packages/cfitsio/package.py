c66e0599aaa900d3855d526c9fd5a35e94997573
bnikolic <bojan@bnikolic.co.uk>
2026-03-13 10:44:30 +0000

    Add location of the git repository
    Co-authored-by: Richarda Butler <39577672+RikkiButler20@users.noreply.github.com>

--- repos/spack_repo/builtin/packages/cfitsio/package.py
+++ repos/spack_repo/builtin/packages/cfitsio/package.py
@@ -14,6 +14,7 @@ class Cfitsio(AutotoolsPackage):
 
     homepage = "https://heasarc.gsfc.nasa.gov/fitsio/"
     url = "https://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio-3.49.tar.gz"
+git = "https://github.com/HEASARC/cfitsio.git"
 
     license("custom")
 
