739a1ceba917c1bfb375da7dbeff8a9f80a08d4d
Kilian Schnelle <57074573+KiSchnelle@users.noreply.github.com>
2026-03-19 15:25:11 +0000

    fix import style

--- repos/spack_repo/builtin/packages/aretomo3/package.py
+++ repos/spack_repo/builtin/packages/aretomo3/package.py
@@ -2,9 +2,10 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-from spack.package import *
 from spack_repo.builtin.build_systems.cuda import CudaPackage
 from spack_repo.builtin.build_systems.makefile import MakefilePackage
+
+from spack.package import *
 
 
 class Aretomo3(MakefilePackage, CudaPackage):
