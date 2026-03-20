e839df75d14f18cdd67099464a14c96f5c7b311f
Lina Muryanto <muryanto1@llnl.gov>
2026-03-20 11:18:27 -0700

    removed import of cached_cmake

--- repos/spack_repo/builtin/packages/samrai/package.py
+++ repos/spack_repo/builtin/packages/samrai/package.py
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 
-from spack_repo.builtin.build_systems import autotools, cached_cmake
+from spack_repo.builtin.build_systems import autotools
 from spack_repo.builtin.build_systems.autotools import AutotoolsPackage
 from spack_repo.builtin.build_systems.cached_cmake import (
     CachedCMakeBuilder,
