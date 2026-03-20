bc743986c641bb1405b5d3fb5ebdce84ec3c9533
renjithravindrankannath <94420380+renjithravindrankannath@users.noreply.github.com>
2026-03-20 01:30:40 +0000

    [@spackbot] updating style on behalf of renjithravindrankannath

--- repos/spack_repo/builtin/packages/py_torch/package.py
+++ repos/spack_repo/builtin/packages/py_torch/package.py
@@ -657,12 +657,12 @@ class PyTorch(PythonPackage, CudaPackage, ROCmPackage):
                     "#ifdef __HIP_PLATFORM_AMD__\n"
                     "#ifndef CHECK_NOSPARSE_CONTIGUOUS_CUDA\n"
                     "#define CHECK_NOSPARSE_CONTIGUOUS_CUDA(t) \\\n"
-                    "  TORCH_CHECK(!(t).is_sparse(), \"Expected dense tensor\"); \\\n"
-                    "  TORCH_CHECK((t).is_contiguous(), \"Expected contiguous tensor\")\n"
+                    '  TORCH_CHECK(!(t).is_sparse(), "Expected dense tensor"); \\\n'
+                    '  TORCH_CHECK((t).is_contiguous(), "Expected contiguous tensor")\n'
                     "#endif\n"
                     "#ifndef CHECK_NOSPARSE_LASTCONTIGUOUS_CUDA\n"
                     "#define CHECK_NOSPARSE_LASTCONTIGUOUS_CUDA(t) \\\n"
-                    "  TORCH_CHECK(!(t).is_sparse(), \"Expected dense tensor\"); \\\n"
+                    '  TORCH_CHECK(!(t).is_sparse(), "Expected dense tensor"); \\\n'
                     "  TORCH_CHECK((t).is_contiguous(), "
                     '"Expected last-dim contiguous tensor")\n'
                     "#endif\n"
