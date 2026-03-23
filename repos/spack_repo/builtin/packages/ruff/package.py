1dc6f3e2554eae96f5cae118dc1af2b58610ed8c
John Parent <john.parent@kitware.com>
2026-03-23 16:08:50 -0400

    Ruff: add new package
    Add new pure rust based ruff package
    
    Signed-off-by: John Parent <john.parent@kitware.com>

---
+++ repos/spack_repo/builtin/packages/ruff/package.py
@@ -0,0 +1,50 @@
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack_repo.builtin.build_systems.cargo import CargoPackage
+
+from spack.package import *
+
+
+class Ruff(CargoPackage):
+    """An extremely fast Python linter and code formatter, written in Rust."""
+
+    homepage = "https://docs.astral.sh/ruff"
+    git = "https://github.com/astral-sh/ruff.git"
+    url = "https://github.com/astral-sh/ruff/archive/refs/tags/0.11.11.tar.gz"
+
+    license("MIT")
+    maintainers("adamjstewart")
+
+    build_directory = "crates/ruff"
+
+    version("0.15.7", sha256="370003574c8bde1eef286ece925f33e43be4d3564c8eca8dfb4fb100a1dce797")
+    version("0.14.14", sha256="6a6a952a0b273df14eadd4e5a61a48fcc02fa268d2b258062bf332e6b53d4090")
+    version("0.13.0", sha256="1be5402b5ca6925725fcb73af70a07b515246009d7bbb14f17e7f5adacd8a307")
+    version("0.12.0", sha256="3623e20815ae84254ca5dec780165e89c2f1947c73824167e3a44d41fde74f57")
+    version("0.11.11", sha256="fcd8fdd349559421494b653e53a2fc6441a35e51d2992af035c5e5c84e060702")
+    
+
+    with default_args(type="build"):
+        depends_on("c")
+        depends_on("gmake")
+
+        # Found in Cargo.toml
+        depends_on("rust@1.91:", when="@0.15.0:")
+        depends_on("rust@1.90:", when="@0.14.10:")
+        depends_on("rust@1.89:", when="@0.14.4:")
+        depends_on("rust@1.88:", when="@0.12.0:")
+        depends_on("rust@1.85:", when="@0.11.11:")
+        depends_on("rust@1.84:", when="@0.11.4:")
+        depends_on("rust@1.83:", when="@0.9.8:")
+        depends_on("rust@1.80:", when="@0.7.1:")
+        depends_on("rust@1.76:", when="@0.5.6:")
+        depends_on("rust@1.71:")
+
+    def url_for_version(self, version):
+        v = ""
+        if version < Version("0.5.0"):
+            v = "v"
+        return f"https://github.com/astral-sh/ruff/archive/refs/tags/{v}{version.dotted}.tar.gz"
+        
