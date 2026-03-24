f0c65dae11067ae113de13b6d35525ba4da72726
chapman39 <100869159+chapman39@users.noreply.github.com>
2026-03-24 22:26:46 +0000

    [@spackbot] updating style on behalf of chapman39

--- repos/spack_repo/builtin/packages/trng/package.py
+++ repos/spack_repo/builtin/packages/trng/package.py
@@ -2,9 +2,10 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
+from spack_repo.builtin.build_systems.cmake import CMakePackage
+
 from spack.package import *
 
-from spack_repo.builtin.build_systems.cmake import CMakePackage
 
 class Trng(CMakePackage):
     """Tina's Random Number Generator Library"""
@@ -14,29 +15,17 @@ class Trng(CMakePackage):
 
     maintainers("chapman39")
 
-    version("4.28",
-        commit="691817a4a0331421776e7f00efa9fbe81554da86",
-        submodules=True)
-    version("4.24",
-        commit="a22a32b9a285d1293b74b17b34f81af5dcec6311")
-    version("4.23.1",
-        commit="610f7836610e9e01788f8461fbe83b67544ded7c")
-    version("4.23",
-        commit="22a30c493eff4cb547185fa48275683751df18e0")
-    version("4.22",
-        commit="d5f48c88f23af98a004ed26b4053971ace130ae6")
-    version("4.21",
-        commit="9156b3aecf7543b6ef7b43c8e8719b1ebf5b1ca1")
-    version("4.20",
-        commit="e14edeb978b2bc88fd3a03b5ee6b40b284ca44e3")
-    version("4.19",
-        commit="5e130e97a8019941d58c59dfa221d9d4ba30a00c")
-    version("4.18",
-        commit="5663e53af675c0810e20c4ba3c53940e25a3e334")
-    version("4.17",
-        commit="1c8cbb0ecdc59983d5ec29c514058fdffae1effa")
-    version("4.16",
-        commit="8191fd2729b88deebe094766b8e9144a7fb032f5")
+    version("4.28", commit="691817a4a0331421776e7f00efa9fbe81554da86", submodules=True)
+    version("4.24", commit="a22a32b9a285d1293b74b17b34f81af5dcec6311")
+    version("4.23.1", commit="610f7836610e9e01788f8461fbe83b67544ded7c")
+    version("4.23", commit="22a30c493eff4cb547185fa48275683751df18e0")
+    version("4.22", commit="d5f48c88f23af98a004ed26b4053971ace130ae6")
+    version("4.21", commit="9156b3aecf7543b6ef7b43c8e8719b1ebf5b1ca1")
+    version("4.20", commit="e14edeb978b2bc88fd3a03b5ee6b40b284ca44e3")
+    version("4.19", commit="5e130e97a8019941d58c59dfa221d9d4ba30a00c")
+    version("4.18", commit="5663e53af675c0810e20c4ba3c53940e25a3e334")
+    version("4.17", commit="1c8cbb0ecdc59983d5ec29c514058fdffae1effa")
+    version("4.16", commit="8191fd2729b88deebe094766b8e9144a7fb032f5")
 
     variant("tests", default=True, description="Build TRNG tests")
     variant("examples", default=True, description="Build TRNG examples", when="@4.28:")
