226e84d38f13c0fcb5a4804f8e4d78437e123997
V. Karch <luna.karch05@gmail.com>
2026-03-26 12:22:04 -0400

    Update repos/spack_repo/builtin/packages/megahit/package.py
    Co-authored-by: Massimiliano Culpo <massimiliano.culpo@gmail.com>

--- repos/spack_repo/builtin/packages/megahit/package.py
+++ repos/spack_repo/builtin/packages/megahit/package.py
@@ -19,7 +19,7 @@ class Megahit(CMakePackage):
 
     version("1.2.9", sha256="09026eb07cc4e2d24f58b0a13f7a826ae8bb73da735a47cb1cbe6e4693118852")
 
-    depends_on("zlib", type="build")
+    depends_on("zlib-api")
     depends_on("cmake@2.8:", type="build")
     depends_on("gcc@4.8.4:", type="build")
     depends_on("gzip", type="run")
