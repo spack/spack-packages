06ccaa4917f1c8b4f561e57a40f27e346c2475c0
V. Karch <luna.karch05@gmail.com>
2026-03-26 12:29:15 -0400

    ran spack style

--- repos/spack_repo/builtin/packages/megahit/package.py
+++ repos/spack_repo/builtin/packages/megahit/package.py
@@ -17,10 +17,10 @@ class Megahit(CMakePackage):
     license("GPL-3.0")
 
     version("1.2.9", sha256="09026eb07cc4e2d24f58b0a13f7a826ae8bb73da735a47cb1cbe6e4693118852")
-    
+
     depends_on("c", type="build")
     depends_on("cxx", type="build")
-    
+
     depends_on("zlib-api")
     depends_on("cmake@2.8:", type="build")
     depends_on("gcc@4.8.4:", type="build")
