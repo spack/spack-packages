757aa2eb9951191d83fe5ac8567ca0b9a39bac33
V. Karch <luna.karch05@gmail.com>
2026-03-26 12:23:32 -0400

    Update copyright notice and dependencies in package.py

--- repos/spack_repo/builtin/packages/megahit/package.py
+++ repos/spack_repo/builtin/packages/megahit/package.py
@@ -1,5 +1,4 @@
-# Copyright 2013-2024 Lawrence Livermore National Security, LLC and other
-# Spack Project Developers. See the top-level COPYRIGHT file for details.
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
@@ -18,7 +17,10 @@ class Megahit(CMakePackage):
     license("GPL-3.0")
 
     version("1.2.9", sha256="09026eb07cc4e2d24f58b0a13f7a826ae8bb73da735a47cb1cbe6e4693118852")
-
+    
+    depends_on("c", type="build")
+    depends_on("cxx", type="build")
+    
     depends_on("zlib-api")
     depends_on("cmake@2.8:", type="build")
     depends_on("gcc@4.8.4:", type="build")
