1001cf4536b2c073ea4872677c453a02172ab8c7
Shane Nehring <snehring@iastate.edu>
2026-03-31 15:22:48 -0500

    r-gtable: add v0.3.6
    Signed-off-by: Shane Nehring <snehring@iastate.edu>

--- repos/spack_repo/builtin/packages/r_gtable/package.py
+++ repos/spack_repo/builtin/packages/r_gtable/package.py
@@ -19,7 +19,7 @@ class RGtable(RPackage):
     cran = "gtable"
 
     license("MIT")
-
+    version("0.3.6", sha256="d305a5fa11278b649d2d8edc5288bf28009be888a42be58ff8714018e49de0ef")
     version("0.3.5", sha256="b19fc1a30359945adbab7d4e915fe95523a839c380e34ae705d70b7ebddeea72")
     version("0.3.3", sha256="2f9a58d978e2a487b7fd8841539ea33cf948e55ddf6f7a9bd2dd3362600a7b3a")
     version("0.3.1", sha256="8bd62c5722d5188914d667cabab12991c555f657f4f5ce7b547571ae3aec7cb5")
