aff0a175197a8fce9cda54ea2ee6d54c9d3d2e6d
john bowen <john.bowen42@gmail.com>
2026-04-02 14:32:55 -0700

    add conflicts

--- repos/spack_repo/builtin/packages/raja/package.py
+++ repos/spack_repo/builtin/packages/raja/package.py
@@ -258,6 +258,7 @@ class Raja(CachedCMakePackage, CudaPackage, ROCmPackage):
     )
     conflicts("cxxstd=11", when="@0.14.0:")
     conflicts("cxxstd=14", when="@2025.09.0:")
+    conflicts("cxxstd=17", when="@2026.02:")
     conflicts("+sycl cxxstd=14", when="@2024.07.0:")
 
     depends_on("cxx", type="build")
