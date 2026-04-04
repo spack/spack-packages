38f730eed9ca2828da2072313d66e34209730c4b
Wouter Deconinck <wdconinc@gmail.com>
2026-04-03 19:00:32 -0500

    cry: new pkg

---
+++ repos/spack_repo/builtin/packages/cry/package.py
@@ -0,0 +1,26 @@
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack_repo.builtin.build_systems.makefile import MakefilePackage
+
+from spack.package import *
+
+
+class Cry(MakefilePackage):
+    """CRY — Cosmic-RaY shower generator library.
+
+    A library for generating correlated cosmic-ray particle showers.
+    """
+
+    homepage = "http://nuclear.llnl.gov/simulation"
+    url = "http://nuclear.llnl.gov/simulation/cry_v1.7.tar.gz"
+
+    maintainers("wdconinc")
+
+    version("1.7", sha256="dcee2428f81cba113f82e0c7c42f4d85bff4b8530e5ab5c82c059bed3e570c20")
+
+    def install(self, spec, prefix):
+        install_tree("lib", prefix.lib)
+        install_tree("src", prefix.include.cry)
+        install_tree("data", prefix.share.cry.data)
