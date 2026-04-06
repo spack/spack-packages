aa7534c019a2159943ca2bf560c8a62031c0a85a
Adam J. Stewart <ajstewart426@gmail.com>
2026-04-06 17:46:24 -0400

    ty: add v0.0.28

--- repos/spack_repo/builtin/packages/ty/package.py
+++ repos/spack_repo/builtin/packages/ty/package.py
@@ -21,6 +21,7 @@ class Ty(CargoPackage):
     license("MIT")
     maintainers("adamjstewart")
 
+    version("0.0.28", sha256="1fbde7bc5d154d6f047b570d95665954fa83b75a0dce50d88cf081b40a27ea32")
     version("0.0.26", sha256="a7cdf959700f96784396dd673a7bcd82300a16e337995fbbc6c4842a730ad5e5")
     version("0.0.25", sha256="e22a3f371b260f482fdece3ebd44e42342241661224283e4b7be283df0dfcc3b")
     version("0.0.24", sha256="cddb2c6022e2b96faf289c9a275bf4ee05a3430e922ffe9e123fad6a9542a325")
