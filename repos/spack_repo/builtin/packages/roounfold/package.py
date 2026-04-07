24d5c65f041cbd6d259faba5bfacd84f44d4d725
Wouter Deconinck <wdconinc@gmail.com>
2026-04-07 10:07:05 -0500

    roounfold: fix copyright header

--- repos/spack_repo/builtin/packages/roounfold/package.py
+++ repos/spack_repo/builtin/packages/roounfold/package.py
@@ -1,4 +1,5 @@
-# Copyright Belle2 Collaboration
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
+#
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 from spack_repo.builtin.build_systems.cmake import CMakePackage, generator
