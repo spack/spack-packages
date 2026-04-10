671a220a9160ddfcf7829cbf16faf7f6d39ab12b
Thomas Bouvier <contact@thomas-bouvier.io>
2026-04-10 10:37:31 +0200

    fakredis-py: add v2.35.0

--- repos/spack_repo/builtin/packages/py_fakeredis/package.py
+++ repos/spack_repo/builtin/packages/py_fakeredis/package.py
@@ -12,9 +12,9 @@ class PyFakeredis(PythonPackage):
     """Python implementation of redis API, can be used for testing purposes."""
 
     homepage = "https://github.com/cunla/fakeredis-py"
-    pypi = "fakeredis/fakeredis-2.34.2.tar.gz"
+    pypi = "fakeredis/fakeredis-2.35.0.tar.gz"
 
-    version("2.34.2", sha256="")
+    version("2.35.0", sha256="5d1a0192c2c559e55b2d05328d86282ddd2079c1712a91e6d1b3010e0dd45ca6")
 
     variant("lua", default=False, description="Enable Lua scripting support")
 
