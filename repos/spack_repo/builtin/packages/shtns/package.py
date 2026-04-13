e79a79d789d3977d67b6e019be9eeeac5f70fb8a
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2026-04-13 13:15:34 +0900

    Reformatted package definition

--- repos/spack_repo/builtin/packages/shtns/package.py
+++ repos/spack_repo/builtin/packages/shtns/package.py
@@ -8,7 +8,9 @@ from spack.package import *
 
 
 class Shtns(AutotoolsPackage):
-    """SHTns is a high performance library for Spherical Harmonic Transform written in C, aimed at numerical simulation (fluid flows, mhd, ...) in spherical geometries."""
+    """SHTns is a high performance library for Spherical Harmonic
+    Transform written in C, aimed at numerical simulation (fluid
+    flows, mhd, ...) in spherical geometries."""
 
     homepage = "https://nschaeff.bitbucket.io/shtns/index.html"
 
@@ -20,9 +22,11 @@ class Shtns(AutotoolsPackage):
     # whole repository, not tagged releases. The commit
     # f94e... corresponds to version 3.7.5, the latest version as of
     # this writing (2026-03-04).
-    version("3.7.5", sha256="b9a834e4ae0df5b9df070cb5df22d147614341fd99a788e41d97a29d2f5bf4f2",
-            url = "https://bitbucket.org/nschaeff/shtns/get/f94ecca74a08.zip")
-
+    version(
+        "3.7.5",
+        sha256="b9a834e4ae0df5b9df070cb5df22d147614341fd99a788e41d97a29d2f5bf4f2",
+        url="https://bitbucket.org/nschaeff/shtns/get/f94ecca74a08.zip",
+    )
     depends_on("c", type="build")
 
     depends_on("fftw")
