151f5742341fe2a27bbc0f34a39f22a796d1d920
Simon Pintarelli <simon.pintarelli@cscs.ch>
2026-04-16 14:50:47 +0200

    gtest

--- repos/spack_repo/builtin/packages/costa/package.py
+++ repos/spack_repo/builtin/packages/costa/package.py
@@ -50,7 +50,7 @@ class Costa(CMakePackage):
     depends_on("cxxopts", when="+apps")
     depends_on("cxxopts", when="+tests")
     depends_on("semiprof", when="+profiling")
-    depends_on("googletest", when="+tests")
+    depends_on("googletest", when="@2.3.1: +tests")
 
     def url_for_version(self, version):
         if version == Version("2.0"):
