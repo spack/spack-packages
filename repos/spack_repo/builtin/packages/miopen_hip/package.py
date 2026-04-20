5c168a4f3d7cd9ac313f448d9d4ebf3a7ca99ea6
Sreenivasa Murthy Kolam <sreenivasamurthy.kolam@amd.com>
2026-04-20 12:54:44 +0530

    pass the amdgpu_target to hipblas dependency

--- repos/spack_repo/builtin/packages/miopen_hip/package.py
+++ repos/spack_repo/builtin/packages/miopen_hip/package.py
@@ -182,13 +182,13 @@ class MiopenHip(CMakePackage):
         "7.1.1",
         "7.2.0",
     ]:
-        depends_on(f"hipblas@{ver}", when=f"@{ver}")
         depends_on(f"rocmlir@{ver}", when=f"@{ver}")
         for tgt in itertools.chain(["auto"], amdgpu_targets):
             depends_on(
                 f"hipblaslt@{ver} amdgpu_target={tgt}",
                 when=f"@{ver} +hipblaslt amdgpu_target={tgt}",
             )
+            depends_on(f"hipblas@{ver} amdgpu_target={tgt}", when=f"@{ver} amdgpu_target={tgt}")
 
     depends_on("nlohmann-json", type="link")
     depends_on("googletest", when="@6.1:")
