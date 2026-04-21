ac6d2cbaf45aecde93a22b4f1f55ad5da0db72c5
Jen Herting <jen@herting.cc>
2026-04-21 12:07:17 -0400

    [r-ucsc-utils] new package

---
+++ repos/spack_repo/builtin/packages/r_ucsc_utils/package.py
@@ -0,0 +1,28 @@
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack_repo.builtin.build_systems.r import RPackage
+
+from spack.package import *
+
+
+class RUcscUtils(RPackage):
+    """A set of low-level utilities to retrieve data from the UCSC Genome Browser.
+    Most functions in the package access the data via the UCSC REST API but some of
+    them query the UCSC MySQL server directly. Note that the primary purpose of the
+    package is to support higher-level functionalities implemented in downstream
+    packages like GenomeInfoDb or txdbmaker."""
+
+    bioc = "UCSC.utils"
+
+    license("Artistic-2.0")
+
+    version("1.6.1", commit="625d5544d0de9cb47ea67364136efc1535c67682")
+
+    depends_on("c", type="build")
+
+    with default_args(type=("build", "run")):
+        depends_on("r-httr")
+        depends_on("r-jsonlite")
+        depends_on("r-s4vectors@0.47.6:")
