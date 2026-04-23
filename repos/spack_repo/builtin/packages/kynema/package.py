97b0652d545313747e50a4df53e2863b21422c84
Jon Rood <jon.rood@nrel.gov>
2026-04-23 13:01:52 -0600

    Add Kynema packages.

--- repos/spack_repo/builtin/packages/kynema/package.py
+++ repos/spack_repo/builtin/packages/kynema/package.py
@@ -10,101 +10,134 @@ from spack.package import *
 
 
 class Kynema(CMakePackage, CudaPackage, ROCmPackage):
-    """An open-source performance-oriented structural dynamics simulation code."""
+    """Multi-application driver for Kynema project."""
 
-    license("MIT License", checked_by="ddement")
+    homepage = "https://github.com/Kynema/kynema-driver"
+    git = "https://github.com/Kynema/kynema-driver.git"
 
-    homepage = "https://kynema.github.io/kynema/"
-    url = "https://github.com/kynema/kynema.git"
-    git = "https://github.com/kynema/kynema.git"
+    maintainers("jrood-nrel")
 
-    maintainers("faisal-bhuiyan", "ddement", "deslaughter")
+    tags = ["ecp", "ecp-apps"]
+    submodules = True
+
+    license("Apache-2.0")
 
     version("main", branch="main")
+    version("2.1.0", tag="v2.1.0", commit="b726a1128f02edb72f504130830d44622710225a")
+    version("2.0.0", tag="v2.0.0", commit="d25aa549c7cbd9d6213541cd4b046bd9c0c54652")
+    version("1.2.0", tag="v1.2.0", commit="4c49c7775c580b6bd2556e6c00fd13c08737d5eb")
+    version("1.1.0", tag="v1.1.0", commit="c8823f19fc8d19ea051df0ff68780e56981a7f94")
+    version("1.0.0", tag="v1.0.0", commit="85718893d2510c8a2e8c8e94c768ce6a67f94703")
 
-    variant("tests", default=False, description="Build Kynema Test Suite")
-    variant("openmp", default=False, description="Build Kynema with OpenMP support")
-    variant("vtk", default=False, description="Enable VTK")
-    variant("adi", default=False, description="Build the OpenFAST ADI external project")
-    variant("rosco", default=False, description="Build the ROSCO controller external project")
-    variant("klu", default=True, description="Build with support for the KLU sparse direct solver")
-    variant(
-        "umfpack",
-        default=False,
-        description="Build with support for the UMFPACK sparse direct solver",
-    )
-    variant(
-        "superlu",
-        default=False,
-        description="Build with support for the SuperLU sparse direct solver",
-    )
-    variant(
-        "superlu-mt",
-        default=False,
-        description="Build with support for the SuperLU_MT sparse direct solver",
-    )
-    variant(
-        "mkl",
-        default=False,
-        description="Build with support for the MKL Pardiso sparse direct solver",
-    )
-    variant(
-        "cusolversp",
-        default=True,
-        when="+cuda",
-        description="Build with support for the cuSolverSP sparse direct solver",
-    )
-    variant(
-        "cudss",
-        default=False,
-        when="+cuda",
-        description="Build with support for the cuDSS sparse direct solver",
-    )
+    variant("kynema_sgf_gpu", default=False, description="Enable Kynema-SGF on the GPU")
+    variant("kynema_ugf_gpu", default=False, description="Enable Kynema-UGF on the GPU")
+    variant("sycl", default=False, description="Enable SYCL backend for Kynema-SGF")
+    variant("gpu-aware-mpi", default=False, description="gpu-aware-mpi")
+    variant("kynema-fmb", default=False, description="Couple with Kynema-FMB structural solver")
 
-    depends_on("cxx", type="build")
     depends_on("c", type="build")
-    depends_on("netcdf-c@4.9:")
+    depends_on("cxx", type="build")
+    depends_on("fortran", type="build")
+
+    for arch in CudaPackage.cuda_arch_values:
+        depends_on(
+            "kynema-sgf+cuda cuda_arch=%s" % arch, when="+kynema_sgf_gpu+cuda cuda_arch=%s" % arch
+        )
+        depends_on(
+            "kynema-ugf+cuda cuda_arch=%s" % arch, when="+kynema_ugf_gpu+cuda cuda_arch=%s" % arch
+        )
+        depends_on(
+            "trilinos+cuda cuda_arch=%s" % arch, when="+kynema_ugf_gpu+cuda cuda_arch=%s" % arch
+        )
+
+    for arch in ROCmPackage.amdgpu_targets:
+        depends_on(
+            "kynema-sgf+rocm amdgpu_target=%s" % arch,
+            when="+kynema_sgf_gpu+rocm amdgpu_target=%s" % arch,
+        )
+        depends_on(
+            "kynema-ugf+rocm amdgpu_target=%s" % arch,
+            when="+kynema_ugf_gpu+rocm amdgpu_target=%s" % arch,
+        )
+        depends_on(
+            "trilinos+rocm amdgpu_target=%s" % arch,
+            when="+kynema_ugf_gpu+rocm amdgpu_target=%s" % arch,
+        )
+
+    depends_on("kynema-ugf+hypre+openfast+tioga")
+    depends_on("kynema-sgf+netcdf+mpi+tiny_profile")
+    depends_on("trilinos")
     depends_on("yaml-cpp@0.6:")
-    depends_on("kokkos-kernels")
-    depends_on("lapack")
-    depends_on("eigen")
+    depends_on("tioga~nodegid")
+    depends_on("openfast+cxx@2.6.0:")
+    depends_on("kynema-ugf+kynema-fmb", when="+kynema-fmb")
+    depends_on("kynema-sgf+sycl", when="+kynema_sgf_gpu+sycl")
+    depends_on("kokkos-nvcc-wrapper", type="build", when="+cuda")
+    depends_on("mpi")
+    depends_on("kynema-ugf+gpu-aware-mpi", when="+gpu-aware-mpi")
+    depends_on("kynema-sgf+gpu-aware-mpi", when="+gpu-aware-mpi")
+    depends_on("kynema-ugf@2.3:", when="@2:")
+    depends_on("kynema-sgf@3.5:", when="@2:")
+    depends_on("kynema-ugf@2.0.0:", when="@1.0.0:")
+    depends_on("kynema-sgf@0.9.0:", when="@1.0.0:")
+    depends_on("tioga@1.0.0:", when="@1.0.0:")
 
-    depends_on("kokkos@4.6:")
-    depends_on("kokkos-kernels@4.6:")
-
-    depends_on("kokkos+cuda+wrapper", when="+cuda")
-    depends_on("kokkos+rocm", when="+rocm")
-    depends_on("kokkos~cuda", when="~cuda")
-    depends_on("kokkos~rocm", when="~rocm")
-
-    depends_on("kokkos-kernels+cuda+cublas+cusparse+cusolver", when="+cuda")
-    depends_on("kokkos-kernels+rocblas+rocsparse+rocsolver", when="+rocm")
-    depends_on("kokkos-kernels+openmp", when="+openmp")
-    depends_on("kokkos-kernels~cuda", when="~cuda")
-    depends_on("kokkos-kernels~openmp", when="~openmp")
-
-    depends_on("suite-sparse@7.4:", when="+klu")
-    depends_on("suite-sparse@7.4:", when="+umfpack")
-    depends_on("superlu", when="+superlu")
-    depends_on("superlu-mt", when="+superlu-mt")
-    depends_on("mkl", when="+mkl")
-    depends_on("cudss", when="+cudss")
-
-    depends_on("googletest", when="+tests")
-    depends_on("rosco", when="+rosco")
-    depends_on("openfast", when="+adi")
+    with when("~kynema_sgf_gpu~kynema_ugf_gpu"):
+        conflicts("+cuda")
+        conflicts("+rocm")
+        conflicts("+sycl")
+    with when("~kynema_ugf_gpu"):
+        conflicts("^kynema-ugf+cuda")
+        conflicts("^kynema-ugf+rocm")
+    with when("~kynema_sgf_gpu"):
+        conflicts("^kynema-sgf+cuda")
+        conflicts("^kynema-sgf+rocm")
+        conflicts("^kynema-sgf+sycl")
+    conflicts("+kynema_sgf_gpu", when="~cuda~rocm~sycl")
+    conflicts("+kynema_ugf_gpu", when="~cuda~rocm")
+    conflicts("+kynema_ugf_gpu", when="+sycl")
+    conflicts("^kynema-sgf+hypre", when="~kynema_sgf_gpu+kynema_ugf_gpu")
+    conflicts("^kynema-sgf+hypre", when="+kynema_sgf_gpu~kynema_ugf_gpu")
+    conflicts("+sycl", when="+cuda")
+    conflicts("+rocm", when="+cuda")
+    conflicts("+sycl", when="+rocm")
 
     def cmake_args(self):
-        options = [
-            self.define_from_variant("KYNEMA_FMB_ENABLE_TESTS", "tests"),
-            self.define_from_variant("KYNEMA_FMB_ENABLE_OPENFAST_ADI", "adi"),
-            self.define_from_variant("KYNEMA_FMB_ENABLE_ROSCO_CONTROLLER", "rosco"),
-            self.define_from_variant("KYNEMA_FMB_ENABLE_KLU", "klu"),
-            self.define_from_variant("KYNEMA_FMB_ENABLE_UMFPACK", "umfpack"),
-            self.define_from_variant("KYNEMA_FMB_ENABLE_SUPERLU", "superlu"),
-            self.define_from_variant("KYNEMA_FMB_ENABLE_SUPERLU_MT", "superlu-mt"),
-            self.define_from_variant("KYNEMA_FMB_ENABLE_MKL", "mkl"),
-            self.define_from_variant("KYNEMA_FMB_ENABLE_CUSOLVERSP", "cusolversp"),
-            self.define_from_variant("KYNEMA_FMB_ENABLE_CUDSS", "cudss"),
-        ]
-        return options
+        spec = self.spec
+
+        args = [self.define("MPI_HOME", spec["mpi"].prefix)]
+
+        if spec.satisfies("+cuda"):
+            args.append(self.define("CMAKE_CXX_COMPILER", spec["mpi"].mpicxx))
+            args.append(self.define("CMAKE_C_COMPILER", spec["mpi"].mpicc))
+            args.append(self.define("KYNEMA_ENABLE_CUDA", True))
+            args.append(self.define("CUDAToolkit_ROOT", self.spec["cuda"].prefix))
+            args.append(self.define("KYNEMA_CUDA_ARCH", self.spec.variants["cuda_arch"].value))
+
+        if spec.satisfies("+rocm"):
+            targets = self.spec.variants["amdgpu_target"].value
+            args.append(self.define("KYNEMA_ENABLE_ROCM", True))
+            args.append(self.define("CMAKE_CXX_COMPILER", self.spec["hip"].hipcc))
+            # Optimization to only build one specific target architecture:
+            args.append(self.define("CMAKE_HIP_ARCHITECTURES", ";".join(str(x) for x in targets)))
+            args.append(self.define("AMDGPU_TARGETS", ";".join(str(x) for x in targets)))
+            args.append(self.define("GPU_TARGETS", ";".join(str(x) for x in targets)))
+
+        if spec.satisfies("^kynema-sgf+hdf5"):
+            args.append(self.define("H5Z_ZFP_USE_STATIC_LIBS", True))
+
+        return args
+
+    def setup_build_environment(self, env: EnvironmentModifications) -> None:
+        env.append_flags("CXXFLAGS", "-DUSE_STK_SIMD_NONE")
+        if self.spec.satisfies("+rocm+kynema_sgf_gpu~kynema_ugf_gpu"):
+            # Manually turn off device self.defines to solve Kokkos issues in Kynema-UGF headers
+            env.append_flags("CXXFLAGS", "-U__HIP_DEVICE_COMPILE__ -DDESUL_HIP_RDC")
+        if self.spec.satisfies("+cuda"):
+            env.set("OMPI_CXX", self["kokkos-nvcc-wrapper"].kokkos_cxx)
+            env.set("MPICH_CXX", self["kokkos-nvcc-wrapper"].kokkos_cxx)
+            env.set("MPICXX_CXX", self["kokkos-nvcc-wrapper"].kokkos_cxx)
+        if self.spec.satisfies("+rocm"):
+            env.set("OMPI_CXX", self.spec["hip"].hipcc)
+            env.set("MPICH_CXX", self.spec["hip"].hipcc)
+            env.set("MPICXX_CXX", self.spec["hip"].hipcc)
---
+++ repos/spack_repo/builtin/packages/kynema_sgf/package.py
@@ -0,0 +1,207 @@
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack_repo.builtin.build_systems.cmake import CMakePackage
+from spack_repo.builtin.build_systems.cuda import CudaPackage
+from spack_repo.builtin.build_systems.rocm import ROCmPackage
+
+from spack.package import *
+
+
+class KynemaSgf(CMakePackage, CudaPackage, ROCmPackage):
+    """Kynema-SGF is a massively parallel, block-structured adaptive-mesh,
+    incompressible flow solver for the Kynema project."""
+
+    homepage = "https://github.com/Kynema/kynema-sgf"
+    url = "https://github.com/Kynema/kynema-sgf/archive/refs/tags/v1.3.0.tar.gz"
+    git = "https://github.com/Kynema/kynema-sgf.git"
+
+    maintainers("jrood-nrel")
+
+    tags = ["ecp", "ecp-apps"]
+    submodules = True
+
+    license("BSD-3-Clause")
+
+    version("main", branch="main")
+    version("3.9.1", tag="v3.9.1", commit="c41e3a727ad7ad9fe14df33c2db565b4248dc7ed")
+    version("3.9.0", tag="v3.9.0", commit="6c15368dc93d749fb0c3b216b0ccccaab6907251")
+    version("3.8.1", tag="v3.8.1", commit="464ce0891979dbef9972c5b868facee1c974779d")
+    version("3.8.0", tag="v3.8.0", commit="35d63755ce213caedc1c1e96e93e9a4bfe5bffc3")
+    version("3.7.0", tag="v3.7.0", commit="243e620c263761d34b3ef54694cfd3f787321c24")
+    version("3.6.0", tag="v3.6.0", commit="7346fd2a99e5d66ecf2ef3bafa1ebe621211d21f")
+    version("3.5.0", tag="v3.5.0", commit="412f015f2496cb0c0c802240bb9c11848b408fa0")
+    version("3.4.2", tag="v3.4.2", commit="ed475a0533dfacf1fdff0b707518ccf99040d9f9")
+    version("3.4.1", tag="v3.4.1", commit="effe63ca9061e6d2bd5c5e84b690d29c0869f029")
+    version("3.4.0", tag="v3.4.0", commit="38d1b9fd0b70aab4a01fd507f039750c2508bd1c")
+    version("3.3.1", tag="v3.3.1", commit="30fc9a15a65670ebe3226b85e76b2155b4e4251d")
+    version("3.3.0", tag="v3.3.0", commit="cba28f0fb9d7c5c6d1aeb57e67469c962f9a12d4")
+    version("3.2.3", tag="v3.2.3", commit="a609630eb7e21f6054a40d34865b2e7dd2be4eab")
+    version("3.2.2", tag="v3.2.2", commit="e12ec7b5a973a7e9d57dffdbf9a2875c601bb0cf")
+    version("3.2.1", tag="v3.2.1", commit="2b309bdb578d549eba08cd8bdf852aa85fbb33f2")
+    version("3.2.0", tag="v3.2.0", commit="bb0e9dcc59c694812a03703199ade0a3984161d5")
+    version("3.1.7", tag="v3.1.7", commit="b9858be2c7ae85ad3c78e414699fad4acf2a7a22")
+    version("3.1.6", tag="v3.1.6", commit="ca437affc6fd00490d8b14e244e53bf641207224")
+    version("3.1.5", tag="v3.1.5", commit="554f8aa1ac36c2bae17565c64d5bc33333cee396")
+    version("3.1.4", tag="v3.1.4", commit="e10f5ebd3141b9990a65ebe9f1bdca8554b59472")
+    version("3.1.3", tag="v3.1.3", commit="af8231ace69119133c4c8a906e98946ec5aa79c8")
+    version("3.1.2", tag="v3.1.2", commit="5edcac4496e30e450c0f21e7fa74f8b590dc3860")
+    version("3.1.1", tag="v3.1.1", commit="8ae06194fa47bf473615988f97a7b423d467b023")
+    version("3.1.0", tag="v3.1.0", commit="3e23581b132532bf70b09c38217ff9c46204f047")
+    version("3.0.2", tag="v3.0.2", commit="f867288dffecc6404189afa965189c2558cf9922")
+    version("3.0.1", tag="v3.0.1", commit="65aa85db5cb3bbabc767d5dde4b106b7022a0f90")
+    version("3.0.0", tag="v3.0.0", commit="2fbd345cfa7cb7277c1cb6a1323247579e1bbc32")
+
+    variant("hypre", default=False, description="Enable Hypre integration")
+    variant("ascent", default=False, description="Enable Ascent integration")
+    variant("masa", default=False, description="Enable MASA integration")
+    variant("mpi", default=True, description="Enable MPI support")
+    variant("netcdf", default=False, description="Enable NetCDF support")
+    variant("openfast", default=False, description="Enable OpenFAST integration")
+    variant("kynema-fmb", default=False, description="Enable Kynema integration")
+    variant("openmp", default=False, description="Enable OpenMP for CPU builds")
+    variant("shared", default=True, description="Build shared libraries")
+    variant("tests", default=True, description="Activate regression tests")
+    variant("tiny_profile", default=False, description="Activate tiny profile")
+    variant("hdf5", default=False, description="Enable HDF5 plots with ZFP compression")
+    variant("umpire", default=False, description="Enable UMPIRE memory pooling")
+    variant("sycl", default=False, description="Enable SYCL backend")
+    variant("gpu-aware-mpi", default=False, description="Enable GPU aware MPI")
+    variant("helics", default=False, description="Enable HELICS support for control interface")
+    variant(
+        "waves2amr", default=False, description="Enable Waves2AMR support for ocean wave input"
+    )
+    variant("fft", default=False, description="Enable FFT support for MAC projection")
+
+    depends_on("c", type="build")
+    depends_on("cxx", type="build")
+    depends_on("fortran", type="build", when="+openfast")
+    depends_on("mpi", when="+mpi")
+    depends_on("hdf5~mpi", when="+hdf5~mpi")
+    depends_on("hdf5+mpi", when="+hdf5+mpi")
+    # New versions of HDF5 have CMake problems finding ZLIB::ZLIB target
+    depends_on("hdf5@:1.14.4-3", when="+hdf5")
+    depends_on("h5z-zfp", when="+hdf5")
+    depends_on("zfp", when="+hdf5")
+    depends_on("hypre~int64@2.29.0:", when="+hypre")
+    depends_on("hypre+mpi", when="+hypre+mpi")
+    depends_on("hypre+umpire", when="+hypre+umpire")
+    depends_on("hypre+sycl", when="+hypre+sycl")
+    depends_on("hypre+gpu-aware-mpi", when="+hypre+gpu-aware-mpi")
+    depends_on("masa", when="+masa")
+    depends_on("ascent~mpi", when="+ascent~mpi")
+    depends_on("ascent+mpi", when="+ascent+mpi")
+    depends_on("netcdf-c", when="+netcdf")
+    depends_on("py-netcdf4", when="+netcdf")
+    depends_on("py-numpy@2:", when="+netcdf")
+    depends_on("py-matplotlib", when="+masa")
+    depends_on("py-pandas", when="+masa")
+    depends_on("openfast+cxx@3.5:4.0", when="@:3.5 +openfast")
+    depends_on("openfast+cxx@3.5.0:3.5.9,4.1:", when="@3.6: +openfast")
+    depends_on("openfast+netcdf", when="+openfast+netcdf")
+    depends_on("kynema-fmb", when="+kynema-fmb")
+    depends_on("helics@:3.3.2", when="+helics")
+    depends_on("helics@:3.3.2+mpi", when="+helics+mpi")
+    depends_on("fftw", when="@2.1: +waves2amr")
+    depends_on("fftw", when="@3.3.1: +fft")
+
+    depends_on("rocrand", when="+rocm")
+    depends_on("rocprim", when="+rocm")
+
+    for arch in CudaPackage.cuda_arch_values:
+        depends_on("hypre+cuda cuda_arch=%s" % arch, when="+cuda+hypre cuda_arch=%s" % arch)
+    for arch in ROCmPackage.amdgpu_targets:
+        depends_on(
+            "hypre+rocm amdgpu_target=%s" % arch, when="+rocm+hypre amdgpu_target=%s" % arch
+        )
+    for arch in CudaPackage.cuda_arch_values:
+        depends_on("ascent+cuda cuda_arch=%s" % arch, when="+ascent+cuda cuda_arch=%s" % arch)
+
+    conflicts("+openmp", when="+cuda")
+    conflicts("+shared", when="+cuda")
+    conflicts(
+        "openfast@4.0.0:4.0.3", msg="OpenFAST 4.0.0:4.0.3 contains a bug. Use OpenFAST >= 4.0.4."
+    )
+
+    def setup_build_environment(self, env: EnvironmentModifications) -> None:
+        # Avoid compile errors with Intel interprocedural optimization
+        if self.spec.satisfies("%intel"):
+            env.append_flags("CXXFLAGS", "-no-ipo")
+        if self.spec.satisfies("+cuda"):
+            env.set("CUDAHOSTCXX", spack_cxx)
+
+    def cmake_args(self):
+        define = self.define
+        spec = self.spec
+
+        vs = [
+            "mpi",
+            "cuda",
+            "openmp",
+            "netcdf",
+            "hypre",
+            "masa",
+            "ascent",
+            "openfast",
+            "rocm",
+            "tests",
+            "tiny_profile",
+            "fft",
+            "helics",
+            "umpire",
+            "sycl",
+            "kynema-fmb",
+        ]
+        args = [self.define_from_variant("KYNEMA_SGF_ENABLE_%s" % v.upper(), v) for v in vs]
+
+        args += [self.define_from_variant("BUILD_SHARED_LIBS", "shared")]
+
+        if spec.satisfies("+mpi"):
+            args.append(define("MPI_HOME", spec["mpi"].prefix))
+            args.append(define("MPI_CXX_COMPILER", spec["mpi"].mpicxx))
+            args.append(define("MPI_C_COMPILER", spec["mpi"].mpicc))
+
+        if spec.satisfies("+hdf5"):
+            args.append(define("KYNEMA_SGF_ENABLE_HDF5", True))
+            args.append(define("KYNEMA_SGF_ENABLE_HDF5_ZFP", True))
+            # Help AMReX understand if HDF5 is parallel or not.
+            # Building HDF5 with CMake as Spack does, causes this inspection to break.
+            args.append(define("HDF5_IS_PARALLEL", spec.satisfies("+mpi")))
+
+        if spec.satisfies("+cuda"):
+            args.append(define("CMAKE_CUDA_ARCHITECTURES", spec.variants["cuda_arch"].value))
+
+        if spec.satisfies("+rocm"):
+            args.append(define("CMAKE_CXX_COMPILER", spec["hip"].hipcc))
+            targets = spec.variants["amdgpu_target"].value
+            args.append("-DAMReX_AMD_ARCH=" + ";".join(str(x) for x in targets))
+
+        if spec.satisfies("+umpire"):
+            args.append(define("UMPIRE_DIR", spec["umpire"].prefix))
+
+        if spec.satisfies("+helics"):
+            args.append(define("HELICS_DIR", spec["helics"].prefix))
+
+        if spec.satisfies("+waves2amr"):
+            args.append(self.define_from_variant("KYNEMA_SGF_ENABLE_W2A", "waves2amr"))
+            args.append(define("FFTW_DIR", spec["fftw"].prefix))
+
+        if spec.satisfies("+fft"):
+            args.append(define("FFTW_DIR", spec["fftw"].prefix))
+
+        if spec.satisfies("+sycl"):
+            requires(
+                "%dpcpp",
+                "%oneapi",
+                policy="one_of",
+                msg=(
+                    "AMReX's SYCL GPU Backend requires DPC++ (dpcpp) "
+                    "or the oneAPI CXX (icpx) compiler."
+                ),
+            )
+
+        if spec.satisfies("+openfast"):
+            args.append(define("KYNEMA_SGF_OPENFAST_VERSION", spec["openfast"].version))
+
+        return args
