8f88e2fb859a2d1271b21a9c8ac8b48b76522e2b
wjunLu <wjunlu217@gmail.com>
2026-05-01 23:01:34 +0800

    hpckit: add copyright header
    Signed-off-by: wjunLu <wjunlu217@gmail.com>

--- repos/spack_repo/builtin/packages/hpckit/package.py
+++ repos/spack_repo/builtin/packages/hpckit/package.py
@@ -1,3 +1,7 @@
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
 from spack_repo.builtin.build_systems.generic import Package
 
 from spack.package import *
