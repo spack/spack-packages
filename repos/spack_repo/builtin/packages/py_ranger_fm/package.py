58ea01334d16352e406baf8c178345cc08aaccda
James Taliaferro <james@taliafer.ro>
2026-05-04 09:07:26 -0700

    set Python dependency upper bound due to use of imghdr

--- repos/spack_repo/builtin/packages/py_ranger_fm/package.py
+++ repos/spack_repo/builtin/packages/py_ranger_fm/package.py
@@ -15,5 +15,11 @@ class PyRangerFm(PythonPackage):
 
     version("1.9.2", sha256="0ec62031185ad1f40b9faebd5a2d517c8597019c2eee919e3f1c60ce466d8625")
 
+    # Ranger uses the `imghdr` module from the standard library to determine the type of an image
+    # file, as the first step in determining whether and how to display a preview of the image.
+    # The `imghdr` module was removed from the standard library in Python 3.13, so older versions
+    # crash when run with Pythons newer than that.
+    depends_on("python@:3.12", when="@:1.9.2")
+
     # pip silently replaces distutils with setuptools
     depends_on("py-setuptools", type="build")
