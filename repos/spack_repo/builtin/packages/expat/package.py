1c05fa3d4674f92cfdb1738247a1b3abd7a059ed
Sebastian Pipping <sebastian@pipping.org>
2026-05-11 13:56:00 +0200

    expat: Apply Black 26.3.1 formatting

--- repos/spack_repo/builtin/packages/expat/package.py
+++ repos/spack_repo/builtin/packages/expat/package.py
@@ -18,7 +18,10 @@ class Expat(AutotoolsPackage, CMakePackage):
     url = "https://github.com/libexpat/libexpat/releases/download/R_2_2_9/expat-2.2.9.tar.bz2"
 
     license("MIT")
-    version("2.8.0", sha256="586494499ac3ad46d87f3beda7b1f770c1c8026a9b60e151593f8b29089a52ca")
+    version(
+        "2.8.0",
+        sha256="586494499ac3ad46d87f3beda7b1f770c1c8026a9b60e151593f8b29089a52ca",
+    )
     # deprecate all releases before 2.8.0 because of various security issues
     version(
         "2.7.5",
