d27d995f531bf2f38a9ae29554b48afcc5980fba
Röhrig-Zöllner, Melven <melven.roehrig-zoellner@dlr.de>
2026-05-19 10:49:51 +0200

    t8code: add version 4.0.0 (uses CMake now)

--- repos/spack_repo/builtin/packages/t8code/package.py
+++ repos/spack_repo/builtin/packages/t8code/package.py
@@ -5,27 +5,27 @@
 import os
 
 from spack_repo.builtin.build_systems.autotools import AutotoolsPackage
+from spack_repo.builtin.build_systems.cmake import CMakePackage
 
 from spack.package import *
 
 
-class T8code(AutotoolsPackage):
+class T8code(AutotoolsPackage, CMakePackage):
     """t8code is a C/C++ library to manage parallel adaptive meshes with various element types.
     t8code uses a collection (a forest) of multiple connected adaptive space-trees in parallel
     and scales to at least one million MPI ranks and over one Trillion mesh elements."""
 
     homepage = "https://github.com/DLR-AMR/t8code"
-    url = "https://github.com/DLR-AMR/t8code/releases/download/v1.4.1/t8-1.4.1.tar.gz"
+
+    url = "https://github.com/DLR-AMR/t8code/releases/download/v4.0.0/T8CODE-4.0.0-Source.tar.gz"
 
     maintainers("Davknapp", "melven")
 
-    license("GPL-2.0-or-later")
+    license("GPL-2.0-or-later", checked_by="melven")
 
-    version("2.0.0", sha256="b83f6c204cdb663cec7e0c1059406afc4c06df236b71d7b190fb698bec44c1e0")
-    version("1.6.1", sha256="dc96effa7c1ad1d50437fefdd0963f6ef7c943eb10a372a4e8546a5f2970a412")
-    version("1.6.0", sha256="94fb8dd9d9401130867ff18e8f71249cbb0fc34995fd04412a983eb2c93db3d5")
-    version("1.5.0", sha256="22ce6492c0f808c6859a42921352d857639fddd48ecdc9935e419db95c466f28")
-    version("1.4.1", sha256="b0ec0c9b4a182f8ac7e930ba80cd20e6dc5baefc328630e4a9dac8c688749e9a")
+    version("4.0.0", sha256="668536f82730a23fc6fd96ff13e64762b6b0890d04e99a7a38d66341332d5770")
+    version("2.0.0", sha256="b83f6c204cdb663cec7e0c1059406afc4c06df236b71d7b190fb698bec44c1e0", deprecated=True,
+            url = "https://github.com/DLR-AMR/t8code/releases/download/v2.0.0/t8-2.0.0.tar.gz")
 
     variant("mpi", default=True, description="Enable MPI parallel code")
     variant("vtk", default=False, description="Enable vtk-dependent code")
@@ -39,15 +39,17 @@ class T8code(AutotoolsPackage):
     depends_on("mpi", when="+mpi")
     depends_on("vtk@9.1:", when="+vtk")
     # t8code@1.4.1 doesn't build with petsc@3.19.1
-    depends_on("petsc@3.18", when="+petsc")
+    depends_on("petsc@3.18", when="@:2.0.0 +petsc")
     depends_on("netcdf-c~mpi", when="+netcdf~mpi")
     depends_on("netcdf-c+mpi", when="+netcdf+mpi")
     depends_on("metis", when="+metis")
 
+    build_system(conditional("cmake", when="@4:"), conditional("autotools", when="@:2"), default="cmake")
+
     # Per default, t8code uses hardcoded zlib library from vtk package
     # The configure command is overwritten to choose the integrated spack package
     def patch(self):
-        if "+vtk" in self.spec:
+        if "@:2 +vtk" in self.spec:
             filter_file(r"vtkzlib-\$t8_vtk_version", "z", "configure")
 
     def configure_args(self):
@@ -86,5 +88,14 @@ class T8code(AutotoolsPackage):
 
         if "+metis" in spec:
             args.append(f"--with-metis={spec['metis'].prefix}")
+
+        return args
+
+    def cmake_args(self):
+        args = [
+            self.define_from_variant("T8CODE_ENABLE_MPI", "mpi"),
+            self.define_from_variant("T8CODE_ENABLE_VTK", "vtk"),
+            self.define_from_variant("T8CODE_ENABLE_NETCDF", "netcdf"),
+        ]
 
         return args
