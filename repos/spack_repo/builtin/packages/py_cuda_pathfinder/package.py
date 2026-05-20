6833cf08a489101f8b3e68e7031b08b4d8d54266
Till Ehrengruber <till.ehrengruber@cscs.ch>
2026-05-21 00:37:28 +0200

    Cleanup

--- repos/spack_repo/builtin/packages/py_cuda_pathfinder/package.py
+++ repos/spack_repo/builtin/packages/py_cuda_pathfinder/package.py
@@ -2,23 +2,6 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-cuda-pathfinder
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-cuda-pathfinder
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack_repo.builtin.build_systems.python import PythonPackage
 
 from spack.package import *
