a806e9895f0763ffe4c42d13a2d53589d6ab74bb
Thomas Bouvier <contact@thomas-bouvier.io>
2026-05-22 20:14:13 +0200

    py-vllm: fix compilation issue

--- repos/spack_repo/builtin/packages/py_llguidance/package.py
+++ repos/spack_repo/builtin/packages/py_llguidance/package.py
@@ -14,7 +14,9 @@ class PyLlguidance(PythonPackage):
     pypi = "llguidance/llguidance-1.6.1.tar.gz"
 
     version("1.6.1", sha256="01611f85f834725fb359b568673cfaa5a352423779c097b03aa5ce68f0db3a51")
-
-    depends_on("python@3.10:", type=("build", "run"))
+    version("1.3.0", sha256="861249afd51dc325646834462ea827e57a5c2b2042e108e6aae7059fdad9104d")
+    
+    depends_on("python@3.10:", type=("build", "run"), when="@1.4.0:")
+    depends_on("python@3.9", type=("build", "run"))
     depends_on("py-maturin@1", type="build")
     depends_on("rust@1.87:", type="build")
