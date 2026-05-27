c661ae11f3a8f62b6eea2988460b70da10087e56
John Parent <john.parent@kitware.com>
2026-05-27 09:15:31 -0400

    Version bump for better upstream path with space support
    Signed-off-by: John Parent <john.parent@kitware.com>

--- repos/spack_repo/builtin/packages/compiler_wrapper/package.py
+++ repos/spack_repo/builtin/packages/compiler_wrapper/package.py
@@ -57,7 +57,7 @@ class CompilerWrapper(Package, NMakePackage):
         version("1.0", sha256="ac876f7600fa6cb0c74ae172ef1c61661aacff03a6befbc7d87e092e2f2233f9")
     else:
         # version("develop", branch="main")
-        version("1.0", commit="38cec9fb6c71e018e9cf416f938fb639a95fc377")
+        version("1.0", commit="1220713c8ca38d1ef745fac5c2b77ff678f71c53")
 
     depends_on("msvc", when="platform=windows", type="build")
 
