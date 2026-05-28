c0d877312dffc0f4b8502fd00eacaa46a7cffa88
Thomas Bouvier <contact@thomas-bouvier.io>
2026-05-28 11:57:13 +0200

    py-griffe: add v1.15.0

--- repos/spack_repo/builtin/packages/py_griffe/package.py
+++ repos/spack_repo/builtin/packages/py_griffe/package.py
@@ -18,8 +18,12 @@ class PyGriffe(PythonPackage):
 
     license("ISC")
 
+    version("1.15.0", sha256="7726e3afd6f298fbc3696e67958803e7ac843c1cfe59734b6251a40cdbfb5eea")
     version("0.22.0", sha256="a3c25a2b7bf729ecee7cd455b4eff548f01c620b8f58a8097a800caad221f12e")
 
+    depends_on("python@3.10:", when="@1.15:", type=("build", "run"))
     depends_on("python@3.7:", type=("build", "run"))
-    depends_on("py-pdm-pep517", type="build")
+    depends_on("py-pdm-backend", type="build")
+
+    depends_on("py-colorama@0.4:", type=("build", "run"), when="@1.15.0:")
     depends_on("py-cached-property", type=("build", "run"), when="^python@:3.7")
