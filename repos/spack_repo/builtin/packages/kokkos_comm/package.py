86e1ecddcaa54f571deacd1ade3255533f3c5e8a
Cedric Chevalier <cedric.chevalier@cea.fr>
2026-06-03 11:47:27 +0200

    Adding Kokkos Comm
    Signed-off-by: Cedric Chevalier <cedric.chevalier@cea.fr>

---
+++ repos/spack_repo/builtin/packages/kokkos_comm/package.py
@@ -0,0 +1,42 @@
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack_repo.builtin.build_systems.cmake import CMakePackage
+
+from spack.package import *
+
+
+class KokkosComm(CMakePackage):
+    """Kokkos Comm is an experimental performance portable explicit communication interface for the Kokkos ecosystem."""
+
+    homepage = "https://github.com/kokkos/kokkos-comm"
+    url = "https://github.com/kokkos/kokkos-comm/archive/refs/tags/v0.1.0.tar.gz"
+
+    maintainers("cedricchevalier19", "dssgabriel", "cwpearson")
+
+    license("Apache-2.0 WITH LLVM-exception", checked_by="cedricchevalier19")
+
+    version("0.1.0", sha256="59f4b953a795adb62f306e0861c7e69ee60c8cd2a6f1bd58eb4623b9ab774d45")
+
+
+    depends_on("cxx", type=("build", "link",))
+    depends_on("cmake@3.22:3", type="build")
+
+    depends_on("kokkos@4.7:")
+
+    depends_on("mpi")
+    
+    def cmake_args(self):
+        args = [
+            self.define("KokkosComm_ENABLE_MPI", True),
+        ]
+
+        if self.spec.satisfies("^kokkos+rocm") and not (
+            self.spec.satisfies("^kokkos %cxx=clang") or self.spec.satisfies("^kokkos %cxx=rocmcc")
+        ):
+            args.append(self.define("CMAKE_CXX_COMPILER", self.spec["hip"].hipcc))
+        else:
+            args.append(self.define("CMAKE_CXX_COMPILER", self["kokkos"].kokkos_cxx))
+
+        return args
