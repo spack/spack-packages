a788341acb05b47991e9d3e3a84464dfdf66dd3f
John Parent <john.parent@kitware.com>
2026-06-02 21:30:27 -0400

    Support lctg binaries
    Signed-off-by: John Parent <john.parent@kitware.com>

--- repos/spack_repo/builtin/packages/compiler_wrapper/package.py
+++ repos/spack_repo/builtin/packages/compiler_wrapper/package.py
@@ -57,7 +57,7 @@ class CompilerWrapper(Package, NMakePackage):
         version("1.0", sha256="ac876f7600fa6cb0c74ae172ef1c61661aacff03a6befbc7d87e092e2f2233f9")
     else:
         # version("develop", branch="main")
-        version("1.0", commit="7b41382bcb5f8782344a319d95eaf29d2758a904")
+        version("1.0", commit="9d3b269b9822fbf184e7ba0193a14d9b1d6e9fcc")
 
     depends_on("msvc", when="platform=windows", type=("build", "run"))
 
