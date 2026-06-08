902fff58628c38f8ae9c6e21239109e649f69417
Guanyuming He <guanyuminghe@proton.me>
2026-06-08 11:21:18 +0800

    fill in details for cuda-samples.

--- repos/spack_repo/builtin/packages/cuda_samples/package.py
+++ repos/spack_repo/builtin/packages/cuda_samples/package.py
@@ -1,61 +1,84 @@
 # Copyright Spack Project Developers. See COPYRIGHT file for details.
 #
-# SPDX-License-Identifier: (Apache-2.0 OR MIT)
-
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install cuda-samples
-#
-# You can edit this file again by typing:
-#
-#     spack edit cuda-samples
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
+# SPDX-License-Identifier: (NVIDIA Software License Agreement)
 
 from spack_repo.builtin.build_systems.cmake import CMakePackage
+from spack_repo.builtin.build_systems.cuda import CudaPackage
+from spack_repo.builtin.build_systems.makefile import MakefilePackage
 
 from spack.package import *
 
 
-class CudaSamples(CMakePackage):
-    """FIXME: Put a proper description of your package here."""
+class CudaSamples(CMakePackage, MakefilePackage, CudaPackage):
+    """Samples for CUDA Developers which demonstrates features in CUDA
+    Toolkit."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
+    homepage = "https://github.com/NVIDIA/cuda-samples"
+    git = "https://github.com/NVIDIA/cuda-samples.git"
     url = "https://github.com/NVIDIA/cuda-samples/archive/refs/tags/v13.0.tar.gz"
 
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers("github_user1", "github_user2")
+    maintainers("guanyuming-he")
 
-    # FIXME: Add the SPDX identifier of the project's license below.
-    # See https://spdx.org/licenses/ for a list. Upon manually verifying
-    # the license, set checked_by to your Github username.
-    license("UNKNOWN", checked_by="github_user1")
+    # The license is custom:
+    # https://github.com/NVIDIA/cuda-samples?tab=License-1-ov-file
+    license("NVIDIA Software License Agreement", checked_by="guanyuming-he")
 
-    version("13.0", sha256="63cc9d5d8280c87df3c1f4e2276234a0f42cc497c52b40dd5bdda2836607db79")
-    version("12.9", sha256="2e67e1f6bdb15bf11b21e07e988e2f9f60fb054eff51ef01cebdd47229788015")
-    version("12.8", sha256="fe82484f9a87334075498f4e023a304cc70f240a285c11678f720f0a1e54a89d")
-    version("12.5", sha256="5c40cc096706045b067ec5897f039403014aa7a39b970905698466a2d029b972")
-    version("12.4.1", sha256="01bb311cc8f802a0d243700e4abe6a2d402132c9d97ecf2c64f3fbb1006c304c")
-    version("12.4", sha256="aa28fa2227768dd31ebbf9cd48b265a0c8810fae03e02c6079c0fa71bbea7319")
+    # Update this as CUDA is updated in Spack.
+    # Can use spack checksum cuda-samples
+    _ver_map = {
+        "13.3": "fab59f405d6c0b87395ce6fc1d46d3f559c380c9a2704ab14d6dc0d3ce1cff16",
+        "13.2update": "057e68d22bd02e41d60c9826e7622ac1b88de0f1dbe25ed49bd995f768306f9d",
+        "13.2": "c7d8da987a43fd6ed7c2641df204dfc639768adbae070bc22f9df0e03005f7de",
+        "13.1": "03d7748a773fcd2350c2de88f2d167252c78ea90a52e229e7eb2a6922e3ba350",
+        "13.0": "63cc9d5d8280c87df3c1f4e2276234a0f42cc497c52b40dd5bdda2836607db79",
+        "12.9": "2e67e1f6bdb15bf11b21e07e988e2f9f60fb054eff51ef01cebdd47229788015",
+        "12.8": "fe82484f9a87334075498f4e023a304cc70f240a285c11678f720f0a1e54a89d",
+        "12.5": "5c40cc096706045b067ec5897f039403014aa7a39b970905698466a2d029b972",
+        "12.4.1": "01bb311cc8f802a0d243700e4abe6a2d402132c9d97ecf2c64f3fbb1006c304c",
+        "12.4": "aa28fa2227768dd31ebbf9cd48b265a0c8810fae03e02c6079c0fa71bbea7319",
+    }
+    for v,h in _ver_map.items():
+        version(v, sha256=h)
+        depends_on("cuda@"+v, when="@"+v)
+
+    variant("cuda", default=True, description="build using CUDA (required)")
 
     depends_on("c", type="build")
     depends_on("cxx", type="build")
 
-    # FIXME: Add dependencies if required.
-    # depends_on("foo")
+    # cuda-samples changed build systems starting with 12.8
+    build_system(
+        conditional("cmake", when="@12.8:"),
+        conditional("makefile", when="@:12.5"),
+        default="cmake",
+    )
 
+    # After the change to CMake, the build defaults to all GPU arches
+    # supported by the NVCC it depends on
+    conflicts("cuda_arch=none", when="@:12.5+cuda", 
+           msg="Please specify cuda_arch as variant for installation.")
+
+    @when("@:12.5")
+    def setup_build_environment(self, env):
+        env.set("CUDA_PATH", self.spec["cuda"].prefix)
+        env.set("SMS", self.spec.variants["cuda_arch"].value[0])
+
+    @when("@12.8:")
     def cmake_args(self):
-        # FIXME: Add arguments other than
-        # FIXME: CMAKE_INSTALL_PREFIX and CMAKE_BUILD_TYPE
-        # FIXME: If not needed delete this function
-        args = []
+        args = ["-DCUDAToolkit_ROOT=" + self.spec["cuda"].prefix]
         return args
+
+    # cuda-samples doesn't actually install the samples in the
+    # CMAKE_INSTALL_PREFIX dir, so this copies them
+    @when("@12.8:")
+    def install(self, spec, prefix):
+        short_hash = spec.prefix.split("-")[-1][0:7]
+        shutil.copytree("../spack-build-" + short_hash + "/Samples", 
+                  prefix + "/bin/")
+
+    # Similar to the CMake version, the Make version doesn't have an install
+    # phase but instead just creates binaries in a `bin` folder under the build
+    # directory
+    @when("@:12.5")
+    def install(self, spec, prefix):
+        shutil.copytree("./bin/", prefix + "/bin/")
