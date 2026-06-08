f962610e8b0ace4f32fab063bd2cf3159c613e46
Guanyuming He <guanyuminghe@proton.me>
2026-06-08 16:23:20 +0800

    make rodinia compatible with latest cuda-samples.

--- repos/spack_repo/builtin/packages/rodinia/package.py
+++ repos/spack_repo/builtin/packages/rodinia/package.py
@@ -19,12 +19,18 @@ class Rodinia(MakefilePackage, CudaPackage):
     version("3.1", sha256="faebac7c11ed8f8fcf6bf2d7e85c3086fc2d11f72204d6dfc28dc5b2e8f2acfd")
 
     depends_on("cuda")
+    depends_on("cuda-samples")
     depends_on("freeglut")
     depends_on("glew")
     depends_on("gl")
     depends_on("glu")
 
     conflicts("~cuda")
+    conflicts("cuda_arch=none",
+        msg="Please specify cuda_arch as variant for installation.\n"
+            "You can query it with \n"
+            "nvidia-smi --query-gpu=name,compute_cap --format=csv"
+    )
 
     build_targets = ["CUDA"]
 
@@ -32,6 +38,7 @@ class Rodinia(MakefilePackage, CudaPackage):
         # set cuda paths
 
         cuda_prefix = self.spec["cuda"].prefix
+        cuda_samples_prefix = self.spec["cuda-samples"].prefix
         cuda_arch = self.spec.variants["cuda_arch"].value[0]
         if cuda_arch == "none":
             raise InstallError(
@@ -52,7 +59,7 @@ class Rodinia(MakefilePackage, CudaPackage):
 
         filter_file(
             "SDK_DIR = /usr/local/cuda-5.5/samples/",
-            "SDK_DIR = {0}/samples".format(cuda_prefix),
+            "SDK_DIR = {0}".format(cuda_samples_prefix),
             "common/make.config",
             string=True,
         )
@@ -65,18 +72,46 @@ class Rodinia(MakefilePackage, CudaPackage):
             string=True,
         )
 
+        # Produced by find . -type f -name "Makefile"
         makefiles = [
-            "cuda/lavaMD/makefile",
-            "cuda/particlefilter/Makefile",
-            "cuda/hybridsort/Makefile",
-            "cuda/dwt2d/Makefile",
+            "cuda/srad/srad_v2/Makefile",
+            "cuda/srad/Makefile",
+            "cuda/myocyte/Makefile",
+            "cuda/mummergpu/Makefile",
+            "cuda/mummergpu/src/Makefile",
+            "cuda/backprop/Makefile",
+            "cuda/nn/Makefile",
+            "cuda/gaussian/Makefile",
+            "cuda/lud/tools/Makefile",
+            "cuda/lud/base/Makefile",
+            "cuda/lud/Makefile",
+            "cuda/lud/cuda/Makefile",
+            "cuda/huffman/Makefile",
             "cuda/hotspot3D/Makefile",
+            "cuda/kmeans/Makefile",
+            "cuda/hotspot/Makefile",
+            "cuda/leukocyte/CUDA/Makefile",
+            "cuda/leukocyte/Makefile",
+            "cuda/heartwall/Makefile",
+            "cuda/bfs/Makefile",
+            "cuda/hybridsort/Makefile",
+            "cuda/pathfinder/Makefile",
+            "cuda/streamcluster/Makefile",
             "cuda/b+tree/Makefile",
+            "cuda/dwt2d/Makefile",
+            "cuda/nw/Makefile",
+            "cuda/cfd/Makefile",
+            "cuda/particlefilter/Makefile",
         ]
 
         for makefile in makefiles:
             filter_file(
                 "sm_[0-9]+", "sm_{0}".format(cuda_arch), makefile
+            )
+            # Cuda samples include path has changed
+            # To find all where SDK_DIR is used, grep -rn "SDK_DIR" .
+            filter_file(
+                r"/common/inc", "/common", makefile
             )
 
         # fix broken makefile rule
