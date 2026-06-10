a74d36c89f627f5c2ce1533a4164678a7380db5d
Cory Quammen <cory.quammen@kitware.com>
2026-06-10 17:52:59 -0400

    paraview: add v6.1.1 (#5140)

--- repos/spack_repo/builtin/packages/paraview/package.py
+++ repos/spack_repo/builtin/packages/paraview/package.py
@@ -64,6 +64,7 @@ class Paraview(CMakePackage, CudaPackage, ROCmPackage):
     license("Apache-2.0")
 
     version("master", branch="master", submodules=True)
+    version("6.1.1", sha256="43671df11e1629cf9079815bfdfa9f22063a90cd1baed694072f80622f5ad92f")
     version("6.1.0", sha256="4e9d882874b2a161f3338a6644a5d8bc63748f0d7846f4690701b86a8a821dfc")
     version("6.0.1", sha256="5e56ac7af5e925b3cfd3fab82470933cbabc7e8fda87e14af64f995d6064eb06")
     version("6.0.0", sha256="0ee07ae6377e5e97766aebf858eb9758668a52df041f319e7c975037a63bf189")
