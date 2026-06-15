cee77bc46178ac155e4d310c7c33d8fa8899af96
Jon Rood <jon.rood@nrel.gov>
2026-06-15 15:18:44 -0600

    Rename exawind applications to kynema (#4838)
    * Add Kynema packages.
    
    * Fix kynema-ugf branch.
    
    * Kynema ugf needs (#1)
    
    * consistency with fmb variant
    
    * add forgotten option from PR #2270
    
    * Rename kynema to kynema-driver.
    
    * Add kynema bundle package.
    
    * Update maintainers in kynema-fmb.
    
    * [@spackbot] updating style on behalf of jrood-nrel
    
    * Remove git branches from old exawind packages.
    
    * Remove old version tags.
    
    * Add +test to trilinos.
    
    * Fix kynema-fmb option.
    
    * Update versions.
    
    * Update kynema packages.
    
    * Remove cxxstd from trilinos requirement.
    
    * Update packages.
    
    * Fix syntax error.
    
    ---------
    
    Co-authored-by: Michael B Kuhn <31661049+mbkuhn@users.noreply.github.com>
    Co-authored-by: jrood-nrel <jrood-nrel@users.noreply.github.com>

--- repos/spack_repo/builtin/packages/kynema_sgf/package.py
+++ repos/spack_repo/builtin/packages/kynema_sgf/package.py
@@ -9,13 +9,13 @@ from spack_repo.builtin.build_systems.rocm import ROCmPackage
 from spack.package import *
 
 
-class AmrWind(CMakePackage, CudaPackage, ROCmPackage):
-    """AMR-Wind is a massively parallel, block-structured adaptive-mesh,
-    incompressible flow sover for wind turbine and wind farm simulations."""
+class KynemaSgf(CMakePackage, CudaPackage, ROCmPackage):
+    """Kynema-SGF is a massively parallel, block-structured adaptive-mesh,
+    incompressible flow solver for the Kynema project."""
 
-    homepage = "https://github.com/Exawind/amr-wind"
-    url = "https://github.com/Exawind/amr-wind/archive/refs/tags/v1.3.0.tar.gz"
-    git = "https://github.com/Exawind/amr-wind.git"
+    homepage = "https://github.com/Kynema/kynema-sgf"
+    url = "https://github.com/Kynema/kynema-sgf/archive/refs/tags/v3.9.1.tar.gz"
+    git = "https://github.com/Kynema/kynema-sgf.git"
 
     maintainers("jrood-nrel")
 
@@ -24,33 +24,7 @@ class AmrWind(CMakePackage, CudaPackage, ROCmPackage):
 
     license("BSD-3-Clause")
 
-    version("3.9.1", tag="v3.9.1", commit="c41e3a727ad7ad9fe14df33c2db565b4248dc7ed")
-    version("3.9.0", tag="v3.9.0", commit="6c15368dc93d749fb0c3b216b0ccccaab6907251")
-    version("3.8.1", tag="v3.8.1", commit="464ce0891979dbef9972c5b868facee1c974779d")
-    version("3.8.0", tag="v3.8.0", commit="35d63755ce213caedc1c1e96e93e9a4bfe5bffc3")
-    version("3.7.0", tag="v3.7.0", commit="243e620c263761d34b3ef54694cfd3f787321c24")
-    version("3.6.0", tag="v3.6.0", commit="7346fd2a99e5d66ecf2ef3bafa1ebe621211d21f")
-    version("3.5.0", tag="v3.5.0", commit="412f015f2496cb0c0c802240bb9c11848b408fa0")
-    version("3.4.2", tag="v3.4.2", commit="ed475a0533dfacf1fdff0b707518ccf99040d9f9")
-    version("3.4.1", tag="v3.4.1", commit="effe63ca9061e6d2bd5c5e84b690d29c0869f029")
-    version("3.4.0", tag="v3.4.0", commit="38d1b9fd0b70aab4a01fd507f039750c2508bd1c")
-    version("3.3.1", tag="v3.3.1", commit="30fc9a15a65670ebe3226b85e76b2155b4e4251d")
-    version("3.3.0", tag="v3.3.0", commit="cba28f0fb9d7c5c6d1aeb57e67469c962f9a12d4")
-    version("3.2.3", tag="v3.2.3", commit="a609630eb7e21f6054a40d34865b2e7dd2be4eab")
-    version("3.2.2", tag="v3.2.2", commit="e12ec7b5a973a7e9d57dffdbf9a2875c601bb0cf")
-    version("3.2.1", tag="v3.2.1", commit="2b309bdb578d549eba08cd8bdf852aa85fbb33f2")
-    version("3.2.0", tag="v3.2.0", commit="bb0e9dcc59c694812a03703199ade0a3984161d5")
-    version("3.1.7", tag="v3.1.7", commit="b9858be2c7ae85ad3c78e414699fad4acf2a7a22")
-    version("3.1.6", tag="v3.1.6", commit="ca437affc6fd00490d8b14e244e53bf641207224")
-    version("3.1.5", tag="v3.1.5", commit="554f8aa1ac36c2bae17565c64d5bc33333cee396")
-    version("3.1.4", tag="v3.1.4", commit="e10f5ebd3141b9990a65ebe9f1bdca8554b59472")
-    version("3.1.3", tag="v3.1.3", commit="af8231ace69119133c4c8a906e98946ec5aa79c8")
-    version("3.1.2", tag="v3.1.2", commit="5edcac4496e30e450c0f21e7fa74f8b590dc3860")
-    version("3.1.1", tag="v3.1.1", commit="8ae06194fa47bf473615988f97a7b423d467b023")
-    version("3.1.0", tag="v3.1.0", commit="3e23581b132532bf70b09c38217ff9c46204f047")
-    version("3.0.2", tag="v3.0.2", commit="f867288dffecc6404189afa965189c2558cf9922")
-    version("3.0.1", tag="v3.0.1", commit="65aa85db5cb3bbabc767d5dde4b106b7022a0f90")
-    version("3.0.0", tag="v3.0.0", commit="2fbd345cfa7cb7277c1cb6a1323247579e1bbc32")
+    version("main", branch="main")
 
     variant("hypre", default=False, description="Enable Hypre integration")
     variant("ascent", default=False, description="Enable Ascent integration")
@@ -58,8 +32,9 @@ class AmrWind(CMakePackage, CudaPackage, ROCmPackage):
     variant("mpi", default=True, description="Enable MPI support")
     variant("netcdf", default=False, description="Enable NetCDF support")
     variant("openfast", default=False, description="Enable OpenFAST integration")
-    variant("kynema", default=False, description="Enable Kynema integration")
+    variant("kynema-fmb", default=False, description="Enable Kynema integration")
     variant("openmp", default=False, description="Enable OpenMP for CPU builds")
+    variant("pic", default=True, description="Position independent code")
     variant("shared", default=True, description="Build shared libraries")
     variant("tests", default=True, description="Activate regression tests")
     variant("tiny_profile", default=False, description="Activate tiny profile")
@@ -72,6 +47,7 @@ class AmrWind(CMakePackage, CudaPackage, ROCmPackage):
         "waves2amr", default=False, description="Enable Waves2AMR support for ocean wave input"
     )
     variant("fft", default=False, description="Enable FFT support for MAC projection")
+    variant("single-precision", default=False, description="Use single precision")
 
     depends_on("c", type="build")
     depends_on("cxx", type="build")
@@ -92,21 +68,23 @@ class AmrWind(CMakePackage, CudaPackage, ROCmPackage):
     depends_on("ascent~mpi", when="+ascent~mpi")
     depends_on("ascent+mpi", when="+ascent+mpi")
     depends_on("netcdf-c", when="+netcdf")
+    depends_on("netcdf-c+mpi", when="+netcdf+mpi")
     depends_on("py-netcdf4", when="+netcdf")
     depends_on("py-numpy@2:", when="+netcdf")
     depends_on("py-matplotlib", when="+masa")
     depends_on("py-pandas", when="+masa")
-    depends_on("openfast+cxx@3.5:4.0", when="@:3.5 +openfast")
-    depends_on("openfast+cxx@3.5.0:3.5.9,4.1:", when="@3.6: +openfast")
+    depends_on("openfast+cxx@3.5.0:3.5.9,4.1:", when="+openfast")
     depends_on("openfast+netcdf", when="+openfast+netcdf")
-    depends_on("kynema", when="+kynema")
+    depends_on("kynema-fmb", when="+kynema-fmb")
     depends_on("helics@:3.3.2", when="+helics")
     depends_on("helics@:3.3.2+mpi", when="+helics+mpi")
-    depends_on("fftw", when="@2.1: +waves2amr")
-    depends_on("fftw", when="@3.3.1: +fft")
-
+    depends_on("fftw", when="+waves2amr")
+    depends_on("fftw", when="+fft")
     depends_on("rocrand", when="+rocm")
     depends_on("rocprim", when="+rocm")
+
+    requires("+mpi", when="+kynema-fmb")
+    requires("+mpi", when="+openfast")
 
     for arch in CudaPackage.cuda_arch_values:
         depends_on("hypre+cuda cuda_arch=%s" % arch, when="+cuda+hypre cuda_arch=%s" % arch)
@@ -119,9 +97,6 @@ class AmrWind(CMakePackage, CudaPackage, ROCmPackage):
 
     conflicts("+openmp", when="+cuda")
     conflicts("+shared", when="+cuda")
-    conflicts(
-        "openfast@4.0.0:4.0.3", msg="OpenFAST 4.0.0:4.0.3 contains a bug. Use OpenFAST >= 4.0.4."
-    )
 
     def setup_build_environment(self, env: EnvironmentModifications) -> None:
         # Avoid compile errors with Intel interprocedural optimization
@@ -150,11 +125,12 @@ class AmrWind(CMakePackage, CudaPackage, ROCmPackage):
             "helics",
             "umpire",
             "sycl",
-            "kynema",
         ]
-        args = [self.define_from_variant("AMR_WIND_ENABLE_%s" % v.upper(), v) for v in vs]
 
-        args += [self.define_from_variant("BUILD_SHARED_LIBS", "shared")]
+        args = [self.define_from_variant("KYNEMA_SGF_ENABLE_%s" % v.upper(), v) for v in vs]
+
+        args.append(self.define_from_variant("KYNEMA_SGF_ENABLE_KYNEMA_FMB", "kynema-fmb"))
+        args.append(self.define_from_variant("BUILD_SHARED_LIBS", "shared"))
 
         if spec.satisfies("+mpi"):
             args.append(define("MPI_HOME", spec["mpi"].prefix))
@@ -162,8 +138,8 @@ class AmrWind(CMakePackage, CudaPackage, ROCmPackage):
             args.append(define("MPI_C_COMPILER", spec["mpi"].mpicc))
 
         if spec.satisfies("+hdf5"):
-            args.append(define("AMR_WIND_ENABLE_HDF5", True))
-            args.append(define("AMR_WIND_ENABLE_HDF5_ZFP", True))
+            args.append(self.define_from_variant("KYNEMA_SGF_ENABLE_HDF5", "hdf5"))
+            args.append(self.define_from_variant("KYNEMA_SGF_ENABLE_HDF5_ZFP", "hdf5"))
             # Help AMReX understand if HDF5 is parallel or not.
             # Building HDF5 with CMake as Spack does, causes this inspection to break.
             args.append(define("HDF5_IS_PARALLEL", spec.satisfies("+mpi")))
@@ -183,7 +159,7 @@ class AmrWind(CMakePackage, CudaPackage, ROCmPackage):
             args.append(define("HELICS_DIR", spec["helics"].prefix))
 
         if spec.satisfies("+waves2amr"):
-            args.append(self.define_from_variant("AMR_WIND_ENABLE_W2A", "waves2amr"))
+            args.append(self.define_from_variant("KYNEMA_SGF_ENABLE_W2A", "waves2amr"))
             args.append(define("FFTW_DIR", spec["fftw"].prefix))
 
         if spec.satisfies("+fft"):
@@ -201,6 +177,9 @@ class AmrWind(CMakePackage, CudaPackage, ROCmPackage):
             )
 
         if spec.satisfies("+openfast"):
-            args.append(define("AMR_WIND_OPENFAST_VERSION", spec["openfast"].version))
+            args.append(define("KYNEMA_SGF_OPENFAST_VERSION", spec["openfast"].version))
+
+        if spec.satisfies("+single-precision"):
+            args.append(define("KYNEMA_SGF_PRECISION", "SINGLE"))
 
         return args
