9583d400744adef32862889e9ea856f3c498b75e
williampiat3 <32038421+williampiat3@users.noreply.github.com>
2026-06-16 16:17:00 +0200

    Update repos/spack_repo/builtin/packages/py_elecsolver/package.py
    Co-authored-by: Alec Scott <hi@alecbcs.com>

--- repos/spack_repo/builtin/packages/py_elecsolver/package.py
+++ repos/spack_repo/builtin/packages/py_elecsolver/package.py
@@ -13,7 +13,7 @@ class PyElecsolver(PythonPackage):
     homepage = "https://github.com/williampiat3/ElecSolver"
     pypi = "ElecSolver/elecsolver-2.0.1.tar.gz"
 
-    maintainers = ["williampiat3"]
+    maintainers("williampiat3")
 
     version("2.1.0", sha256="17a33dd4d6f8551904baa55c840d561cff00dc046b4bf7e11bad7e69722c4faa")
     version("2.0.1", sha256="126b02e90e01405109ddd52255a43015d5e0e634945c46baa13c6d41d0e4e05e")
