ce4c20c5152e9b01849f9bf2b59e11b39529d6cc
Anurag Mishra <anurag.mishra@eleve.isep.fr>
2026-06-23 11:19:36 +0200

    Add py-numkong, py-albucore and py-albumentationsx recipes

--- repos/spack_repo/builtin/packages/py_albucore/package.py
+++ repos/spack_repo/builtin/packages/py_albucore/package.py
@@ -11,16 +11,18 @@ class PyAlbucore(PythonPackage):
     """High-performance image processing functions for deep learning and computer vision."""
 
     homepage = "https://github.com/albumentations-team/albucore"
-    pypi = "albucore/albucore-0.0.24.tar.gz"
+    pypi = "albucore/albucore-0.1.6.tar.gz"
 
     license("MIT")
 
-    version("0.0.24", sha256="f2cab5431fadf94abf87fd0c89d9f59046e49fe5de34afea8f89bc8390253746")
+    version("0.1.6", sha256="0afdb9c4840bf060cab8c5b85ebf43c2df4de53c42013988a808aaba1ec0b5b1")
 
-    depends_on("python@3.9:", type=("build", "run"))
-    depends_on("py-setuptools@45:", type="build")
+    depends_on("python@3.10:", type=("build", "run"))
+    depends_on("py-hatchling", type="build")
+
     depends_on("py-numpy@1.24.4:", type=("build", "run"))
-    depends_on("py-typing-extensions@4.9:", type=("build", "run"), when="^python@:3.9")
+    depends_on("opencv@=4.9.0+contrib+python3+imgproc+photo", type=("build", "run"))
+    depends_on("py-numkong@7.4.5:", type=("build", "run"))
     depends_on("py-stringzilla@3.10.4:", type=("build", "run"))
-    depends_on("py-simsimd@5.9.2:", type=("build", "run"))
-    depends_on("opencv@4.9.0.80:+python3+contrib", type=("build", "run"))
+
+    import_modules = ["albucore"]
