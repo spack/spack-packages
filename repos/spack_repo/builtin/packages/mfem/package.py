490505c41695c98544a03ec17fb219ce598892c9
chapman39 <chapman39@llnl.gov>
2026-06-24 12:00:48 -0700

    only pick one lib

--- repos/spack_repo/builtin/packages/mfem/package.py
+++ repos/spack_repo/builtin/packages/mfem/package.py
@@ -882,9 +882,12 @@ class Mfem(Package, CudaPackage, ROCmPackage):
             if "^netlib-scalapack" in strumpack:
                 scalapack = strumpack["scalapack"]
                 sp_opt += ["-I%s" % scalapack.prefix.include]
+                scalapack_lib_dir = scalapack.prefix.lib
+                if not os.path.exists(scalapack_lib_dir):
+                    scalapack_lib_dir = scalapack.prefix.lib64
                 sp_lib += [
                     ld_flags_from_dirs(
-                        [scalapack.prefix.lib, scalapack.prefix.lib64], ["scalapack"]
+                        [scalapack_lib_dir], ["scalapack"]
                     )
                 ]
             elif "^scalapack" in strumpack:
