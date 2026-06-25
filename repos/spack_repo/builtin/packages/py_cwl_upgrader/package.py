3eaf506eadcb8a457268519879b622e1a6d91c4b
V-Karch <179492080+V-Karch@users.noreply.github.com>
2026-06-25 19:00:27 +0000

    [@spackbot] updating style on behalf of V-Karch

--- repos/spack_repo/builtin/packages/py_cwl_upgrader/package.py
+++ repos/spack_repo/builtin/packages/py_cwl_upgrader/package.py
@@ -19,7 +19,7 @@ class PyCwlUpgrader(PythonPackage):
     version(
         "1.2.15",
         sha256="bb9e098ba6d1cc82c36dff1cbaad6779d6d9beab5324cf44a121d7faf7d58672",
-        url="https://files.pythonhosted.org/packages/d7/f8/0159b1785cbb3e0b1538cd53ee64ffe94d9b3c2e4867ad47f2e071c5f9ec/cwl_upgrader-1.2.15.tar.gz"
+        url="https://files.pythonhosted.org/packages/d7/f8/0159b1785cbb3e0b1538cd53ee64ffe94d9b3c2e4867ad47f2e071c5f9ec/cwl_upgrader-1.2.15.tar.gz",
     )
     version("1.2.4", sha256="b25fc236407343d44cc830ac3f63eed395b8d872fc7e17db92cde583d4a3b2ec")
 
