71f5a47e8310e3dc39d2cfd4423b38cce661b5e6
renjithravindrankannath <rravindr@amd.com>
2026-06-30 16:23:47 -0400

    Fix struct redefinition conflict by limiting libdrm version

--- repos/spack_repo/builtin/packages/amdsmi/package.py
+++ repos/spack_repo/builtin/packages/amdsmi/package.py
@@ -61,18 +61,12 @@ class Amdsmi(CMakePackage):
     depends_on("python@3.6:")
     depends_on("py-virtualenv")
     depends_on("pkgconfig")
-    depends_on("libdrm")
+    depends_on("libdrm@:2.4.124", when="@:7.2")
     depends_on("py-pyyaml")
     patch(
         "https://github.com/ROCm/amdsmi/commit/2858e51b4e8ff124ed67e23e0cd131e8b2140fae.patch?full_index=1",
         sha256="1cac40d057cb19f0cfac83ea427c8e98f7808be9a2778cd53cdbf963910798e8",
         when="@6.2",
-    )
-    # Fix struct redefinition conflict with newer libdrm versions
-    patch(
-        "amdsmi-libdrm-compat.patch",
-        sha256="bb20b8fedb0207d6c1a258af5dc68331463df9b92aa84286efe939fc3e924eb7",
-        when="@7.2:",
     )
 
     @property
