7e593e2669d37b989380615b991f264f57babf72
Maciej Wójcik <w8jcik@gmail.com>
2026-06-30 12:43:20 +0300

    r-statmod: add new versions

--- repos/spack_repo/builtin/packages/r_statmod/package.py
+++ repos/spack_repo/builtin/packages/r_statmod/package.py
@@ -22,6 +22,8 @@ class RStatmod(RPackage):
 
     license("GPL-2.0-only OR GPL-3.0-only")
 
+    version("1.5.2", sha256="5f83d82043c5e26f1048fc0139580da1b7e40becf95fb4cb26ed3afcd46a581d")
+    version("1.5.1", sha256="8f04388117352a75e55d72f320ca12c32bca484d640216885eff378df2291397")
     version("1.5.0", sha256="d61c3ef9b09d55b42e038f8d767fa483ebbdec2a9c7172b1b0ccda0ae0016ec9")
     version("1.4.37", sha256="90d2c8a79e0cb291f2685686436bcf4c5b9abd2efb84759a8553d1b1adb76913")
     version("1.4.36", sha256="14e897c83d426caca4d920d3d5bead7ae9a679276b3cb2e227f299ad189d7bc2")
@@ -29,7 +31,7 @@ class RStatmod(RPackage):
     version("1.4.32", sha256="2f67a1cfa66126e6345f8a40564a3077d08f1748f17cb8c8fb05c94ed0f57e20")
     version("1.4.30", sha256="9d2c1722a85f53623a9ee9f73d835119ae22ae2b8ec7b50d675401e314ea641f")
 
-    depends_on("c", type="build")  # generated
-    depends_on("fortran", type="build")  # generated
+    depends_on("c", type="build")
+    depends_on("fortran", type="build")
 
     depends_on("r@3.0.0:", type=("build", "run"))
