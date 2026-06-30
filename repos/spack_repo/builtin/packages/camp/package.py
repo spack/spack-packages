ad8d5c9052809edfc152d5c174a31229f675db68
Peter Josef Scheibel <scheibel1@llnl.gov>
2026-06-29 17:12:19 -0700

    camp locate hip

--- repos/spack_repo/builtin/packages/camp/package.py
+++ repos/spack_repo/builtin/packages/camp/package.py
@@ -137,8 +137,10 @@ class Camp(CMakePackage, CudaPackage, ROCmPackage):
 
         options.append(self.define_from_variant("ENABLE_HIP", "rocm"))
         if spec.satisfies("+rocm"):
-            rocm_root = spec["llvm-amdgpu"].prefix
+            rocm_root = spec["hip"].prefix
             options.append(self.define("ROCM_PATH", rocm_root))
+
+            options.append(self.define("CMAKE_HIP_COMPILER", env["HIPCXX"]))
 
             archs = ";".join(self.spec.variants["amdgpu_target"].value)
             options.append("-DCMAKE_HIP_ARCHITECTURES={0}".format(archs))
