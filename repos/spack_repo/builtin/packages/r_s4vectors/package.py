15205052ee46f07601af026310aba1890712fcd2
Maciej Wójcik <w8jcik@gmail.com>
2026-06-30 18:05:44 +0300

    r-s4vectors: add new versions

--- repos/spack_repo/builtin/packages/r_s4vectors/package.py
+++ repos/spack_repo/builtin/packages/r_s4vectors/package.py
@@ -22,7 +22,15 @@ class RS4vectors(RPackage):
     bioc = "S4Vectors"
 
     with default_args(get_full_repo=True):
+        version("0.50.1", commit="dfc837c68d33d9dfb2ba620c8cad386b8a730fa2")  # bioc 3.23
+        version("0.48.1", commit="ae25d08aa5b02dca895179b5b6ac52656c4547f3")  # bioc 3.22
+        version("0.46.0", commit="f4a665d66e3d84099b551ae03840c727a374c178")  # bioc 3.21
+        version("0.44.0", commit="79c39487a5fa9470cc86fe3f62033ff2cc9aae79")  # bioc 3.20
+        version("0.42.1", commit="cc0a6d7b29ba55545f979d7448e2f5029563a219")  # bioc 3.19
+        version("0.40.2", commit="8cd5cb3cbd469be6a8c9621461e0e16a314506c6")  # bioc 3.18
+        version("0.38.2", commit="6878c63a79eaacfd164a16d6815746cca5e80dba")  # bioc 3.17
         version("0.38.0", commit="e80c24e1481033741147a0677fb42ce455e7da74")
+        version("0.36.2", commit="16710086622163297633a06af763892cef210a5c")  # bioc 3.16
         version("0.36.0", commit="af58701957ffdd9209031dd6a8dee3acdc58e999")
         version("0.34.0", commit="f590de3ec4d896a63351d0c1925d3856c0bd5292")
         version("0.32.3", commit="ad90e78fd3a4059cfcf2846498fb0748b4394e1a")
@@ -34,12 +42,15 @@ class RS4vectors(RPackage):
         version("0.16.0", commit="00fec03fcbcb7cff37917fab0da28d91fdf9dc3d")
         version("0.14.7", commit="40af17fe0b8e93b6a72fc787540d2961773b8e23")
 
-    depends_on("c", type="build")  # generated
+    depends_on("c", type="build")
 
-    depends_on("r@3.3.0:", type=("build", "run"))
+    depends_on("r@4.1.0:", type=("build", "run"), when="@0.49.3:")
     depends_on("r@4.0.0:", type=("build", "run"), when="@0.28.1:")
-    depends_on("r-biocgenerics@0.21.1:", type=("build", "run"))
-    depends_on("r-biocgenerics@0.23.3:", type=("build", "run"), when="@0.16.0:")
-    depends_on("r-biocgenerics@0.31.1:", type=("build", "run"), when="@0.26.1:")
-    depends_on("r-biocgenerics@0.36.0:", type=("build", "run"), when="@0.28.1:")
+    depends_on("r@3.3.0:", type=("build", "run"))
+
+    depends_on("r-biocgenerics@0.53.2:", type=("build", "run"), when="@0.45.2:")
     depends_on("r-biocgenerics@0.37.0:", type=("build", "run"), when="@0.32.3:")
+    depends_on("r-biocgenerics@0.36.0:", type=("build", "run"), when="@0.28.1:")
+    depends_on("r-biocgenerics@0.31.1:", type=("build", "run"), when="@0.26.1:")
+    depends_on("r-biocgenerics@0.23.3:", type=("build", "run"), when="@0.16.0:")
+    depends_on("r-biocgenerics@0.21.1:", type=("build", "run"))
