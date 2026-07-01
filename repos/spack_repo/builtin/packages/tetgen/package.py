fc1e0105bf9240c050b05068d9e2c13ba608d70b
Lukas Troska <lukas.troska@scai.fraunhofer.de>
2026-07-01 12:44:15 +0200

    TetGen: add CMake support

--- repos/spack_repo/builtin/packages/tetgen/package.py
+++ repos/spack_repo/builtin/packages/tetgen/package.py
@@ -4,13 +4,13 @@
 
 
 import glob
+import os
 
-from spack_repo.builtin.build_systems.generic import Package
+from spack_repo.builtin.build_systems import makefile, cmake
 
 from spack.package import *
 
-
-class Tetgen(Package):
+class Tetgen(makefile.MakefilePackage, cmake.CMakePackage):
     """TetGen is a program and library that can be used to generate
     tetrahedral meshes for given 3D polyhedral domains. TetGen
     generates exact constrained Delaunay tetrahedralizations,
@@ -43,8 +43,20 @@ class Tetgen(Package):
         url="http://www.tetgen.org/files/tetgen1.4.3.tar.gz",
     )
 
+    build_system("makefile", "cmake", default="makefile")
+
+    with when("build_system=makefile"):
+      variant("debug", default=False, description="Builds the library in debug mode.")
+
+      depends_on("gmake", type="build")
+
+    with when("build_system=cmake"):
+      variant("build_type", default="Release",
+          description="CMake build type",
+          values=("Debug", "Release", "RelWithDebInfo", "MinSizeRel"))
+      variant("shared", default=True, description="Enables the build of shared libraries.")
+
     variant("pic", default=True, description="Builds the library in pic mode.")
-    variant("debug", default=False, description="Builds the library in debug mode.")
     variant(
         "except",
         default=False,
@@ -54,25 +66,16 @@ class Tetgen(Package):
 
     patch("tetgen-1.5.0-free.patch", when="@1.5.0")
 
-    depends_on("cxx", type="build")  # generated
-    depends_on("gmake", type="build")
+    depends_on("cxx", type="build")
 
     def patch(self):
-        cflags = "-g -O0" if "+debug" in self.spec else "-g0 -O3"
-        cflags = cflags + " -fPIC" if "+pic" in self.spec else cflags
-        predcflags = "-fPIC" if "+pic" in self.spec else ""
-
-        mff = FileFilter("makefile")
-        mff.filter(r"^(C(XX)?FLAGS\s*=)(.*)$", r"\1 {0}".format(cflags))
-        mff.filter(r"^(PREDC(XX)?FLAGS\s*=.*)$", r"\1 {0}".format(predcflags))
-
-        if "+except" in self.spec:
-            hff = FileFilter("tetgen.h")
-            hff.filter(r"(\b)(throw)(\b)(.*);", r"\1assert_throw(false);")
-            hff.filter(
-                r"^(#define\s*tetgenH\s*)$",
-                r"\1{0}".format(
-                    """\n
+      if "+except" in self.spec:
+          hff = FileFilter("tetgen.h")
+          hff.filter(r"(\b)(throw)(\b)(.*);", r"\1assert_throw(false);")
+          hff.filter(
+              r"^(#define\s*tetgenH\s*)$",
+              r"\1{0}".format(
+                  """\n
 #include <stdexcept>
 
 inline void assert_throw(bool assertion)
@@ -83,17 +86,41 @@ inline void assert_throw(bool assertion)
                 ),
             )
 
-            sff = FileFilter(*(glob.glob("*.cxx")))
-            sff.filter(r"(\b)(assert)(\b)", r"\1assert_throw\3")
+      sff = FileFilter(*(glob.glob("*.cxx")))
+      sff.filter(r"(\b)(assert)(\b)", r"\1assert_throw\3")
 
-    def install(self, spec, prefix):
-        make("tetgen", "tetlib")
+class MakefileBuilder(makefile.MakefileBuilder):
+    def build(self, pkg, spec, prefix):
+      make("tetgen", "tetlib")
 
-        mkdirp(prefix.bin)
-        install("tetgen", prefix.bin)
+    def install(self, pkg, spec, prefix):
+      mkdirp(prefix.bin)
+      install("tetgen", prefix.bin)
 
-        mkdirp(prefix.include)
-        install("tetgen.h", prefix.include)
+      mkdirp(prefix.include)
+      install("tetgen.h", prefix.include)
 
-        mkdirp(prefix.lib)
-        install("libtet.a", prefix.lib)
+      mkdirp(prefix.lib)
+      install("libtet.a", prefix.lib)
+  
+class CMakeBuilder(cmake.CMakeBuilder):
+    def cmake_args(self):
+      args = [
+          self.pkg.define_from_variant("CMAKE_POSITION_INDEPENDENT_CODE", "pic"),
+          self. pkg.define_from_variant("BUILD_SHARED_LIBS", "shared"),
+      ]
+
+      return args
+
+    @run_before("cmake")
+    def run_before_cmake_is_invoked(self) -> None:
+      print("self is", dir(self))
+      with open(join_path(os.path.dirname(__file__), "CMakeLists.txt.in"), 'r') as file:
+        cmakelists_content = file.read()
+
+      cmakelists_content = cmakelists_content.replace("%VERSION%", str(self.pkg.version))
+
+      with open("CMakeLists.txt", "w") as file:
+        file.write(cmakelists_content)
+
+      copy(join_path(os.path.dirname(__file__), "tetgen-config.cmake.in"), "tetgen-config.cmake.in")
