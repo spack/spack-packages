bc4061092e8441c17ccfbe8758dee0954353cda4
Maciej Wójcik <w8jcik@gmail.com>
2026-07-02 10:46:09 +0300

    r-annotationdbi: add @1.60.2:1.74

--- repos/spack_repo/builtin/packages/r_annotationdbi/package.py
+++ repos/spack_repo/builtin/packages/r_annotationdbi/package.py
@@ -16,7 +16,15 @@ class RAnnotationdbi(RPackage):
     bioc = "AnnotationDbi"
 
     with default_args(get_full_repo=True):
+        version("1.74.0", commit="c7dbd19487bbf100ad1a6ab58b475c5ec001cc4b")  # bioc 3.23
+        version("1.72.0", commit="ffdaf5d5dda16995f1afb7276be8f96cf738e16b")  # bioc 3.22
+        version("1.70.0", commit="a8184e6a1b0bc175040118c73fd22062ddcd3392")  # bioc 3.21
+        version("1.68.0", commit="6a2aa3361bb114fbfe7a25b51bcaed36450a57e0")  # bioc 3.20
+        version("1.66.0", commit="989c1dcf56db17646e79bd7caa70484e4cda73d1")  # bioc 3.19
+        version("1.64.1", commit="e5b997eac9f538d6ad5418fbe90716848d8c5f2e")  # bioc 3.18
+        version("1.62.2", commit="baefc543c43b039c6ebf28acb4f82be3e491b8c5")  # bioc 3.17
         version("1.62.0", commit="7ca03a0332d0a284ea27d16edb7b386c86cf99ea")
+        version("1.60.2", commit="eebebb2401fc57a7a9a103e77bf18ff06dd7b2a8")  # bioc 3.16
         version("1.60.0", commit="cd61bd1b1538e2f1f411fd7087820749ecf39da8")
         version("1.58.0", commit="05fcf7a28a6b15b195da23474d7ba89bd0cfd891")
         version("1.56.2", commit="13fdc4a93852199ca6ec120a2fe1078f9f445f67")
@@ -28,12 +36,19 @@ class RAnnotationdbi(RPackage):
         version("1.38.2", commit="67d46facba8c15fa5f0eb47c4e39b53dbdc67c36")
 
     depends_on("r@2.7.0:", type=("build", "run"))
-    depends_on("r-biocgenerics@0.15.10:", type=("build", "run"))
-    depends_on("r-biocgenerics@0.23.1:", type=("build", "run"), when="@1.40.0:")
-    depends_on("r-biocgenerics@0.29.2:", type=("build", "run"), when="@1.46.1:")
+
     depends_on("r-biobase@1.17.0:", type=("build", "run"))
-    depends_on("r-iranges", type=("build", "run"))
+
+    depends_on("r-biocgenerics@0.29.2:", type=("build", "run"), when="@1.46.1:")
+    depends_on("r-biocgenerics@0.23.1:", type=("build", "run"), when="@1.40.0:")
+    depends_on("r-biocgenerics@0.15.10:", type=("build", "run"))
+
     depends_on("r-dbi", type=("build", "run"))
-    depends_on("r-rsqlite", type=("build", "run"))
-    depends_on("r-s4vectors@0.9.25:", type=("build", "run"))
+
+    depends_on("r-iranges", type=("build", "run"))
+
     depends_on("r-keggrest", type=("build", "run"), when="@1.56.2:")
+
+    depends_on("r-rsqlite", type=("build", "run"))
+
+    depends_on("r-s4vectors@0.9.25:", type=("build", "run"))
