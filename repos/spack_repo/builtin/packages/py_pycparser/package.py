875180a27eb01e396ce83b1ef34afb0330c4563e
Thomas Bouvier <contact@thomas-bouvier.io>
2026-07-06 21:44:59 +0200

    py-pycparser: add comment

--- repos/spack_repo/builtin/packages/py_pycparser/package.py
+++ repos/spack_repo/builtin/packages/py_pycparser/package.py
@@ -27,6 +27,9 @@ class PyPycparser(PythonPackage):
     with default_args(type="build"):
         depends_on("c")
 
+        # Upstream repo indicates py-setuptools@69, but a newer version
+        # is required to compile this package.
+        # See: https://github.com/spack/spack-packages/pull/5356
         depends_on("py-setuptools@77:", when="@3:")
         depends_on("py-setuptools")
 
