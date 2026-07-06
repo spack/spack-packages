728e25d625326b2b01d6b6d65fa3ce7ff1529881
Thomas Bouvier <contact@thomas-bouvier.io>
2026-07-06 10:22:29 +0200

    py-iops-benchmark: add new package

---
+++ repos/spack_repo/builtin/packages/py_iops_benchmark/package.py
@@ -0,0 +1,24 @@
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack_repo.builtin.build_systems.python import PythonPackage
+
+from spack.package import *
+
+
+class PyIopsBenchmark(PythonPackage):
+    """A generic benchmark orchestration framework for automated parametric experiments."""
+
+    homepage = "https://gitlab.inria.fr/lgouveia/iops"
+    pypi = "iops_benchmark/iops_benchmark-3.5.8.tar.gz"
+
+    license("BSD-3-Clause")
+
+    version("3.5.8", sha256="04b77c9513702d081fd0af3ba48211dace4bb4425482e51c17684832f2970984")
+
+    depends_on("py-setuptools@61:", type="build")
+    depends_on("py-pyyaml", type=("build", "run"))
+    depends_on("py-pandas@2.1:", type=("build", "run"))
+    depends_on("py-jinja2@3:", type=("build", "run"))
+    depends_on("py-plotly", type=("build", "run"))
