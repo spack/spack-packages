525d3fdfc7545dba8499fe1fa7dc73685bea40cf
gabe.turner <gabe.turner@sana.com>
2026-07-06 14:08:57 -0500

    Add gomplate 5.1.0

--- repos/spack_repo/builtin/packages/gomplate/package.py
+++ repos/spack_repo/builtin/packages/gomplate/package.py
@@ -20,8 +20,11 @@ class Gomplate(GoPackage):
 
     license("MIT")
 
+    version("5.1.0", sha256="b6763aaf2c52a2e57a02f5e4cae199166b1ae8df8beb43ef5c927bb10ca775fc")
     version("4.3.3", sha256="d15c66230d72bdc13b0155f28d391c55cac45b7fdbe1ff4a73db8ee263471a3d")
 
+    depends_on("go@1.26:", type="build", when="@5.1.0:")
+    depends_on("go@1.25:", type="build", when="@5:")
     depends_on("go@1.24.5:", type="build")
 
     build_directory = "cmd/gomplate"
