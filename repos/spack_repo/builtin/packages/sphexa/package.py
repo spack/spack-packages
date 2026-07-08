7eaf94ae1e961132a44819b413ff9ea69772e898
jgphpc <jgp@cscs.ch>
2026-07-08 10:41:08 +0200

    Fix for review

--- repos/spack_repo/builtin/packages/sphexa/package.py
+++ repos/spack_repo/builtin/packages/sphexa/package.py
@@ -20,6 +20,7 @@ class Sphexa(CMakePackage, CudaPackage, ROCmPackage):
 
     license("MIT")
 
+    version("0.96.2", sha256="2ff6edb422eadf47634f98d20258458c82374efe617de0d0c3d5bb3d7945be23")
     version("0.95", sha256="1007ffa97eb2085d50173676ec5e6387d1da7a8b78f204308223fbdbbecc60a1")
     version("0.93.1", sha256="95a93d0063ac8857b9be12c1aca24f5b2eef9dd4ffe8cf3f6b552a4dd54b940f")
     version("develop", branch="develop")
@@ -32,7 +33,8 @@ class Sphexa(CMakePackage, CudaPackage, ROCmPackage):
     variant("hdf5", default=True, description="Enable support for HDF5 I/O")
     variant("gpu_aware_mpi", default=True, description="GPU aware MPI")
 
-    depends_on("cmake@3.22:", type="build")
+    depends_on("cmake@3.24:", when="@0.95:")
+    depends_on("cmake@3.22:", when=":@0.93.1")
     depends_on("c", type="build")
     depends_on("cxx", type="build")
 
@@ -43,7 +45,8 @@ class Sphexa(CMakePackage, CudaPackage, ROCmPackage):
     depends_on("rocthrust", when="+rocm")
     depends_on("hipcub", when="+rocm")
     depends_on("hdf5 +mpi", when="+hdf5")
-    depends_on("h5hut@master", when="@0.95: +hdf5")
+    # h5hut is built internally by sphexa, disabling dependency for now
+    # depends_on("h5hut@master", when="@0.95: +hdf5")
 
     # Build MPI with GPU support when GPU aware MPI is requested.
     # For cray-mpich, the user is responsible to configure it for GPU aware MPI.
@@ -81,8 +84,6 @@ class Sphexa(CMakePackage, CudaPackage, ROCmPackage):
             self.define_from_variant("RYOANJI_WITH_HIP", "rocm"),
             self.define_from_variant("CSTONE_WITH_HIP", "rocm"),
         ]
-        # args.append('-DCMAKE_C_COMPILER=mpicc')
-        # args.append('-DCMAKE_CXX_COMPILER=mpicxx')
 
         if spec.satisfies("+rocm") or spec.satisfies("+cuda"):
             args.append(self.define_from_variant("CSTONE_WITH_GPU_AWARE_MPI", "gpu_aware_mpi"))
