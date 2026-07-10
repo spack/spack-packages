a76580ba0d45dc94e4cccf09a57eef597a77544f
Richard Berger <richard.berger@outlook.com>
2026-07-10 01:24:33 -0600

    Update repos/spack_repo/builtin/packages/opencl_clhpp/package.py
    Co-authored-by: Bernhard Kaindl <bernhardkaindl7@gmail.com>

--- repos/spack_repo/builtin/packages/opencl_clhpp/package.py
+++ repos/spack_repo/builtin/packages/opencl_clhpp/package.py
@@ -53,7 +53,6 @@ class OpenclClhpp(CMakePackage):
     version("2.0.12", sha256="20b28709ce74d3602f1a946d78a2024c1f6b0ef51358b9686612669897a58719")
     version("2.0.11", sha256="ffc2ca08cf4ae90ee55f14ea3735ccc388f454f4422b69498b2e9b93a1d45181")
     version("2.0.10", sha256="fa27456295c3fa534ce824eb0314190a8b3ebd3ba4d93a0b1270fc65bf378f2b")
-    version("2.0.9", sha256="ba8ac4977650d833804f208a1b0c198006c65c5eac7c83b25dc32cea6199f58c")
 
     depends_on("c", type="build", when="@:2")
     depends_on("cxx", type="build")
