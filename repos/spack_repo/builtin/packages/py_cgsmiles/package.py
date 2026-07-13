f62f9e369fd3b3d4270821fcba579dbeda9a4fc8
Adam Witmer <adamwitmer@gmail.com>
2026-07-13 10:26:34 -0600

    remove old versions

--- repos/spack_repo/builtin/packages/py_cgsmiles/package.py
+++ repos/spack_repo/builtin/packages/py_cgsmiles/package.py
@@ -11,14 +11,12 @@ class PyCgsmiles(PythonPackage):
     """Coarse-Grained SMILES (CGsmiles)"""
 
     homepage = "https://github.com/gruenewald-lab/CGsmiles#"
-    pypi = "cgsmiles/cgsmiles-1.0.0.tar.gz"
+    pypi = "cgsmiles/cgsmiles-1.0.2.tar.gz"
     git = "https://github.com/gruenewald-lab/CGsmiles.git"
 
     license("Apache-2.0")
     maintainers("adamwitmer")
 
-    version("1.0.0", sha256="83fdb6dadfc4efa065fb8ef66af5d461ec50a629680ba6faf117877f37174aa9")
-    version("1.0.1", sha256="e265f2cbc87e7a406817606c993c14e8c8d530426b39b20eec0a2aff9b593d22")
     version("1.0.2", sha256="d74a21b5c54139d980ff457946b99a56364294cad46bf6147c0d166a67dfafca")
 
     depends_on("py-networkx@2.0:", type=("build", "run"))
