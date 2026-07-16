f3227f1ba14e36495cb9d19845f965043761a7cb
Markus Vieth <vieth@uni-mainz.de>
2026-07-16 11:16:54 +0200

    Add CUDA and CXX dependencies

--- repos/spack_repo/builtin/packages/nvbench/package.py
+++ repos/spack_repo/builtin/packages/nvbench/package.py
@@ -3,11 +3,12 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 from spack_repo.builtin.build_systems.cmake import CMakePackage
+from spack_repo.builtin.build_systems.cuda import CudaPackage
 
 from spack.package import *
 
 
-class Nvbench(CMakePackage):
+class Nvbench(CudaPackage, CMakePackage):
     """NVBench is a C++17 library designed to simplify CUDA kernel benchmarking.
     It solves challenges inherent to CUDA kernel benchmarking."""
 
@@ -22,4 +23,13 @@ class Nvbench(CMakePackage):
     license("Apache-2.0 WITH LLVM-exception", checked_by="gusser93")
 
     version("main", branch="main")
+
+    variant("cuda", default=True, description="Build with CUDA")
+
     depends_on("cmake@4:", type="build")
+    depends_on("cxx")
+    depends_on("cuda@12")
+
+    requires("+cuda")
+    requires("%gcc@7:", when="%gcc")
+    requires("%clang@14:", when="%clang")
