7e39195989f463f03d4394ada4f0b2c24f9a1fdc
Wouter Deconinck <wdconinc@gmail.com>
2026-07-19 07:20:03 -0500

    py-wxpython: add v4.2.5

--- repos/spack_repo/builtin/packages/py_wxpython/package.py
+++ repos/spack_repo/builtin/packages/py_wxpython/package.py
@@ -11,8 +11,11 @@ class PyWxpython(PythonPackage):
     """Cross platform GUI toolkit for Python."""
 
     homepage = "https://www.wxpython.org/"
-    pypi = "wxPython/wxPython-4.0.6.tar.gz"
+    pypi = "wxpython/wxpython-4.0.6.tar.gz"
+    git = "https://github.com/wxWidgets/Phoenix.git"
 
+    version("4.2.5", sha256="44e836d1bccd99c38790bb034b6ecf70d9060f6734320560f7c4b0d006144793")
+    version("4.2.4", sha256="2eb123979c87bcb329e8a2452269d60ff8f9f651e9bf25c67579e53c4ebbae3c")
     version("4.2.3", sha256="20d6e0c927e27ced85643719bd63e9f7fd501df6e9a8aab1489b039897fd7c01")
     version("4.2.2", sha256="5dbcb0650f67fdc2c5965795a255ffaa3d7b09fb149aa8da2d0d9aa44e38e2ba")
     version("4.1.1", sha256="00e5e3180ac7f2852f342ad341d57c44e7e4326de0b550b9a5c4a8361b6c3528")
@@ -23,10 +26,15 @@ class PyWxpython(PythonPackage):
 
     # Versions before 4.2.3 require distutils which is removed in python 3.12
     depends_on("python@:3.11", when="@:4.2.2")
+    # Pre-generated Cython C files fail to build with free-threaded Python
+    # https://github.com/wxWidgets/Phoenix/issues/2707
+    conflicts("^python+freethreading", when="@:4.2.3")
 
     depends_on("wxwidgets +gui")
     depends_on("wxwidgets@3.2.6 +gui", when="@4.2.2")
     depends_on("wxwidgets@3.2.7 +gui", when="@4.2.3")
+    depends_on("wxwidgets@3.2.8.1 +gui", when="@4.2.4")
+    depends_on("wxwidgets@3.2.9 +gui", when="@4.2.5")
 
     # Needed by the buildtools/config.py script
     depends_on("pkgconfig", type="build")
@@ -40,6 +48,12 @@ class PyWxpython(PythonPackage):
     depends_on("py-numpy", type=("build", "run"))
     depends_on("pil", type=("build", "run"))
     depends_on("py-six", type=("build", "run"))
+
+    def url_for_version(self, version):
+        url = "https://files.pythonhosted.org/packages/source/w/wxPython/wxPython-{0}.tar.gz"
+        if version >= Version("4.2.3"):
+            url = url.lower()
+        return url.format(version)
 
     def setup_build_environment(self, env: EnvironmentModifications) -> None:
         # By default wxWdigets is built as well instead of using spack provided version,
