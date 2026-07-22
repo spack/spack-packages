9e865c5cd30dcdc529a3d4e76d566078c80eed9e
dtaller <66029857+dtaller@users.noreply.github.com>
2026-07-22 22:10:46 +0000

    [@spackbot] updating style on behalf of dtaller

--- repos/spack_repo/builtin/packages/polyclipper/package.py
+++ repos/spack_repo/builtin/packages/polyclipper/package.py
@@ -1,5 +1,5 @@
 # Copyright Spack Project Developers. See COPYRIGHT file for details.
-# 
+#
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 from spack_repo.builtin.build_systems.cmake import CMakePackage
