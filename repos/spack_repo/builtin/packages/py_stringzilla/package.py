4c1e419462c1a2c20ee51d7e46bccfcdd7ee4f9a
Jen Herting <jen@herting.cc>
2026-07-22 09:35:11 -0400

    [py-stringzilla] added version 5.0.3

--- repos/spack_repo/builtin/packages/py_stringzilla/package.py
+++ repos/spack_repo/builtin/packages/py_stringzilla/package.py
@@ -15,9 +15,11 @@ class PyStringzilla(PythonPackage):
 
     license("Apache-2.0")
 
+    version("5.0.3", sha256="804806a1ffc10b87c558d007aa68443f3b5eba78adbcd4acdcff97c18ff6cb5f")
     version("4.2.1", sha256="fd15835ab3b78b09dba678c66b36715bcf7f9e550994ea09abcc8eb7a5e1c9f7")
 
     depends_on("c", type="build")
     depends_on("cxx", type="build")
     depends_on("python@3.8:", type=("build", "run"))
+    depends_on("python@3.10:", type=("build", "run"), when="@5.0.3:")
     depends_on("py-setuptools@61:", type="build")
