249398764b49a66cfc7e7a07f7c4486f3eca9c98
Daniel Taller <taller1@llnl.gov>
2026-07-22 16:20:16 -0700

    try to fix invalid checksum

--- repos/spack_repo/builtin/packages/spheral/package.py
+++ repos/spack_repo/builtin/packages/spheral/package.py
@@ -29,9 +29,7 @@ class Spheral(CMakePackage):
     version("2023.03.1", sha256="a4e818fdb7d5d1fa9b67247ef2a0e5b80f3346a87da166112a4b1944f79df211")
     version("2023.03.0", sha256="52544b9f031914a2b625d8f9ec822348e4b61076980a0029ac34c07ab459dc1c")
     version("2022.6.1", sha256="d2efc04b1e82d711089ce54c7843d219b17c97eca093a8c19c410abb35d57118")
-    version("2022.6.0", sha256="f1c8668c47b721f62206bc9f8e57b0e98fb9c171d858dd9ea8b4b4db4bfa9711")
-    version("2022.06.0", sha256="5267db6a0ce4ce4394db4c6eb1588180e80cc2954b03209142ee3571c1254e9b")
-
+    
     # Define variants
     variant("mpi", default=True, description="Enable MPI Support.")
     variant("openmp", default=True, description="Enable OpenMP Support.")
