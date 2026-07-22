4af481dd5c768a2909a353e407b79bfa72cc88ae
Renat Sibgatulin <sibgatulin@gea.mpg.de>
2026-07-22 18:27:19 +0200

    ruff format the changes

--- repos/spack_repo/builtin/packages/admixtools/package.py
+++ repos/spack_repo/builtin/packages/admixtools/package.py
@@ -50,7 +50,8 @@ class Admixtools(MakefilePackage):
 
         makefile.filter(
             "override LDLIBS += -lgsl -lopenblas -lm -lnick",
-            "override LDLIBS += -lgsl -lm -lnick " + lapackflags, string=True
+            "override LDLIBS += -lgsl -lm -lnick " + lapackflags,
+            string=True,
         )
 
         makefile.filter("HOMEL=$(PWD)", "HOMEL=$(CURDIR)", string=True)
