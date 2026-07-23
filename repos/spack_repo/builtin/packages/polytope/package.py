f7e780ed5c9384b09fe81b182a6a9ded87ec6876
Daniel Taller <taller1@llnl.gov>
2026-07-23 09:29:34 -0700

    remove urls and pray

--- repos/spack_repo/builtin/packages/polytope/package.py
+++ repos/spack_repo/builtin/packages/polytope/package.py
@@ -22,53 +22,43 @@ class Polytope(CMakePackage, CudaPackage, ROCmPackage):
 
     version(
         "0.7.5",
-        sha256="ee249cfbb38632a704d177bb3269124ab7b227d29a6c36c9857e822cf4df0430",
-        url="https://github.com/llnl/polytope/archive/refs/tags/v0.7.5.tar.gz",
+        sha256="ee249cfbb38632a704d177bb3269124ab7b227d29a6c36c9857e822cf4df0430"
     )
     version(
         "0.7.4",
-        sha256="a1901b0feaf3c4d3660766e8a6c325c0fe1fcaeb236b5ae95c8d139c386147c8",
-        url="https://github.com/llnl/polytope/archive/refs/tags/0.7.4.tar.gz",
+        sha256="a1901b0feaf3c4d3660766e8a6c325c0fe1fcaeb236b5ae95c8d139c386147c8"
     )
     version(
         "0.7.3",
-        sha256="f32817b44d2a3b98407531980b89d0a31b0c14b8b30de37a6a7bc6ec91e48bf1",
-        url="https://github.com/llnl/polytope/archive/refs/tags/0.7.3.tar.gz",
+        sha256="f32817b44d2a3b98407531980b89d0a31b0c14b8b30de37a6a7bc6ec91e48bf1"
     )
     version(
         "0.7.2",
-        sha256="94a42ac30226da28ec07ad06b745101235c6aa45e8e8e1d218c8e991837b6867",
-        url="https://github.com/llnl/polytope/archive/refs/tags/0.7.2.tar.gz",
+        sha256="94a42ac30226da28ec07ad06b745101235c6aa45e8e8e1d218c8e991837b6867"
     )
     version(
         "0.7.1",
-        sha256="83b12db2a2dc419488e986e40f32b085824ca0345f2958a48e66547f2e4f60fa",
-        url="https://github.com/llnl/polytope/archive/refs/tags/0.7.1.tar.gz",
+        sha256="83b12db2a2dc419488e986e40f32b085824ca0345f2958a48e66547f2e4f60fa"
     )
     version(
         "0.7.0",
-        sha256="e7be5a5d06a95309b9100f02ab80c2f3401c11ac6304a3204fb1f25052efd77e",
-        url="https://github.com/llnl/polytope/archive/refs/tags/0.7.0.tar.gz",
+        sha256="e7be5a5d06a95309b9100f02ab80c2f3401c11ac6304a3204fb1f25052efd77e"
     )
     version(
         "0.6.2",
-        sha256="e9ed18c3ebc7b4b231a0563235cc032c26daa7de88839c64141975170e55bcfd",
-        url="https://github.com/llnl/polytope/archive/refs/tags/0.6.2.tar.gz",
+        sha256="e9ed18c3ebc7b4b231a0563235cc032c26daa7de88839c64141975170e55bcfd"
     )
     version(
         "0.6.1",
-        sha256="f4f35e327d788305f7f7b95f90eda928f6632c58fc1a272bc5d208e54ba2a27f",
-        url="https://github.com/llnl/polytope/archive/refs/tags/0.6.1.tar.gz",
+        sha256="f4f35e327d788305f7f7b95f90eda928f6632c58fc1a272bc5d208e54ba2a27f"
     )
     version(
         "0.6.0",
-        sha256="25b10759d784de2f2ea0b93200194b69319079b68c73785d1aaa397cacce2eea",
-        url="https://github.com/llnl/polytope/archive/refs/tags/0.6.0.tar.gz",
+        sha256="25b10759d784de2f2ea0b93200194b69319079b68c73785d1aaa397cacce2eea"
     )
     version(
         "0.5.24",
-        sha256="ff45624cfc522d62f21c69ac8aa90477bf00d6ee472f4e0a07b508702304fa57",
-        url="https://github.com/llnl/polytope/archive/refs/tags/0.5.24.tar.gz",
+        sha256="ff45624cfc522d62f21c69ac8aa90477bf00d6ee472f4e0a07b508702304fa57"
     )
 
     variant("mpi", default=True, description="Enable MPI support")
