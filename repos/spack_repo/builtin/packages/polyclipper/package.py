95fb21d24414961293fc7dfaf1f57f51430bcda1
Daniel Taller <taller1@llnl.gov>
2026-07-23 10:20:21 -0700

    maintainer, versions

--- repos/spack_repo/builtin/packages/polyclipper/package.py
+++ repos/spack_repo/builtin/packages/polyclipper/package.py
@@ -14,10 +14,42 @@ class Polyclipper(CMakePackage):
     url = "https://github.com/llnl/PolyClipper/archive/refs/tags/v1.2.6.tar.gz"
     git = "https://github.com/llnl/PolyClipper.git"
 
-    maintainers("jmikeowen")
+    maintainers("jmikeowen", "ldowen")
     license("BSD-3-Clause")
 
-    version("1.2.6", sha256="ffce2fe36fb888b7aaf93d4b3591b0875909573537ca39c0730b7d85bbc5558c")
+    version(
+        "1.2.6",
+        sha256="ffce2fe36fb888b7aaf93d4b3591b0875909573537ca39c0730b7d85bbc5558c",
+        url="https://github.com/llnl/PolyClipper/archive/refs/tags/v1.2.6.tar.gz",
+    )
+    version(
+        "1.2.5",
+        sha256="914b3f4bcc89f3c63f66b80cf3a45daa772dc71f3bdbc2ee4d312d7c5bbe60f3",
+        url="https://github.com/llnl/PolyClipper/archive/refs/tags/1.2.5.tar.gz",
+    )
+    version(
+        "1.2.4",
+        sha256="02066fbf34b8bdbd22414514583c481c573cbb003e6dace398bc78678d967d38",
+        url="https://github.com/llnl/PolyClipper/archive/refs/tags/1.2.4.tar.gz",
+    )
+    version(
+        "1.2.3",
+        sha256="5c2ed2202db3b4172703a9d6d219ba5dc9d4e9276791d4e45cc201c1974c3572",
+        url="https://github.com/llnl/PolyClipper/archive/refs/tags/v1.2.3.tar.gz",
+    )
+    version(
+        "1.2.2",
+        sha256="fd7ca794c1189bde1563b9704b371c65d7ba94b3582351fa2c3e0dd487b431da",
+        url="https://github.com/llnl/PolyClipper/archive/refs/tags/v1.2.2.tar.gz",
+    )
+
+
+    def url_for_version(self, version):
+        if version <= Version("1.2.5"):
+            url = "https://github.com/llnl/PolyClipper/archive/refs/tags/{0}.tar.gz"
+        else:
+            url = "https://github.com/llnl/PolyClipper/archive/refs/tags/v{0}.tar.gz"
+        return url.format(version)
 
     with default_args(type="build"):
         depends_on("blt")
