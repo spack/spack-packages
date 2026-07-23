0c41896f34ceda6e22f45ef61d93abf53fdace07
Daniel Taller <taller1@llnl.gov>
2026-07-23 10:33:19 -0700

    turn on submodels as project author suggests

--- repos/spack_repo/builtin/packages/polyclipper/package.py
+++ repos/spack_repo/builtin/packages/polyclipper/package.py
@@ -17,11 +17,11 @@ class Polyclipper(CMakePackage):
     maintainers("jmikeowen", "ldowen")
     license("BSD-3-Clause")
 
-    version("1.2.6", sha256="ffce2fe36fb888b7aaf93d4b3591b0875909573537ca39c0730b7d85bbc5558c")
-    version("1.2.5", sha256="914b3f4bcc89f3c63f66b80cf3a45daa772dc71f3bdbc2ee4d312d7c5bbe60f3")
-    version("1.2.4", sha256="02066fbf34b8bdbd22414514583c481c573cbb003e6dace398bc78678d967d38")
-    version("1.2.3", sha256="5c2ed2202db3b4172703a9d6d219ba5dc9d4e9276791d4e45cc201c1974c3572")
-    version("1.2.2", sha256="fd7ca794c1189bde1563b9704b371c65d7ba94b3582351fa2c3e0dd487b431da")
+    version("1.2.6", sha256="ffce2fe36fb888b7aaf93d4b3591b0875909573537ca39c0730b7d85bbc5558c", submodules=True)
+    version("1.2.5", sha256="914b3f4bcc89f3c63f66b80cf3a45daa772dc71f3bdbc2ee4d312d7c5bbe60f3", submodules=True)
+    version("1.2.4", sha256="02066fbf34b8bdbd22414514583c481c573cbb003e6dace398bc78678d967d38", submodules=True)
+    version("1.2.3", sha256="5c2ed2202db3b4172703a9d6d219ba5dc9d4e9276791d4e45cc201c1974c3572", submodules=True)
+    version("1.2.2", sha256="fd7ca794c1189bde1563b9704b371c65d7ba94b3582351fa2c3e0dd487b431da", submodules=True)
 
     variant("python", default=False, description="Enable python bindings.")
     depends_on("python@3.10:", when="+python")
