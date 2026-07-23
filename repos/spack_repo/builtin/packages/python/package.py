ff67bd3116da8f7c06e7d91e4810442ebd12718c
John Parent <john.parent@kitware.com>
2026-07-23 18:06:28 -0400

    style
    Signed-off-by: John Parent <john.parent@kitware.com>

--- repos/spack_repo/builtin/packages/python/package.py
+++ repos/spack_repo/builtin/packages/python/package.py
@@ -253,8 +253,8 @@ class Python(Package):
     patch(
         "https://github.com/python/cpython/commit/106eb532ea3b243423e62a702719e9d3c0e40c16.patch?full_index=1",
         sha256="d01422e6c6cb1df21161f5fa261cfd12853756abbddf816543934f3fe96a5827",
-        when="@3.14:3.14.4 platform=windows"
-        )
+        when="@3.14:3.14.4 platform=windows",
+    )
 
     patch(
         "https://bugs.python.org/file44413/alignment.patch",
@@ -450,7 +450,7 @@ class Python(Package):
         if sys.platform == "win32":
             # on windows, python tries to find another python and if it fails to do
             # so, downloads one via nuget.
-            # Just use Spack's python 
+            # Just use Spack's python
             # temporary fix until the branch moving python to only use Spack based
             # dependencies can land.
             env.set("PYTHON_FOR_BUILD", sys.executable)
