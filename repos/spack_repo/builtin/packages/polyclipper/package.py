38885bfa5870532df7f04b978b9df5fa55d7897d
Daniel Taller <taller1@llnl.gov>
2026-07-23 09:18:05 -0700

    attempt to remove urls and hope for the best

--- repos/spack_repo/builtin/packages/polyclipper/package.py
+++ repos/spack_repo/builtin/packages/polyclipper/package.py
@@ -10,7 +10,7 @@ from spack.package import *
 class Polyclipper(CMakePackage):
     """Library for polyhedral clipping planes."""
 
-    homepage = "https://github.com/LLNL/PolyClipper"
+    homepage = "https://github.com/llnl/PolyClipper"
     url = "https://github.com/llnl/PolyClipper/archive/refs/tags/v1.2.6.tar.gz"
     git = "https://github.com/llnl/PolyClipper.git"
 
@@ -19,40 +19,25 @@ class Polyclipper(CMakePackage):
 
     version(
         "1.2.6",
-        sha256="ffce2fe36fb888b7aaf93d4b3591b0875909573537ca39c0730b7d85bbc5558c",
-        url="https://github.com/llnl/PolyClipper/archive/refs/tags/v1.2.6.tar.gz",
+        sha256="ffce2fe36fb888b7aaf93d4b3591b0875909573537ca39c0730b7d85bbc5558c"
     )
     version(
         "1.2.5",
-        sha256="914b3f4bcc89f3c63f66b80cf3a45daa772dc71f3bdbc2ee4d312d7c5bbe60f3",
-        url="https://github.com/llnl/PolyClipper/archive/refs/tags/1.2.5.tar.gz",
+        sha256="914b3f4bcc89f3c63f66b80cf3a45daa772dc71f3bdbc2ee4d312d7c5bbe60f3"
     )
     version(
         "1.2.4",
-        sha256="02066fbf34b8bdbd22414514583c481c573cbb003e6dace398bc78678d967d38",
-        url="https://github.com/llnl/PolyClipper/archive/refs/tags/1.2.4.tar.gz",
+        sha256="02066fbf34b8bdbd22414514583c481c573cbb003e6dace398bc78678d967d38"
     )
     version(
         "1.2.3",
-        sha256="5c2ed2202db3b4172703a9d6d219ba5dc9d4e9276791d4e45cc201c1974c3572",
-        url="https://github.com/llnl/PolyClipper/archive/refs/tags/v1.2.3.tar.gz",
+        sha256="5c2ed2202db3b4172703a9d6d219ba5dc9d4e9276791d4e45cc201c1974c3572"
     )
     version(
         "1.2.2",
-        sha256="fd7ca794c1189bde1563b9704b371c65d7ba94b3582351fa2c3e0dd487b431da",
-        url="https://github.com/llnl/PolyClipper/archive/refs/tags/v1.2.2.tar.gz",
+        sha256="fd7ca794c1189bde1563b9704b371c65d7ba94b3582351fa2c3e0dd487b431da"
     )
-    version(
-        "1.01",
-        sha256="88196e83fd2e17586d8258332a6677cdaa0695f624285c043a010f71d7f323a4",
-        url="https://github.com/llnl/PolyClipper/archive/refs/tags/v1.01.tar.gz",
-    )
-    version(
-        "1.0",
-        sha256="4b9094d5cc83239cc28ec30fe8048220eaa73ef51bafcffe027f9bbaec050fd2",
-        url="https://github.com/llnl/PolyClipper/archive/refs/tags/v1.0.tar.gz",
-    )
-
+    
     with default_args(type="build"):
         depends_on("blt")
         depends_on("cmake@3.20:")
