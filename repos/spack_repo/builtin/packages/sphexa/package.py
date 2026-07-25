19966beb8c45032a85ff949de64f04076100ff4c
jgphpc <jgphpc@users.noreply.github.com>
2026-07-25 00:06:04 +0000

    [@spackbot] updating style on behalf of jgphpc

--- repos/spack_repo/builtin/packages/sphexa/package.py
+++ repos/spack_repo/builtin/packages/sphexa/package.py
@@ -20,9 +20,15 @@ class Sphexa(CMakePackage, CudaPackage, ROCmPackage):
 
     license("MIT")
 
-    version("0.96.2", tag="v0.96.2", commit="03cc0ca2e770098430677b4a6613e35b6cab793d", submodules=True)
-    version("0.95", tag="v0.95", commit="5e12d2926065cf376de30e8cfd082c4bfed78000", submodules=True)
-    version("0.93.1", tag="v0.93.1", commit="4be3f10a95f260d4e3fb901e98a8336ac6af2478", submodules=True)
+    version(
+        "0.96.2", tag="v0.96.2", commit="03cc0ca2e770098430677b4a6613e35b6cab793d", submodules=True
+    )
+    version(
+        "0.95", tag="v0.95", commit="5e12d2926065cf376de30e8cfd082c4bfed78000", submodules=True
+    )
+    version(
+        "0.93.1", tag="v0.93.1", commit="4be3f10a95f260d4e3fb901e98a8336ac6af2478", submodules=True
+    )
     version("develop", branch="develop", submodules=True)
 
     variant("tests", default=False, description="Enable unit and integration tests")
