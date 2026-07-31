3b566ac23aa13f396160ed338bcb8f2bcfd2bd24
Jen Herting <jen@herting.cc>
2026-07-31 16:04:53 -0400

    [py-array-api-compat] added version 1.15.0

--- repos/spack_repo/builtin/packages/py_array_api_compat/package.py
+++ repos/spack_repo/builtin/packages/py_array_api_compat/package.py
@@ -16,10 +16,14 @@ class PyArrayApiCompat(PythonPackage):
 
     license("MIT")
 
+    version("1.15.0", sha256="53c5f922491bf15f62847afafc4e39eedfae57d218988fefb8cce39c2a9b3dea")
     version("1.14.0", sha256="c819ba707f5c507800cb545f7e6348ff1ecc46538381d9ad9b371ffc9cd6d784")
 
     depends_on("python@3.10:", type=("build", "run"))
 
     with default_args(type="build"):
-        depends_on("py-setuptools")
-        depends_on("py-setuptools-scm")
+        depends_on("py-meson-python", when="@1.15:")
+
+        # Historical
+        depends_on("py-setuptools", when="@:1.14")
+        depends_on("py-setuptools-scm", when="@:1.14")
