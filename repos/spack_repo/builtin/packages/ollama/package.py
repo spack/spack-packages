9bb27e5dfec8d068d9fee3a5e77a45e0fe817d2f
imciner2 <imciner2@users.noreply.github.com>
2026-08-06 15:57:09 +0000

    [@spackbot] updating style on behalf of imciner2

--- repos/spack_repo/builtin/packages/ollama/package.py
+++ repos/spack_repo/builtin/packages/ollama/package.py
@@ -9,6 +9,7 @@ from spack_repo.builtin.build_systems.go import GoPackage
 
 from spack.package import *
 
+
 class Ollama(CMakePackage, GoPackage, CudaPackage):
     """Run Llama 2, Code Llama, and other models. Customize and create your own."""
 
