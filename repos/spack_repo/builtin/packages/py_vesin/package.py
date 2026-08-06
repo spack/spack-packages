11c236e59503fd910d2bf6cf8dd3cfe70736e5df
Rocco Meli <r.meli@bluemail.ch>
2026-08-06 13:48:47 +0200

    py-vesin: v0.6.1

--- repos/spack_repo/builtin/packages/py_vesin/package.py
+++ repos/spack_repo/builtin/packages/py_vesin/package.py
@@ -18,6 +18,7 @@ class PyVesin(PythonPackage):
     maintainers("HaoZeke", "Luthaf", "RMeli")
     license("BSD-3-Clause", checked_by="HaoZeke")
 
+    version("0.6.1", sha256="d76b8c7f21551a494c7bf7697a8c05986b8179bd2d3683f0d634537bc2fe48c0")
     version("0.6.0", sha256="127f11694478f61a47be1c253b5ce0f24247648774494dd15de8ff16ef9881c7")
     version("0.5.8", sha256="0aa89482c098064b107e694ac2e755385fc4d6e2da250b1dfc31b40271a39580")
     version("0.5.2", sha256="d203c9e1fc749ee5375eb71f2521d2fa511a0f9587252766012695dce4be7c78")
