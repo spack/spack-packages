cb478e4f725918492de5779130ac5802343c63a1
Wouter Deconinck <wdconinc@gmail.com>
2026-08-07 13:11:40 -0500

    openblas: add v0.3.34; ensure no fortran auto-detection

--- repos/spack_repo/builtin/packages/openblas/package.py
+++ repos/spack_repo/builtin/packages/openblas/package.py
@@ -28,6 +28,7 @@ class Openblas(CMakePackage, MakefilePackage):
     license("BSD-3-Clause")
 
     version("develop", branch="develop")
+    version("0.3.34", sha256="cd7e129868320cc2d033afa920e31202dfe0b8066a5b66661900ccc0f197dfed")
     version("0.3.33", sha256="6761af1d9f5d353ab4f0b7497be2643313b36c8f31caec0144bfef198e71e6ab")
     version("0.3.32", sha256="f8a1138e01fddca9e4c29f9684fd570ba39dedc9ca76055e1425d5d4b1a4a766")
     version("0.3.30", sha256="27342cff518646afb4c2b976d809102e368957974c250a25ccc965e53063c95d")
@@ -576,6 +577,8 @@ class MakefileBuilder(makefile.MakefileBuilder):
         # Fortran-free compilation
         if "~fortran" in self.spec:
             make_defs += ["NOFORTRAN=1"]
+            # f_check still auto-detects Fortran compilers on PATH
+            make_defs += ["F_COMPILER=none"]
 
         if "~shared" in self.spec:
             if "+pic" in self.spec:
