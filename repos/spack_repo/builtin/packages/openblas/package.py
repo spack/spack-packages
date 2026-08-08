485cff379214a026c611001bd7c8655c24695bad
Matthias Krack <matthias.krack@psi.ch>
2026-08-08 17:35:13 +0200

    Update repos/spack_repo/builtin/packages/openblas/package.py
    Co-authored-by: Bernhard Kaindl <bernhardkaindl7@gmail.com>

--- repos/spack_repo/builtin/packages/openblas/package.py
+++ repos/spack_repo/builtin/packages/openblas/package.py
@@ -647,7 +647,7 @@ class MakefileBuilder(makefile.MakefileBuilder):
             # files created, we suppress makefile command echoing via `-s`.
             make("-s", *self.make_defs)
 
-    def edit(self, spec, prefix):
+    def edit(self, pkg, spec, prefix):
         # https://github.com/spack/spack-packages/pull/5883#issuecomment-5189054355
         filter_file("OBJS += test_fork.o", "", "utest/Makefile", string=True)
 
