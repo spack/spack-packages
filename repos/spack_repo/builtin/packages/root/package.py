3e975e16fccd964097d603ce9c7e3c0545ee0888
Seth R Johnson <johnsonsr@ornl.gov>
2026-08-11 08:56:17 -0400

    Fix typo

--- repos/spack_repo/builtin/packages/root/package.py
+++ repos/spack_repo/builtin/packages/root/package.py
@@ -165,7 +165,7 @@ class Root(CMakePackage):
     # Fix CMake bug that becomes an error in newer versions
     patch(
         "https://github.com/root-project/root/pull/22790.diff?full_index=1",
-        sha256="8f95c3d0532be4880dbe4e74b0180ecaed410c59ed915067c4f585aba270029c.",
+        sha256="8f95c3d0532be4880dbe4e74b0180ecaed410c59ed915067c4f585aba270029c",
         when="@6.40.02 ^cmake@4.4:",
     )
 
