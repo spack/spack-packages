c5bc9aea421a8b2695a78aa8fec24e4d87fbbb4d
guanyuming-he <53397751+guanyuming-he@users.noreply.github.com>
2026-08-11 10:50:33 +0800

    Update repos/spack_repo/builtin/packages/tree_sitter_ptx/package.py
    Co-authored-by: Bernhard Kaindl <bernhardkaindl7@gmail.com>

--- repos/spack_repo/builtin/packages/tree_sitter_ptx/package.py
+++ repos/spack_repo/builtin/packages/tree_sitter_ptx/package.py
@@ -1,6 +1,6 @@
 # Copyright Spack Project Developers. See COPYRIGHT file for details.
 #
-# SPDX-License-Identifier: MIT
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 
 from spack_repo.builtin.build_systems.cmake import CMakePackage
