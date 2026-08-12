4cdd9dfcd8f773bca3145b3d164d0fc4d42425cf
Afzal Patel <122491982+afzpatel@users.noreply.github.com>
2026-08-12 09:26:33 -0400

    Apply suggestion from @renjithravindrankannath
    Co-authored-by: renjithravindrankannath <94420380+renjithravindrankannath@users.noreply.github.com>

--- repos/spack_repo/builtin/packages/rocm_cmake/package.py
+++ repos/spack_repo/builtin/packages/rocm_cmake/package.py
@@ -85,6 +85,7 @@ class RocmCmake(ROCmLibrary, CMakePackage):
         "7.2.1",
         "7.2.3",
         "7.13.0",
+        "7.14.0",
     ]:
         depends_on(f"rocm-core@{ver}", when=f"@{ver}")
 
