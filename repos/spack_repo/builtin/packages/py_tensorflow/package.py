d377ce989bcf1e1fb2fa91c83df2e03a76d2b477
Afzal Patel <afzal.patel@amd.com>
2026-08-13 08:05:40 -0700

    fix typo

--- repos/spack_repo/builtin/packages/py_tensorflow/package.py
+++ repos/spack_repo/builtin/packages/py_tensorflow/package.py
@@ -424,7 +424,7 @@ class PyTensorflow(Package, CudaPackage, ROCmPackage, PythonExtension):
         "2.14-z:2.16.1-a",
         "2.16.1-z:2.18.0-a",
         "2.18.0-z:2.20.0-a",
-        "2.20.0-z:2.20.1-a",
+        "2.20.0-z:2.21.0-a",
         "2.21.0-z:"
     ]
 
