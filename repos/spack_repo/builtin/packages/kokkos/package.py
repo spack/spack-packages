4c7505b8fb5056f771e442e42c006f61931d63c0
Richard Berger <richard.berger@outlook.com>
2026-08-14 23:53:40 -0600

    Update repos/spack_repo/builtin/packages/kokkos/package.py
    Co-authored-by: Daniel Arndt <arndtd@ornl.gov>

--- repos/spack_repo/builtin/packages/kokkos/package.py
+++ repos/spack_repo/builtin/packages/kokkos/package.py
@@ -399,12 +399,11 @@ class Kokkos(CMakePackage, CudaPackage, ROCmPackage):
     # Filter spack-generated files that may include links to the
     # spack compiler wrappers
     filter_compiler_wrappers("kokkos_launch_compiler", relative_root="bin")
-    filter_compiler_wrappers(
-        "KokkosConfigCommon.cmake", relative_root=os.path.join("lib", "cmake", "Kokkos")
-    )
-    filter_compiler_wrappers(
-        "KokkosConfigCommon.cmake", relative_root=os.path.join("lib64", "cmake", "Kokkos")
-    )
+    for libdir in ("lib", "lib64"):
+        filter_compiler_wrappers(
+            "KokkosConfigCommon.cmake",
+            relative_root=os.path.join(libdir, "cmake", "Kokkos"),
+        ) 
 
     # sanity check
     sanity_check_is_file = [
