2c999699cc2f64fdc13a616927e99c3f081990c3
w8jcik <w8jcik@users.noreply.github.com>
2026-08-17 09:08:21 +0000

    [@spackbot] updating style on behalf of w8jcik

--- repos/spack_repo/builtin/packages/r_rcurl/package.py
+++ repos/spack_repo/builtin/packages/r_rcurl/package.py
@@ -31,8 +31,12 @@ class RRcurl(RPackage):
     version("1.98-1.2", sha256="5d74a0cdc3c5684b0348b959f67039e3c2a5da2bbb6176f6800a94124895a7a8")
 
     with default_args(deprecated=True):
-        version("1.95-4.12", sha256="393779efafdf40823dac942a1e028905d65c34f3d41cfd21bcd225e411385ff4")
-        version("1.95-4.8", sha256="e72243251bbbec341bc5864305bb8cc23d311d19c5d0d9310afec7eb35aa2bfb")
+        version(
+            "1.95-4.12", sha256="393779efafdf40823dac942a1e028905d65c34f3d41cfd21bcd225e411385ff4"
+        )
+        version(
+            "1.95-4.8", sha256="e72243251bbbec341bc5864305bb8cc23d311d19c5d0d9310afec7eb35aa2bfb"
+        )
 
     depends_on("c", type="build")
 
