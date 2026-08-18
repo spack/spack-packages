12bc64b1144218d2f50d97ed7388ac52cdfe7ce4
Doug Torrance <dtorrance9@gatech.edu>
2026-08-18 10:48:14 -0400

    flint: apply macos bug fix in all versions since 3.4.0

--- repos/spack_repo/builtin/packages/flint/package.py
+++ repos/spack_repo/builtin/packages/flint/package.py
@@ -48,7 +48,7 @@ class Flint(AutotoolsPackage):
         depends_on("libtool", type="build")
 
     # https://github.com/flintlib/flint/pull/2800
-    @run_before("configure", when="@3.4.0")
+    @run_before("configure", when="@3.4.0:")
     def fix_in_tree_detection(self):
         filter_file(
             'if test "$ac_abs_confdir" = "`pwd`";',
