91c6813999b20d4cdbe2f3097dfc67dacaec0499
Thomas Padioleau <thomas.padioleau@cea.fr>
2026-08-26 13:27:02 +0200

    Remove examples variant

--- repos/spack_repo/builtin/packages/mdspan/package.py
+++ repos/spack_repo/builtin/packages/mdspan/package.py
@@ -20,7 +20,6 @@ class Mdspan(CMakePackage):
     version("stable", branch="stable")
     version("0.6.0", sha256="d6b7b9d4f472106df1d28729bd8383a8a7ea7938adf9f82d3be9c151344830d9")
 
-    variant("examples", default=True, description="Enable examples")
     variant("tests", default=False, description="Enable tests")
     variant("benchmarks", default=False, description="Enable benchmarks")
     variant(
@@ -43,7 +42,7 @@ class Mdspan(CMakePackage):
             self.define_from_variant("MDSPAN_ENABLE_TESTS", "tests"),
             self.define_from_variant("MDSPAN_USE_SYSTEM_GTEST", "tests"),
             self.define_from_variant("MDSPAN_ENABLE_BENCHMARKS", "benchmarks"),
-            self.define_from_variant("MDSPAN_ENABLE_EXAMPLES", "examples"),
+            self.define("MDSPAN_ENABLE_EXAMPLES", False),
             self.define_from_variant("MDSPAN_CXX_STANDARD", "cxxstd"),
             self.define_from_variant("CMAKE_CXX_STANDARD", "cxxstd"),
             self.define_from_variant("MDSPAN_INSTALL_STDMODE_HEADERS", "stdheaders"),
