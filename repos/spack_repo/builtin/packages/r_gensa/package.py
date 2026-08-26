1deb89c12380d485d128d2e07e0bca094c4c267e
Emerson Jacobson <emwjacobson@gmail.com>
2026-08-26 08:25:45 -0700

    Revert "Swapped conflict for depends"
    This reverts commit cf24aa78347d638bc7f994189d0fa43fc174d802.

--- repos/spack_repo/builtin/packages/r_gensa/package.py
+++ repos/spack_repo/builtin/packages/r_gensa/package.py
@@ -26,8 +26,9 @@ class RGensa(RPackage):
     depends_on("r@2.12.0:", type=("build", "run"))
     # Rf_allocLang() was only added in r@4.4.1.
     depends_on("r@4.4.1:", type=("build", "run"), when="@1.1.15:")
+
     # Versions <= 1.1.14 use SET_TYPEOF, which was removed from Rinternals.h in r@4.6.0
-    depends_on("r@4.6:", type=("build", "run"), when="@1.1.15:")
+    conflicts("r@4.6:", when="@:1.1.14")
 
     def patch(self):
         if self.spec.satisfies("@:1.1.8 ^r@4.4.0:"):
