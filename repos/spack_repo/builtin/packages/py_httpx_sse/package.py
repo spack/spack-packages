6bcd9e72789de7f7a238c5e205bc31a4fe434aab
aprozo <alexandr.a.prozorov@gmail.com>
2026-08-31 13:14:14 +0200

    py-httpx-sse: add 0.4.3, drop 0.4.0

--- repos/spack_repo/builtin/packages/py_httpx_sse/package.py
+++ repos/spack_repo/builtin/packages/py_httpx_sse/package.py
@@ -11,15 +11,13 @@ class PyHttpxSse(PythonPackage):
     """Consume Server-Sent Events (SSE) with HTTPX."""
 
     homepage = "https://github.com/florimondmanca/httpx-sse"
-    pypi = "httpx-sse/httpx-sse-0.4.0.tar.gz"
-
-    maintainers("aprozo")
+    pypi = "httpx-sse/httpx_sse-0.4.3.tar.gz"
 
     license("MIT", checked_by="aprozo")
 
-    version("0.4.0", sha256="1e81a3a3070ce322add1d3529ed42eb5f70817f45ed6ec915ab753f961139721")
+    version("0.4.3", sha256="9b1ed0127459a66014aec3c56bebd93da3c1bc8bb6618c8082039a44889a755d")
 
-    depends_on("python@3.8:", type=("build", "run"))
+    depends_on("python@3.9:", type=("build", "run"))
     depends_on("py-setuptools", type="build")
     depends_on("py-setuptools-scm", type="build")
     depends_on("py-wheel", type="build")
