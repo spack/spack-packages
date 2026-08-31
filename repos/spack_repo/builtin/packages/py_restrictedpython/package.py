42da2b75f016b36ebb8d4661f1d9177a263320f6
aprozo <alexandr.a.prozorov@gmail.com>
2026-08-31 13:14:14 +0200

    py-restrictedpython: add 8.5, drop 8.1

--- repos/spack_repo/builtin/packages/py_restrictedpython/package.py
+++ repos/spack_repo/builtin/packages/py_restrictedpython/package.py
@@ -11,14 +11,12 @@ class PyRestrictedpython(PythonPackage):
     """A restricted execution environment for Python to run untrusted code."""
 
     homepage = "https://github.com/zopefoundation/RestrictedPython"
-    pypi = "RestrictedPython/restrictedpython-8.1.tar.gz"
-
-    maintainers("aprozo")
+    pypi = "RestrictedPython/restrictedpython-8.5.tar.gz"
 
     license("ZPL-2.1", checked_by="aprozo")
 
-    version("8.1", sha256="4a69304aceacf6bee74bdf153c728221d4e3109b39acbfe00b3494927080d898")
+    version("8.5", sha256="4ed1269dbe3caa88db650d1af325198a952aeb1451eca05df0cfa65db4466215")
 
-    depends_on("python@3.9:", type=("build", "run"))
-    depends_on("py-setuptools@78.1.1:80", type="build")
+    depends_on("python@3.10:3.15", type=("build", "run"))
+    depends_on("py-setuptools@78.1.1:81", type="build")
     depends_on("py-wheel", type="build")
