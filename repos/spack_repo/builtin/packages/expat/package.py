ee42d28ff28965fee01bafb7f66ffa43aa3e6eea
Sebastian Pipping <sebastian@pipping.org>
2026-08-31 17:48:06 +0200

    expat: Add 2.8.4 with security fixes

--- repos/spack_repo/builtin/packages/expat/package.py
+++ repos/spack_repo/builtin/packages/expat/package.py
@@ -19,6 +19,10 @@ class Expat(AutotoolsPackage, CMakePackage):
 
     license("MIT")
     version(
+        "2.8.4",
+        sha256="963250a823c16a498582b4ad82ad0f88926be0769675d3b6956be4d769a1cd8f",
+    )
+    version(
         "2.8.3",
         sha256="b4cc2483927d5e90bf8c40b44a6b95b368b42a8a96e25883fce188b48a92b670",
     )
