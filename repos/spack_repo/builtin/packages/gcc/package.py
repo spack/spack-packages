4d5277b5f8fa4de0dc3ab69981ddab8058917bc9
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2026-09-07 09:21:24 +0200

    gcc: check if the ci job fails
    Signed-off-by: Massimiliano Culpo <massimiliano.culpo@gmail.com>

--- repos/spack_repo/builtin/packages/gcc/package.py
+++ repos/spack_repo/builtin/packages/gcc/package.py
@@ -884,7 +884,7 @@ class Gcc(AutotoolsPackage, GNUMirrorPackage, CompilerPackage):
                     f.write(bd.read())
 
     # https://gcc.gnu.org/install/configure.html
-    def configure_args(self):
+    def configure_args(self) -> list[str]:
         spec = self.spec
 
         # Generic options to compile GCC
