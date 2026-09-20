94fe7f1c96fdf2449c3e28a71a8c72de829bc2ed
Wouter Deconinck <wdconinc@gmail.com>
2026-09-20 10:53:09 -0500

    glx, opengl: restrict headers/libraries to first found

--- repos/spack_repo/builtin/packages/opengl/package.py
+++ repos/spack_repo/builtin/packages/opengl/package.py
@@ -98,7 +98,7 @@ class Opengl(BundlePackage):
             header_name = "OpenGL/gl"
         else:
             header_name = "GL/gl"
-        return find_headers(header_name, root=self.prefix, recursive=True)
+        return find_headers(header_name, root=self.prefix, recursive=True)[:1]
 
     @property
     def gl_libs(self):
@@ -109,4 +109,4 @@ class Opengl(BundlePackage):
             lib_name = "libOpenGL"
         else:
             lib_name = "libGL"
-        return find_libraries(lib_name, root=self.prefix, recursive=True)
+        return find_libraries(lib_name, root=self.prefix, recursive=True)[:1]
