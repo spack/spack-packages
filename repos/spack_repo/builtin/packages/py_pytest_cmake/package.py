c089edf1972d6773aa5343cb5d8244bbb5c51b6b
Peter Arzt <peter.arzt@tu-darmstadt.de>
2026-09-20 15:27:34 +0200

    py-pytest-cmake: add v1.4.1

--- repos/spack_repo/builtin/packages/py_pytest_cmake/package.py
+++ repos/spack_repo/builtin/packages/py_pytest_cmake/package.py
@@ -16,9 +16,11 @@ class PyPytestCmake(PythonPackage):
 
     maintainers("pearzt")
 
+    version("1.4.1", sha256="eb38d6a1016d6f8dfbfadf25607352a38ed430ad32179bc85e2a7bd782febb46")
     version("1.3.0", sha256="6291881b85f8810068552ac9ccc5c9d74ecd3d0c59b395253905e82c930682b9")
 
     depends_on("py-pytest")
     depends_on("py-hatchling@1.4:")
-    depends_on("cmake@3.20:4.2")
+    depends_on("cmake@3.20:4.2", when="@1.3.0")
+    depends_on("cmake@3.20:4.5", when="@1.4.1:")
     depends_on("python@3.7:4")
