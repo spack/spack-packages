c2f6e8efe9f0bb14c0e8bee00dc79f2a972c38e2
Michael R. Crusoe <michael.crusoe@gmail.com>
2026-09-21 17:34:15 +0200

    add cwltool 3.2.20260720092025

--- repos/spack_repo/builtin/packages/py_cwltool/package.py
+++ repos/spack_repo/builtin/packages/py_cwltool/package.py
@@ -12,10 +12,14 @@ class PyCwltool(PythonPackage):
     """Common workflow language reference implementation"""
 
     homepage = "https://github.com/common-workflow-language/cwltool"
-    pypi = "cwltool/cwltool-3.1.20221201130942.tar.gz"
+    pypi = "cwltool/cwltool-3.2.20260720092025.tar.gz"
 
     license("Apache-2.0")
 
+    version(
+        "3.2.20260720092025",
+        sha256="d0671d429f2e35d8009c77c17625a418bceaa8b236ff5ca2cd0bcfc867cf4bf4",
+    )
     version(
         "3.1.20221201130942",
         sha256="0152d8cdf6acaf3620f557b442941f577bff2851d9e2e866e6051ea48a37bdbe",
@@ -29,21 +33,22 @@ class PyCwltool(PythonPackage):
         sha256="ae1cd4626b5330457b1a62bcb2580f36f530264a80222f2cc17cf65899ebf04e",
     )
 
-    depends_on("python@3.6:3", type=("build", "run"))
+    depends_on("python@3.10:3", type=("build", "run"))
     depends_on("py-setuptools", type="build")
 
     depends_on("py-requests@2.6.1:", type=("build", "run"))
-    depends_on("py-ruamel-yaml@0.15:0.17.21", type=("build", "run"))
-    depends_on("py-rdflib@4.2.2:6.2", type=("build", "run"))
-    depends_on("py-shellescape@3.4.1:3.8", type=("build", "run"))
-    depends_on("py-schema-salad@8.2.20211104054942:8", type=("build", "run"))
+    depends_on("py-ruamel-yaml@0.16:0.19", type=("build", "run"))
+    depends_on("py-rdflib@4.2.2:7.6", type=("build", "run"))
+    depends_on("py-schema-salad@8.9:8", type=("build", "run"))
     depends_on("py-prov@1.5.1", type=("build", "run"))
-    depends_on("py-bagit@1.6.4:", type=("build", "run"))
+    depends_on("py-bagit@1.6.4:1.9", type=("build", "run"))
     depends_on("py-mypy-extensions", type=("build", "run"))
     depends_on("py-psutil@5.6.6:", type=("build", "run"))
     depends_on("py-typing-extensions", type=("build", "run"))
     depends_on("py-coloredlogs", type=("build", "run"))
     depends_on("py-pydot@1.4.1:", type=("build", "run"))
-    depends_on("py-argcomplete", type=("build", "run"))
+    depends_on("py-argcomplete@1.12.0", type=("build", "run"))
     depends_on("py-pyparsing@:3.0.1,3.0.3:", type=("build", "run"))
-    depends_on("py-cwl-utils@0.19:", type=("build", "run"))
+    depends_on("py-cwl-utils@0.41:", type=("build", "run"))
+    depends_on("py-spython@0.3.0:", type=("build", "run"))
+    depends_on("py-rich-argparse", type=("build", "run"))
