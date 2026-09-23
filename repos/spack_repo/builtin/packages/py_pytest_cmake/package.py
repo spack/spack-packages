737006c3724abe59d6fbe78a11d99466d34f8160
Peter Arzt <peter.arzt@tu-darmstadt.de>
2026-09-23 10:23:04 +0200

    py-pytest-cmake: Specify pytest version range
    Co-authored-by: Manuela Kuhn <36827019+manuelakuhn@users.noreply.github.com>

--- repos/spack_repo/builtin/packages/py_pytest_cmake/package.py
+++ repos/spack_repo/builtin/packages/py_pytest_cmake/package.py
@@ -21,7 +21,7 @@ class PyPytestCmake(PythonPackage):
     version("1.4.1", sha256="eb38d6a1016d6f8dfbfadf25607352a38ed430ad32179bc85e2a7bd782febb46")
     version("1.3.0", sha256="6291881b85f8810068552ac9ccc5c9d74ecd3d0c59b395253905e82c930682b9")
 
-    depends_on("py-pytest")
+    depends_on("py-pytest@4:9")
     depends_on("py-hatchling@1.4:")
     depends_on("cmake@3.20:4.2", when="@1.3.0")
     depends_on("cmake@3.20:4.4", when="@1.4.1:")
