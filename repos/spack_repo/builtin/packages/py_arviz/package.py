d4c051490f28f9883590082be793479d8850a554
Manuela Kuhn <manuela.kuhn@ovgu.de>
2026-09-24 10:59:37 +0200

    py-arviz: add v1.3.0

--- repos/spack_repo/builtin/packages/py_arviz/package.py
+++ repos/spack_repo/builtin/packages/py_arviz/package.py
@@ -17,13 +17,26 @@ class PyArviz(PythonPackage):
 
     license("Apache-2.0")
 
+    version("1.3.0", sha256="9e332c0a0da370cb1c00823678065e9b0462fbdf1efa0f38c37485cc78c8a576")
     version("0.6.1", sha256="435edf8db49c41a8fa198f959e7581063006c49a4efdef4755bb778db6fd4f72")
 
-    depends_on("py-setuptools", type="build")
-    depends_on("py-matplotlib@3.0:", type=("build", "run"))
-    depends_on("py-numpy@1.12:", type=("build", "run"))
-    depends_on("py-scipy@0.19:", type=("build", "run"))
-    depends_on("py-packaging", type=("build", "run"))
-    depends_on("py-pandas@0.23:", type=("build", "run"))
-    depends_on("py-xarray@0.11:", type=("build", "run"))
-    depends_on("py-netcdf4", type=("build", "run"))
+    with default_args(type="build"):
+        depends_on("py-flit-core@3.4:3", when="@1:")
+
+        # Historical dependencies
+        depends_on("py-setuptools", when="@:0")
+
+    with default_args(type=("build", "run")):
+        depends_on("py-arviz-base@1.3", when="@1.3")
+        depends_on("py-arviz-stats+xarray@1.3", when="@1.3")
+        depends_on("py-arviz-plots@1.3", when="@1.3")
+
+        # Historical dependencies
+        with when("@0.6"):
+            depends_on("py-matplotlib@3.0:")
+            depends_on("py-numpy@1.12:")
+            depends_on("py-scipy@0.19:")
+            depends_on("py-packaging")
+            depends_on("py-pandas@0.23:")
+            depends_on("py-xarray@0.11:")
+            depends_on("py-netcdf4")
